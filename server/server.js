236104f2748d49889e53b438f420ebf6716e7212
Belen Curcio <curciobelen@gmail.com>
2016-10-31 18:40:42 -0300

    init Server

--- server/server.js
+++ server/server.js
@@ -1,37 +1,23 @@
-/* This is the entrypoint for the Talk Platform server. */
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose'
 
 const app = express();
 
-// Load routes.
-var apiRoutes = require("./api/routes");
-var commentRoutes = require('./comments/routes');
+//var apiRoutes = require("./api/routes");
+//var commentRoutes = require('./comments/routes');
+
+const mainRouter = require('./routers/main.router.js');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || 1618);
 
-// Initialize Static Endpoints.
-const initializeStatic = function() {
-	// TODO
-}
+app.use('/', mainRouter);
 
-// Initialize API endpoints.
-const initializeAPI = function () {
-	app.use('/api/v1', apiRoutes);
-	app.use('/api/v1/comments', commentRoutes);
-}
+//app.use('/api/v1', apiRoutes);
+//app.use('/api/v1/comments', commentRoutes);
 
-// Initialize server when ready.
-const ready = () => {
-	initializeStaticEndpoints();
-	initializeAPI();
-
-	// Listen for incoming requests.
-	app.listen(app.get('port'), () => {
-		console.info(`Listening on port ${app.get('port')}!`);
-	})
-}
+app.listen(app.get('port'), () => {
+	console.info(`Listening on port ${app.get('port')}!`);
+});
