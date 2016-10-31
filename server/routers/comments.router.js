efb12d1c21ab9663ee43cb13ea6ced6e9015db16
Belen Curcio <curciobelen@gmail.com>
2016-10-31 18:38:04 -0300

    Adding routers

---
+++ server/routers/comments.router.js
@@ -0,0 +1,4 @@
+import express from 'express';
+const router = express.Router();
+
+const comment = require('../controllers/comment.controller');
--- server/routers/main.router.js
+++ server/routers/main.router.js
@@ -4,7 +4,12 @@
 
 import express from 'express';
 const router = express.Router();
-
-
 const docs = require('../controllers/docs.controller');
+
+router.get('/', (req, res) => {
+	res.send('Talk API!')
+});
+
 router.get('/docs', docs.get);
+
+module.exports = router;
