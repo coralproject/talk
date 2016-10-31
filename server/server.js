fc3c01713e3dad9c2101ac0e49860fd8369b8e54
Belen Curcio <curciobelen@gmail.com>
2016-10-31 19:04:21 -0300

    serber

--- server/server.js
+++ server/server.js
@@ -2,22 +2,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose'
 
-const app = express();
-
-//var apiRoutes = require("./api/routes");
-//var commentRoutes = require('./comments/routes');
-
 const mainRouter = require('./routers/main.router.js');
+const APIRouter = require('./routers/API.router.js');
+
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || 1618);
 
 app.use('/', mainRouter);
-
-//app.use('/api/v1', apiRoutes);
-//app.use('/api/v1/comments', commentRoutes);
+app.use('/api/v1', APIRouter);
 
 app.listen(app.get('port'), () => {
+	console.info('==> TALK API is listening in ' + process.env.NODE_ENV + ' mode');
 	console.info(`Listening on port ${app.get('port')}!`);
 });
