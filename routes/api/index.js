db6757b99e34377a1a26613a831c509045606633
David Jay <davidgljay@gmail.com>
2016-11-02 14:43:42 -0700

    Adding stream endpoint and mocking

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,5 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/comments', require('./comments'));
+router.use('/stream', require('./stream'));
 
 module.exports = router;
---
+++ routes/api/stream/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  console.log('asset_id, should be assetTest', req.query.asset_id)
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify([
+    {
+      'id': 'abc',
+      'type': 'comment',
+      'body': 'Sample comment',
+      'created_at': new Date().getTime(),
+      'asset_id': 'assetTest'
+    },
+    {
+      'id': 'xyz',
+      'type': 'comment',
+      'body': 'Sample reply',
+      'created_at': new Date().getTime() - 600000,
+      'parent_id': 'abc',
+      'asset_id': 'assetTest'
+    },
+    {
+      'id': 'def',
+      'type': 'comment',
+      'body': 'Another comment',
+      'created_at': new Date().getTime() - 400000,
+      'asset_id': 'assetTest'
+    }
+  ]));
+});
+
+module.exports = router;
