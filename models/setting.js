cb2f0bc10da0e7577ae8f5510e1abe258275b2a3
Riley Davis <riley.davis@gmail.com>
2016-11-02 15:42:24 -0600

    start settings schema

---
+++ models/setting.js
@@ -0,0 +1,35 @@
+const mongoose = require('../mongoose');
+const uuid = require('uuid');
+const Schema = mongoose.Schema;
+
+const SettingSchema = new Schema({
+  anonymous_users: {type: Boolean, default: false},
+  block_mute_enabled: {type: Boolean, default: false},
+  comment_count: {type: Boolean, default: false},
+  comment_editing_enabled: {type: Boolean, default: false},
+  comments_hidden: {type: Boolean, default: false},
+  community_guidelines: {type: Boolean, default: false},
+  detailed_flags: {type: Boolean, default: false},
+  emojis_enabled: {type: Boolean, default: false},
+  following: {type: Boolean, default: false},
+  likes_enabled: {type: Boolean, default: false},
+  mentions: {type: Boolean, default: false},
+  nested_replies: {type: Boolean, default: false},
+  notification_timeout: {type: Number, default: 4500},
+  permalinks: {type: Boolean, default: false},
+  post_button_text: {type: String, default: 'Post'},
+  pseudonyms: {type: Boolean, default: false},
+  public_profile: {type: Boolean, default: false},
+  reactions_enabled: {type: Boolean, default: false},
+  reply_button_text: {type: String, default: 'Reply'},
+  rich_content: {type: Boolean, default: false},
+  show_staff_picks: {type: Boolean, default: false},
+  up_down_voting: {type: Boolean, default: false},
+  user_badges: {type: Boolean, default: false},
+  user_mods_enabled: {type: Boolean, default: false},
+  user_stats_enabled: {type: Boolean, default: false}
+});
+
+const Setting = mongoose.model('Setting', SettingSchema);
+
+module.exports = Setting;
