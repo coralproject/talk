0d4b8ef7480b1b5f7fc8529463bff5d90da5c762
Dan Zajdband <dan.zajdband@gmail.com>
2016-11-03 11:52:28 +0100

    Added coral admin

---
+++ client/coral-admin/src/components/CommentList.js
@@ -0,0 +1,133 @@
+
+import React from 'react'
+import styles from './CommentList.css'
+import key from 'keymaster'
+import Hammer from 'hammerjs'
+import Comment from 'components/Comment'
+
+// Each action has different meaning and configuration
+const actions = {
+  'reject': { status: 'Rejected', icon: 'close', key: 'r' },
+  'approve': { status: 'Approved', icon: 'done', key: 't' },
+  'flag': { status: 'flagged', icon: 'flag', filter: 'Untouched' }
+}
+
+// Renders a comment list and allow performing actions
+export default class CommentList extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = { active: null }
+    this.onClickAction = this.onClickAction.bind(this)
+  }
+
+  // remove key handlers before leaving
+  componentWillUnmount () {
+    this.unbindKeyHandlers()
+  }
+
+  // add key handlers and gestures
+  componentDidMount () {
+    this.bindKeyHandlers()
+    // this.bindGestures() // need to check whether we're on a mobile device or this throws an Error
+  }
+
+  // If entering to singleview and no active, active is the first eleement
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.singleView && !this.state.active) {
+      this.setState({ active: nextProps.commentIds.get(0) })
+    }
+  }
+
+  // Add swipe to approve or reject
+  bindGestures () {
+    const { actions } = this.props
+    this._hammer = new Hammer(this.base)
+    this._hammer.get('swipe').set({ direction: Hammer.DIRECTION_HORIZONTAL })
+
+    if (actions.indexOf('reject') !== -1) {
+      this._hammer.on('swipeleft', () => this.props.singleView && this.actionKeyHandler('Rejected'))
+    }
+    if (actions.indexOf('approve') !== -1) {
+      this._hammer.on('swiperight', () => this.props.singleView && this.actionKeyHandler('Approved'))
+    }
+  }
+
+  // Add key handlers. Each action has one and added j/k for moving around
+  bindKeyHandlers () {
+    this.props.actions.filter(action => actions[action].key).forEach(action => {
+      key(actions[action].key, 'commentList', () => this.props.isActive && this.actionKeyHandler(actions[action].status))
+    })
+    key('j', 'commentList', () => this.props.isActive && this.moveKeyHandler('down'))
+    key('k', 'commentList', () => this.props.isActive && this.moveKeyHandler('up'))
+    key.setScope('commentList')
+  }
+
+  // Perform an action using the keys only if the comment is active
+  actionKeyHandler (action) {
+    if (this.props.isActive && this.state.active) {
+      this.onClickAction(action, this.state.active)
+    }
+  }
+
+  // move around with j/k
+  moveKeyHandler (direction) {
+    if (!this.props.isActive) {
+      return
+    }
+
+    const { commentIds } = this.props
+    const { active } = this.state
+    // check boundaries
+    if (active == null || !commentIds.size) {
+      this.setState({ active: commentIds.get(0) })
+    } else if (direction === 'up' && active !== commentIds.first()) {
+      this.setState({ active: commentIds.get(commentIds.indexOf(active) - 1) })
+    } else if (direction === 'down' && active !== commentIds.last()) {
+      this.setState({ active: commentIds.get(commentIds.indexOf(active) + 1) })
+    }
+
+    // scroll to the position
+    const index = Math.max(commentIds.indexOf(this.state.active), 0)
+    this.base.childNodes[index] && this.base.childNodes[index].focus()
+  }
+
+  unbindKeyHandlers () {
+    key.deleteScope('commentList')
+  }
+
+  // If we are performing an action over a comment (aka removing from the list) we need to select a new active.
+  // TODO: In the future this can be improved and look at the actual state to
+  // resolve since the content of the list could change externally. For now it works as expected
+  onClickAction (action, id) {
+    if (id === this.state.active) {
+      const { commentIds } = this.props
+      if (commentIds.last() === this.state.active) {
+        this.setState({ active: commentIds.get(commentIds.size - 2) })
+      } else {
+        this.setState({ active: commentIds.get(Math.min(commentIds.indexOf(this.state.active) + 1, commentIds.size - 1)) })
+      }
+    }
+    this.props.onClickAction(action, id)
+  }
+
+  render () {
+    const {singleView, actions, commentIds, comments, hideActive} = this.props
+    const {active} = this.state
+    return (
+      <ul className={`${styles.list} ${singleView ? styles.singleView : ''}`}>
+        {commentIds.map((commentId, index) => (
+          <Comment comment={comments.get(commentId)}
+            ref={el => { if (el && commentId === active) { this._active = el } }}
+            key={index}
+            index={index}
+            onClickAction={this.onClickAction}
+            actions={actions}
+            actionsMap={actions}
+            isActive={commentId === active}
+            hideActive={hideActive} />
+        )).toArray()}
+      </ul>
+    )
+  }
+}
---
+++ client/coral-admin/src/containers/ModerationQueue.js
@@ -0,0 +1,115 @@
+
+import React from 'react'
+import { connect } from 'react-redux'
+import ModerationKeysModal from 'components/ModerationKeysModal'
+import CommentList from 'components/CommentList'
+import { updateStatus } from 'actions/comments'
+import styles from './ModerationQueue.css'
+import key from 'keymaster'
+import I18n from 'coral-framework/i18n/i18n'
+import translations from '../translations'
+
+/*
+ * Renders the moderation queue as a tabbed layout with 3 moderation
+ * queues filtered by status (Untouched, Rejected and Approved)
+ */
+
+class ModerationQueue extends React.Component {
+
+  constructor (props) {
+    super(props)
+
+    console.log('ModerationQueue', props)
+
+    this.state = { activeTab: 'pending', singleView: false, modalOpen: false }
+  }
+
+  // Fetch comments and bind singleView key before render
+  componentWillMount () {
+    this.props.dispatch({ type: 'COMMENTS_MODERATION_QUEUE_FETCH' })
+    key('s', () => this.setState({ singleView: !this.state.singleView }))
+    key('shift+/', () => this.setState({ modalOpen: true }))
+    key('esc', () => this.setState({ modalOpen: false }))
+  }
+
+  // Unbind singleView key before unmount
+  componentWillUnmount () {
+    key.unbind('s')
+    key.unbind('shift+/')
+    key.unbind('esc')
+  }
+
+  // Hack for dynamic mdl tabs
+  componentDidMount () {
+    if (typeof componentHandler !== 'undefined') {
+      componentHandler.upgradeAllRegistered()
+    }
+  }
+
+  // Dispatch the update status action
+  onCommentAction (status, id) {
+    this.props.dispatch(updateStatus(status, id))
+  }
+
+  onTabClick (activeTab) {
+    this.setState({ activeTab })
+  }
+
+  // Render the tabbed lists moderation queues
+  render () {
+    const { comments } = this.props
+    const { activeTab, singleView, modalOpen } = this.state
+
+    return (
+      <div className='mdl-tabs mdl-js-tabs mdl-js-ripple-effect'>
+        <div className='mdl-tabs__tab-bar'>
+          <a href='#pending' onClick={() => this.onTabClick('pending')}
+            className={`mdl-tabs__tab is-active ${styles.tab}`}>{lang.t('modqueue.pending')}</a>
+          <a href='#rejected' onClick={() => this.onTabClick('rejected')}
+            className={`mdl-tabs__tab ${styles.tab}`}>{lang.t('modqueue.rejected')}</a>
+          <a href='#flagged' onClick={() => this.onTabClick('flagged')}
+            className={`mdl-tabs__tab ${styles.tab}`}>{lang.t('modqueue.flagged')}</a>
+        </div>
+        <div className={`mdl-tabs__panel is-active ${styles.listContainer}`} id='pending'>
+          <CommentList
+            isActive={activeTab === 'pending'}
+            singleView={singleView}
+            commentIds={comments.get('ids').filter(id => comments.get('byId').get(id).get('data').get('status') === 'Untouched')}
+            comments={comments.get('byId')}
+            onClickAction={(action, id) => this.onCommentAction(action, id)}
+            actions={['reject', 'approve']}
+            loading={comments.loading} />
+        </div>
+        <div className={`mdl-tabs__panel ${styles.listContainer}`} id='rejected'>
+          <CommentList
+            isActive={activeTab === 'rejected'}
+            singleView={singleView}
+            commentIds={comments.get('ids').filter(id => comments.get('byId').get(id).get('data').get('status') === 'Rejected')}
+            comments={comments.get('byId')}
+            onClickAction={(action, id) => this.onCommentAction(action, id)}
+            actions={['approve']}
+            loading={comments.loading} />
+        </div>
+        <div className={`mdl-tabs__panel ${styles.listContainer}`} id='flagged'>
+          <CommentList
+            isActive={activeTab === 'rejected'}
+            singleView={singleView}
+            commentIds={comments.get('ids').filter(id => {
+              const data = comments.get('byId').get(id).get('data')
+              return data.get('status') === 'Untouched' && data.get('flagged') === true
+            })}
+            comments={comments.get('byId')}
+            onClickAction={(action, id) => this.onCommentAction(action, id)}
+            actions={['reject', 'approve']}
+            loading={comments.loading} />
+        </div>
+        <ModerationKeysModal open={modalOpen}
+          onClose={() => this.setState({ modalOpen: false })} />
+      </div>
+    )
+  }
+}
+
+export default connect(({ comments }) => ({ comments }))(ModerationQueue)
+
+const lang = new I18n(translations)
