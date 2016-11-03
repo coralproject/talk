55825c4e03dca0f05a2ca0ba804cbd5ae3480a50
Riley Davis <riley.davis@gmail.com>
2016-11-03 15:21:02 -0600

    just use the mongoose model

--- bin/init.js
+++ bin/init.js
@@ -1,16 +1,7 @@
-const MongoClient = require('mongodb').MongoClient;
-const url = process.env.TALK_MONGO_URL;
+const Setting = require('../models/setting');
+const defaults = {id: '1', moderation: 'pre'};
 
-if (!url) {
-  throw new Error('environment variable TALK_MONGO_URL must be defined');
-}
-
-MongoClient.connect(url).then(db => {
-  const Setting = db.collection('settings');
-  const defaults = {id: 1, moderation: 'pre'};
-
-  Setting.update({id: 1}, {$setOnInsert: defaults}, {upsert: true}).then(() => {
-    console.log('created settings object.');
-    process.exit();
-  });
+Setting.update({id: '1'}, {$setOnInsert: defaults}, {upsert: true}).then(() => {
+  console.log('created settings object.');
+  process.exit();
 }).catch(console.error);
