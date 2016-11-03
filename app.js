d28f46f9caf6227c5f75c0cae49fa03a0e1aa179
Riley Davis <riley.davis@gmail.com>
2016-11-03 16:49:03 -0600

    disallow url-encoded payloads.

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use('/api/v1', require('./routes/api'));
