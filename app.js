7facdf08402658e83cf5ad585cbb5de661521baf
Riley Davis <riley.davis@gmail.com>
2016-11-03 16:34:01 -0600

    make the settings routes actually work

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 
 // Initialize application framework.
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use('/api/v1', require('./routes/api'));
 
--- models/setting.js
+++ models/setting.js
@@ -12,12 +12,22 @@ const SettingSchema = new Schema({
   }
 });
 
+/**
+ * gets the entire settings record and sends it back
+ * @return {Promise} settings the whole settings record
+ */
 SettingSchema.statics.getSettings = function () {
-  return this.findOne();
+  return this.findOne({id: '1'});
 };
 
+/**
+ * this will update the settings object with whatever you pass in
+ * @param  {object} setting a hash of whatever settings you want to update
+ * @return {Promise} settings Promise that resolves to the entire (updated) settings object.
+ */
 SettingSchema.statics.updateSettings = function (setting) {
   // there should only ever be one record unless something has gone wrong.
+  console.log('new setting', setting);
   return this.findOneAndUpdate({id: '1'}, {$set: setting}, {new: true});
 };
 
