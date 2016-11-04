23ea7025eaf9545e2a3d9b725809b6a64e9c605c
gaba <gabelula@gmail.com>
2016-11-04 14:13:22 -0700

    Convert from promises to array

--- routes/api/stream/index.js
+++ routes/api/stream/index.js
@@ -7,11 +7,17 @@ const Action = require('../../../models/action');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+
   const comments = Comment.findByAssetId(req.query.asset_id) || [];
   const users = User.findByIdArray(comments.map((comment) => comment.author_id));
   const actions = Action.findByItemIdArray(comments.map((comment) => comment.id));
 
-  res.json(comments.concat(users).concat(actions)).catch(next);
+  Promise.all([comments, users, actions]).then(([comments, users, actions]) => {
+    res.json(comments.concat(users).concat(actions));
+  }).catch(error => {
+    next(error);
+  });
+
 });
 
 module.exports = router;
