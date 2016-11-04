0cc5bdb04f71dd4556b3a1f654e994dbaee34ddc
Belen Curcio <curciobelen@gmail.com>
2016-11-04 12:47:56 -0300

    throwing

--- bin/init.js
+++ bin/init.js
@@ -1,7 +1,13 @@
 const Setting = require('../models/setting');
 const defaults = {id: '1', moderation: 'pre'};
 
-Setting.update({id: '1'}, {$setOnInsert: defaults}, {upsert: true}).then(() => {
-  console.log('created settings object.');
-  process.exit();
-}).catch(console.error);
+try {
+  Setting.update({id: '1'}, {$setOnInsert: defaults}, {upsert: true})
+    .then(() => {
+      console.log('Created settings object.');
+    }).catch((err) => {
+      if (err) throw err;
+    });
+} catch (err) {
+  console.log('Cannot create settings object.');
+}
