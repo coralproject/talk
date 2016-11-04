5bef61abe927cd0ee06538e5dcb1225186a0d5e1
Riley Davis <riley.davis@gmail.com>
2016-11-04 10:26:39 -0600

    let exceptional circumstances be exceptional

--- bin/init.js
+++ bin/init.js
@@ -8,6 +8,5 @@ Setting.update({id: '1'}, {$setOnInsert: defaults}, {upsert: true})
     mongoose.disconnect();
   }).catch((err) => {
     console.error(`failed to create the settings object ${JSON.stringify(err)}`);
-    mongoose.disconnect();
     throw new Error(err); // just to be safe
   });
