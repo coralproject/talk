c173b0838765fb6eeb828d9fabe5f13b4c7e4f22
David Jay <davidgljay@gmail.com>
2016-11-04 10:16:20 -0700

    Fixing inconsistent test in comment model.

--- tests/models/comment.js
+++ tests/models/comment.js
@@ -35,6 +35,10 @@ describe('Comment: models', () => {
     it('should find an array of comments by asset id', () => {
       return Comment.findByAssetId('123').then((result) => {
         expect(result).to.have.length(2);
+        result.sort((a,b) => {
+          if (a.body < b.body) {return -1;}
+          else {return 1;}
+        });
         expect(result[0]).to.have.property('body')
           .and.to.equal('comment 10');
         expect(result[1]).to.have.property('body')
