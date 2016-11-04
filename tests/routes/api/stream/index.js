6c25ca81d4c03ef196777462c9a536b0d41ab781
David Jay <davidgljay@gmail.com>
2016-11-04 00:02:34 -0700

    Adding stream endpoint tests

---
+++ tests/routes/api/stream/index.js
@@ -0,0 +1,62 @@
+require('../../../utils/mongoose');
+const Action = require('../../../../models/action');
+const User = require('../../../../models/user');
+const Comment = require('../../../../models/comment');
+const expect = require('chai').expect;
+const streamRoutes = require('../../../../routes/api/stream');
+
+describe('api/stream: routes', () => {
+  const comments = [{
+    id: 'abc',
+    body: 'comment 1',
+    asset_id: 'asset',
+    author_id: '123'
+  },{
+    id: 'def',
+    body: 'comment 2',
+    asset_id: 'asset',
+    author_id: '456'
+  },{
+    id: 'hij',
+    body: 'comment 3',
+    asset_id: '456'
+  }]
+
+  const users = [{
+    id: '123',
+    display_name: 'John',
+  },{
+    id: '456',
+    display_name: 'Paul',
+  }]
+
+  const actions = [{
+    action_type: 'flag',
+    item_id: 'abc'
+  },{
+    action_type: 'like',
+    item_id: 'hij'
+  }]
+
+  beforeEach(() => {
+    return Comment.create(comments).then(() => {
+      return User.create(users)
+    }).then(() => {
+      return Action.create(actions)
+    })
+  })
+
+  it('should return a stream with comments, users and actions', () => {
+    console.log(streamRoutes('./'));
+    streamRoutes('./')({
+        query: {
+          asset_id: 'asset'
+      }
+    }, {
+      json: (data) => {
+        console.log(data);
+        expect(data).to.equal(1);
+      }
+    })
+  })
+})
