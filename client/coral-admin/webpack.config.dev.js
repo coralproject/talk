8685e31b578f63f7f527ad67275e9d600254391b
David Erwin <davidjerwin@gmail.com>
2016-11-07 15:31:12 -0500

    Integrating webpack build files, working commit

--- client/coral-admin/webpack.config.dev.js
+++ client/coral-admin/webpack.config.dev.js
@@ -9,9 +9,9 @@ const config = require('./config.json')
 // doing a string replace here because I spent a day trying to do it the "webpack" way
 // ond nothing works. just trying to replace a string in an index.html file is
 // apparently something no one has ever done in the js community.
-let templateString = fs.readFileSync('./index.ejs').toString()
+let templateString = fs.readFileSync(path.join(__dirname, 'index.ejs')).toString()
 templateString = templateString.replace('<%= basePath %>', config.basePath)
-fs.writeFileSync('./public/index.html', templateString)
+fs.writeFileSync(path.join(__dirname, 'public/index.html'), templateString)
 
 console.log(templateString)
 
@@ -24,9 +24,9 @@ module.exports = {
     filename: '[name].js'
   },
   module: {
-    loaders: [
-      { test: /.js$/, loaders: 'buble', include: path.join(__dirname, 'src') },
-      { test: /.json$/, loaders: 'json', include: __dirname, exclude: /node_modules/ },
+    loader: [
+      { test: /\.js$/, loaders: 'buble', include: path.join(__dirname, 'src') },
+      { test: /\.json$/, loaders: 'json', include: __dirname, exclude: /node_modules/ },
       { test: /\.css$/, loader: 'style-loader!css-loader?modules&importLoaders=1!postcss-loader' }
     ]
   },
