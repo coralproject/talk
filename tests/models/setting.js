108c50cb95c14721394b530de23de7269b635caf
Riley Davis <riley.davis@gmail.com>
2016-11-07 11:28:09 -0700

    add tests to settings models/endpoints

---
+++ tests/models/setting.js
@@ -0,0 +1,32 @@
+/* eslint-env node, mocha */
+
+require('../utils/mongoose');
+
+const Setting = require('../../models/setting');
+const expect = require('chai').expect;
+
+describe('Setting: model', () => {
+
+  beforeEach(() => {
+    const defaults = {id: 1, moderation: 'pre'};
+    return Setting.update({id: '1'}, {$setOnInsert: defaults}, {upsert: true})
+  })
+
+  describe('#getSettings()', () => {
+    it('should have a moderation field defined', () => {
+      return Setting.getSettings().then(settings => {
+        expect(settings).to.have.property('moderation').and.to.equal('pre');
+      });
+    });
+  });
+
+  describe('#updateSettings()', () => {
+    it('should update the settings with a passed object', () => {
+      const mockSettings = {moderation: 'post'};
+      return Setting.updateSettings(mockSettings).then(updatedSettings => {
+        expect(updatedSettings).to.have.property('moderation').and.to.equal('post');
+      });
+    });
+  });
+
+});
---
+++ tests/routes/api/settings/index.js
@@ -0,0 +1,62 @@
+process.env.NODE_ENV = 'test';
+
+require('../../../utils/mongoose');
+
+const app = require('../../../../app');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+const Setting = require('../../../../models/setting');
+const defaults = {id: '1', moderation: 'pre'};
+
+describe('GET /settings', () => {
+
+  beforeEach(() => {
+    return Setting.update({id: '1'}, {$setOnInsert: defaults}, {upsert: true});
+  });
+
+  it('should return a settings object', done => {
+    chai.request(app)
+      .get('/api/v1/settings')
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.have.property('moderation');
+        expect(res.body.moderation).to.equal('pre');
+        done(err);
+      })
+  });
+});
+
+// update the settings.
+describe('update settings', () => {
+
+  before(() => {
+    return Setting.update({id: '1'}, {$setOnInsert: defaults}, {upsert: true});
+  });
+
+  it('should respond to a PUT with new settings', () => {
+    chai.request(app)
+    .put('/api/v1/settings')
+    .send({moderation: 'post'}, (err, res) => {
+      expect(err).to.be.null;
+      expect(res).to.have.status(204);
+      done(err);
+    });
+  });
+
+  it('should have updates settings', () => {
+    chai.request(app)
+    .get('/api/v1/settings')
+    .end((err, res) => {
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body).to.have.property('moderation');
+      expect(res.body.moderation).to.equal('post');
+    });
+  });
+});
