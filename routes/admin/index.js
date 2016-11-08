7d5eae2e2f198f0c4ab5ebc5e97070fdc44b4a4e
Wyatt Johnson <wyattjoh@gmail.com>
2016-11-08 16:21:50 -0700

    Added embed stream preview

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/embed/stream/preview', (req, res) => {
+  res.render('embed-stream', {basePath: '/client/embed/stream'});
+});
+
 router.get('*', (req, res) => {
   res.render('admin', {basePath: '/client/coral-admin'});
 });
