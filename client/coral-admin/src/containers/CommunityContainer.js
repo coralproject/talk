32dc71703ec441e03d8a07bf62a890da183733b0
Belen Curcio <curciobelen@gmail.com>
2016-11-08 19:30:46 -0300

    Table

--- client/coral-admin/src/containers/CommunityContainer.js
+++ client/coral-admin/src/containers/CommunityContainer.js
@@ -2,12 +2,32 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import I18n from 'coral-framework/i18n/i18n'
 import translations from '../translations'
+import { Grid, Cell } from 'react-mdl'
 
 export default class CommunityContainer extends Component {
   render() {
     return (
       <div>
-      <h1>Community</h1>
+        <Grid className="demo-grid-ruler">
+          <Cell col={4}>1</Cell>
+          <Cell col={8}>
+            <table className="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">
+              <thead>
+                <tr>
+                  <th class="mdl-data-table__cell--non-numeric">Username and Email</th>
+                  <th>Account Creation Date</th>
+                </tr>
+              </thead>
+              <tbody>
+              <tr>
+                <td class="mdl-data-table__cell--non-numeric">Acrylic (Transparent)</td>
+                <td>25</td>
+                <td>$2.90</td>
+              </tr>
+              </tbody>
+            </table>
+          </Cell>
+        </Grid>
       </div>
     )
   }
