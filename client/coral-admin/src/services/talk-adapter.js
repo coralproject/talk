8c215e08612dbe6ce870434620ff1c5fb14f312c
Dan Zajdband <dan.zajdband@gmail.com>
2016-11-08 17:15:57 -0500

    api-adapt(coral-admin): Get flagging mod queue working with the backend

--- client/coral-admin/src/services/talk-adapter.js
+++ client/coral-admin/src/services/talk-adapter.js
@@ -29,8 +29,12 @@ export default store => next => action => {
 
 // Get comments to fill each of the three lists on the mod queue
 const fetchModerationQueueComments = store =>
-fetch(`/api/v1/queue`)
-.then(res => res.json())
+Promise.all([fetch(`/api/v1/comments/status/pending`), fetch(`/api/v1/comments/status/rejected`), fetch(`/api/v1/comments/action/flag`)])
+.then(res => Promise.all(res.map(r => r.json())))
+.then(res => {
+  res[2] = res[2].map(comment => { comment.flagged = true; return comment; })
+  return res.reduce((prev, curr) => prev.concat(curr), [])
+})
 .then(res => store.dispatch({ type: 'COMMENTS_MODERATION_QUEUE_FETCH_SUCCESS',
   comments: res }))
 .catch(error => store.dispatch({ type: 'COMMENTS_MODERATION_QUEUE_FETCH_FAILED', error }))
