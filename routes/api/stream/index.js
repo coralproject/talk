47cc6363bc79429bd74476b495e52330a1dc5dae
David Erwin <davidjerwin@gmail.com>
2016-11-10 15:59:45 -0500

    Add settings to stream return

--- routes/api/stream/index.js
+++ routes/api/stream/index.js
@@ -1,44 +1,46 @@
+
 const express = require('express');
+const router = express.Router();
 
 const Comment = require('../../../models/comment');
 const User = require('../../../models/user');
 const Action = require('../../../models/action');
-
 const Setting = require('../../../models/setting');
 
-const router = express.Router();
 
-// Find all the comments by a specific asset_id.
+// Find all the comments, users, actions by a specific asset_id + settings.
 //  . if pre: get the comments that are accepted.
 //  . if post: get the comments that are new and accepted.
 router.get('/', (req, res, next) => {
-  const commentsPromise = Setting.getModerationSetting().then(({moderation}) => {
-    switch(moderation){
-    case 'pre':
-      return Comment.findAcceptedByAssetId(req.query.asset_id);
-    case 'post':
-      return  Comment.findAcceptedAndNewByAssetId(req.query.asset_id);
-    default:
-      throw new Error('Moderation setting not found.');
-    }
-  });
 
-  // Get all the users and actions for those comments.
+  const commentsPromise = Setting.getModerationSetting()
+    .then(({moderation}) => {
+      switch(moderation){
+      case 'pre':
+        return Comment.findAcceptedByAssetId(req.query.asset_id);
+      case 'post':
+        return  Comment.findAcceptedAndNewByAssetId(req.query.asset_id);
+      default:
+        throw new Error('Moderation setting not found.');
+      }
+    });
+
+  // Get all the users and actions for those comments + the settings.
   commentsPromise.then(comments => {
     return Promise.all([
       comments,
       User.findByIdArray(comments.map((comment) => comment.author_id)),
-      Action.getActionSummaries(comments.map((comment) => comment.id))
+      Action.getActionSummaries(comments.map((comment) => comment.id)),
+      Setting.getModerationSetting()
     ]);
-  }).then(([comments, users, actions]) => {
+  }).then(([comments, users, actions, settings]) => {
     res.json({
       comments,
       users,
-      actions
+      actions,
+      settings
     });
-  }).catch(error => {
-    next(error);
-  });
+  }).catch(next);
 });
 
 module.exports = router;
