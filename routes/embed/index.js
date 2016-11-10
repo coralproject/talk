75e1f867196dbc0f3973ef2db0931feed63d5db5
David Erwin <davidjerwin@gmail.com>
2016-11-10 17:31:59 -0500

    Create embed router

---
+++ routes/embed/index.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const router = express.Router();
+
+router.use('/:embed', (req, res, next) => {
+  switch (req.params.embed) {
+  case 'stream':
+    return res.render('embed/stream', {});
+  default:
+    // will return a 404.
+    return next();
+  }
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,10 @@
-const express = require('express');
 
+const express = require('express');
 const router = express.Router();
 
 router.use('/api/v1', require('./api'));
 router.use('/admin', require('./admin'));
-
-router.use('/embed/:embed', (req, res, next) => {
-  switch (req.params.embed) {
-  case 'stream':
-    return res.render('embed/stream', {});
-  default:
-    // will return a 404.
-    return next();
-  }
-});
+router.use('/embed', require('./embed'));
 
 router.get('/', (req, res) => {
   return res.render('home', {});
