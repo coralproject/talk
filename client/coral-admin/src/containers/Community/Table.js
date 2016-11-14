223887616d4983a31248256938b2f1c3e65c7ba3
Dan Zajdband <dan.zajdband@gmail.com>
2016-11-14 16:20:56 -0500

    feature(coral-admin): Add users role change

--- client/coral-admin/src/containers/Community/Table.js
+++ client/coral-admin/src/containers/Community/Table.js
@@ -1,34 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { SelectField, Option } from 'react-mdl-selectfield';
 import styles from './Community.css';
+import I18n from 'coral-framework/i18n/i18n';
+import translations from '../../translations';
+import { setRole } from '../../actions/community';
 
-const Table = ({headers, data, onHeaderClickHandler}) => (
-  <table className={`mdl-data-table ${styles.dataTable}`}>
-    <thead>
-      <tr>
-        {headers.map((header, i) =>(
-          <th
-          key={i}
-          className="mdl-data-table__cell--non-numeric"
-          onClick={() => onHeaderClickHandler({field: header.field})}>
-            {header.title}
-          </th>
-        ))}
-      </tr>
-    </thead>
-    <tbody>
-      {data.map((row, i)=> (
-        <tr key={i}>
-          <td className="mdl-data-table__cell--non-numeric">
-            {row.displayName}
-            <span className={styles.email}>{row.profiles.map(({id}) => id)}</span>
-          </td>
-          <td className="mdl-data-table__cell--non-numeric">
-            {row.created_at}
-          </td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-);
+const lang = new I18n(translations);
 
-export default Table;
+class Table extends Component {
+
+  constructor (props) {
+    super(props);
+    this.onRoleChange = this.onRoleChange.bind(this);
+  }
+
+  onRoleChange (id, role) {
+    this.props.dispatch(setRole(id, role));
+  }
+
+  render () {
+    const { headers, commenters, onHeaderClickHandler } = this.props;
+
+    return (
+      <table className={`mdl-data-table ${styles.dataTable}`}>
+        <thead>
+          <tr>
+            {headers.map((header, i) =>(
+              <th
+              key={i}
+              className="mdl-data-table__cell--non-numeric"
+              onClick={() => onHeaderClickHandler({field: header.field})}>
+                {header.title}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {commenters.map((row, i)=> (
+            <tr key={i}>
+              <td className="mdl-data-table__cell--non-numeric">
+                {row.displayName}
+                <span className={styles.email}>{row.profiles.map(({id}) => id)}</span>
+              </td>
+              <td className="mdl-data-table__cell--non-numeric">
+                {row.created_at}
+              </td>
+              <td className="mdl-data-table__cell--non-numeric">
+                <SelectField label={'Select me'} value={row.roles[0] || ''}
+                  label={lang.t('community.role')}
+                  onChange={role => this.onRoleChange(row.id, role)}>
+                  <Option value={''}>.</Option>
+                  <Option value={'moderator'}>{lang.t('community.moderator')}</Option>
+                  <Option value={'admin'}>{lang.t('community.admin')}</Option>
+                </SelectField>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default connect(state => ({ commenters: state.community.get('commenters') }))(Table);
--- client/coral-admin/src/reducers/community.js
+++ client/coral-admin/src/reducers/community.js
@@ -4,7 +4,8 @@ import {
   FETCH_COMMENTERS_REQUEST,
   FETCH_COMMENTERS_FAILURE,
   FETCH_COMMENTERS_SUCCESS,
-  SORT_UPDATE
+  SORT_UPDATE,
+  SET_ROLE
 } from '../constants/community';
 
 const initialState = Map({
@@ -37,6 +38,12 @@ export default function community (state = initialState, action) {
       })
       .set('commenters', commenters); // Sets to normal array
   }
+  case SET_ROLE:
+    const commenters = state.get('commenters');
+    const idx = commenters.findIndex(el => el.id === action.id);
+
+    commenters[idx].roles[0] = action.role
+    return state.set('commenters', commenters);
   case SORT_UPDATE :
     return state
       .set('field', action.sort.field)
