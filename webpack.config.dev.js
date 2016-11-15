721c0413321a44554f87304a4b57499a52cc6ebe
Wyatt Johnson <wyattjoh@gmail.com>
2016-11-15 13:33:02 -0700

    Added fix for webpack building

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -85,7 +85,7 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': `"${'development'}"`,
+        'NODE_ENV': `"${process.env.NODE_ENV}"`,
         'VERSION': `"${require('./package.json').version}"`
       }
     })
