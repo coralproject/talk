d389680224ed739a1def67bca5fe820c2ebace72
David Jay <davidgljay@gmail.com>
2016-11-16 17:18:17 -0500

    Utilizing test db for e2e tests.

---
+++ pree2e.sh
@@ -0,0 +1,5 @@
+node_modules/selenium-standalone/bin/selenium-standalone install
+export TALK_PORT=3011
+export TALK_MONGO_URL='mongodb://localhost/talk_test'
+npm start &
+wait 2
--- tests/e2e/globals.js
+++ tests/e2e/globals.js
@@ -1,4 +1,4 @@
 module.exports = {
   waitForConditionTimeout: 20000,
-  baseUrl: 'http://localhost:3000'
+  baseUrl: 'http://localhost:3011'
 };
