a0469301bb452dcaff725d037b12e50e8205b1f7
Riley Davis <riley.davis@gmail.com>
2016-11-21 16:32:07 -0700

    add password reset form

--- routes/api/user/index.js
+++ routes/api/user/index.js
@@ -79,6 +79,10 @@ router.post('/', (req, res, next) => {
 router.post('/update-password', (req, res, next) => {
   const {token, password} = req.body;
 
+  if (!password || password.length < 8) {
+    return res.status(400).send('Password must be at least 8 characters');
+  }
+
   User.verifyPasswordResetToken(token)
     .then(user => {
       return User.changePassword(user.id, password);
