f7daa35c41c792c37c8b5c01024a76b3e16d50ee
Belen Curcio <curciobelen@gmail.com>
2016-11-22 08:32:26 -0300

    Active Routes

--- client/coral-admin/src/components/ui/Header.js
+++ client/coral-admin/src/components/ui/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Navigation, Header} from 'react-mdl';
-import {Link} from 'react-router';
+import {Link, IndexLink} from 'react-router';
 import styles from './Header.css';
 import I18n from 'coral-framework/modules/i18n/i18n';
 import translations from '../../translations.json';
@@ -10,9 +10,9 @@ export default () => (
   <Header className={styles.header}>
     <Logo />
     <Navigation>
-      <Link className={styles.navLink} to="/admin">{lang.t('configure.moderate')}</Link>
-      <Link className={styles.navLink} to="/admin/community">{lang.t('configure.community')}</Link>
-      <Link className={styles.navLink} to="/admin/configure">{lang.t('configure.configure')}</Link>
+      <IndexLink className={styles.navLink} to="/admin" activeClassName="active">{lang.t('configure.moderate')}</IndexLink>
+      <Link className={styles.navLink} to="/admin/community" activeClassName="active">{lang.t('configure.community')}</Link>
+      <Link className={styles.navLink} to="/admin/configure" activeClassName="active">{lang.t('configure.configure')}</Link>
       <span>
         {`v${process.env.VERSION}`}
       </span>
