823c223cf12c0be75c515915288d06ea8a7dde36
David Jay <davidgljay@gmail.com>
2016-11-28 17:11:37 -0500

    Adding settings to redux store from stream.

--- client/coral-framework/index.js
+++ client/coral-framework/index.js
@@ -1,6 +1,5 @@
 import Notification from './modules/notification/Notification';
 import store from './store';
-import {fetchConfig} from './actions/config';
 import * as itemActions from './actions/items';
 import I18n from './modules/i18n/i18n';
 import * as notificationActions from './actions/notification';
@@ -9,7 +8,6 @@ import * as authActions from './actions/auth';
 export {
   Notification,
   store,
-  fetchConfig,
   itemActions,
   I18n,
   notificationActions,
