647fb06c9925c70161d7cf7798e139214dc80705
Belen Curcio <curciobelen@gmail.com>
2016-12-01 12:58:37 -0300

    HandleSave on Submit

--- client/coral-settings/components/Bio.js
+++ client/coral-settings/components/Bio.js
@@ -6,11 +6,11 @@ export default ({bio, handleSave, handleInput, handleCancel}) => (
   <div className={styles.bio}>
     <h1>Bio</h1>
     <p>Tell the community about yourself</p>
-    <form onSubmit={handleSave}>
+    <form>
       <textarea value={bio} onChange={handleInput} />
       <div className={styles.actions}>
         <Button cStyle='cancel' type="button" onClick={handleCancel} raised>Cancel</Button>
-        <Button cStyle='success' type="submit">Save Changes</Button>
+        <Button cStyle='success' type="submit" onClick={handleSave}>Save Changes</Button>
       </div>
     </form>
   </div>
