0e75319c04f920bdaa645eb3f9c29e0a971781ba
David Jay <davidgljay@gmail.com>
2016-12-01 21:49:55 -0500

    Moving banned wordlist text to translations.

--- client/coral-admin/src/containers/Configure/Wordlist.js
+++ client/coral-admin/src/containers/Configure/Wordlist.js
@@ -1,15 +1,14 @@
 import React from 'react';
-// import I18n from 'coral-framework/modules/i18n/i18n';
-// import translations from '../../translations.json';
+import I18n from 'coral-framework/modules/i18n/i18n';
+import translations from '../../translations.json';
 import styles from './Configure.css';
 import {
   Card
 } from 'react-mdl';
 
 const Wordlist = ({wordlist, onChangeWordlist}) => <Card id={styles.bannedWordlist} shadow={2}>
-  <p className={styles.bannedWordHeader}>Write the bannned words list</p>
-  <p className={styles.bannedWordText}>Comments which contain these words or phrases, not seperated by commas and not
-  case sensitive, will be automatically removed from the comment stream.</p>
+  <p className={styles.bannedWordHeader}>{lang.t('configure.banned-word-header')}</p>
+  <p className={styles.bannedWordText}>{lang.t('configure.banned-word-text')}</p>
   <textarea
     rows={5}
     type='text'
@@ -20,4 +19,4 @@ const Wordlist = ({wordlist, onChangeWordlist}) => <Card id={styles.bannedWordli
 
 export default Wordlist;
 
-// const lang = new I18n(translations);
+const lang = new I18n(translations);
