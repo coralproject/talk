44c12fb59a6e2bc628f408afccaffbd2c4ed5366
Dan Zajdband <dan.zajdband@gmail.com>
2016-12-13 11:48:28 -0500

    Added basic ui

--- client/coral-admin/src/containers/Configure/CommentSettings.js
+++ client/coral-admin/src/containers/Configure/CommentSettings.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {SelectField, Option} from 'react-mdl-selectfield';
 import I18n from 'coral-framework/modules/i18n/i18n';
 import translations from '../../translations.json';
 import styles from './Configure.css';
@@ -31,6 +32,16 @@ const updateClosedMessage = (updateSettings) => (event) => {
   updateSettings({closedMessage});
 };
 
+const updateClosedTimeout = (updateSettings, isMeasure) => (event) => {
+  if (isMeasure) {
+    
+  }
+
+  const closedTimeout = event.target.value;
+
+  updateSettings({ closedTimeout });
+});
+
 const CommentSettings = (props) => <List>
   <ListItem className={styles.configSetting}>
     <ListItemAction>
@@ -51,6 +62,25 @@ const CommentSettings = (props) => <List>
       <p>
         {lang.t('configure.include-comment-stream-desc')}
       </p>
+    </ListItemContent>
+  </ListItem>
+  <ListItem className={styles.configSettingInfoBox}>
+    <ListItemContent>
+      <p>{lang.t('configure.close-after')}</p>
+      <Textfield
+        type='number'
+        pattern='[0-9]+'
+        style={{width: 200}}
+        onChange={updateClosedMessage(props.updateSettings)}
+        value={getTimeoutAmount(props.settings.closedTimeout)}
+        label={lang.t('configure.closed-comments-label')} />
+      <SelectField value={getTimeoutMeasure(props.settings.closedTimeout)}
+        onChange={updateClosedTimeout(props.updateSettings, true)}>
+        <Option value={'never'}>{lang.t('configure.never')}</Option>
+        <Option value={'hours'}>{lang.t('configure.hours')}</Option>
+        <Option value={'days'}>{lang.t('configure.days')}</Option>
+        <Option value={'weeks'}>{lang.t('configure.weeks')}</Option>
+      </SelectField>
     </ListItemContent>
   </ListItem>
   <ListItem className={`${styles.configSettingInfoBox} ${props.settings.infoBoxEnable ? null : styles.hidden}`} >
@@ -75,5 +105,32 @@ const CommentSettings = (props) => <List>
 </List>;
 
 export default CommentSettings;
+
+const getTimeoutMeasure = ts => {
+  if (isNaN(ts)) {
+    return 'never';
+  } else if (ts % (60 * 60 * 24 * 7) === 0) {
+    return 'weeks';
+  } else if (ts % (60 * 60 * 24) === 0) {
+    return 'days';
+  } else {
+    return 'hours';
+  }
+}
+
+const getTimeoutAmount = ts => {
+  const measure = getTimeoutMeasure(ts);
+  switch (measure) {
+    case 'weeks':
+      return ts / (60 * 60 * 24 * 7);
+    case 'days':
+      return ts / (60 * 60 * 24);
+    case 'hours':
+      return ts / (60 * 60);
+    case 'never':
+    default:
+      return '';
+  }
+}
 
 const lang = new I18n(translations);
