cd63f49a5e56706068d6c84bccb8585c4ffb8bb6
David Jay <davidgljay@gmail.com>
2016-12-12 19:08:35 -0500

    Disabling save button on error and updating translations.

--- client/coral-admin/src/containers/Configure/CommentSettings.js
+++ client/coral-admin/src/containers/Configure/CommentSettings.js
@@ -12,132 +12,115 @@ import {
   Icon
 } from 'react-mdl';
 
-class CommentSettings extends Component {
+const updateCharCountEnable = (updateSettings, charCountChecked) => () => {
+  const charCountEnable = !charCountChecked;
+  updateSettings({charCountEnable});
+};
 
-  state = {
-    charCountError: false
+const updateCharCount = (updateSettings, settingsError) => (event) => {
+  const charCount = event.target.value;
+  if (charCount.match(/[^0-9]/)) {
+    settingsError('charCount', true);
+  } else {
+    settingsError('charCount', false);
   }
+  updateSettings({charCount: charCount});
+};
 
-  static propTypes = {
-    updateSettings: PropTypes.func.isRequired,
-    settings: PropTypes.object.isRequired
-  }
+const updateModeration = (updateSettings, mod) => () => {
+  const moderation = mod === 'pre' ? 'post' : 'pre';
+  updateSettings({moderation});
+};
 
-  updateCharCountEnable = (updateSettings, charCountChecked) => () => {
-    const charCountEnable = !charCountChecked;
-    updateSettings({charCountEnable});
-  };
+const updateInfoBoxEnable = (updateSettings, infoBox) => () => {
+  const infoBoxEnable = !infoBox;
+  updateSettings({infoBoxEnable});
+};
 
-  updateCharCount = (updateSettings) => (event) => {
-    const charCount = event.target.value;
-    if (charCount.match(/[^0-9]/)) {
-      this.setState({charCountError: true});
-    } else {
-      this.setState({charCountError: false});
-    }
-    updateSettings({charCount: charCount});
-  };
+const updateInfoBoxContent = (updateSettings) => (event) => {
+  const infoBoxContent =  event.target.value;
+  updateSettings({infoBoxContent});
+};
 
-  updateModeration = (updateSettings, mod) => () => {
-    const moderation = mod === 'pre' ? 'post' : 'pre';
-    updateSettings({moderation});
-  };
+const updateClosedMessage = (updateSettings) => (event) => {
+  const closedMessage = event.target.value;
+  updateSettings({closedMessage});
+};
 
-  updateInfoBoxEnable = (updateSettings, infoBox) => () => {
-    const infoBoxEnable = !infoBox;
-    updateSettings({infoBoxEnable});
-  };
-
-  updateInfoBoxContent = (updateSettings) => (event) => {
-    const infoBoxContent =  event.target.value;
-    updateSettings({infoBoxContent});
-  };
-
-  updateClosedMessage = (updateSettings) => (event) => {
-    const closedMessage = event.target.value;
-    updateSettings({closedMessage});
-  };
-
-  render() {
-
-    const {updateSettings, settings} = this.props;
-
-    return <List>
-      <ListItem className={`${styles.configSetting} ${settings.moderation === 'pre' ? styles.enabledSetting : styles.disabledSetting}`}>
-        <ListItemAction>
-          <Checkbox
-            onClick={this.updateModeration(updateSettings, settings.moderation)}
-            checked={settings.moderation === 'pre'} />
-        </ListItemAction>
-        <ListItemContent>
-        <div className={styles.settingsHeader}>{lang.t('configure.enable-pre-moderation')}</div>
-        <p className={settings.moderation === 'pre' ? '' : styles.disabledSettingText}>
-          {lang.t('configure.enable-pre-moderation-text')}
+const CommentSettings = ({updateSettings, settingsError, settings, errors}) => <List>
+    <ListItem className={`${styles.configSetting} ${settings.moderation === 'pre' ? styles.enabledSetting : styles.disabledSetting}`}>
+      <ListItemAction>
+        <Checkbox
+          onClick={updateModeration(updateSettings, settings.moderation)}
+          checked={settings.moderation === 'pre'} />
+      </ListItemAction>
+      <ListItemContent>
+      <div className={styles.settingsHeader}>{lang.t('configure.enable-pre-moderation')}</div>
+      <p className={settings.moderation === 'pre' ? '' : styles.disabledSettingText}>
+        {lang.t('configure.enable-pre-moderation-text')}
+      </p>
+    </ListItemContent>
+    </ListItem>
+    <ListItem className={`${styles.configSetting} ${settings.charCountEnable ? styles.enabledSetting : styles.disabledSetting}`}>
+      <ListItemAction>
+        <Checkbox
+          onClick={updateCharCountEnable(updateSettings, settings.charCountEnable)}
+          checked={settings.charCountEnable} />
+      </ListItemAction>
+      <ListItemContent>
+        <div className={styles.settingsHeader}>{lang.t('configure.comment-count-header')}</div>
+        <p className={settings.charCountEnable ? '' : styles.disabledSettingText}>
+          <span>{lang.t('configure.comment-count-text-pre')}</span>
+          <input type='text'
+            className={`${styles.charCountTexfield} ${settings.charCountEnable && styles.charCountTexfieldEnabled}`}
+            htmlFor='charCount'
+            onChange={updateCharCount(updateSettings, settingsError)}
+            value={settings.charCount}/>
+          <span>{lang.t('configure.comment-count-text-post')}</span>
+            {
+              errors.charCount &&
+              <span className={styles.settingsError}>
+                <br/>
+                <Icon name="error_outline"/>
+                {lang.t('configure.comment-count-error')}
+              </span>
+            }
         </p>
       </ListItemContent>
-      </ListItem>
-      <ListItem className={`${styles.configSetting} ${settings.charCountEnable ? styles.enabledSetting : styles.disabledSetting}`}>
-        <ListItemAction>
-          <Checkbox
-            onClick={this.updateCharCountEnable(updateSettings, settings.charCountEnable)}
-            checked={settings.charCountEnable} />
-        </ListItemAction>
-        <ListItemContent>
-          <div className={styles.settingsHeader}>{lang.t('configure.comment-count-header')}</div>
-          <p className={settings.charCountEnable ? '' : styles.disabledSettingText}>
-            <span>{lang.t('configure.comment-count-text-pre')}</span>
-            <input type='text'
-              className={`${styles.charCountTexfield} ${settings.charCountEnable && styles.charCountTexfieldEnabled}`}
-              htmlFor='charCount'
-              onChange={this.updateCharCount(updateSettings)}
-              value={settings.charCount}/>
-            <span>{lang.t('configure.comment-count-text-post')}</span>
-              {
-                this.state.charCountError &&
-                <span className={styles.settingsError}>
-                  <br/>
-                  <Icon name="error_outline"/>
-                  {lang.t('configure.comment-count-error')}
-                </span>
-              }
-          </p>
-        </ListItemContent>
-      </ListItem>
-      <ListItem threeLine className={`${styles.configSettingInfoBox} ${settings.infoBoxEnable ? styles.enabledSetting : styles.disabledSetting}`}>
-        <ListItemAction>
-          <Checkbox
-            onClick={this.updateInfoBoxEnable(updateSettings, settings.infoBoxEnable)}
-            checked={settings.infoBoxEnable} />
-        </ListItemAction>
-        <ListItemContent>
-          {lang.t('configure.include-comment-stream')}
-          <p>
-            {lang.t('configure.include-comment-stream-desc')}
-          </p>
-        </ListItemContent>
-      </ListItem>
-      <ListItem className={`${styles.configSettingInfoBox} ${settings.infoBoxEnable ? null : styles.hidden}`} >
-        <ListItemContent>
-          <Textfield
-            onChange={this.updateInfoBoxContent(updateSettings)}
-            value={settings.infoBoxContent}
-            label={lang.t('configure.include-text')}
-            rows={3}/>
-        </ListItemContent>
-      </ListItem>
-      <ListItem className={styles.configSettingInfoBox}>
-        <ListItemContent>
-          {lang.t('configure.closed-comments-desc')}
-          <Textfield
-            onChange={this.updateClosedMessage(updateSettings)}
-            value={settings.closedMessage}
-            label={lang.t('configure.closed-comments-label')}
-            rows={3}/>
-        </ListItemContent>
-      </ListItem>
-    </List>;
-  }
-}
+    </ListItem>
+    <ListItem threeLine className={`${styles.configSettingInfoBox} ${settings.infoBoxEnable ? styles.enabledSetting : styles.disabledSetting}`}>
+      <ListItemAction>
+        <Checkbox
+          onClick={updateInfoBoxEnable(updateSettings, settings.infoBoxEnable)}
+          checked={settings.infoBoxEnable} />
+      </ListItemAction>
+      <ListItemContent>
+        {lang.t('configure.include-comment-stream')}
+        <p>
+          {lang.t('configure.include-comment-stream-desc')}
+        </p>
+      </ListItemContent>
+    </ListItem>
+    <ListItem className={`${styles.configSettingInfoBox} ${settings.infoBoxEnable ? null : styles.hidden}`} >
+      <ListItemContent>
+        <Textfield
+          onChange={updateInfoBoxContent(updateSettings)}
+          value={settings.infoBoxContent}
+          label={lang.t('configure.include-text')}
+          rows={3}/>
+      </ListItemContent>
+    </ListItem>
+    <ListItem className={styles.configSettingInfoBox}>
+      <ListItemContent>
+        {lang.t('configure.closed-comments-desc')}
+        <Textfield
+          onChange={updateClosedMessage(updateSettings)}
+          value={settings.closedMessage}
+          label={lang.t('configure.closed-comments-label')}
+          rows={3}/>
+      </ListItemContent>
+    </ListItem>
+  </List>;
 
 export default CommentSettings;
 
