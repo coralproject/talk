27ffb054540959737940606fbcd474e0c6604352
Belen Curcio <curciobelen@gmail.com>
2016-12-17 11:22:01 -0300

    Creating the test users before de e2e

--- scripts/pree2e.sh
+++ scripts/pree2e.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 # install selenium
-../node_modules/selenium-standalone/bin/selenium-standalone install
+# ../node_modules/selenium-standalone/bin/selenium-standalone install
+
+# Creating Admin Test User
+{ echo admin@test.com; echo test; echo test; echo Admin Test User; echo admin;} | dotenv ./bin/cli-users create
+
+# Creating Moderator Test User
+{ echo moderator@test.com; echo test; echo test; echo Moderator Test User; echo moderator;} | dotenv ./bin/cli-users create
+
+# Creating Commenter Test User
+{ echo test@test.com; echo test; echo test; echo Commenter Test User; echo '';} | dotenv ./bin/cli-users create
 
 # start the app server
-npm start &
+# npm start &
