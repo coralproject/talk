298e1e8d7366759321329a7acf58f3a0d89112f2
Wyatt Johnson <wyattjoh@gmail.com>
2017-01-04 10:16:51 -0700

    Updates to user cli + e2e + tests
    - Updates to before + beforeEach for mongooose
    - Removed reference to dotenv from cli in e2e, should use
      NODE_ENV=test instead.
    - Changed test port from 30?? to 3000 to be consistent with what
      nightwatch was expecting

--- tests/mongoose.js
+++ tests/mongoose.js
@@ -1,27 +1,50 @@
 const mongoose = require('../services/mongoose');
 
-beforeEach(function (done) {
-  function clearDB() {
-    for (let collection in mongoose.connection.collections) {
-      mongoose.connection.collections[collection].remove(function() {});
-    }
-    return done();
-  }
-
-  if (mongoose.connection.readyState === 0) {
-    mongoose.on('open', function() {
+function waitTillConnect() {
+  return new Promise((resolve, reject) => {
+    mongoose.connection.on('open', function(err) {
       if (err) {
-        throw err;
+        return reject(err);
       }
 
-      return clearDB();
+      return resolve();
     });
-  } else {
-    return clearDB();
-  }
+  });
+}
+
+before(function(done) {
+  this.timeout(30000);
+
+  waitTillConnect()
+    .then(() => {
+      done();
+    })
+    .catch((err) => {
+      done(err);
+    });
 });
 
-after(function (done) {
+beforeEach(function(done) {
+  Promise.all(Object.keys(mongoose.connection.collections).map((collection) => {
+    return new Promise((resolve, reject) => {
+      mongoose.connection.collections[collection].remove(function(err) {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve();
+      });
+    });
+  }))
+  .then(() => {
+    done();
+  })
+  .catch((err) => {
+    done(err);
+  });
+});
+
+after(function(done) {
   mongoose.disconnect();
   return done();
 });
