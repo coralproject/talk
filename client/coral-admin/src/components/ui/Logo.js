4f4ecd8dfb745e9b4c2b2bfa57685d294cfc6d0c
Belen Curcio <curciobelen@gmail.com>
2017-01-09 18:07:02 -0300

    header

--- client/coral-admin/src/components/ui/Logo.js
+++ client/coral-admin/src/components/ui/Logo.js
@@ -5,7 +5,7 @@ import {CoralLogo} from 'coral-ui';
 export const Logo = () => (
   <div className={styles.logo}>
     <h1>
-      <CoralLogo stroke="#E5E5E5" />
+      <CoralLogo stroke="#E5E5E5" height="35px" width="35px" />
       <span>Talk</span>
     </h1>
   </div>
