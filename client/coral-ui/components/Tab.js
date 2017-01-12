87b350e3a90a3f5a2f30ebaea0154f68417a781e
Belen Curcio <curciobelen@gmail.com>
2017-01-12 17:56:00 -0300

    Missing tab wrapper

--- client/coral-ui/components/Tab.js
+++ client/coral-ui/components/Tab.js
@@ -5,7 +5,7 @@ export default ({children, tabId, active, onTabClick, cStyle = 'base'}) => (
   <li
     key={tabId}
     className={active ? styles[`${cStyle}--active`] : ''}
-    onClick={onTabClick(tabId)}
+    onClick={() => onTabClick(tabId)}
   >
     {children}
   </li>
