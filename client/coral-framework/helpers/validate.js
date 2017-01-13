7295d7852339703ceef72d955a5cf81ae5b4ac87
Riley Davis <riley.davis@gmail.com>
2017-01-13 11:13:28 -0700

    typos

--- client/coral-framework/helpers/validate.js
+++ client/coral-framework/helpers/validate.js
@@ -1,6 +1,6 @@
 export default {
   email: email => (/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(email)),
-  password: pass => (/^(?=.{8,}).*$/.test(pass)),
+  password: pass => !(/^(?=.{8,}).*$/.test(pass)),
   confirmPassword: () => true,
   displayName: displayName => (/^[a-z0-9_]+$/.test(displayName))
 };
