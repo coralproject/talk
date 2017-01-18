89a188255f335ea56a62109bcece48928a08b370
Riley Davis <riley.davis@gmail.com>
2017-01-18 15:23:07 -0700

    super amazing code.

--- client/coral-plugin-stream/Stream.js
+++ client/coral-plugin-stream/Stream.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
+import {fetchSignIn} from 'coral-framework/actions/auth';
+import RileysAwesomeCommentBox from 'coral-plugin-stream/RileysAwesomeCommentBox';
 
-const assetID = '';
+const assetID = '6187a94b-0b6d-4a96-ac6b-62b529cd8410';
 
 // MyComponent is a "presentational" or apollo-unaware component,
 // It could be a simple React class:
@@ -12,14 +14,46 @@ class Stream extends Component {
     super(props);
   }
 
+  logMeIn() {
+    fetchSignIn({email: 'riley.davis@gmail.com', password: 'film2255'})(() => {});
+  }
+
   render() {
     console.log(this.props);
-    return <div>...</div>;
+    const {data} = this.props;
+    return <div>
+      <button onClick={this.logMeIn.bind(this)}>Login or whatever</button>
+      {
+        data.loading
+        ? 'loading!'
+        : <div>
+            <RileysAwesomeCommentBox asset_id={data.asset.id} />
+            <p>Asset ID: {data.asset.id}</p>
+            <ul>
+            {
+              data.asset.comments.map(comment => {
+                return <li key={comment.id}>
+                  {comment.body} [{comment.id}]
+                  <ul>
+                    {
+                      comment.replies.map(reply => {
+                        return <li key={reply.id}>{reply.body}</li>;
+                      })
+                    }
+                  </ul>
+                </li>;
+              })
+            }
+            </ul>
+          </div>
+      }
+    </div>;
   }
 }
 
 // Initialize GraphQL queries or mutations with the `gql` tag
 const StreamQuery = gql`fragment commentView on Comment {
+  id
   body
   user {
     name: displayName
@@ -36,6 +70,7 @@ const StreamQuery = gql`fragment commentView on Comment {
 
 query AssetQuery($asset_id: ID!) {
   asset(id: $asset_id) {
+    id
     title
     url
     comments {
@@ -47,8 +82,17 @@ query AssetQuery($asset_id: ID!) {
   }
 }`;
 
+
 // We then can use `graphql` to pass the query results returned by MyQuery
 // to MyComponent as a prop (and update them as the results change)
-const StreamWithData = graphql(StreamQuery, {options: {variables: {asset_id: assetID}}})(Stream);
+const StreamWithData = graphql(
+  StreamQuery, {
+    options: {
+      variables: {
+        asset_id: assetID
+      }
+    }
+  }
+)(Stream);
 
 export default StreamWithData;
