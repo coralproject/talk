8f374852ac5d88ddded211ce005996eff580c25b
Riley Davis <riley.davis@gmail.com>
2017-02-03 10:49:52 -0700

    hide social stuff after sign up

--- client/coral-sign-in/components/SignUpContent.js
+++ client/coral-sign-in/components/SignUpContent.js
@@ -17,65 +17,68 @@ const SignUpContent = ({handleChange, formData, emailVerificationEnabled, ...pro
           {lang.t('signIn.signUp')}
         </h1>
       </div>
-      <div className={styles.socialConnections}>
-        <Button cStyle="facebook" onClick={props.fetchSignInFacebook} full>
-          {lang.t('signIn.facebookSignUp')}
-        </Button>
-      </div>
-      <div className={styles.separator}>
-        <h1>
-          {lang.t('signIn.or')}
-        </h1>
-      </div>
+
       { props.auth.error && <Alert>{props.auth.error}</Alert> }
       { beforeSignup &&
-        <form onSubmit={props.handleSignUp}>
-          <FormField
-            id="email"
-            type="email"
-            label={lang.t('signIn.email')}
-            value={formData.email}
-            showErrors={props.showErrors}
-            errorMsg={props.errors.email}
-            onChange={handleChange}
-          />
-          <FormField
-            id="displayName"
-            type="text"
-            label={lang.t('signIn.displayName')}
-            value={formData.displayName}
-            showErrors={props.showErrors}
-            errorMsg={props.errors.displayName}
-            onChange={handleChange}
-          />
-          <FormField
-            id="password"
-            type="password"
-            label={lang.t('signIn.password')}
-            value={formData.password}
-            showErrors={props.showErrors}
-            errorMsg={props.errors.password}
-            onChange={handleChange}
-            minLength="8"
-          />
-          { props.errors.password && <span className={styles.hint}> Password must be at least 8 characters. </span> }
-          <FormField
-            id="confirmPassword"
-            type="password"
-            label={lang.t('signIn.confirmPassword')}
-            value={formData.confirmPassword}
-            showErrors={props.showErrors}
-            errorMsg={props.errors.confirmPassword}
-            onChange={handleChange}
-            minLength="8"
-          />
-          <div className={styles.action}>
-            <Button type="submit" cStyle="black" id='coralSignUpButton' className={styles.signInButton} full>
-              {lang.t('signIn.signUp')}
+        <div>
+          <div className={styles.socialConnections}>
+            <Button cStyle="facebook" onClick={props.fetchSignInFacebook} full>
+              {lang.t('signIn.facebookSignUp')}
             </Button>
-            { props.auth.isLoading && <Spinner /> }
           </div>
-        </form>
+          <div className={styles.separator}>
+            <h1>
+              {lang.t('signIn.or')}
+            </h1>
+          </div>
+          <form onSubmit={props.handleSignUp}>
+            <FormField
+              id="email"
+              type="email"
+              label={lang.t('signIn.email')}
+              value={formData.email}
+              showErrors={props.showErrors}
+              errorMsg={props.errors.email}
+              onChange={handleChange}
+            />
+            <FormField
+              id="displayName"
+              type="text"
+              label={lang.t('signIn.displayName')}
+              value={formData.displayName}
+              showErrors={props.showErrors}
+              errorMsg={props.errors.displayName}
+              onChange={handleChange}
+            />
+            <FormField
+              id="password"
+              type="password"
+              label={lang.t('signIn.password')}
+              value={formData.password}
+              showErrors={props.showErrors}
+              errorMsg={props.errors.password}
+              onChange={handleChange}
+              minLength="8"
+            />
+            { props.errors.password && <span className={styles.hint}> Password must be at least 8 characters. </span> }
+            <FormField
+              id="confirmPassword"
+              type="password"
+              label={lang.t('signIn.confirmPassword')}
+              value={formData.confirmPassword}
+              showErrors={props.showErrors}
+              errorMsg={props.errors.confirmPassword}
+              onChange={handleChange}
+              minLength="8"
+            />
+            <div className={styles.action}>
+              <Button type="submit" cStyle="black" id='coralSignUpButton' className={styles.signInButton} full>
+                {lang.t('signIn.signUp')}
+              </Button>
+              { props.auth.isLoading && <Spinner /> }
+            </div>
+          </form>
+        </div>
       }
       {
         successfulSignup &&
