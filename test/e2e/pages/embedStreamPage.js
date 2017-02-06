fe6d6f611db972497cd5b94c12e49a91377e6f84
Riley Davis <riley.davis@gmail.com>
2017-02-06 14:59:23 -0700

    pause suddenly not a function?

--- test/e2e/pages/embedStreamPage.js
+++ test/e2e/pages/embedStreamPage.js
@@ -35,10 +35,8 @@ const embedStreamCommands = {
       .waitForElementVisible('@signInDialogPassword')
       .setValue('@signInDialogEmail', user.email)
       .setValue('@signInDialogPassword', user.pass)
-      .pause(2500)
       .waitForElementVisible('@logInButton')
       .click('@logInButton')
-      .pause(2500)
       .waitForElementVisible('@logoutButton', 5000);
   },
   logout() {
