f47638ae07bec0816f02467df3c1eb02a8f7316d
Wyatt Johnson <wyattjoh@gmail.com>
2017-02-07 09:45:43 -0700

    Removed provisioning text.

--- client/coral-admin/src/containers/Install/components/Steps/InitialStep.js
+++ client/coral-admin/src/containers/Install/components/Steps/InitialStep.js
@@ -8,8 +8,6 @@ const InitialStep = props => {
     <div className={styles.step}>
       <p>
         The remainder of the Talk installation will take about ten minutes.
-        Once you complete the following three steps, you will have a free
-        installation and provision of Mongo and Redis.
       </p>
       <Button cStyle='green' onClick={nextStep} raised>Get Started</Button>
     </div>
