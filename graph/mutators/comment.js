3be8ae39c4265347d0e8e8d89f35cbbf57a8917b
Wyatt Johnson <wyattjoh@gmail.com>
2017-02-15 17:53:13 -0700

    Removed old TODO's

--- graph/mutators/comment.js
+++ graph/mutators/comment.js
@@ -189,10 +189,6 @@ const setCommentStatus = ({loaders: {Comments}}, {id, status}) => {
 };
 
 module.exports = (context) => {
-
-  // TODO: refactor to something that'll return an error in the event an attempt
-  // is made to mutate state while not logged in. There's got to be a better way
-  // to do this.
   let mutators = {
     Comment: {
       create: () => Promise.reject(errors.ErrNotAuthorized),
--- graph/mutators/user.js
+++ graph/mutators/user.js
@@ -7,10 +7,6 @@ const setUserStatus = ({user}, {id, status}) => {
 };
 
 module.exports = (context) => {
-
-  // TODO: refactor to something that'll return an error in the event an attempt
-  // is made to mutate state while not logged in. There's got to be a better way
-  // to do this.
   if (context.user && context.user.can('mutation:setUserStatus')) {
     return {
       User: {
