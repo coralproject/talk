1dcdd4a9c92b1f190d40819bdfc2e6574d936f9d
Riley Davis <riley.davis@gmail.com>
2017-02-16 12:04:24 -0700

    make Dashboard first tab per Sam. simplify tabs

--- client/coral-admin/src/containers/ModerationQueue/components/ModerationMenu.js
+++ client/coral-admin/src/containers/ModerationQueue/components/ModerationMenu.js
@@ -6,38 +6,27 @@ import {Link} from 'react-router';
 
 const lang = new I18n(translations);
 
-const ModerationMenu = (props) => (
-  <div className='mdl-tabs'>
-    <div className={`mdl-tabs__tab-bar ${styles.tabBar}`}>
-      {
-        props.asset ? (
-          <div>
-            <Link to={`/admin/moderate/premod/${props.asset.id}`} className={`mdl-tabs__tab ${styles.tab}`} activeClassName={styles.active}>
-              {lang.t('modqueue.premod')}
-            </Link>
-            <Link to={`/admin/moderate/rejected/${props.asset.id}`} className={`mdl-tabs__tab ${styles.tab}`} activeClassName={styles.active}>
-              {lang.t('modqueue.rejected')}
-            </Link>
-            <Link to={`/admin/moderate/flagged/${props.asset.id}`} className={`mdl-tabs__tab ${styles.tab}`} activeClassName={styles.active}>
-            {lang.t('modqueue.flagged')}
-            </Link>
-          </div>
-          ) : (
-            <div>
-              <Link to='/admin/moderate/premod' className={`mdl-tabs__tab ${styles.tab}`} activeClassName={styles.active}>
-                {lang.t('modqueue.premod')}
-              </Link>
-              <Link to='/admin/moderate/rejected' className={`mdl-tabs__tab ${styles.tab}`} activeClassName={styles.active}>
-                {lang.t('modqueue.rejected')}
-              </Link>
-              <Link to='/admin/moderate/flagged' className={`mdl-tabs__tab ${styles.tab}`} activeClassName={styles.active}>
-                {lang.t('modqueue.flagged')}
-              </Link>
-            </div>
-          )
-      }
+const ModerationMenu = ({asset}) => {
+  const premodPath = asset ? `/admin/moderate/premod/${asset.id}` : '/admin/moderate/premod';
+  const rejectPath = asset ? `/admin/moderate/rejected/${asset.id}` : '/admin/moderate/rejected';
+  const flagPath = asset ? `/admin/modetate/flagged/${asset.id}` : '/admin/moderate/flagged';
+  return (
+    <div className='mdl-tabs'>
+      <div className={`mdl-tabs__tab-bar ${styles.tabBar}`}>
+        <div>
+          <Link to={premodPath} className={`mdl-tabs__tab ${styles.tab}`} activeClassName={styles.active}>
+            {lang.t('modqueue.premod')}
+          </Link>
+          <Link to={rejectPath} className={`mdl-tabs__tab ${styles.tab}`} activeClassName={styles.active}>
+            {lang.t('modqueue.rejected')}
+          </Link>
+          <Link to={flagPath} className={`mdl-tabs__tab ${styles.tab}`} activeClassName={styles.active}>
+          {lang.t('modqueue.flagged')}
+          </Link>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ModerationMenu;
