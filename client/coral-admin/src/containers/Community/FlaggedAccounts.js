ad1e640b8775d6b8bccdc301081b1e9b409df382
gaba <gabelula@gmail.com>
2017-02-23 16:44:30 -0800

    Adds emptystate.

--- client/coral-admin/src/containers/Community/FlaggedAccounts.js
+++ client/coral-admin/src/containers/Community/FlaggedAccounts.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
-// import I18n from 'coral-framework/modules/i18n/i18n';
-// import translations from 'coral-admin/src/translations.json';
+import I18n from 'coral-framework/modules/i18n/i18n';
+import translations from 'coral-admin/src/translations.json';
+const lang = new I18n(translations);
 
 import styles from './Community.css';
 
-// import EmptyCard from '../../components/EmptyCard';
-// const lang = new I18n(translations);
+import Loading from './Loading';
+import EmptyCard from '../../components/EmptyCard';
 
-const FlaggedAccounts = () => {
+const FlaggedAccounts = ({isFetching}) => {
+  const hasResults = false; // !isFetching && !!commenters.length;
+
   return (
     <div className={styles.container}>
-      Flagged Accounts
+    <div className={styles.mainContent}>
+      { isFetching && <Loading /> }
+      {
+        hasResults
+        ? <div></div>
+        : <EmptyCard>{lang.t('community.no-flagged-accounts')}</EmptyCard>
+      }
+    </div>
     </div>
   );
 };
