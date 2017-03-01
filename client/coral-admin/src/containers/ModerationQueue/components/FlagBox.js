42b380b942ac56066f93101fbfb189e767c91abe
Belen Curcio <curciobelen@gmail.com>
2017-02-28 22:35:37 -0300

    Flag box

--- client/coral-admin/src/containers/ModerationQueue/components/FlagBox.js
+++ client/coral-admin/src/containers/ModerationQueue/components/FlagBox.js
@@ -1,14 +1,20 @@
 import React, {PropTypes} from 'react';
-import styles from './styles.css';
+import {Icon} from 'coral-ui';
+import styles from './FlagBox.css';
 
 const FlagBox = props => (
   <div className={styles.flagBox}>
-    <h3>Flags:</h3>
-    <ul>
-      {props.actionSummaries.map((action, i) =>
-        <li key={i}>{!action.reason ? <i>No reason provided</i> : action.reason} (<strong>{action.count}</strong>)</li>
-      )}
-    </ul>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <Icon name='flag'/><h3>Flags ({props.actionSummaries.length}):</h3>
+        <ul>
+          {props.actionSummaries.map((action, i) =>
+            <li key={i}>{!action.reason ? <i>No reason provided</i> : action.reason} (<strong>{action.count}</strong>)</li>
+          )}
+        </ul>
+        <span className={styles.moreDetail}></span>
+      </div>
+    </div>
   </div>
 );
 
