ec4fc566c143cc050b5a2a1ecc7c31b4a3e435a3
Belen Curcio <curciobelen@gmail.com>
2017-03-03 16:15:30 -0500

    props

--- client/coral-ui/components/TextArea.js
+++ client/coral-ui/components/TextArea.js
@@ -2,8 +2,8 @@ import React, {PropTypes} from 'react';
 import styles from './TextArea.css';
 
 const TextArea = ({className, value = '', ...props}) => (
-  <div className={`${styles.textArea} ${className ? className : ''}`} {...props}>
-    <textarea value={value} />
+  <div className={`${styles.textArea} ${className ? className : ''}`}>
+    <textarea value={value} {...props}/>
   </div>
 );
 
