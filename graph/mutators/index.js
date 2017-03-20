258fd8b25aebafe1a55cb0be229915cdc272b123
gaba <gabelula@gmail.com>
2017-03-20 07:33:34 -0700

    Initial implementation at plugins

--- graph/mutators/index.js
+++ graph/mutators/index.js
@@ -1,17 +1,37 @@
 const _ = require('lodash');
+const debug = require('debug')('talk:graph:mutators');
 
 const Comment = require('./comment');
 const Action = require('./action');
 const User = require('./user');
 
+const plugins = require('../../plugins');
+
+let mutators = [
+
+  // Load in the core mutators.
+  Comment,
+  Action,
+  User,
+
+  // Load the plugin mutators from the manager.
+  ...plugins
+    .get('server', 'mutators').map(({plugin, mutators}) => {
+      debug(`added plugin '${plugin.name}'`);
+
+      return mutators;
+    })
+];
+
+/**
+ * Creates a set of mutators based on a GraphQL context.
+ * @param  {Object} context the context of the GraphQL request
+ * @return {Object}         object of mutators
+ */
 module.exports = (context) => {
 
   // We need to return an object to be accessed.
-  return _.merge(...[
-    Comment,
-    Action,
-    User,
-  ].map((mutators) => {
+  return _.merge(...mutators.map((mutators) => {
 
     // Each set of mutators is a function which takes the context.
     return mutators(context);
