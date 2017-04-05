b3d1053fd748906414f72f2d90f44b72095574b5
Chi Vinh Le <vinh@wikiwi.io>
2017-04-05 18:57:12 +0700

    Get rid of plugin actions in the framework

--- client/coral-framework/actions/index.js
+++ client/coral-framework/actions/index.js
@@ -1,11 +1,9 @@
 import * as authActions from './auth';
 import * as assetActions from './asset';
 import * as notificationActions from './notification';
-import {getPluginActions} from '../helpers/plugins';
 
 export default {
   authActions,
   assetActions,
   notificationActions,
-  pluginActions: getPluginActions(),
 };
