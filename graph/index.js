350d9e74f237230312a434db3e6b81ff43891356
David Erwin <davidjerwin@gmail.com>
2017-04-05 16:56:32 -0400

    Get plugin setupFuncitons for subscriptions

--- graph/index.js
+++ graph/index.js
@@ -1,12 +1,31 @@
 const schema = require('./schema');
 const Context = require('./context');
+const plugins = require('../services/plugins');
 
 const {connectionOptions} = require('../services/redis');
 const {RedisPubSub} = require('graphql-redis-subscriptions');
 const {SubscriptionManager} = require('graphql-subscriptions');
 const {SubscriptionServer} = require('subscriptions-transport-ws');
+const _ = require('lodash');
 
 const pubsub = new RedisPubSub(connectionOptions);
+
+let setupFunctions = {
+  commentAdded: (options, args) => ({
+    commentAdded: {
+      filter: (comment) => comment.asset_id === args.asset_id
+    },
+  }),
+};
+
+/**
+ * Plugin support requires that we merge in existing setupFunctions with our new
+ * plugin based ones. This allows plugins to extend existing setupFunctions as well
+ * as provide new ones.
+ */
+setupFunctions = plugins.get('server', 'setupFunctions').reduce((acc, {setupFunctions}) => {
+  return _.merge(acc, setupFunctions);
+}, setupFunctions);
 
 module.exports = {
   pubsub,
@@ -23,13 +42,7 @@ module.exports = {
     subscriptionManager: new SubscriptionManager({
       schema,
       pubsub,
-      setupFunctions: {
-        commentAdded: (options, args) => ({
-          commentAdded: {
-            filter: (comment) => comment.asset_id === args.asset_id
-          },
-        }),
-      }
+      setupFunctions,
     }),
     onSubscribe: (parsedMessage, baseParams, connection) => {
 
