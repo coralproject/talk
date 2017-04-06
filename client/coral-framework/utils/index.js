ae5bcf690087a210c1bbc4f41113019d217a0352
Belen Curcio <curciobelen@gmail.com>
2017-04-06 18:15:02 -0300

    getActionSummary exp

--- client/coral-framework/utils/index.js
+++ client/coral-framework/utils/index.js
@@ -1,2 +1,7 @@
-export const getActionSummary = (type, comment) => comment.action_summaries
-  .filter((a) => a.__typename === type)[0];
+/**
+* getActionSummary
+* retrieves the action summary based on the type and the comment
+*/
+
+export const getActionSummary = (type, comment) =>
+  comment.action_summaries.filter(a => a.__typename === type)[0];
