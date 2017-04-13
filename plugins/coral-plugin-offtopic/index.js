22fc8555adc8c98727aeb88e7aad85b1627721c8
Belen Curcio <curciobelen@gmail.com>
2017-04-12 22:26:30 -0300

    Extending typeDefs with Off-Topic Plugin

--- plugins/coral-plugin-offtopic/index.js
+++ plugins/coral-plugin-offtopic/index.js
@@ -1,3 +1,6 @@
-module.exports = {
+const {readFileSync} = require('fs');
+const path = require('path');
 
+module.exports = {
+  typeDefs: readFileSync(path.join(__dirname, 'server/typeDefs.graphql'), 'utf8')
 };
