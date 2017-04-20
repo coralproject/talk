3bf218768835e7b9adc425883e43e63726c36450
Chi Vinh Le <vinh@wikiwi.io>
2017-04-20 18:45:55 +0700

    Fix crashing admin when using framework

--- client/coral-framework/index.js
+++ client/coral-framework/index.js
@@ -1,16 +1,12 @@
-import store from './services/store';
 import pym from './services/PymConnection';
 import I18n from './modules/i18n/i18n';
 import actions from './actions';
-import Slot from './components/Slot';
 
 // TODO (bc): Deprecate old actions. Spreading actions is now needed.
 
 export default {
   pym,
-  Slot,
   I18n,
-  store,
   actions,
   ...actions
 };
