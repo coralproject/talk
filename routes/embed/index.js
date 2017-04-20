b559b8119847a575e07f5b491ad61bd9dab573a9
Wyatt Johnson <wyattjoh@gmail.com>
2017-04-20 15:19:46 -0600

    Removed some demo code

--- routes/embed/index.js
+++ routes/embed/index.js
@@ -13,8 +13,6 @@ router.use('/:embed', (req, res, next) => {
 
         return res.render('embed/stream', {customCssUrl, data});
       });
-  case 'live-stream':
-    return res.render('embed/live-stream');
   default:
 
     // will return a 404.
