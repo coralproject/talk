a91db35f91625c0987513783f98657eee6267336
Chi Vinh Le <vinh@wikiwi.io>
2017-04-24 23:34:53 +0700

    Add comments for filter

--- client/coral-framework/hocs/withFragments.js
+++ client/coral-framework/hocs/withFragments.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// TODO: revisit `filtering` after https://github.com/apollographql/graphql-anywhere/issues/38.
+
 function getDisplayName(WrappedComponent) {
   return WrappedComponent.displayName || WrappedComponent.name || 'Component';
 }
