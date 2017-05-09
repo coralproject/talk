52b0819e7874802182dfa317efb761927a26c81c
Belen Curcio <curciobelen@gmail.com>
2017-05-09 14:02:35 -0300

    Adding example for plugins within the Admin Comment Queue

---
+++ plugins/coral-plugin-mod/client/components/Container.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Box from './Box';
+import {Button} from 'coral-ui'
+import styles from './styles.css';
+
+export default class Footer extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      show: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState(state => ({
+      show: !state.show
+    }))
+  }
+
+  render() {
+    const {show} = this.state;
+    return (
+      <div className={styles.container}>
+        <Button cStyle="darkGrey" onClick={this.handleClick}>
+          Show Comment Status
+        </Button>
+        {show ? <Box comment={this.props.comment} /> : null}
+      </div>
+    );
+  }
+}
