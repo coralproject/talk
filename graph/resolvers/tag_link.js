c59346eb0ae5b347669bb73de89f1c03b7ba8b65
Belen Curcio <curciobelen@gmail.com>
2017-05-15 18:13:45 -0300

    tags, and assignedby nul

--- graph/resolvers/tag_link.js
+++ graph/resolvers/tag_link.js
@@ -1,6 +1,6 @@
 const TagLink = {
   assigned_by({assigned_by}, _, {user, loaders: {Users}}) {
-    if (user && user.hasRoles('ADMIN')) {
+    if (user && user.hasRoles('ADMIN') && assigned_by != null) {
       return Users.getByID.load(assigned_by);
     }
   }
