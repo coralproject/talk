57ced201ebbe6ccabcbdd14812254332e26bd51e
Belen Curcio <curciobelen@gmail.com>
2017-05-15 12:52:54 -0300

    WebStorage events

--- client/coral-framework/helpers/storage.js
+++ client/coral-framework/helpers/storage.js
@@ -82,7 +82,8 @@ export function clear() {
   }
 }
 
-window.addEventListener('storage', function(e) {
-  const msg = `${e.key} " was changed in page ${e.url} from ${e.oldValue} to ${e.newValue}`;
-  console.log(msg);
-});
+// Enable this to debug WEB Storage events
+// window.addEventListener('storage', function(e) {
+//   const msg = `${e.key} " was changed in page ${e.url} from ${e.oldValue} to ${e.newValue}`;
+//   console.log(msg);
+// });
