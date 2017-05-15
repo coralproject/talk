76b96a66a25eed369f47bf8864bec95d19c694af
Belen Curcio <curciobelen@gmail.com>
2017-05-15 11:41:36 -0300

    Adding Bearer token to the Graph Client

--- client/coral-framework/services/transport.js
+++ client/coral-framework/services/transport.js
@@ -1,11 +1,15 @@
 import {createNetworkInterface} from 'apollo-client';
+import * as Storage from '../helpers/storage';
 
 export default function getNetworkInterface(apiUrl = '/api/v1/graph/ql', headers = {}) {
   return new createNetworkInterface({
     uri: apiUrl,
     opts: {
       credentials: 'same-origin',
-      headers,
+      headers: {
+        Authorization: `Bearer ${Storage.getItem('token')}`,
+        ...headers
+      },
     },
   });
 }
