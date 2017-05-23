57576827322a2403bde3991ebe1dab68fabfa45b
gaba <gabelula@gmail.com>
2017-05-23 11:57:51 -0400

    Removes files that came from the merge and do not exist in master.

--- plugins/coral-plugin-love/client/index.js
+++ plugins/coral-plugin-love/client/index.js
@@ -3,7 +3,7 @@ import LoveButton from './LoveButton';
 import translations from './translations.json';
 import {loadTranslations} from '/coral-framework/services/i18n';
 
-Promise.all([loadTranslations(translations)]);
+loadTranslations(translations);
 
 export default {
   slots: {
