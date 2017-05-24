03f70bcab9dbfacb227fd5133d493a7b0f7ae926
gaba <gabelula@gmail.com>
2017-05-24 11:32:52 -0400

    Adds more missing translation.

--- client/coral-admin/src/containers/ModerationQueue/helpers/moderationQueueActionsMap.js
+++ client/coral-admin/src/containers/ModerationQueue/helpers/moderationQueueActionsMap.js
@@ -8,7 +8,7 @@ export const menuActionsMap = {
   'REJECT': {status: 'REJECTED', text: 'reject', icon: 'close', key: 'r'},
   'REJECTED': {status: 'REJECTED', text: 'rejected', icon: 'close'},
   'APPROVE': {status: 'ACCEPTED', text: 'approve', icon: 'done', key: 't'},
-  'FLAGGED': {status: 'FLAGGED', text: 'rlag', icon: 'flag', filter: 'Untouched'},
+  'FLAGGED': {status: 'FLAGGED', text: 'flag', icon: 'flag', filter: 'Untouched'},
   'BAN': {status: 'BANNED', text: 'ban_user', icon: 'not interested'},
   '': {icon: 'done'}
 };
