d5f6beea22cdb41f4ef0b8f63cb8d6f2747b5007
StephanieDClark <stephanie@devisolutions.com>
2017-06-05 14:49:46 -0700

    Adjusting tab classes

--- client/coral-ui/components/Tab.js
+++ client/coral-ui/components/Tab.js
@@ -4,7 +4,7 @@ import styles from './Tab.css';
 export default ({children, tabId, active, onTabClick, cStyle = 'base', cNames}) => (
   <li
     key={tabId}
-    className={`${active ? styles[`${cStyle}--active`] : ''} tab ${cNames}`}
+    className={`${active ? styles[`${cStyle}--active`] : ''} tab ${cNames ? cNames : ''}`}
     onClick={() => onTabClick(tabId)}
   >
     {children}
