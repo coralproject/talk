ad4c4a0a5b3e203ed1ff7655983893d458d152d6
Riley Davis <riley.davis@gmail.com>
2017-06-05 16:46:58 -0600

    fix for active tab not showing. user box classNames

--- client/coral-embed-stream/src/components/Embed.js
+++ client/coral-embed-stream/src/components/Embed.js
@@ -38,13 +38,15 @@ export default class Embed extends React.Component {
     const {asset: {totalCommentCount}} = this.props.root;
     const {user} = this.props.auth;
 
+    console.log('activeTab', activeTab);
+
     return (
       <div>
         <div className="commentStream">
           <TabBar onChange={this.changeTab} activeTab={activeTab} className='talk-stream-tabbar'>
-            <Tab className={'talk-stream-comment-count-tab'}><Count count={totalCommentCount}/></Tab>
-            <Tab className={'talk-stream-profile-tab'}>{t('framework.my_profile')}</Tab>
-            <Tab className={'talk-stream-configuration-tab'} restricted={!can(user, 'UPDATE_CONFIG')}>{t('framework.configure_stream')}</Tab>
+            <Tab className={'talk-stream-comment-count-tab'} id='stream'><Count count={totalCommentCount}/></Tab>
+            <Tab className={'talk-stream-profile-tab'} id='profile'>{t('framework.my_profile')}</Tab>
+            <Tab className={'talk-stream-configuration-tab'} id='config' restricted={!can(user, 'UPDATE_CONFIG')}>{t('framework.configure_stream')}</Tab>
           </TabBar>
           {commentId &&
             <Button
