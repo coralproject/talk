0fcdd09f9fc9dfcd87328f96b4e5abe0fee27398
Chi Vinh Le <vinh@wikiwi.io>
2017-06-06 20:50:07 +0700

    Lazily create apollo client

--- client/coral-framework/services/client.js
+++ client/coral-framework/services/client.js
@@ -2,27 +2,34 @@ import ApolloClient, {addTypename} from 'apollo-client';
 import {networkInterface} from './transport';
 import {SubscriptionClient, addGraphQLSubscriptions} from 'subscriptions-transport-ws';
 
-const wsClient = new SubscriptionClient(`ws://${location.host}/api/v1/live`, {
-  reconnect: true
-});
+let client;
 
-const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
-  networkInterface,
-  wsClient,
-);
+export function getClient() {
+  if (client) {
+    return client;
+  }
 
-export const client = new ApolloClient({
-  connectToDevTools: true,
-  addTypename: true,
-  queryTransformer: addTypename,
-  dataIdFromObject: (result) => {
-    if (result.id && result.__typename) { // eslint-disable-line no-underscore-dangle
-      return `${result.__typename}_${result.id}`; // eslint-disable-line no-underscore-dangle
-    }
-    return null;
-  },
-  networkInterface: networkInterfaceWithSubscriptions,
-});
+  const wsClient = new SubscriptionClient(`ws://${location.host}/api/v1/live`, {
+    reconnect: true
+  });
 
-export default client;
+  const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+    networkInterface,
+    wsClient,
+  );
 
+  client = new ApolloClient({
+    connectToDevTools: true,
+    addTypename: true,
+    queryTransformer: addTypename,
+    dataIdFromObject: (result) => {
+      if (result.id && result.__typename) { // eslint-disable-line no-underscore-dangle
+        return `${result.__typename}_${result.id}`; // eslint-disable-line no-underscore-dangle
+      }
+      return null;
+    },
+    networkInterface: networkInterfaceWithSubscriptions,
+  });
+
+  return client;
+}
