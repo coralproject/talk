3d8478dfb24da7d017921d318ddd22addf9db363
Erica Irving <erica.irving@washpost.com>
2017-06-06 12:17:32 -0400

    add semantic classname to active tab

--- client/coral-ui/components/Tab.js
+++ client/coral-ui/components/Tab.js
@@ -4,7 +4,7 @@ import styles from './Tab.css';
 export default ({children, tabId, active, onTabClick, cStyle = 'base', ...props}) => (
   <li
     key={tabId}
-    className={`${active ? styles[`${cStyle}--active`] : ''} tab ${props.className}`}
+    className={`${active ? `${styles[`${cStyle}--active`]} tab-active` : ''} tab ${props.className}`}
     onClick={() => onTabClick(tabId)}
   >
     {children}
