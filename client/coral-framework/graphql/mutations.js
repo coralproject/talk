f6798af74d38f0b115e98dde1f3fcd59a0ac7577
Belen Curcio <curciobelen@gmail.com>
2017-06-08 12:37:37 -0300

    Lint

--- client/coral-framework/graphql/mutations.js
+++ client/coral-framework/graphql/mutations.js
@@ -252,7 +252,7 @@ export const withRemoveTag = withMutation(
             // Read the data from our cache for this query.
             const data = proxy.readFragment({fragment: COMMENT_FRAGMENT, id: fragmentId});
 
-            const idx = data.tags.findIndex(i => i.tag.name === 'BEST');
+            const idx = data.tags.findIndex((i) => i.tag.name === 'BEST');
 
             data.tags = [...data.tags.slice(0, idx), ...data.tags.slice(idx + 1)];
 
