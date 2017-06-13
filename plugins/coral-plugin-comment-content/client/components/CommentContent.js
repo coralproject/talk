5633dd27fc7119751587d6da25c47e26fd639981
Belen Curcio <curciobelen@gmail.com>
2017-06-13 15:53:59 -0300

    Link should be target blank

--- plugins/coral-plugin-comment-content/client/components/CommentContent.js
+++ plugins/coral-plugin-comment-content/client/components/CommentContent.js
@@ -10,7 +10,7 @@ const CommentContent = ({comment}) => {
       textbreaks.map((line, i) => {
         return (
           <span key={i} className={`${name}-line`}>
-            <Linkify properties={{target: '_parent'}}>
+            <Linkify properties={{target: '_blank'}}>
               {line}
             </Linkify>
             <br className={`${name}-linebreak`}/>
