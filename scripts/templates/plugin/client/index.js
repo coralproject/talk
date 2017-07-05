7c753ee518d4a89606a6cfc05ae7b6b97ea575d2
Belen Curcio <curciobelen@gmail.com>
2017-07-05 16:41:52 -0300

    create-plugin

---
+++ scripts/templates/plugin/client/index.js
@@ -0,0 +1,20 @@
+
+/**
+  This is a client index example file and it should look like this:
+  
+  ```
+  import LoveButton from './components/LoveButton';
+
+  export default {
+    slots: {
+      commentReactions: [LoveButton]
+    },
+    reducer,
+    translations
+  };
+  ```
+
+  To read more info on how to build client plugins. Please, go to: https://coralproject.github.io/talk/plugins-client.html
+ */
+
+export default {};
