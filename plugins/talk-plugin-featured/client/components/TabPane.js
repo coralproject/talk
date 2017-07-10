ae52b27009977bfbe6beb85fb321b0e37c4a7628
Chi Vinh Le <vinh@wikiwi.io>
2017-07-10 21:53:55 +0700

    Don't use div inside p

--- plugins/talk-plugin-featured/client/components/TabPane.js
+++ plugins/talk-plugin-featured/client/components/TabPane.js
@@ -3,12 +3,13 @@ import React from 'react';
 export default ({asset: {recentComments}}) => (
   <div>
     {recentComments.map((comment) => (
-      <p key={comment.id}>
+      <div key={comment.id}>
         <div><strong>{comment.user.username}</strong></div>
         <div>
           {comment.body}
         </div>
-      </p>
+        <hr />
+      </div>
     ))}
   </div>
 );
