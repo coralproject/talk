d72df73d6f6836503d4f37af86b1c1093d7d3f89
Belen Curcio <curciobelen@gmail.com>
2017-07-13 12:53:01 -0300

    Implementing first draft of FC

--- plugins/talk-plugin-featured-comments/client/index.js
+++ plugins/talk-plugin-featured-comments/client/index.js
@@ -1,13 +1,15 @@
 import translations from './translations.json';
 import FeaturedTag from './components/FeaturedTag';
 import FeaturedButton from './components/FeaturedButton';
-import FeaturedComments from './components/FeaturedComments';
+import Tab from './containers/Tab';
+import TabPane from './containers/TabPane';
 
 export default {
   translations,
   slots: {
     commentInfoBar: [FeaturedTag],
     commentReactions: [FeaturedButton],
-    stream: [FeaturedComments]
+    streamTabs: [Tab],
+    streamTabPanes: [TabPane]
   }
 };
