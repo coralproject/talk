d90f87428ca15da2675f5882f4bb14000cb9bab5
Chi Vinh Le <vinh@wikiwi.io>
2017-07-17 19:37:01 +0700

    Remove changes in featured

--- plugins/talk-plugin-featured/client/containers/Tab.js
+++ plugins/talk-plugin-featured/client/containers/Tab.js
@@ -1,14 +1,9 @@
 import {compose, gql} from 'react-apollo';
 import withFragments from 'coral-framework/hocs/withFragments';
-import excludeIf from 'coral-framework/hocs/excludeIf';
-import connect from 'coral-framework/hocs/connect';
 import Tab from '../components/Tab';
 
 // TODO: This is just example code, and needs to replaced by an actual implementation.
 const enhance = compose(
-  connect((state) => ({
-    stream: state.stream,
-  })),
   withFragments({
     asset: gql`
       fragment TalkFeatured_Tab_asset on Asset {
@@ -17,9 +12,6 @@ const enhance = compose(
         }
       }`,
   }),
-  excludeIf((props) => {
-    return props.asset.recentComments.length === 0;
-  })
 );
 
 export default enhance(Tab);
