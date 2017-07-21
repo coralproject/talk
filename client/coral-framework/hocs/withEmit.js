1db61941ed7ea11049819f72a239cabb2011e9a7
Chi Vinh Le <vinh@wikiwi.io>
2017-07-22 00:10:45 +0700

    Comment WithEmit

--- client/coral-framework/hocs/withEmit.js
+++ client/coral-framework/hocs/withEmit.js
@@ -1,14 +1,18 @@
 import React from 'react';
 const PropTypes = require('prop-types');
 
+/**
+ * WithEmit provides a property `emit: (eventName, value)`
+ * to the wrapped component.
+ */
 export default (WrappedComponent) => {
   class WithEmit extends React.Component {
     static contextTypes = {
       eventEmitter: PropTypes.object,
     };
 
-    emit = (eventName, value, context) => {
-      this.context.eventEmitter.emit(eventName, value, context);
+    emit = (eventName, value) => {
+      this.context.eventEmitter.emit(eventName, value);
     };
 
     render() {
