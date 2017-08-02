e6b7fcbd9d079d0953330ce554994baf87781e8c
Chi Vinh Le <vinh@wikiwi.io>
2017-08-02 23:41:26 +0700

    Show success notification

--- plugins/talk-plugin-featured-comments/client/components/ModTag.js
+++ plugins/talk-plugin-featured-comments/client/components/ModTag.js
@@ -3,6 +3,7 @@ import cn from 'classnames';
 import styles from './ModTag.css';
 import {t} from 'plugin-api/beta/client/services';
 import {Icon} from 'plugin-api/beta/client/components/ui';
+import * as notification from 'coral-admin/src/services/notification';
 
 export default class ModTag extends React.Component {
   constructor() {
@@ -27,9 +28,19 @@ export default class ModTag extends React.Component {
       on: false
     });
   }
-  
+
+  postTag = async () => {
+    try {
+      await this.props.postTag();
+      notification.success(t('talk-plugin-featured-comments.notify_self_featured', this.props.comment.user.username));
+    }
+    catch(err) {
+      notification.showMutationErrors(err);
+    }
+  }
+
   render() {
-    const {alreadyTagged, deleteTag, postTag} = this.props;
+    const {alreadyTagged, deleteTag} = this.props;
 
     return alreadyTagged ? (
       <span className={cn(styles.tag, styles.featured)}
@@ -37,13 +48,13 @@ export default class ModTag extends React.Component {
             onMouseEnter={this.handleMouseEnter}
             onMouseLeave={this.handleMouseLeave} >
         <Icon name="star_outline" className={cn(styles.tagIcon)} />
-        {!this.state.on ? t('talk-plugin-featured-comments.featured') : t('talk-plugin-featured-comments.un_feature')}  
+        {!this.state.on ? t('talk-plugin-featured-comments.featured') : t('talk-plugin-featured-comments.un_feature')}
       </span>
     ) : (
       <span className={cn(styles.tag, {[styles.featured]: alreadyTagged})}
-            onClick={postTag} >
+            onClick={this.postTag} >
         <Icon name="star_outline" className={cn(styles.tagIcon)} />
-        {alreadyTagged ? t('talk-plugin-featured-comments.featured') : t('talk-plugin-featured-comments.feature')} 
+        {alreadyTagged ? t('talk-plugin-featured-comments.featured') : t('talk-plugin-featured-comments.feature')}
       </span>
     );
   }
