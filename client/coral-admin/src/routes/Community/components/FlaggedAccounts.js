f582cc2534198ce5a75bbea68cda14505a42c94a
Chi Vinh Le <vinh@wikiwi.io>
2017-08-30 22:25:03 +0700

    Add transition effect

--- client/coral-admin/src/routes/Community/components/FlaggedAccounts.js
+++ client/coral-admin/src/routes/Community/components/FlaggedAccounts.js
@@ -1,59 +1,74 @@
 import React from 'react';
 
 import t from 'coral-framework/services/i18n';
-import styles from './FlaggedAccounts.css';
 import EmptyCard from 'coral-admin/src/components/EmptyCard';
 import LoadMore from '../../../components/LoadMore';
 import FlaggedUser from '../containers/FlaggedUser';
+import {CSSTransitionGroup} from 'react-transition-group';
+import styles from './FlaggedAccounts.css';
 
-const FlaggedAccounts = (props) => {
-  const {
-    users,
-    loadMore,
-    showBanUserDialog,
-    showSuspendUserDialog,
-    showRejectUsernameDialog,
-    approveUser,
-    me,
-    viewUserDetail,
-  } = props;
+class FlaggedAccounts extends React.Component {
+  render() {
+    const {
+      users,
+      loadMore,
+      showBanUserDialog,
+      showSuspendUserDialog,
+      showRejectUsernameDialog,
+      approveUser,
+      me,
+      viewUserDetail,
+    } = this.props;
 
-  const hasResults = users.nodes && !!users.nodes.length;
+    const hasResults = users.nodes && !!users.nodes.length;
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.mainFlaggedContent}>
-        {
-          hasResults
-          ? <ul className={styles.list}>
-              {
-                users.nodes.map((user, index) => {
-                  return (
-                    <FlaggedUser
-                      user={user}
-                      key={index}
-                      index={index}
-                      modActionButtons={['APPROVE', 'REJECT']}
-                      showBanUserDialog={showBanUserDialog}
-                      showSuspendUserDialog={showSuspendUserDialog}
-                      showRejectUsernameDialog={showRejectUsernameDialog}
-                      approveUser={approveUser}
-                      me={me}
-                      viewUserDetail={viewUserDetail}
-                    />
-                  );
-                })
-              }
-            </ul>
-          : <EmptyCard>{t('community.no_flagged_accounts')}</EmptyCard>
-        }
-        <LoadMore
-          loadMore={loadMore}
-          showLoadMore={users.hasNextPage}
-          />
+    return (
+      <div className={styles.container}>
+        <div className={styles.mainFlaggedContent}>
+          {
+            hasResults
+            ? <CSSTransitionGroup
+                component={'ul'}
+                className={styles.list}
+                transitionName={{
+                  enter: styles.userEnter,
+                  enterActive: styles.userEnterActive,
+                  leave: styles.userLeave,
+                  leaveActive: styles.userLeaveActive,
+                }}
+                transitionEnter={true}
+                transitionLeave={true}
+                transitionEnterTimeout={100000}
+                transitionLeaveTimeout={100000}
+              >
+                {
+                  users.nodes.map((user) => {
+                    return (
+                      <FlaggedUser
+                        user={user}
+                        key={user.id}
+                        modActionButtons={['APPROVE', 'REJECT']}
+                        showBanUserDialog={showBanUserDialog}
+                        showSuspendUserDialog={showSuspendUserDialog}
+                        showRejectUsernameDialog={showRejectUsernameDialog}
+                        approveUser={approveUser}
+                        me={me}
+                        viewUserDetail={viewUserDetail}
+                      />
+                    );
+                  })
+                }
+              </CSSTransitionGroup>
+            : <EmptyCard>{t('community.no_flagged_accounts')}</EmptyCard>
+          }
+          <LoadMore
+            loadMore={loadMore}
+            showLoadMore={users.hasNextPage}
+            />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default FlaggedAccounts;
