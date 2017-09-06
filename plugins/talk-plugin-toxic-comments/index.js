a6b517b3dd647e49ea3c2555bd1701afde3106c8
Chi Vinh Le <vinh@wikiwi.io>
2017-09-06 22:14:58 +0700

    Remove unused code

--- plugins/talk-plugin-toxic-comments/index.js
+++ plugins/talk-plugin-toxic-comments/index.js
@@ -1,10 +1,8 @@
 const {readFileSync} = require('fs');
 const path = require('path');
-const router = require('./server/router');
 const hooks = require('./server/hooks');
 
 module.exports = {
   typeDefs: readFileSync(path.join(__dirname, 'server/typeDefs.graphql'), 'utf8'),
-  router,
   hooks,
 };
--- plugins/talk-plugin-toxic-comments/server/hooks.js
+++ plugins/talk-plugin-toxic-comments/server/hooks.js
@@ -1,19 +1,8 @@
 const perspective = require('./perspective');
-const {ADD_COMMENT_TAG} = require('../../../perms/constants');
 const {ErrToxic} = require('./errors');
 const {TOXICITY_THRESHOLD} = require('./constants');
 
 module.exports = {
-  Comment: {
-    tags: {
-      post(comment, input, {user}, _info, result) {
-        if (comment.metadata.perspective && user && user.can(ADD_COMMENT_TAG)) {
-          return result.concat({tag: {name: 'TOXIC', created_at: new Date()}});
-        }
-        return result;
-      }
-    },
-  },
   RootMutation: {
     createComment: {
       async pre(_, {input}, _context, _info) {
