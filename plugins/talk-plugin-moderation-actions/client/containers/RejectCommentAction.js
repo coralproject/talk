65085fea65aabdde425b8253f5dd9b47b7ff72dd
Belen Curcio <curciobelen@gmail.com>
2017-09-11 11:02:02 -0300

    No need to handle errors here

--- plugins/talk-plugin-moderation-actions/client/containers/RejectCommentAction.js
+++ plugins/talk-plugin-moderation-actions/client/containers/RejectCommentAction.js
@@ -1,28 +1,16 @@
 import React from 'react';
-import {getErrorMessages} from 'plugin-api/beta/client/utils';
 import {withSetCommentStatus} from 'plugin-api/beta/client/hocs';
-import {notify} from 'plugin-api/beta/client/actions/notification';
 import RejectCommentAction from '../components/RejectCommentAction';
-import isNil from 'lodash/isNil';
 
 class RejectCommentActionContainer extends React.Component {
 
-  rejectComment = async () => {
+  rejectComment = () => {
     const {setCommentStatus, comment} = this.props;
 
-    try {
-      const result = await setCommentStatus({
-        commentId: comment.id,
-        status: 'REJECTED'
-      });
-
-      if (!isNil(result.data.setCommentStatus)) {
-        throw result.data.setCommentStatus.errors;
-      }
-      
-    } catch (err) {
-      notify('error', getErrorMessages(err));
-    }
+    setCommentStatus({
+      commentId: comment.id,
+      status: 'REJECTED'
+    });
   }
 
   render() {
