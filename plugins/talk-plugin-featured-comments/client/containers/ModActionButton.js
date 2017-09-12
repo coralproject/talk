dbeca830a28860cf5040e3a4157a41c1929ba52c
Belen Curcio <curciobelen@gmail.com>
2017-09-12 13:30:15 -0300

    Closing after featuring and unfeaturing

--- plugins/talk-plugin-featured-comments/client/containers/ModActionButton.js
+++ plugins/talk-plugin-featured-comments/client/containers/ModActionButton.js
@@ -1,9 +1,17 @@
 import {compose} from 'react-apollo';
+import {bindActionCreators} from 'redux';
 import ModActionButton from '../components/ModActionButton';
-import {withTags} from 'plugin-api/beta/client/hocs';
+import {withTags, connect} from 'plugin-api/beta/client/hocs';
+import {closeTooltip} from 'plugins/talk-plugin-moderation-actions/client/actions';
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({
+    closeTooltip,
+  }, dispatch);
 
 const enhance = compose(
   withTags('featured'),
+  connect(null, mapDispatchToProps),
 );
 
 export default enhance(ModActionButton);
