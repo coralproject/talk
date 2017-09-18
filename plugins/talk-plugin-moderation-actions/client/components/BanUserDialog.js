aa3327945ed7c0d29df8db24cd239192090dc31a
Belen Curcio <curciobelen@gmail.com>
2017-09-18 12:42:21 -0300

    Styles and copy

--- plugins/talk-plugin-moderation-actions/client/components/BanUserDialog.js
+++ plugins/talk-plugin-moderation-actions/client/components/BanUserDialog.js
@@ -6,15 +6,16 @@ import {Dialog, Button} from 'plugin-api/beta/client/components/ui';
 export default ({showBanDialog, closeBanDialog, banUser}) => (
   <Dialog open={showBanDialog} className={styles.dialog}>
     <span className={styles.close} onClick={closeBanDialog}>×</span>
-    <h3>{t('talk-plugin-moderation-actions.ban_user_dialog_headline')}</h3>
-    <p>
+    <h2>{t('talk-plugin-moderation-actions.ban_user_dialog_headline')}</h2>
+    <h3>{t('talk-plugin-moderation-actions.ban_user_dialog_sub')}</h3>
+    <p className={styles.copy}>
       {t('talk-plugin-moderation-actions.ban_user_dialog_copy')}
     </p>
     <div className={styles.buttons}>
-      <Button cStyle="cancel" onClick={closeBanDialog} raised>
+      <Button cStyle="cancel" onClick={closeBanDialog} className={styles.cancel} raised>
         {t('talk-plugin-moderation-actions.ban_user_dialog_cancel')}
       </Button>
-      <Button cStyle="black" onClick={banUser} raised>
+      <Button cStyle="black" onClick={banUser} className={styles.confirm} raised>
         {t('talk-plugin-moderation-actions.ban_user_dialog_yes')}
       </Button>
     </div>
