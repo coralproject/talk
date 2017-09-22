66518f50dd91cf53148e0a2c6c23455a8102bc33
Ryan Yun <ryan.yun@dowjones.com>
2017-09-22 11:41:54 -0400

    add class name to off topic tag

--- plugins/talk-plugin-offtopic/client/components/OffTopicTag.js
+++ plugins/talk-plugin-offtopic/client/components/OffTopicTag.js
@@ -2,12 +2,13 @@ import React from 'react';
 import styles from './OffTopicTag.css';
 import {t} from 'plugin-api/beta/client/services';
 import {isTagged} from 'plugin-api/beta/client/utils';
+import cn from 'classnames';
 
 export default (props) => (
   <span>
     {
       isTagged(props.comment.tags, 'OFF_TOPIC') && props.depth === 0 ? (
-        <span className={styles.tag}>
+        <span className={cn(styles.tag, 'offTopicTagLabel')}>
           {t('off_topic')}
         </span>
       ) : null
