c2f22bbf98a5abc284f7e1ecdfe669fafb1bb851
Chi Vinh Le <vinh@wikiwi.io>
2017-10-02 21:37:47 +0700

    Implement user flag details as slot component

--- plugins/talk-plugin-flag-details/client/components/FlagDetails.js
+++ plugins/talk-plugin-flag-details/client/components/FlagDetails.js
@@ -7,23 +7,21 @@ import {Slot, IfSlotIsNotEmpty, CommentDetail} from 'plugin-api/beta/client/comp
 class FlagDetails extends Component {
 
   render() {
-    const {comment: {actions}, viewUserDetail, more, data, root, comment} = this.props;
+    const {comment: {actions}, more, data, root, comment} = this.props;
 
     const flagActions = actions && actions.filter((a) => a.__typename === 'FlagAction');
     const summaries = flagActions.reduce((sum, action) => {
       if (!(action.reason in sum)) {
-        sum[action.reason] = {count: 0, userFlagged: false, actions: []};
+        sum[action.reason] = {count: 0, actions: []};
       }
       sum[action.reason].count++;
       if (action.user) {
         sum[action.reason].userFlagged = true;
       }
-      sum[action.reason].actions.push(action);
       return sum;
     }, {});
 
-    const userFlagReasons = Object.keys(summaries).filter((reason) => summaries[reason].userFlagged);
-
+    const reasons = Object.keys(summaries);
     const queryData = {
       root,
       comment,
@@ -35,36 +33,13 @@ class FlagDetails extends Component {
         header={`${t('community.flags')} (${Object.keys(summaries).length})`}
         info={
           <ul className={styles.info}>
-            {Object.keys(summaries).map((reason) =>
+            {reasons.map((reason) =>
               <li key={reason} className={styles.lessDetail}>
                 {reason} {summaries[reason].userFlagged && `(${summaries[reason].count})`}
               </li>
             )}
           </ul>
         }>
-        {more && userFlagReasons.length > 0 && (
-          <ul className={styles.detail}>
-            {userFlagReasons
-              .map((reason) => (
-                <li key={reason}>
-                  {reason} ({summaries[reason].count})
-                  <ul className={styles.subDetail}>
-                    {summaries[reason].actions.map((action) =>
-                      <li key={action.user.id}>
-                        {action.user &&
-                          <a className={styles.username} onClick={() => viewUserDetail(action.user.id)}>
-                            {action.user.username}
-                          </a>
-                        }
-                        {action.message}
-                      </li>
-                    )}
-                  </ul>
-                </li>
-              ))
-            }
-          </ul>
-        )}
         {more && (
           <IfSlotIsNotEmpty
             slot="adminCommentMoreFlagDetails"
@@ -84,13 +59,14 @@ class FlagDetails extends Component {
 
 FlagDetails.propTypes = {
   more: PropTypes.bool,
+  data: PropTypes.object,
+  root: PropTypes.object,
   comment: PropTypes.shape({
     actions: PropTypes.arrayOf(PropTypes.shape({
       message: PropTypes.string,
       user: PropTypes.shape({username: PropTypes.string})
     })).isRequired,
   }).isRequired,
-  viewUserDetail: PropTypes.func.isRequired,
 };
 
 export default FlagDetails;
--- plugins/talk-plugin-flag-details/client/containers/FlagDetails.js
+++ plugins/talk-plugin-flag-details/client/containers/FlagDetails.js
@@ -1,23 +1,13 @@
 import {compose, gql} from 'react-apollo';
 import FlagDetails from '../components/FlagDetails';
-import {bindActionCreators} from 'redux';
 import {withFragments, excludeIf} from 'plugin-api/beta/client/hocs';
-import {viewUserDetail} from 'plugin-api/beta/client/actions/admin';
-import {connect} from 'react-redux';
 import {getSlotFragmentSpreads} from 'plugin-api/beta/client/utils';
 
 const slots = [
   'adminCommentMoreFlagDetails',
 ];
 
-const mapDispatchToProps = (dispatch) => ({
-  ...bindActionCreators({
-    viewUserDetail,
-  }, dispatch)
-});
-
 const enhance = compose(
-  connect(null, mapDispatchToProps),
   withFragments({
     root: gql`
       fragment CoralAdmin_FlagDetails_root on RootQuery {
