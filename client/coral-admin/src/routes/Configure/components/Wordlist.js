0434ecf7552abe100911962645f8837f78627e50
Chi Vinh Le <vinh@wikiwi.io>
2017-10-09 22:58:58 +0700

    Refactor Wordlist styles

--- client/coral-admin/src/routes/Configure/components/Wordlist.js
+++ client/coral-admin/src/routes/Configure/components/Wordlist.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import t from 'coral-framework/services/i18n';
 import TagsInput from 'coral-admin/src/components/TagsInput';
-import styles from './Configure.css';
+import styles from './Wordlist.css';
 import PropTypes from 'prop-types';
 import {Card} from 'coral-ui';
 
 const Wordlist = ({suspectWords, bannedWords, onChangeWordlist}) => (
   <div>
-    <Card id={styles.bannedWordlist} className={styles.configSetting}>
-      <div className={styles.settingsHeader}>{t('configure.banned_words_title')}</div>
-      <p className={styles.wordlistDesc}>{t('configure.banned_word_text')}</p>
+    <Card className={styles.card}>
+      <div className={styles.header}>{t('configure.banned_words_title')}</div>
+      <p>{t('configure.banned_word_text')}</p>
       <div className={styles.wrapper}>
         <TagsInput
           value={bannedWords}
@@ -18,9 +18,9 @@ const Wordlist = ({suspectWords, bannedWords, onChangeWordlist}) => (
         />
       </div>
     </Card>
-    <Card id={styles.suspectWordlist} className={styles.configSetting}>
-      <div className={styles.settingsHeader}>{t('configure.suspect_word_title')}</div>
-      <p className={styles.wordlistDesc}>{t('configure.suspect_word_text')}</p>
+    <Card className={styles.card}>
+      <div className={styles.header}>{t('configure.suspect_word_title')}</div>
+      <p>{t('configure.suspect_word_text')}</p>
       <div className={styles.wrapper}>
         <TagsInput
           value={suspectWords}
