1d089ee1517bf8cf48c740f59a517fe0e40198ff
Chi Vinh Le <vinh@wikiwi.io>
2017-10-09 23:00:21 +0700

    Refactor Domainlist styles

--- client/coral-admin/src/routes/Configure/components/Domainlist.js
+++ client/coral-admin/src/routes/Configure/components/Domainlist.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Card} from 'coral-ui';
-import styles from './Configure.css';
+import styles from './Domainlist.css';
 import TagsInput from 'coral-admin/src/components/TagsInput';
 import t from 'coral-framework/services/i18n';
 
 const Domainlist = ({domains, onChangeDomainlist}) => {
   return (
-    <Card id={styles.domainlist} className={styles.configSetting}>
+    <Card className={styles.card}>
       <div className={styles.wrapper}>
-        <div className={styles.settingsHeader}>{t('configure.domain_list_title')}</div>
-        <p className={styles.domainlistDesc}>{t('configure.domain_list_text')}</p>
+        <div className={styles.header}>{t('configure.domain_list_title')}</div>
+        <p>{t('configure.domain_list_text')}</p>
         <div className={styles.wrapper}>
           <TagsInput
             value={domains}
