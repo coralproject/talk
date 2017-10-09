59eff3e29e36252b6ad9cac63e54265af8d357a0
Chi Vinh Le <vinh@wikiwi.io>
2017-10-09 23:37:37 +0700

    Refactor moderationSettings styles

--- client/coral-admin/src/routes/Configure/components/ModerationSettings.js
+++ client/coral-admin/src/routes/Configure/components/ModerationSettings.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './Configure.css';
+import styles from './ModerationSettings.css';
 import {Card} from 'coral-ui';
 import {Checkbox} from 'react-mdl';
 import Wordlist from './Wordlist';
 import Slot from 'coral-framework/components/Slot';
 import t from 'coral-framework/services/i18n';
+import cn from 'classnames';
 
 class ModerationSettings extends React.Component {
 
@@ -47,43 +48,43 @@ class ModerationSettings extends React.Component {
     const off = styles.disabledSetting;
 
     return (
-      <div className={styles.Configure}>
+      <div>
         <h3>{t('configure.moderation_settings')}</h3>
-        <Card className={`${styles.configSetting} ${settings.requireEmailConfirmation ? on : off}`}>
+        <Card className={cn(styles.card, settings.requireEmailConfirmation ? on : off)}>
           <div className={styles.action}>
             <Checkbox
               onChange={this.updateEmailConfirmation}
               checked={settings.requireEmailConfirmation} />
           </div>
           <div className={styles.content}>
-            <div className={styles.settingsHeader}>{t('configure.require_email_verification')}</div>
+            <div className={styles.header}>{t('configure.require_email_verification')}</div>
             <p className={settings.requireEmailConfirmation ? '' : styles.disabledSettingText}>
               {t('configure.require_email_verification_text')}
             </p>
           </div>
         </Card>
-        <Card className={`${styles.configSetting} ${settings.moderation === 'PRE' ? on : off}`}>
+        <Card className={cn(styles.card, settings.moderation === 'PRE' ? on : off)}>
           <div className={styles.action}>
             <Checkbox
               onChange={this.updateModeration}
               checked={settings.moderation === 'PRE'} />
           </div>
           <div className={styles.content}>
-            <div className={styles.settingsHeader}>{t('configure.enable_pre_moderation')}</div>
+            <div className={styles.header}>{t('configure.enable_pre_moderation')}</div>
             <p className={settings.moderation === 'PRE' ? '' : styles.disabledSettingText}>
               {t('configure.enable_pre_moderation_text')}
             </p>
           </div>
         </Card>
-        <Card className={`${styles.configSetting} ${settings.premodLinksEnable ? on : off}`}>
+        <Card className={`${styles.card} ${settings.premodLinksEnable ? on : off}`}>
           <div className={styles.action}>
             <Checkbox
               onChange={this.updatePremodLinksEnable}
               checked={settings.premodLinksEnable} />
           </div>
           <div className={styles.content}>
-            <div className={styles.settingsHeader}>{t('configure.enable_premod_links')}</div>
-            <p>
+            <div className={styles.header}>{t('configure.enable_premod_links')}</div>
+            <p className={settings.premodLinksEnable ? '' : styles.disabledSettingText}>
               {t('configure.enable_premod_links_text')}
             </p>
           </div>
