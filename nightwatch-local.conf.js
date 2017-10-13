895eb5a69a7d1bf7bbdedf2e2f99cc402114b00f
Belen Curcio <curciobelen@gmail.com>
2017-10-13 09:58:19 -0300

    Adding local nightwatch config

---
+++ nightwatch-local.conf.js
@@ -0,0 +1,44 @@
+module.exports = {
+  'src_folders': './test/e2e/specs/',
+  'output_folder': './test/e2e/tests_output',
+  'page_objects_path': './test/e2e/page_objects',
+  'selenium': {
+    'start_process': true,
+    'server_path': 'node_modules/selenium-standalone/.selenium/selenium-server/2.53.1-server.jar',
+    'log_path': './test/e2e/reports',
+    'host': '127.0.0.1',
+    'port': 6666,
+    'cli_args': {
+      'webdriver.chrome.driver': 'node_modules/selenium-standalone/.selenium/chromedriver/2.25-x64-chromedriver'
+    }
+  },
+  'test_settings': {
+    'default': {
+      'launch_url' : 'http://localhost:3000',
+      'selenium_port': 6666,
+      'selenium_host': 'localhost',
+      'silent': true,
+      'desiredCapabilities': {
+        'browserName': 'chrome',
+        'javascriptEnabled': true,
+        'acceptSslCerts': true,
+        'webStorageEnabled': true,
+        'databaseEnabled': true,
+        'applicationCacheEnabled': false,
+        'nativeEvents': true
+      },
+      'screenshots' : {
+        'enabled': true,
+        'on_failure': true,
+        'on_error': true,
+        'path': './tests/e2e/reports'
+      },
+      'exclude': [
+        './tests/e2e/tests/EmbedStreamTests.js'
+      ]
+    },
+    'integration': {
+      'launch_url': 'http://localhost:3000'
+    }
+  }
+};
