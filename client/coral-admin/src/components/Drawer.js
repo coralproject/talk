b06ae9001ea89297ad6f3b403c390b9f2d98113e
Belen Curcio <curciobelen@gmail.com>
2017-10-25 16:31:46 -0300

    Adding missing classes

--- client/coral-admin/src/components/Drawer.js
+++ client/coral-admin/src/components/Drawer.js
@@ -5,6 +5,7 @@ import {IndexLink, Link} from 'react-router';
 import styles from './Drawer.css';
 import t from 'coral-framework/services/i18n';
 import {can} from 'coral-framework/services/perms';
+import cn from 'classnames';
 
 const CoralDrawer = ({handleLogout, auth = {}}) => (
   <Drawer className={styles.drawer}>
@@ -12,7 +13,7 @@ const CoralDrawer = ({handleLogout, auth = {}}) => (
       <div>
         <Navigation className={styles.nav}>
           <IndexLink
-            className={styles.navLink}
+            className={cn('talk-admin-nav-dashboard', styles.navLink)}
             to="/admin/dashboard"
             activeClassName={styles.active}>
             {t('configure.dashboard')}
@@ -20,19 +21,21 @@ const CoralDrawer = ({handleLogout, auth = {}}) => (
           {
             can(auth.user, 'MODERATE_COMMENTS') && (
               <Link
-                className={styles.navLink}
+                className={cn('talk-admin-nav-moderate', styles.navLink)}   
                 to="/admin/moderate"
                 activeClassName={styles.active}>
                 {t('configure.moderate')}
               </Link>
             )
           }
-          <Link className={styles.navLink}
+          <Link
+            className={cn('talk-admin-nav-stories', styles.navLink)}
             to="/admin/stories"
             activeClassName={styles.active}>
             {t('configure.stories')}
           </Link>
-          <Link className={styles.navLink}
+          <Link
+            className={cn('talk-admin-nav-community', styles.navLink)}
             to="/admin/community"
             activeClassName={styles.active}>
             {t('configure.community')}
@@ -41,7 +44,7 @@ const CoralDrawer = ({handleLogout, auth = {}}) => (
             can(auth.user, 'UPDATE_CONFIG') &&
             (
               <Link
-                className={styles.navLink}
+                className={cn('talk-admin-nav-configure', styles.navLink)}
                 to="/admin/configure"
                 activeClassName={styles.active}>
                 {t('configure.configure')}
