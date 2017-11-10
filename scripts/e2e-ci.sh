789fb3dfce5c9ad4310e0921b5903009565ce75a
Chi Vinh Le <vinh@wikiwi.io>
2017-11-10 20:59:01 +0100

    Disable bs on this branch

--- scripts/e2e-ci.sh
+++ scripts/e2e-ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPORTS_FOLDER=${CIRCLE_TEST_REPORTS:-./test/e2e/reports}
-CIRCLE_BRANCH=${CIRCLE_BRANCH_XX:-master}
+CIRCLE_BRANCH=${CIRCLE_BRANCH:-master}
 
 # Amount of retries before failure.
 E2E_MAX_RETRIES=${E2E_MAX_RETRIES:-1}
