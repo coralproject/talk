4727c52de42ee29eb5bbdddde7d2129adb405aa7
Belen Curcio <curciobelen@gmail.com>
2017-11-10 10:48:12 -0300

    camelCase lodash

--- client/coral-admin/src/components/ActionsMenuItem.js
+++ client/coral-admin/src/components/ActionsMenuItem.js
@@ -3,7 +3,7 @@ import cn from 'classnames';
 import {MenuItem} from 'react-mdl';
 import PropTypes from 'prop-types';
 import styles from './ActionsMenu.css';
-import camelCase from 'lodash.camelcase';
+import camelCase from 'lodash/camelCase';
 
 const ActionsMenuItem = (props) =>
   <MenuItem className={cn(styles.menuItem, props.className, 'action-menu-item')} {...props} id={camelCase(props.children)}/>;
