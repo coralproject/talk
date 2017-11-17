285ec98d54afbc1d886ae69c5cfe6f7055badcf4
Chi Vinh Le <vinh@wikiwi.io>
2017-11-17 16:53:56 +0100

    Refactor styles

--- client/coral-embed-stream/src/tabs/configure/components/AssetStatusInfo.js
+++ client/coral-embed-stream/src/tabs/configure/components/AssetStatusInfo.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Button} from 'coral-ui';
 import PropTypes from 'prop-types';
 import t, {timeago} from 'coral-framework/services/i18n';
+import cn from 'classnames';
+import styles from './AssetStatusInfo.css';
 
 class AssetStatusInfo extends React.Component {
 
@@ -45,11 +47,11 @@ class AssetStatusInfo extends React.Component {
       <div>
         <h3>{!isClosed ? t('configure.close') : t('configure.open')} {t('configure.comment_stream')}</h3>
         {(!isClosed && closedAt) ? <p>{t('configure.comment_stream_will_close')} {timeago(new Date(closedAt))}.</p> : ''}
-        <div className="close-comments-intro-wrapper">
+        <div className={cn('close-comments-intro-wrapper', styles.wrapper)}>
           <p>
             {!isClosed ? t('configure.open_stream_configuration') : t('configure.close_stream_configuration')}
           </p>
-          <Button onClick={!isClosed ? onClose : onOpen}>
+          <Button className={styles.button} onClick={!isClosed ? onClose : onOpen}>
             {!isClosed ? t('configure.close_stream') : t('configure.open_stream')}
           </Button>
         </div>
