4477c0981ff6bbb97654af2fc50539b98c51960f
Chi Vinh Le <vinh@wikiwi.io>
2017-12-08 23:38:10 +0100

    Reapply optimizations

--- client/coral-framework/services/graphql.js
+++ client/coral-framework/services/graphql.js
@@ -1,4 +1,3 @@
-import {transformDocument, createTypeGetter} from 'graphql-ast-tools';
 import {addTypenameToDocument} from 'apollo-client/queries/queryTransform';
 
 /**
@@ -6,18 +5,7 @@ import {addTypenameToDocument} from 'apollo-client/queries/queryTransform';
  * @param  {string}  basename  base path of the url
  * @return {Object}  histor service
  */
-export function createGraphQLService(registry, {
-  introspectionData,
-  optimize = false,
-}) {
-  const transformOptions = {
-    typeGetter: optimize && introspectionData ? createTypeGetter(introspectionData) : null,
-
-    // Use shared fragment map.
-    // Attention: Fragment names must be unique otherwise weird things will happen.
-    fragmentMap: {},
-  };
-
+export function createGraphQLService(registry) {
   return {
     registry,
     resolveDocument(documentOrCallback, props, context) {
@@ -25,10 +13,6 @@ export function createGraphQLService(registry, {
         ? documentOrCallback(props, context)
         : documentOrCallback;
       document = registry.resolveFragments(document);
-
-      if (optimize) {
-        document = transformDocument(document, transformOptions);
-      }
 
       // We also add typenames to the document which apollo would usually do,
       // but we also use the network interface in subscriptions directly
