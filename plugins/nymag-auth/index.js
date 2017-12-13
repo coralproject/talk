cb7cb1edc0918d5ea315409a51b4d24ec9516788
Jon Winton <winton.jon@gmail.com>
2017-12-13 12:27:02 -0500

    updating plugins

---
+++ plugins/nymag-auth/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * Creates a new Coral Talk user from the provided JWT
+ * if an existing user is not already found. Derives
+ * user ID from the JWT's "sub" claim and username from
+ * its "usn" claim. See the coral-talk Clay component
+ * for JWT claim generation.
+**/
+const UserModel = require('../../models/user');
+
+module.exports.tokenUserNotFound = ({jwt}) => {
+  const id = jwt.sub,
+    username = jwt.usn;
+
+  return UserModel.findOneAndUpdate({
+    id
+  }, {
+    id,
+    username,
+    lowercaseUsername: username.toLowerCase(),
+    roles: [],
+    profiles: [{
+      provider: 'nymag',
+      id
+    }]
+  }, {
+    setDefaultsOnInsert: true,
+    new: true,
+    upsert: true
+  });
+};
