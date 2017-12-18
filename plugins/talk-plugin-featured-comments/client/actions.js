8f1f875d76b0d3dd1a212ab78a88ca89882fda2f
Belen Curcio <curciobelen@gmail.com>
2017-12-18 13:36:30 -0300

    less data to the store

--- plugins/talk-plugin-featured-comments/client/actions.js
+++ plugins/talk-plugin-featured-comments/client/actions.js
@@ -2,8 +2,13 @@ import {OPEN_FEATURED_DIALOG, CLOSE_FEATURED_DIALOG} from './constants';
 
 export const openFeaturedDialog = (comment, asset) => ({
   type: OPEN_FEATURED_DIALOG,
-  comment,
-  asset,
+  comment: {
+    id: comment.id,
+    tags: comment.tags,
+  },
+  asset: {
+    id: asset.id,
+  },
 });
 
 export const closeFeaturedDialog = () => ({
