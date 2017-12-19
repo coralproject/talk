e4f1f5d08b58b2a1f80567625d246d80ad779dd9
Chi Vinh Le <vinh@wikiwi.io>
2017-12-19 16:14:59 +0100

    Allow disabling e2e in ci

--- scripts/e2e-ci.sh
+++ scripts/e2e-ci.sh
@@ -2,6 +2,7 @@
 
 REPORTS_FOLDER=${CIRCLE_TEST_REPORTS:-./test/e2e/reports}
 CIRCLE_BRANCH=${CIRCLE_BRANCH:-master}
+E2E_DISABLE=${E2E_DISABLE:-false}
 
 # Amount of retries before failure.
 E2E_MAX_RETRIES=${E2E_MAX_RETRIES:-1}
@@ -13,6 +14,12 @@ E2E_WAIT_FOR_TIMEOUT=${E2E_WAIT_FOR_TIMEOUT:-5000}
 # IE 64bit has issues with receiving keyboard input. Let's wait for them to fix it.
 
 E2E_BROWSERS=${E2E_BROWSERS:-chrome,firefox,edge} #ie safari
+
+
+if [[ "${E2E_DISABLE}" == "true" ]]; then
+  echo E2E is disabled.
+  exit
+fi
 
 if [[ "${CIRCLE_BRANCH}" == "master" && -n "$BROWSERSTACK_KEY" ]]; then
   echo Testing on browserstack
