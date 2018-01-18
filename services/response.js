49d20db26f453cc2022dbf97839de8cfba7cba43
Wyatt Johnson <wyattjoh@gmail.com>
2018-01-18 14:22:19 -0700

    replaced homegrown with package

--- services/response.js
+++ services/response.js
@@ -1,21 +1,4 @@
-/**
- * escapeHTMLEntities will escape HTML entities with their unicode counterparts.
- *
- * @param {String} string the string containing potentially unsafe characters
- */
-const escapeHTMLEntities = string =>
-  string.replace(/[<>&]/g, function(c) {
-    switch (c.charCodeAt(0)) {
-      case 0x3c:
-        return '\\u003c';
-      case 0x3e:
-        return '\\u003e';
-      case 0x26:
-        return '\\u0026';
-      default:
-        return c;
-    }
-  });
+const escapeHTMLEntities = require('escape-html');
 
 /**
  * encodeJSONForHTML will encode an object to be loaded on an HTML page.
