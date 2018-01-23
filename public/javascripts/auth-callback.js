e4834027b09b0af94f52a1c33d59cddace9fdd1d
Wyatt Johnson <wyattjoh@gmail.com>
2018-01-23 11:03:53 -0700

    Auth Callback Regression

--- public/javascripts/auth-callback.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-  localStorage.setItem('auth', document.getElementById('auth').innerText);
-  setTimeout(function() { window.close(); }, 50);
-});
