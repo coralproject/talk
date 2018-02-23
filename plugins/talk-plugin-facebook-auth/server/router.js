898e28fe1be91a2fec8614525888ca39a5bc891f
Mendel Konikov <mkonikov@gmail.com>
2018-02-23 08:27:21 -0500

    Typos in comments

--- plugins/talk-plugin-facebook-auth/server/router.js
+++ plugins/talk-plugin-facebook-auth/server/router.js
@@ -2,7 +2,7 @@ module.exports = router => {
   const { passport, HandleAuthPopupCallback } = require('services/passport');
 
   /**
-   * Facebook auth endpoint, this will redirect the user immediatly to facebook
+   * Facebook auth endpoint, this will redirect the user immediately to facebook
    * for authorization.
    */
   router.get(
@@ -16,7 +16,7 @@ module.exports = router => {
 
   /**
    * Facebook callback endpoint, this will send the user a html page designed to
-   * send back the user credentials upon sucesfull login.
+   * send back the user credentials upon sucessful login.
    */
   router.get('/api/v1/auth/facebook/callback', (req, res, next) => {
     // Perform the facebook login flow and pass the data back through the opener.
