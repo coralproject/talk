1fd64e873d82c7c55db05120bffc25af2f73cb2f
Chi Vinh Le <vinh@wikiwi.io>
2018-02-26 22:58:29 +0100

    Purge config.auth_token from redux on logout

--- client/coral-framework/reducers/config.js
+++ client/coral-framework/reducers/config.js
@@ -1,9 +1,15 @@
 import { MERGE_CONFIG } from '../constants/config';
+import { LOGOUT } from '../constants/auth';
 
 const initialState = {};
 
 export default function config(state = initialState, action) {
   switch (action.type) {
+    case LOGOUT:
+      return {
+        ...state,
+        auth_token: null,
+      };
     case MERGE_CONFIG:
       return {
         ...state,
