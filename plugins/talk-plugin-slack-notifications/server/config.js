cdd77339b0f22fb2ce45810195c2d7662a57a7b2
Wyatt Johnson <wyattjoh@gmail.com>
2018-02-27 15:23:29 -0700

    fixed linting error

--- plugins/talk-plugin-slack-notifications/server/config.js
+++ plugins/talk-plugin-slack-notifications/server/config.js
@@ -5,7 +5,9 @@ const config = {
 
 if (process.env.NODE_ENV !== 'test' && !config.SLACK_WEBHOOK_URL) {
   // TODO this error should point users to Talk's Slack app once that's in place
-  throw new Error('Please set the TALK_SLACK_WEBHOOK_URL environment variable to use the slack-notifications plugin.');
+  throw new Error(
+    'Please set the TALK_SLACK_WEBHOOK_URL environment variable to use the slack-notifications plugin.'
+  );
 }
 
 module.exports = config;
