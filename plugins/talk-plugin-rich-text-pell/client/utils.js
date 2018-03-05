4320263bcc2794976f4586cf87b2d66f600ba62c
okbel <curciobelen@gmail.com>
2018-03-05 17:55:56 -0300

    Add regext to test multiline strings

--- plugins/talk-plugin-rich-text-pell/client/utils.js
+++ plugins/talk-plugin-rich-text-pell/client/utils.js
@@ -2,9 +2,10 @@ export function htmlNormalizer(htmlInput) {
   const str = htmlInput;
   // We are normalizing the input from contenteditable of each browser, also removing unnecesary html tags
   // https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Editable_content#Differences_in_markup_generation
+  console.log(htmlInput);
   return str
-    .replace('<p>', '<div>') // IE outputs <p> instead of <div>s
-    .replace('</p>', '</div>') // IE outputs <p> instead of <div>s
-    .replace('<div>', '<br>')
-    .replace('</div>', '');
+    .replace(/<p>/g, '<br>')
+    .replace(/<\/p>/g, '')
+    .replace(/<div>/g, '<br>')
+    .replace(/<\/div>/g, '');
 }
