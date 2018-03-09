59137f5f540624d1e27087be03170d78c2644585
Wyatt Johnson <wyattjoh@gmail.com>
2018-03-09 13:55:53 -0700

    added tags to plugin directory

--- docs/themes/coral/scripts/lunr.js
+++ docs/themes/coral/scripts/lunr.js
@@ -8,6 +8,7 @@ hexo.extend.helper.register('lunr_index', data => {
   const index = lunr(function() {
     this.field('name', { boost: 10 });
     this.field('description');
+    this.field('tags', { boost: 50 });
     this.ref('id');
 
     sortBy(data, 'name').forEach((item, i) => {
