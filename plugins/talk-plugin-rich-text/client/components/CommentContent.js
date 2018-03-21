99955b82f5dda97b917e4a40ace1046886e93b0e
Chi Vinh Le <vinh@wikiwi.io>
2018-03-21 18:41:27 +0100

    Blockquote styling

--- plugins/talk-plugin-rich-text/client/components/CommentContent.js
+++ plugins/talk-plugin-rich-text/client/components/CommentContent.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { PLUGIN_NAME } from '../constants';
+import cn from 'classnames';
+import styles from './CommentContent.css';
 
 class CommentContent extends React.Component {
   render() {
     const { comment } = this.props;
+    const className = cn(`${PLUGIN_NAME}-text`, styles.content);
     return comment.richTextBody ? (
       <div
-        className={`${PLUGIN_NAME}-text`}
+        className={className}
         dangerouslySetInnerHTML={{ __html: comment.richTextBody }}
       />
     ) : (
-      <div className={`${PLUGIN_NAME}-text`}>{comment.body}</div>
+      <div className={className}>{comment.body}</div>
     );
   }
 }
