f8fbedd72776cc375dcc7f4bac52dc39438db063
okbel <curciobelen@gmail.com>
2018-03-26 12:06:38 -0300

    Styling and translations

--- plugins/talk-plugin-health-report/client/components/TermsAndConditionsField.js
+++ plugins/talk-plugin-health-report/client/components/TermsAndConditionsField.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { Checkbox } from 'plugin-api/beta/client/components/ui';
 import { t } from 'plugin-api/beta/client/services';
+import styles from './TermsAndConditionsField.css';
+import cn from 'classnames';
 
 const TermsLink = () => (
-  <a href={t('talk-plugin-health-report.termslink')}>
+  <a
+    className={styles.link}
+    href={t('talk-plugin-health-report.terms-link')}
+    target="_blank"
+  >
     {t('talk-plugin-health-report.terms')}
+  </a>
+);
+
+const PrivacyLink = () => (
+  <a
+    className={styles.link}
+    href={t('talk-plugin-health-report.privacy-policy-link')}
+    target="_blank"
+  >
+    {t('talk-plugin-health-report.privacy-policy')}
   </a>
 );
 
@@ -28,14 +44,24 @@ class TermsAndConditionsField extends React.Component {
 
   render() {
     return (
-      <div>
+      <div
+        className={cn(
+          styles.fieldContainer,
+          'talk-plugin-health-report-field-container'
+        )}
+      >
         <Checkbox
           checked={this.state.checked}
+          className="talk-plugin-health-report-checkbox"
           onChange={this.onChange}
           id={this.id}
         />
-        <label id={this.id}>
-          {t('talk-plugin-health-report.copy', <a>hola</a>, TermsLink)}
+        <label id={this.id} className="talk-plugin-health-report-label">
+          {t('talk-plugin-health-report.copy')}
+          <TermsLink />
+          {t('talk-plugin-health-report.and')}
+          <PrivacyLink />
+          {t('talk-plugin-health-report.from')}
         </label>
       </div>
     );
