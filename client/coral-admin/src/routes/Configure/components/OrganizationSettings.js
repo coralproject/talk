133984ab368972f1e0f62f71233b7a1655b225db
okbel <curciobelen@gmail.com>
2018-03-28 13:13:29 -0300

    wip

--- client/coral-admin/src/routes/Configure/components/OrganizationSettings.js
+++ client/coral-admin/src/routes/Configure/components/OrganizationSettings.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { Textfield } from 'react-mdl';
+import styles from './OrganizationSettings.css';
 import Slot from 'coral-framework/components/Slot';
 import t from 'coral-framework/services/i18n';
 import ConfigurePage from './ConfigurePage';
 import ConfigureCard from 'coral-framework/components/ConfigureCard';
 
 class OrganizationSettings extends React.Component {
+  updateEmail = event => {
+    const updater = { organizationContactEmail: { $set: event.target.value } };
+    this.props.updatePending({ updater });
+  };
+
   render() {
     const { settings, slotPassthrough } = this.props;
-    console.log(this.props);
+
     return (
       <ConfigurePage title={t('configure.organization_settings')}>
         <ConfigureCard title={t('configure.organization_details')}>
@@ -19,7 +25,26 @@ class OrganizationSettings extends React.Component {
             </li>
             <li>
               {t('configure.organization_contact_email')}:{' '}
-              {settings.organizationContactEmail}
+              <input
+                defaultValue={settings.organizationContactEmail}
+                readOnly
+              />
+            </li>
+          </ul>
+        </ConfigureCard>
+        <ConfigureCard title={'Edit Organization Settings'}>
+          <ul>
+            <li>
+              <label className={styles.label}>
+                {t('configure.organization_contact_email')}
+              </label>
+
+              <Textfield
+                type="text"
+                onChange={this.updateEmail}
+                value={settings.organizationContactEmail}
+                label={t('configure.organizationContactEmail')}
+              />
             </li>
           </ul>
         </ConfigureCard>
