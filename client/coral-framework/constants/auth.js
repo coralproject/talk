e6b8cd32203036674aaf2eedd290bbd23b6cb7a0
Wyatt Johnson <wyattjoh@gmail.com>
2018-04-05 14:19:11 -0600

    fixes related to external auth

--- client/coral-framework/constants/auth.js
+++ client/coral-framework/constants/auth.js
@@ -1,5 +1,7 @@
 const prefix = `TALK_FRAMEWORK`;
 
+export const SET_AUTH_TOKEN = `${prefix}_SET_AUTH_TOKEN`;
+
 export const CHECK_LOGIN_REQUEST = `${prefix}_CHECK_LOGIN_REQUEST`;
 export const CHECK_LOGIN_SUCCESS = `${prefix}_CHECK_LOGIN_SUCCESS`;
 export const CHECK_LOGIN_FAILURE = `${prefix}_CHECK_LOGIN_FAILURE`;
