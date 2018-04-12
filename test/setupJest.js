68473e5a29cd88c4efd097d49bfc3a1f09a4f696
Wyatt Johnson <wyattjoh@gmail.com>
2018-04-12 08:36:45 -0600

    added beforeTest to wait for mongoose to be ready

--- test/setupJest.js
+++ test/setupJest.js
@@ -1,5 +1,15 @@
 const mongoose = require('../services/mongoose');
 
+beforeAll(function(done) {
+  mongoose.connection.on('open', function(err) {
+    if (err) {
+      return done(err);
+    }
+
+    return done();
+  });
+}, 30000);
+
 beforeEach(async () => {
   await Promise.all(
     Object.keys(mongoose.connection.collections).map(collection => {
