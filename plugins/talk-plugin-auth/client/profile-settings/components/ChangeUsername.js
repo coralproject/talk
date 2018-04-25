754b5c5fbb6216e8344e744a7fb10ba49e9af1e7
okbel <curciobelen@gmail.com>
2018-04-25 12:20:51 -0300

    Adding Dialog, styles and more

--- plugins/talk-plugin-auth/client/profile-settings/components/ChangeUsername.js
+++ plugins/talk-plugin-auth/client/profile-settings/components/ChangeUsername.js
@@ -2,41 +2,163 @@ import React from 'react';
 import cn from 'classnames';
 import PropTypes from 'prop-types';
 import styles from './ChangeUsername.css';
-import { Button } from 'plugin-api/beta/client/components/ui';
+import { Icon, Button } from 'plugin-api/beta/client/components/ui';
+import ChangeUsernameDialog from './ChangeUsernameDialog';
 
 const initialState = {
   editing: false,
+  formData: {},
+  showDialog: false,
 };
 
 class ChangeUsername extends React.Component {
   state = initialState;
 
+  clearForm = () => {
+    this.setState(initialState);
+  };
+
+  enableEditing = () => {
+    this.setState({
+      editing: true,
+    });
+  };
+
+  disableEditing = () => {
+    this.setState({
+      editing: false,
+    });
+  };
+
+  cancel = () => {
+    this.clearForm();
+    this.disableEditing();
+  };
+
+  showDialog = () => {
+    this.setState({
+      showDialog: true,
+    });
+  };
+
+  onSave = async () => {
+    this.showDialog();
+    // this.clearForm();
+    // this.disableEditing();
+  };
+
+  onChange = e => {
+    const { name, value } = e.target;
+    this.setState(
+      state => ({
+        formData: {
+          ...state.formData,
+          [name]: value,
+        },
+      }),
+      () => {
+        console.log(this.state.formData);
+        // Validation
+        // this.fieldValidation(value, type, name);
+        // // Perform equality validation if password fields have changed
+        // if (name === 'newPassword' || name === 'confirmNewPassword') {
+        //   this.equalityValidation('newPassword', 'confirmNewPassword');
+        // }
+      }
+    );
+  };
+
+  closeDialog = () => {
+    this.setState({
+      showDialog: false,
+    });
+  };
+
   render() {
     const { username, emailAddress } = this.props;
+    const { editing } = this.state;
+
+    console.log('loading xxxx');
+
     return (
-      <section className={styles.container}>
-        <div className={styles.content}>
-          <h2 className={styles.username}>{username}</h2>
-          {emailAddress ? <p className={styles.email}>{emailAddress}</p> : null}
-        </div>
-        <div className={styles.actions}>
-          <Button
-            className={cn(styles.button, styles.saveButton)}
-            icon="save"
-            onClick={this.onSave}
-            disabled={this.isSubmitBlocked()}
-          >
-            Save
-          </Button>
-          <a className={styles.cancelButton} onClick={this.cancel}>
-            Cancel
-          </a>
-        </div>
-        <div className={styles.actions}>
-          <Button className={styles.button} onClick={this.enableEditing}>
-            Edit
-          </Button>
-        </div>
+      <section
+        className={cn('talk-plugin-auth--edit-profile', styles.container, {
+          [styles.editing]: editing,
+        })}
+      >
+        <ChangeUsernameDialog
+          showDialog={this.state.showDialog}
+          onChange={this.onChange}
+          formData={this.state.formData}
+          username={username}
+          closeDialog={this.closeDialog}
+        />
+
+        {editing ? (
+          <div className={styles.content}>
+            <ul className={styles.detailList}>
+              <li className={styles.detailItem}>
+                <label className={cn(styles.detailLabel)}>
+                  <Icon name="person" className={styles.detailLabelIcon} />
+                  <input
+                    name="username"
+                    type="text"
+                    className={styles.detailValue}
+                    onChange={this.onChange}
+                    defaultValue={username}
+                  />
+                </label>
+                <span className={styles.bottomText}>
+                  Usernames can be changed every 14 days
+                </span>
+              </li>
+              <li className={styles.detailItem}>
+                <label className={cn(styles.detailLabel, styles.disabled)}>
+                  <Icon name="email" className={styles.detailLabelIcon} />
+                  <input
+                    name="email"
+                    type="email"
+                    className={styles.detailValue}
+                    defaultValue={emailAddress}
+                    disabled={true}
+                  />
+                </label>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <div className={styles.content}>
+            <h2 className={styles.username}>{username}</h2>
+            {emailAddress ? (
+              <p className={styles.email}>{emailAddress}</p>
+            ) : null}
+          </div>
+        )}
+
+        {editing ? (
+          <div className={styles.actions}>
+            <Button
+              className={cn(styles.button, styles.saveButton)}
+              icon="save"
+              onClick={this.onSave}
+            >
+              Save
+            </Button>
+            <a className={styles.cancelButton} onClick={this.cancel}>
+              Cancel
+            </a>
+          </div>
+        ) : (
+          <div className={styles.actions}>
+            <Button
+              className={styles.button}
+              icon="settings"
+              onClick={this.enableEditing}
+            >
+              Edit Profile
+            </Button>
+          </div>
+        )}
       </section>
     );
   }
---
+++ plugins/talk-plugin-auth/client/profile-settings/components/ChangeUsernameDialog.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import cn from 'classnames';
+import styles from './ChangeUsernameDialog.css';
+import InputField from './InputField';
+import Form from './Form';
+import { Button, Dialog } from 'plugin-api/beta/client/components/ui';
+
+class ChangeUsernameDialog extends React.Component {
+  render() {
+    return (
+      <Dialog
+        open={this.props.showDialog}
+        className={cn(styles.dialog, 'talk-plugin-auth--edit-profile-dialog')}
+      >
+        <span className={styles.close} onClick={this.props.closeDialog}>
+          ×
+        </span>
+        <h1 className={styles.title}>Confirm Username Change</h1>
+        <div className={styles.content}>
+          <p className={styles.description}>
+            You are attempting to change your username. Your new username will
+            appear on all of your past and future comments.
+          </p>
+          <div className={styles.usernamesChange}>
+            <span className={styles.item}>
+              Old Username: {this.props.username}
+            </span>
+            <span className={styles.item}>
+              New Username: {this.props.formData.newUsername}
+            </span>
+          </div>
+          <Form>
+            <InputField
+              id="confirmNewUsername"
+              label="Re-enter new username"
+              name="confirmNewUsername"
+              type="text"
+              onChange={this.props.onChange}
+              value={this.props.formData.confirmNewUsername}
+            >
+              <span className={styles.bottomNote}>
+                Note: You will not be able to change your username again for 14
+                days
+              </span>
+            </InputField>
+          </Form>
+          <div className={styles.bottomActions}>
+            <Button className={styles.cancel}>Cancel</Button>
+            <Button className={styles.confirmChanges}>Confirm Changes</Button>
+          </div>
+        </div>
+      </Dialog>
+    );
+  }
+}
+
+export default ChangeUsernameDialog;
