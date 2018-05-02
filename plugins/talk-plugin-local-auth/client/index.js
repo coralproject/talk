48a78735bd4d099a40fdb33a7dc531d9d9390014
okbel <curciobelen@gmail.com>
2018-05-02 14:11:11 -0300

    Importing translations

--- plugins/talk-plugin-local-auth/client/index.js
+++ plugins/talk-plugin-local-auth/client/index.js
@@ -1,7 +1,9 @@
 import ChangePassword from './containers/ChangePassword';
 import Profile from './containers/Profile';
+import translations from './translations.yml';
 
 export default {
+  translations,
   slots: {
     profileHeader: [Profile],
     profileSettings: [ChangePassword],
