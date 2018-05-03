788cf25fa50acb9fa7888260b228c6aae77ba858
okbel <curciobelen@gmail.com>
2018-05-03 15:20:07 -0300

    moving mutations to /hocs folder

--- plugins/talk-plugin-local-auth/client/containers/Profile.js
+++ plugins/talk-plugin-local-auth/client/containers/Profile.js
@@ -4,7 +4,7 @@ import { connect, withFragments } from 'plugin-api/beta/client/hocs';
 import Profile from '../components/Profile';
 import { notify } from 'coral-framework/actions/notification';
 import { withChangeUsername } from 'plugin-api/beta/client/hocs';
-import { withUpdateEmailAddress } from './hocs';
+import { withUpdateEmailAddress } from '../hocs';
 
 const mapDispatchToProps = dispatch => bindActionCreators({ notify }, dispatch);
 
