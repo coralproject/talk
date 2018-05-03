fa66f07db6cbd8bf915a07e327186a387f4b8caa
Wyatt Johnson <wyattjoh@gmail.com>
2018-05-03 15:58:52 -0600

    standardized styles, fixed mutation bug

--- plugins/talk-plugin-profile-data/client/graphql.js
+++ plugins/talk-plugin-profile-data/client/graphql.js
@@ -10,7 +10,7 @@ export default {
     ),
   },
   mutations: {
-    RequestDownloadLink: () => ({
+    DownloadCommentHistory: () => ({
       updateQueries: {
         CoralEmbedStream_Profile: previousData =>
           update(previousData, {
