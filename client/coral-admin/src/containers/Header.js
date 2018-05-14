beef4e8b3b91a495715d3363b9832e4606d61cce
Chi Vinh Le <vinh@wikiwi.io>
2018-05-14 21:28:48 +0200

    Remove ModerationIndicator from container

--- client/coral-admin/src/containers/Header.js
+++ client/coral-admin/src/containers/Header.js
@@ -2,16 +2,13 @@ import { gql } from 'react-apollo';
 import withQuery from 'coral-framework/hocs/withQuery';
 import Header from '../components/Header';
 import CommunityIndicator from '../routes/Community/containers/Indicator';
-import ModerationIndicator from '../routes/Moderation/containers/Indicator';
 import { getDefinitionName } from 'coral-framework/utils';
 
 export default withQuery(
   gql`
     query TalkAdmin_Header($nullID: ID) {
-      ...${getDefinitionName(ModerationIndicator.fragments.root)}
       ...${getDefinitionName(CommunityIndicator.fragments.root)}
     }
-    ${ModerationIndicator.fragments.root}
     ${CommunityIndicator.fragments.root}
   `,
   {
