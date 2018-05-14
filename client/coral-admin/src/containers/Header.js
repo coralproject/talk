b5adea4b2a0ebc0e16cf08888b6222f5108713ef
Chi Vinh Le <vinh@wikiwi.io>
2018-05-14 21:30:09 +0200

    Remove unused variable

--- client/coral-admin/src/containers/Header.js
+++ client/coral-admin/src/containers/Header.js
@@ -2,6 +2,8 @@ import { gql } from 'react-apollo';
 import withQuery from 'coral-framework/hocs/withQuery';
 import Header from '../components/Header';
 import CommunityIndicator from '../routes/Community/containers/Indicator';
+// TODO: eventually we will readd modqueue counts
+// import ModerationIndicator from '../routes/Moderation/containers/Indicator';
 import { getDefinitionName } from 'coral-framework/utils';
 
 export default withQuery(
@@ -13,7 +15,7 @@ export default withQuery(
   `,
   {
     options: {
-      variables: { nullID: null },
+      // variables: { nullID: null },
     },
   }
 )(Header);
