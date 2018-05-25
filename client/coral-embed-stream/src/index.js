c1eab3f2c65b46de00daa33f662ffc8322b2e91e
Wyatt Johnson <wyattjoh@gmail.com>
2018-05-25 11:51:43 -0600

    moved event handler to bundle

--- client/coral-embed-stream/src/index.js
+++ client/coral-embed-stream/src/index.js
@@ -8,6 +8,11 @@ import reducers from './reducers';
 import TalkProvider from 'coral-framework/components/TalkProvider';
 import pluginsConfig from 'pluginsConfig';
 
+// Resolves touch handling issues encountered on IOS Safari under certain
+// circumstances. It may be related to issues reported here:
+//   https://stackoverflow.com/questions/12363742/touchstart-event-is-not-firing-inside-iframe-ios-6
+document.body.addEventListener('touchstart', () => {});
+
 async function main() {
   const context = await createContext({
     reducers,
