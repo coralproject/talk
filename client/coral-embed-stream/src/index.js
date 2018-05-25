ba2abab3589211aaf6ebec1854147f67ef892670
Wyatt Johnson <wyattjoh@gmail.com>
2018-05-25 11:54:34 -0600

    added pivotal tracker ref

--- client/coral-embed-stream/src/index.js
+++ client/coral-embed-stream/src/index.js
@@ -10,7 +10,10 @@ import pluginsConfig from 'pluginsConfig';
 
 // Resolves touch handling issues encountered on IOS Safari under certain
 // circumstances. It may be related to issues reported here:
-//   https://stackoverflow.com/questions/12363742/touchstart-event-is-not-firing-inside-iframe-ios-6
+//
+//  https://stackoverflow.com/questions/12363742/touchstart-event-is-not-firing-inside-iframe-ios-6
+//
+// Further details: https://www.pivotaltracker.com/story/show/157794038
 document.body.addEventListener('touchstart', () => {});
 
 async function main() {
