ed679096c84f2437ee5fc9b25c9850020eccc441
Chi Vinh Le <vinh@wikiwi.io>
2018-05-28 17:16:44 +0200

    Workaround RTE Button focus bug on IOS

--- plugins/talk-plugin-rich-text/client/components/rte/factories/createToggle.js
+++ plugins/talk-plugin-rich-text/client/components/rte/factories/createToggle.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '../components/Button';
-import bowser from 'bowser';
 
 /**
  *  createToggle creates a button that can be active, inactive or disabled
@@ -33,17 +32,7 @@ const createToggle = (
       this.execCommand();
     };
 
-    // Detect whether there was focus on the RTE before the click.
-    hadFocusBeforeClick = false;
-    handleMouseDown = () => (this.hadFocusBeforeClick = this.props.api.focused);
-
     handleClick = () => {
-      // Skip IOS when the focus was not there before.
-      // IOS fails to focus to the RTE correctly and scrolls to nirvana.
-      // See https://www.pivotaltracker.com/story/show/157607216
-      if (!this.hadFocusBeforeClick && bowser.ios) {
-        return;
-      }
       this.props.api.focus();
       this.formatToggle();
       this.props.api.focus();
