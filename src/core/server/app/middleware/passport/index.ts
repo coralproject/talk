92beaf6fd37a0abb654ed2328fa33cae552543a5
Wyatt Johnson <wyattjoh@gmail.com>
2018-06-27 22:03:06 +0000

    Update index.ts

--- src/core/server/app/middleware/passport/index.ts
+++ src/core/server/app/middleware/passport/index.ts
@@ -5,7 +5,7 @@ export interface PassportOptions {
   db: Db;
 }
 
-export function createPassport(_opts: PassportOptions): passport.Authenticator {
+export function createPassport(opts: PassportOptions): passport.Authenticator {
   // Create the authenticator.
   const auth = new Authenticator();
 
