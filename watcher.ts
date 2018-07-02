9f34a1c66bc4ed9fac6061f3f96a1aec7569ec85
okbel <curciobelen@gmail.com>
2018-07-02 10:58:42 -0300

    Adding chokidar and types

--- watcher.ts
+++ watcher.ts
@@ -1,51 +1,88 @@
 import watchman from "fb-watchman";
+import chokidar, { WatchOptions } from "chokidar";
 import chalk from "chalk";
 import path from "path";
 import spawn from "cross-spawn";
+var log = console.log.bind(console);
 
-const log = console.log;
-
+// path to watch
 const dir = path.resolve(__dirname, "./src");
 
+// types
 interface File {
   name: string;
 }
 
 interface Watch {}
 
-function init() {
-  const client = new watchman.Client();
+// this performs a capability checks returning a promise if watchman is available in the system
+function capabilityCheck(client: watchman.Client) {
+  return new Promise((resolve, reject) => {
+    client.capabilityCheck(
+      { optional: [], required: ["relative_root"] },
+      (error, resp) => {
+        if (error) {
+          log(chalk.red(`Error in the capability check: ${error}`));
+          client.end();
+          reject(error);
+        }
+        resolve({ client, resp });
+      }
+    );
+  });
+}
 
-  client.capabilityCheck(
-    { optional: [], required: ["relative_root"] },
-    function(error, resp) {
+// initializes a watchman client
+function initializeWatchmanClient(
+  opts?: watchman.ClientOptions
+): watchman.Client {
+  return new watchman.Client(opts);
+}
+
+// initializes a chokidar watcher client {
+function inializeChokidarClient(opts?: WatchOptions) {
+  return chokidar.watch("file", opts);
+}
+
+// main init function
+async function init() {
+  try {
+    const client = initializeWatchmanClient();
+
+    // checking if watchman is available, if not we run chokidar
+    await capabilityCheck(client);
+
+    client.command(["watch-project", dir], (error, resp) => {
+      // handling errors
       if (error) {
-        log(chalk.red(`Error in the capability check: ${error}`));
-        client.end();
+        log(chalk.red(`Error initiating watch: ${error}`));
         return;
       }
 
-      // Initiate the watch
-      client.command(["watch-project", dir], (error, resp) => {
-        // Handling errors
-        if (error) {
-          log(chalk.red(`Error initiating watch: ${error}`));
-          return;
-        }
+      // handling warnings
+      if ("warning" in resp) {
+        log(chalk.yellow(`Error initiating watch: ${resp.warning}`));
+      }
 
-        // Handling warnings
-        if ("warning" in resp) {
-          log(chalk.yellow(`Error initiating watch: ${resp.warning}`));
-        }
+      // watch established, building subscription
+      buildWatchmanSubscription(client, resp.watch);
+    });
+  } catch (error) {
+    // runing chokidars
+    log(chalk.yellow(`Running chokidar`));
 
-        // Watch established, building subscription
-        buildSubscription(client, resp.watch);
-      });
-    }
-  );
+    const client = inializeChokidarClient({
+      persistent: true,
+    });
+
+    client.on("change", path => {
+      console.log(`file changed: ${path}`);
+    });
+  }
 }
 
-function buildSubscription(client: watchman.Client, watch: Watch) {
+// builds a watchman subscription aware of the clock
+function buildWatchmanSubscription(client: watchman.Client, watch: Watch) {
   client.command(["clock", watch], (error, resp: any) => {
     if (error) {
       console.error(`Failed to query clock: ${error}`);
@@ -81,6 +118,7 @@ function buildSubscription(client: watchman.Client, watch: Watch) {
   });
 }
 
+// runs the relay compiler
 function runRelayCompiler() {
   const command = "npm";
   const args = ["run", "compile:relay:stream"];
