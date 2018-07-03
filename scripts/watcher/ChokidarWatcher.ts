044e1c2863e414a07236e0ba54c869afb88a3ae6
Kiwi <vinh@wikiwi.io>
2018-07-03 15:21:58 -0300

    Watcher infrastructure (#1724)
    * wip
    
    * Adding chokidar and types
    
    * specifiying build tasks
    
    * new structure, new types, executor and watchers
    
    * Adding log
    
    * Fully implemented watchers
    
    * adapt vscode launc
    
    * Add .babelrc.js to toplevel tsconfig project
    
    * Typo
    
    * Get schema path from .graphqlconfig
    
    * Use watcher binary
    
    * Add joi validation to watcher
    
    * Remove fb-watchman for now
    
    * Use correct ignore path
    
    * Fix dist folder
    
    * Allow setting watcher
    
    * Per default only spawn one process at a time
    
    * Support runOnInit
    
    * Rename RestartingExecutor to LongRunningExecutor
    
    * Use debounce instead of throttle
    
    * Remove console log
    
    * Debounce command execution
    
    * Simplify debounce
    
    * Watcher name change
    
    * Typos
    
    * Rename "watcher" root level config to "backend"

---
+++ scripts/watcher/ChokidarWatcher.ts
@@ -0,0 +1,81 @@
+import chokidar from "chokidar";
+import { Watcher, WatchOptions } from "./types";
+
+export default class ChokidarWatcher implements Watcher {
+  public watch(
+    paths: ReadonlyArray<string>,
+    options: WatchOptions = {}
+  ): AsyncIterable<string> {
+    const client = chokidar.watch(paths as string[], {
+      ignored: options.ignore,
+    });
+
+    // An array to hold all changes, that has not yet been yield.
+    const queue: string[] = [];
+    let firstError: Error | null = null;
+
+    // If this is set, a pending promise is waiting for the next result.
+    let pending:
+      | ({ resolve: (result: string) => void; reject: (error: Error) => void })
+      | null = null;
+
+    // Listen for errors
+    client.on("error", (error: Error) => {
+      // Resolve pending request.
+      if (pending) {
+        pending.reject(error);
+        pending = null;
+        return;
+      }
+      if (!firstError) {
+        firstError = error;
+      }
+    });
+
+    // Listen for changes
+    client.on("change", (pathFile: string) => {
+      // Resolve pending request.
+      if (pending) {
+        pending.resolve(pathFile);
+        pending = null;
+        return;
+      }
+
+      // There is no pending request, save it into the queue.
+      queue.unshift(pathFile);
+    });
+    return {
+      [Symbol.asyncIterator]() {
+        return {
+          next: () =>
+            new Promise<IteratorResult<string>>((resolve, reject) => {
+              const wrapped = {
+                resolve: (pathFile: string) =>
+                  resolve({
+                    done: false,
+                    value: pathFile,
+                  }),
+                reject: (error: Error) =>
+                  reject({
+                    done: true,
+                    value: error,
+                  }),
+              };
+
+              // We already have a change to return
+              if (firstError) {
+                wrapped.reject(firstError);
+                return;
+              }
+              if (queue.length) {
+                wrapped.resolve(queue.pop()!);
+                return;
+              }
+              // We need to wait for the next change event.
+              pending = wrapped;
+            }),
+        };
+      },
+    };
+  }
+}
---
+++ scripts/watcher/LongRunningExecutor.ts
@@ -0,0 +1,88 @@
+import { ChildProcess } from "child_process";
+import spawn from "cross-spawn";
+import { Cancelable, debounce } from "lodash";
+import { Executor } from "./types";
+
+interface LongRunningExecutorOptions {
+  args?: ReadonlyArray<string>;
+
+  // Specify the period in which the process is restarted at max once.
+  debounce?: number;
+}
+
+export default class LongRunningExecutor implements Executor {
+  private cmd: string;
+  private args?: ReadonlyArray<string>;
+  private process: ChildProcess | null = null;
+  private isRunning: boolean = false;
+  private shouldRestart: boolean = false;
+  private restartDebounced: (() => void) & Cancelable;
+
+  constructor(cmd: string, opts: LongRunningExecutorOptions = {}) {
+    this.cmd = cmd;
+    this.args = opts.args;
+    this.restartDebounced = debounce(
+      () => this.restart(),
+      opts.debounce || 500
+    );
+  }
+
+  private spawnProcess() {
+    this.isRunning = true;
+    this.process = spawn(this.cmd, this.args as string[], {
+      stdio: "inherit",
+      // Have all child processes in their own group.
+      // See `process.kill` below.
+      detached: true,
+      shell: !this.args,
+    });
+
+    this.process!.on("exit", (code: number) => {
+      this.isRunning = false;
+
+      if (code !== 0 && code !== null) {
+        // tslint:disable-next-line: no-console
+        console.error(`Exit code returned ${code}`);
+        return;
+      }
+      if (this.shouldRestart) {
+        this.spawnProcess();
+      }
+    });
+  }
+
+  private restart(): void {
+    this.shouldRestart = true;
+    // Using the `-` will kill all child procceses in the group.
+    // See: https://azimi.me/2014/12/31/kill-child_process-node-js.html
+    process.kill(-this.process!.pid, "SIGTERM");
+  }
+
+  private kill(): void {
+    this.shouldRestart = false;
+    // Using the `-` will kill all child procceses in the group.
+    // See: https://azimi.me/2014/12/31/kill-child_process-node-js.html
+    process.kill(-this.process!.pid, "SIGTERM");
+  }
+
+  // This is called before watching starts.
+  public onInit(): void {
+    this.spawnProcess();
+  }
+
+  // This is called before exiting.
+  public onCleanup() {
+    this.restartDebounced.cancel();
+    if (this.isRunning) {
+      this.kill();
+    }
+  }
+
+  public execute(filePath: string) {
+    if (this.isRunning) {
+      this.restartDebounced();
+      return;
+    }
+    this.spawnProcess();
+  }
+}
