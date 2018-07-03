33c63f899c609e8af5fc442404d506d9ace02725
Chi Vinh Le <vinh@wikiwi.io>
2018-07-03 13:37:45 -0300

    Per default only spawn one process at a time

--- scripts/watcher/CommandExecutor.ts
+++ scripts/watcher/CommandExecutor.ts
@@ -1,31 +1,50 @@
 import spawn from "cross-spawn";
+
 import { Executor } from "./types";
 
 interface CommandExecutorOptions {
   args?: ReadonlyArray<string>;
+  // If true, will spawn a new process for each file change.
+  spawnMutiple?: boolean;
 }
 
 export default class CommandExecutor implements Executor {
   private cmd: string;
   private args?: ReadonlyArray<string>;
+  private isRunning: boolean = false;
+  private spawnMultiple: boolean = false;
+  private shouldRespawn: boolean = false;
 
   constructor(cmd: string, opts: CommandExecutorOptions = {}) {
     this.cmd = cmd;
     this.args = opts.args;
   }
 
-  public execute(filePath: string) {
+  private spawn() {
+    if (this.isRunning && !this.spawnMultiple) {
+      this.shouldRespawn = true;
+      return;
+    }
+    this.isRunning = true;
+    this.shouldRespawn = false;
     const child = spawn(this.cmd, this.args as string[], {
       stdio: "inherit",
       shell: !this.args,
     });
 
     child.on("close", (code: number) => {
+      this.isRunning = false;
       if (code !== 0) {
         // tslint:disable-next-line: no-console
         console.log(`We had an error building ${code}`);
-        return;
+      }
+      if (this.shouldRespawn) {
+        this.spawn();
       }
     });
+  }
+
+  public execute(filePath: string) {
+    this.spawn();
   }
 }
