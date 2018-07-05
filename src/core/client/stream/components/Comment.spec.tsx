37cb23dd59b47f0e4a9b1b2830b9292f2b018f4d
Kiwi <vinh@wikiwi.io>
2018-07-05 12:04:38 -0300

    [next] Jest implementation for React Components (#1733)
    * Make jest testing work with custom path and css modules
    
    * Add first test
    
    * feat: added unit tests to ci
    
    * fix: updated package-lock.json
    
    * Update cssTransform.js
    
    * Update cssTransform.js
    
    * Fix test in ci

---
+++ src/core/client/stream/components/Comment.spec.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { createRenderer } from "react-test-renderer/shallow";
+import Comment from "./Comment";
+
+it("renders username and body", () => {
+  const props = {
+    author: {
+      username: "Marvin",
+    },
+    body: "Woof",
+  };
+  const renderer = createRenderer();
+  renderer.render(<Comment {...props} />);
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
+
+it("renders with gutterBottom", () => {
+  const props = {
+    author: {
+      username: "Marvin",
+    },
+    body: "Woof",
+    gutterBottom: true,
+  };
+  const renderer = createRenderer();
+  renderer.render(<Comment {...props} />);
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
