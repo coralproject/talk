cac0afa61e79b19ddcae873f543a6910b83c92e7
Kiwi <vinh@wikiwi.io>
2018-07-13 13:57:25 -0300

    [next] Support server side jest testing (#1747)
    * Support server side jest testing
    
    * feat: more tests

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,48 +1,6 @@
-const path = require("path");
-const paths = require("./paths");
-
 module.exports = {
-  rootDir: "../",
-  roots: ["<rootDir>/src", "<rootDir>/scripts"],
-  collectCoverageFrom: ["src/**/*.{js,jsx,mjs,ts,tsx}"],
-  coveragePathIgnorePatterns: ["/node_modules/"],
-  setupFiles: ["<rootDir>/config/polyfills.js", "<rootDir>/test/setupjest.ts"],
-  testMatch: ["**/*.spec.{js,jsx,mjs,ts,tsx}"],
-  testEnvironment: "node",
-  testURL: "http://localhost",
-  transform: {
-    "^.+\\.(js|jsx|mjs|ts|tsx)$": "<rootDir>/node_modules/ts-jest",
-    "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
-    "^(?!.*\\.(js|jsx|mjs|css|json|ftl)$)":
-      "<rootDir>/config/jest/fileTransform.js",
-  },
-  transformIgnorePatterns: [
-    "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|ts|tsx)$",
+  projects: [
+    "<rootDir>/jest-client.config.js",
+    "<rootDir>/jest-server.config.js",
   ],
-  moduleNameMapper: {
-    "^talk-admin/(.*)$": "<rootDir>/src/core/client/admin/$1",
-    "^talk-ui/(.*)$": "<rootDir>/src/core/client/ui/$1",
-    "^talk-stream/(.*)$": "<rootDir>/src/core/client/stream/$1",
-    "^talk-framework/(.*)$": "<rootDir>/src/core/client/framework/$1",
-    "^talk-common/(.*)$": "<rootDir>/src/core/common/$1",
-    "^talk-server/(.*)$": "<rootDir>/src/core/server/$1",
-  },
-  moduleFileExtensions: [
-    "web.js",
-    "js",
-    "json",
-    "web.jsx",
-    "jsx",
-    "node",
-    "mjs",
-    "ts",
-    "tsx",
-  ],
-  snapshotSerializers: ["enzyme-to-json/serializer"],
-  globals: {
-    "ts-jest": {
-      useBabelrc: true,
-      tsConfigFile: path.resolve(__dirname, "tsconfig.jest.json"),
-    },
-  },
 };
