6c7e303f4e26dae7f3733154a4055f1e074c970e
Chi Vinh Le <vinh@wikiwi.io>
2018-07-20 16:37:48 -0300

    e render props for TrapFocus

--- src/core/client/ui/components/TrapFocus/TrapFocus.tsx
+++ src/core/client/ui/components/TrapFocus/TrapFocus.tsx
@@ -1,25 +1,34 @@
-import React from "react";
+import React, { RefObject } from "react";
 
 export interface Focusable {
   focus: () => void;
 }
 
+interface RenderProps {
+  firstFocusableRef: RefObject<Focusable>;
+  lastFocusableRef: RefObject<Focusable>;
+}
+
 export interface TrapFocusProps {
-  firstFocusable: Focusable | null;
-  lastFocusable: Focusable | null;
-  children: React.ReactNode;
+  children: (props: RenderProps) => React.ReactNode;
 }
 
 export default class TrapFocus extends React.Component<TrapFocusProps> {
+  private firstFocusableRef = React.createRef<Focusable>();
+  private lastFocusableRef = React.createRef<Focusable>();
+
   // Trap keyboard focus inside the dropdown until a value has been chosen.
-  public focusBegin = () => this.props.firstFocusable!.focus();
-  public focusEnd = () => this.props.lastFocusable!.focus();
+  private focusBegin = () => this.firstFocusableRef.current!.focus();
+  private focusEnd = () => this.lastFocusableRef.current!.focus();
 
   public render() {
     return (
       <>
         <div tabIndex={0} onFocus={this.focusEnd} />
-        {this.props.children}
+        {this.props.children({
+          firstFocusableRef: this.firstFocusableRef,
+          lastFocusableRef: this.lastFocusableRef,
+        })}
         <div tabIndex={0} onFocus={this.focusBegin} />
       </>
     );
