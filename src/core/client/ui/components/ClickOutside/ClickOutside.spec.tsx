e0a8f2e067f8dc871980e6ccbd7aa7e739f1a052
Belén Curcio <curciobelen@gmail.com>
2018-07-25 17:33:29 -0300

    [next] ClickOutside Component (#1757)
    * working on the tests
    
    Merge branch 'next' of github.com:coralproject/talk into clickoutside
    
    * 'next' of github.com:coralproject/talk: (36 commits)
      [next] Implement AriaInfo (#1756)
      e render props for TrapFocus (#1755)
      [ToggleShow] Fix typo and better description (#1753)
      Fix wrong hour pluralization
      Implement button active state
      Styling fixes
      Fix Playground
      Commit snapshots
      Harmonize typography
      Fix Flex margin bug and add examples
      Update package-lock.json
      Use explicit export
      Implement ButtonIcon component
      Remove wrong ref prop
      Implement new button api
      Read env in watchers.ts
      updated tests
      Update ToggleShow.spec.tsx
      ToggleShow Component
      Fix CI thanks to wyatt
      ...
    
    docz added
    
    Added simulant
    
    Added simulant
    
    unnused
    
    Merge branch 'clickoutside' of github.com:coralproject/talk into clickoutside
    
    * 'clickoutside' of github.com:coralproject/talk: (40 commits)
      Added simulant
      Added simulant
      docz added
      [next] Implement AriaInfo (#1756)
      e render props for TrapFocus (#1755)
      [ToggleShow] Fix typo and better description (#1753)
      Fix wrong hour pluralization
      Implement button active state
      Styling fixes
      Fix Playground
      Commit snapshots
      Harmonize typography
      Fix Flex margin bug and add examples
      Update package-lock.json
      Use explicit export
      Implement ButtonIcon component
      Remove wrong ref prop
      Implement new button api
      Read env in watchers.ts
      working on the tests
      ...
    
    unnused
    
    Merge branch 'clickoutside' of github.com:coralproject/talk into clickoutside
    
    * 'clickoutside' of github.com:coralproject/talk: (42 commits)
      unnused
      working on the tests
      Added simulant
      Added simulant
      docz added
      [next] Implement AriaInfo (#1756)
      e render props for TrapFocus (#1755)
      [ToggleShow] Fix typo and better description (#1753)
      Fix wrong hour pluralization
      Implement button active state
      Styling fixes
      Fix Playground
      Commit snapshots
      Harmonize typography
      Fix Flex margin bug and add examples
      Update package-lock.json
      Use explicit export
      Implement ButtonIcon component
      Remove wrong ref prop
      Implement new button api
      ...
    
    Merge branch 'clickoutside' of github.com:coralproject/talk into clickoutside
    
    * 'clickoutside' of github.com:coralproject/talk: (42 commits)
      unnused
      working on the tests
      Added simulant
      Added simulant
      docz added
      [next] Implement AriaInfo (#1756)
      e render props for TrapFocus (#1755)
      [ToggleShow] Fix typo and better description (#1753)
      Fix wrong hour pluralization
      Implement button active state
      Styling fixes
      Fix Playground
      Commit snapshots
      Harmonize typography
      Fix Flex margin bug and add examples
      Update package-lock.json
      Use explicit export
      Implement ButtonIcon component
      Remove wrong ref prop
      Implement new button api
      ...
    
    Merge branch 'clickoutside' of github.com:coralproject/talk into clickoutside
    
    * 'clickoutside' of github.com:coralproject/talk: (43 commits)
      working on the tests
      unnused
      working on the tests
      Added simulant
      Added simulant
      docz added
      [next] Implement AriaInfo (#1756)
      e render props for TrapFocus (#1755)
      [ToggleShow] Fix typo and better description (#1753)
      Fix wrong hour pluralization
      Implement button active state
      Styling fixes
      Fix Playground
      Commit snapshots
      Harmonize typography
      Fix Flex margin bug and add examples
      Update package-lock.json
      Use explicit export
      Implement ButtonIcon component
      Remove wrong ref prop
      ...
    
    * Implement passing tests
    
    * Use html as snapshot
    
    * Refactor and simplify test
    
    * Remove unused import
    
    * ts
    
    * simulant moved to devdeps

---
+++ src/core/client/ui/components/ClickOutside/ClickOutside.spec.tsx
@@ -0,0 +1,62 @@
+import { mount } from "enzyme";
+import React from "react";
+import simulant from "simulant";
+import sinon from "sinon";
+
+import ClickOutside from "./ClickOutside";
+
+let container: HTMLElement;
+
+beforeAll(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterAll(() => {
+  document.body.removeChild(container);
+});
+
+it("should render correctly", () => {
+  const noop = () => null;
+  const wrapper = mount(
+    <ClickOutside onClickOutside={noop}>
+      <span>Hello World!</span>
+    </ClickOutside>
+  );
+  expect(wrapper.html()).toMatchSnapshot();
+  wrapper.unmount();
+});
+
+it("should detect click outside", () => {
+  const onClickOutside = sinon.spy();
+  const wrapper = mount(
+    <ClickOutside onClickOutside={onClickOutside}>
+      <span>Hello World!</span>
+    </ClickOutside>,
+    {
+      attachTo: container,
+    }
+  );
+  simulant.fire(container, "click");
+
+  expect(onClickOutside.calledOnce).toEqual(true);
+  wrapper.unmount();
+});
+
+it("should ignore click inside", () => {
+  const onClickOutside = sinon.spy();
+  const wrapper = mount(
+    <ClickOutside onClickOutside={onClickOutside}>
+      <button id="click-outside-test-button">Push Me</button>
+    </ClickOutside>,
+    {
+      attachTo: container,
+    }
+  );
+
+  const target = document.getElementById("click-outside-test-button")!;
+  simulant.fire(target, "click");
+
+  expect(onClickOutside.calledOnce).toEqual(false);
+  wrapper.unmount();
+});
---
+++ src/core/client/ui/components/ClickOutside/ClickOutside.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { findDOMNode } from "react-dom";
+
+interface Props {
+  onClickOutside: () => void;
+  children: React.ReactNode;
+}
+
+class ClickOutside extends React.Component<Props> {
+  public domNode: Element | null = null;
+
+  public handleClick = (e: MouseEvent) => {
+    const { onClickOutside } = this.props;
+    if (!e || !this.domNode!.contains(e.target as HTMLInputElement)) {
+      // tslint:disable-next-line:no-unused-expression
+      onClickOutside && onClickOutside();
+    }
+  };
+
+  public componentDidMount() {
+    this.domNode = findDOMNode(this) as Element;
+    document.addEventListener("click", this.handleClick, true);
+  }
+
+  public componentWillUnmount() {
+    document.removeEventListener("click", this.handleClick, true);
+  }
+
+  public render() {
+    return this.props.children;
+  }
+}
+export default ClickOutside;
