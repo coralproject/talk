34fb53b3b0823a2cc1b435d8a9d2882eaec046c9
Chi Vinh Le <vinh@wikiwi.io>
2018-08-08 01:03:49 +0200

    Add auth target

---
+++ src/core/client/auth/components/App.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { StatelessComponent } from "react";
+
+import { Flex } from "talk-ui/components";
+
+import * as styles from "./App.css";
+
+export interface AppProps {
+  // TODO: (cvle) Remove %future added value when we have Relay 1.6
+  // https://github.com/facebook/relay/commit/1e87e43add7667a494f7ff4cfa7f03f1ab8d81a2
+  view: "SIGN_UP" | "SIGN_IN" | "FORGOT_PASSWORD" | "%future added value";
+}
+
+const App: StatelessComponent<AppProps> = props => {
+  return (
+    <Flex justifyContent="center" className={styles.root}>
+      Hello World
+    </Flex>
+  );
+};
+
+export default App;
---
+++ src/core/client/auth/containers/AppContainer.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { StatelessComponent } from "react";
+
+import { AppContainerLocal as Local } from "talk-auth/__generated__/AppContainerLocal.graphql";
+import { graphql, withLocalStateContainer } from "talk-framework/lib/relay";
+
+import App from "../components/App";
+
+interface InnerProps {
+  local: Local;
+}
+
+const AppContainer: StatelessComponent<InnerProps> = ({ local: { view } }) => {
+  return <App view={view} />;
+};
+
+const enhanced = withLocalStateContainer<Local>(
+  graphql`
+    fragment AppContainerLocal on Local {
+      view
+    }
+  `
+)(AppContainer);
+
+export default enhanced;
