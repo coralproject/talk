25bbefdfb1e7d263bdc4b217ed2ce3e12d122b28
Wyatt Johnson <wyattjoh@gmail.com>
2018-08-09 11:28:44 -0600

    feat: added edit metadata

--- src/core/server/graph/tenant/resolvers/comment.ts
+++ src/core/server/graph/tenant/resolvers/comment.ts
@@ -2,6 +2,16 @@ import { GQLCommentTypeResolver } from "talk-server/graph/tenant/schema/__genera
 import { Comment } from "talk-server/models/comment";
 
 const Comment: GQLCommentTypeResolver<Comment> = {
+  editing: (comment, input, ctx) => ({
+    // When there is more than one body history, then the comment has been
+    // edited.
+    edited: comment.body_history.length > 1,
+    // The date that the comment is editable until is the tenant's edit window
+    // length added to the comment created date.
+    editableUntil: new Date(
+      comment.created_at.valueOf() + ctx.tenant.editCommentWindowLength
+    ),
+  }),
   createdAt: comment => comment.created_at,
   author: (comment, input, ctx) =>
     ctx.loaders.Users.user.load(comment.author_id),
