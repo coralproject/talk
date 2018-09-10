eeff0ec26d65cead281e721cc08192923f46aaf2
Wyatt Johnson <wyattjoh@gmail.com>
2018-09-10 16:09:11 -0600

    fix: set query using new syntax

--- client/coral-framework/utils/url.js
+++ client/coral-framework/utils/url.js
@@ -1,4 +1,4 @@
-import URL from 'url-parse';
+import parse from 'url-parse';
 
 export function buildUrl(
   { protocol, hostname, port, pathname, search, hash } = window.location
@@ -14,7 +14,8 @@ export function buildUrl(
 }
 
 export function buildCommentURL(assetURL, commentID) {
-  const u = new URL(assetURL);
-  u.searchParams.set('commentId', commentID);
+  const u = parse(assetURL, true);
+  u.query.commentId = commentID;
+  u.set('query', u.query);
   return u.href;
 }
