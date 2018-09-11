91d421ad46a215be976a0a0fd24924e56873721e
Belén Curcio <curciobelen@gmail.com>
2018-09-11 10:01:14 -0300

    Updated tests

--- src/core/client/ui/components/Message/Message.spec.tsx
+++ src/core/client/ui/components/Message/Message.spec.tsx
@@ -4,6 +4,7 @@ import TestRenderer from "react-test-renderer";
 import { PropTypesOf } from "talk-ui/types";
 
 import Message from "./Message";
+import MessageIcon from "./MessageIcon";
 
 it("renders correctly", () => {
   const props: PropTypesOf<typeof Message> = {
@@ -11,5 +12,18 @@ it("renders correctly", () => {
     children: "Hello World",
   };
   const renderer = TestRenderer.create(<Message {...props} />);
+  expect(renderer.toJSON()).toMatchSnapshot();
+});
+
+it("renders icon", () => {
+  const props: PropTypesOf<typeof Message> = {
+    className: "custom",
+  };
+
+  const renderer = TestRenderer.create(
+    <Message {...props}>
+      <MessageIcon>alert</MessageIcon>Alert Message
+    </Message>
+  );
   expect(renderer.toJSON()).toMatchSnapshot();
 });
