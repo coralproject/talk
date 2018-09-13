4b1e2dd2fd3a1ad45ef86e736b1cc5f5da695272
Chi Vinh Le <vinh@wikiwi.io>
2018-09-13 21:49:30 +0200

    Implement SetActiveTabMutation

---
+++ src/core/client/stream/mutations/SetActiveTabMutation.ts
@@ -0,0 +1,24 @@
+import { commitLocalUpdate, Environment } from "relay-runtime";
+
+import { createMutationContainer, LOCAL_ID } from "talk-framework/lib/relay";
+
+export interface SetActiveTabInput {
+  tab: "COMMENTS";
+}
+
+export type SetActiveTabMutation = (input: SetActiveTabInput) => Promise<void>;
+
+export async function commit(
+  environment: Environment,
+  input: SetActiveTabInput
+) {
+  return commitLocalUpdate(environment, store => {
+    const record = store.get(LOCAL_ID)!;
+    record.setValue(input.tab, "activeTab");
+  });
+}
+
+export const withSetActiveTabMutation = createMutationContainer(
+  "setActiveTab",
+  commit
+);
