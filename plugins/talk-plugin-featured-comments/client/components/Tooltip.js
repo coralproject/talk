a3dd208fbd51b7f9b9774dd68100100a3117f267
Kiwi <vinh@wikiwi.io>
2018-09-18 23:45:29 +0200

    Add static className to featured tooltip (#1885)

--- plugins/talk-plugin-featured-comments/client/components/Tooltip.js
+++ plugins/talk-plugin-featured-comments/client/components/Tooltip.js
@@ -5,7 +5,13 @@ import { t } from 'plugin-api/beta/client/services';
 import { Icon } from 'plugin-api/beta/client/components/ui';
 
 export default ({ className = '' }) => (
-  <div className={cn(styles.tooltip, className)}>
+  <div
+    className={cn(
+      styles.tooltip,
+      className,
+      'talk-plugin-featured-comments-tooltip'
+    )}
+  >
     <Icon name="info_outline" className={styles.icon} />
     <h3 className={styles.headline}>
       {t('talk-plugin-featured-comments.featured_comments')}:
