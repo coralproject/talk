6b814cee0df522c600db65e47d798fa7b40d303f
Wyatt Johnson <wyattjoh@gmail.com>
2018-09-21 12:50:21 -0600

    fix: patched issues with static

--- src/core/server/app/middleware/serveStatic.ts
+++ src/core/server/app/middleware/serveStatic.ts
@@ -2,7 +2,7 @@ import serveStatic from "express-static-gzip";
 import path from "path";
 
 const staticPath = path.resolve(
-  path.join(__dirname, "..", "..", "..", "..", "static", "assets")
+  path.join(__dirname, "..", "..", "..", "..", "..", "dist", "static", "assets")
 );
 
 export default serveStatic(staticPath, { index: false });
