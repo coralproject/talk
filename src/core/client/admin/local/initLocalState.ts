465603526e0937b5552876c94063baadf322883e
Belén Curcio <curciobelen@gmail.com>
2018-09-27 11:47:55 -0300

    No need for local state for now

--- src/core/client/admin/local/initLocalState.ts
+++ src/core/client/admin/local/initLocalState.ts
@@ -1,4 +1,3 @@
-import qs from "query-string";
 import { commitLocalUpdate, Environment } from "relay-runtime";
 
 import {
@@ -16,12 +15,6 @@ export default async function initLocalState(environment: Environment) {
 
     // Create the Local Record which is the Root for the client states.
     const localRecord = createAndRetain(environment, s, LOCAL_ID, LOCAL_TYPE);
-
-    // Parse query params
-    const query = qs.parse(location.search);
-
-    // Set default view.
-    localRecord.setValue(query.view || "MODERATE", "view");
 
     root.setLinkedRecord(localRecord, "local");
   });
