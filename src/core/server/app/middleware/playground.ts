846bff7557f160c9b0a78f95080fafb5122570bd
Wyatt Johnson <wyattjoh@gmail.com>
2018-10-04 14:48:21 -0600

    fix: replaced express middleware with custom html based middleware

--- src/core/server/app/middleware/playground.ts
+++ src/core/server/app/middleware/playground.ts
@@ -1,14 +1,24 @@
 import { RequestHandler } from "express";
-import { MiddlewareOptions } from "graphql-playground-html";
-import playground from "graphql-playground-middleware-express";
+import {
+  MiddlewareOptions,
+  RenderPageOptions,
+  renderPlaygroundPage,
+} from "graphql-playground-html";
 
-export default (options: MiddlewareOptions): RequestHandler => (
-  req,
-  res,
-  next
-) =>
-  playground(options)(req, res, (err?: Error) => {
-    if (err) {
+export default (options: MiddlewareOptions): RequestHandler => {
+  const middlewareOptions: RenderPageOptions = {
+    ...options,
+    version: "1.6.0",
+  };
+
+  return (req, res, next) => {
+    try {
+      res.setHeader("Content-Type", "text/html");
+      const playground = renderPlaygroundPage(middlewareOptions);
+      res.write(playground);
+      res.end();
+    } catch (err) {
       return next(err);
     }
-  });
+  };
+};
