842dab2147d27ae8e84675ac87fa86da3d74f8ae
Wyatt Johnson <wyattjoh@gmail.com>
2018-10-04 14:42:57 -0600

    fix: fixed playground middleware

--- src/core/server/app/middleware/playground.ts
+++ src/core/server/app/middleware/playground.ts
@@ -1,4 +1,14 @@
+import { RequestHandler } from "express";
 import { MiddlewareOptions } from "graphql-playground-html";
 import playground from "graphql-playground-middleware-express";
 
-export default (options: MiddlewareOptions) => playground(options);
+export default (options: MiddlewareOptions): RequestHandler => (
+  req,
+  res,
+  next
+) =>
+  playground(options)(req, res, (err?: Error) => {
+    if (err) {
+      return next(err);
+    }
+  });
