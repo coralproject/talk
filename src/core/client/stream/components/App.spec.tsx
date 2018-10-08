b69313a01ec254b5f99c46e353622bd1901f26c1
Chi Vinh Le <vinh@wikiwi.io>
2018-10-08 20:52:52 +0200

    fix: Readd and fix App unittest

---
+++ src/core/client/stream/components/App.spec.tsx
@@ -0,0 +1,16 @@
+import { shallow } from "enzyme";
+import noop from "lodash";
+import React from "react";
+
+import { PropTypesOf } from "talk-framework/types";
+
+import App from "./App";
+
+it("renders comments", () => {
+  const props: PropTypesOf<typeof App> = {
+    activeTab: "COMMENTS",
+    onTabClick: noop,
+  };
+  const wrapper = shallow(<App {...props} />);
+  expect(wrapper).toMatchSnapshot();
+});
