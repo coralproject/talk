e18c8c977f08998ff93c1ea03af57da3c22fadd9
Chi Vinh Le <vinh@wikiwi.io>
2018-10-17 23:28:19 +0200

    feat: add pagination to comment history

--- src/core/client/stream/tabs/profile/components/CommentHistory.tsx
+++ src/core/client/stream/tabs/profile/components/CommentHistory.tsx
@@ -1,16 +1,19 @@
 import { Localized } from "fluent-react/compat";
 import * as React from "react";
 import { StatelessComponent } from "react";
-import { HorizontalGutter, Typography } from "talk-ui/components";
-import HistoryCommentContainer from "../containers/HistoryCommentContainer";
-
 import { PropTypesOf } from "talk-framework/types";
+import { Button, HorizontalGutter, Typography } from "talk-ui/components";
+
+import HistoryCommentContainer from "../containers/HistoryCommentContainer";
 
 interface CommentHistoryProps {
   asset: PropTypesOf<typeof HistoryCommentContainer>["asset"];
   comments: Array<
     { id: string } & PropTypesOf<typeof HistoryCommentContainer>["comment"]
   >;
+  onLoadMore?: () => void;
+  hasMore?: boolean;
+  disableLoadMore?: boolean;
 }
 
 const CommentHistory: StatelessComponent<CommentHistoryProps> = props => {
@@ -26,6 +29,20 @@ const CommentHistory: StatelessComponent<CommentHistoryProps> = props => {
           comment={comment}
         />
       ))}
+      {props.hasMore && (
+        <Localized id="profile-commentHistory-loadMore">
+          <Button
+            id={"talk-profile-commentHistory-loadMore"}
+            onClick={props.onLoadMore}
+            variant="outlined"
+            fullWidth
+            disabled={props.disableLoadMore}
+            aria-controls="talk-profile-commentHistory-log"
+          >
+            Load More
+          </Button>
+        </Localized>
+      )}
     </HorizontalGutter>
   );
 };
