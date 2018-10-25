977161db0a485b46ee4e50c909aa41aa625523dc
Wyatt Johnson <wyattjoh@gmail.com>
2018-10-25 08:58:58 -0600

    fix: linting

---
+++ src/core/server/services/comments/moderation/phases/wordList.ts
@@ -0,0 +1,55 @@
+import {
+  GQLCOMMENT_FLAG_REASON,
+  GQLCOMMENT_STATUS,
+} from "talk-server/graph/tenant/schema/__generated__/types";
+import { ACTION_TYPE } from "talk-server/models/action";
+import {
+  IntermediateModerationPhase,
+  IntermediatePhaseResult,
+} from "talk-server/services/comments/moderation";
+import { containsMatchingPhrase } from "talk-server/services/comments/moderation/wordlist";
+
+// This phase checks the comment against the wordList.
+export const wordList: IntermediateModerationPhase = ({
+  tenant,
+  comment,
+}): IntermediatePhaseResult | void => {
+  // If there isn't a body, there can't be a bad word!
+  if (!comment.body) {
+    return;
+  }
+
+  // Decide the status based on whether or not the current asset/settings
+  // has pre-mod enabled or not. If the comment was rejected based on the
+  // wordList, then reject it, otherwise if the moderation setting is
+  // premod, set it to `premod`.
+  if (containsMatchingPhrase(tenant.wordList.banned, comment.body)) {
+    // Add the flag related to Trust to the comment.
+    return {
+      status: GQLCOMMENT_STATUS.REJECTED,
+      actions: [
+        {
+          action_type: ACTION_TYPE.FLAG,
+          reason: GQLCOMMENT_FLAG_REASON.COMMENT_DETECTED_BANNED_WORD,
+        },
+      ],
+    };
+  }
+
+  // If the comment has a suspect word or a link, we need to add a
+  // flag to it to indicate that it needs to be looked at.
+  // Otherwise just return the new comment.
+
+  // If the wordlist has matched the suspect word filter and we haven't disabled
+  // auto-flagging suspect words, then we should flag the comment!
+  if (containsMatchingPhrase(tenant.wordList.suspect, comment.body)) {
+    return {
+      actions: [
+        {
+          action_type: ACTION_TYPE.FLAG,
+          reason: GQLCOMMENT_FLAG_REASON.COMMENT_DETECTED_SUSPECT_WORD,
+        },
+      ],
+    };
+  }
+};
