3fc0bbe9f33853bad1efcc7dfb9f8647b50a6b63
Chi Vinh Le <vinh@wikiwi.io>
2018-10-30 19:15:27 +0100

    chore: refactor NavigationLink

--- src/core/client/admin/components/Navigation.tsx
+++ src/core/client/admin/components/Navigation.tsx
@@ -1,50 +1,26 @@
 import { Localized } from "fluent-react/compat";
-import { Link } from "found";
 import React, { StatelessComponent } from "react";
 
 import { Flex } from "talk-ui/components";
 
 import SignOutButtonContainer from "../containers/SignOutButtonContainer";
 import styles from "./Navigation.css";
+import NavigationLink from "./NavigationLink";
 
 const Navigation: StatelessComponent = () => (
   <Flex className={styles.root} justifyContent="space-between">
     <Flex alignItems="center">
       <Localized id="general-navigation-moderate">
-        <Link
-          to="/admin/moderate"
-          className={styles.link}
-          activeClassName={styles.active}
-        >
-          Moderate
-        </Link>
+        <NavigationLink to="/admin/moderate">Moderate</NavigationLink>
       </Localized>
       <Localized id="general-navigation-community">
-        <Link
-          to="/admin/community"
-          className={styles.link}
-          activeClassName={styles.active}
-        >
-          Community
-        </Link>
+        <NavigationLink to="/admin/community">Community</NavigationLink>
       </Localized>
       <Localized id="general-navigation-stories">
-        <Link
-          to="/admin/stories"
-          className={styles.link}
-          activeClassName={styles.active}
-        >
-          Stories
-        </Link>
+        <NavigationLink to="/admin/stories">Stories</NavigationLink>
       </Localized>
       <Localized id="general-navigation-configure">
-        <Link
-          to="/admin/configure"
-          className={styles.link}
-          activeClassName={styles.active}
-        >
-          Configure
-        </Link>
+        <NavigationLink to="/admin/configure">Configure</NavigationLink>
       </Localized>
     </Flex>
     <Flex alignItems="center">
