625ef9cbeea141e87b2d7e0758fb9d31431627c1
Chi Vinh Le <vinh@wikiwi.io>
2018-11-01 20:28:09 +0100

    refactor: deduplicate embed utils

--- src/core/client/embed/utils/index.ts
+++ src/core/client/embed/utils/index.ts
@@ -1,4 +1,2 @@
-export { default as buildURL } from "./buildURL";
 export { default as startsWith } from "./startsWith";
 export { default as prefixStorage } from "./prefixStorage";
-export { default as parseHashQuery } from "./parseHashQuery";
--- src/core/client/embed/utils/parseHashQuery.ts
+++
@@ -1,5 +0,0 @@
-import qs from "query-string";
-
-export default function parseQueryHash(hash: string): Record<string, string> {
-  return qs.parse(hash);
-}
