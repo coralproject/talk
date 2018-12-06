362f29f77eca5e9fe2d9c12354cc4f8ad4a48dae
Kiwi <vinh@wikiwi.io>
2018-12-06 22:24:31 +0100

    Support lazy rendering (#2109)
    * feat: support lazy rendering
    
    * fix: added new env var to onbuild args
    
    * review: apply suggestions

--- client/coral-embed/src/Stream.js
+++ client/coral-embed/src/Stream.js
@@ -3,6 +3,7 @@ import pym from 'pym.js';
 import EventEmitter from 'eventemitter2';
 import { buildUrl } from 'coral-framework/utils/url';
 import Snackbar from './Snackbar';
+import onIntersect from './onIntersect';
 import {
   createStorage,
   connectStorageToPym,
@@ -50,15 +51,62 @@ export default class Stream {
       ...opts
     } = config;
 
+    this.onAuthChanged = onAuthChanged;
+    this.el = el;
+    this.talkBaseUrl = talkBaseUrl;
     this.opts = opts;
-
-    this.emitter = new EventEmitter({ wildcard: true });
-    this.pym = new pym.Parent(el.id, buildStreamIframeUrl(talkBaseUrl, query), {
-      title: opts.title,
-      id: `${el.id}_iframe`,
-      name: `${el.id}_iframe`,
-    });
     this.snackBar = new Snackbar(snackBarStyles || {});
+    this.emitter = new EventEmitter({ wildcard: true });
+    // Attach to the events emitted by the pym parent.
+    if (events) {
+      events(this.emitter);
+    }
+    if (config.lazy || process.env.TALK_DEFAULT_LAZY_RENDER === 'TRUE') {
+      const renderOnIntersect = () => onIntersect(this.el, () => this.render());
+      if (!window.IntersectionObserver) {
+        // Include a polyfill for the intersection observer.
+        import('intersection-observer')
+          .then(() => {
+            // Polyfill applied.
+            renderOnIntersect();
+          })
+          .catch(e => {
+            console.error(e);
+            // Loading polyfill failed, just render it directly.
+            this.render();
+          });
+      } else {
+        // No need for polyfill.
+        renderOnIntersect();
+      }
+    } else {
+      this.render();
+    }
+  }
+
+  assertRendered() {
+    if (!this.pym) {
+      throw new Error('Stream Embed must be rendered first');
+    }
+  }
+
+  isRendered() {
+    return !!this.pym;
+  }
+
+  render() {
+    if (this.pym) {
+      throw new Error('Stream Embed already rendered');
+    }
+    this.pym = new pym.Parent(
+      this.el.id,
+      buildStreamIframeUrl(this.talkBaseUrl, this.query),
+      {
+        title: this.opts.title,
+        id: `${this.el.id}_iframe`,
+        name: `${this.el.id}_iframe`,
+      }
+    );
 
     // Workaround: IOS Safari ignores `width` but respects `min-width` value.
     this.pym.el.firstChild.style.width = '1px';
@@ -73,19 +121,14 @@ export default class Stream {
       }
     });
 
-    // Attach to the events emitted by the pym parent.
-    if (events) {
-      events(this.emitter);
-    }
-
     this.pym.onMessage('getConfig', () => {
-      this.pym.sendMessage('config', JSON.stringify(opts));
+      this.pym.sendMessage('config', JSON.stringify(this.opts));
     });
 
     // If the auth changes, and someone is listening for it, then re-emit it.
-    if (onAuthChanged) {
+    if (this.onAuthChanged) {
       this.pym.onMessage('coral-auth-changed', message => {
-        onAuthChanged(message ? JSON.parse(message) : null);
+        this.onAuthChanged(message ? JSON.parse(message) : null);
       });
     }
 
@@ -163,22 +206,27 @@ export default class Stream {
   }
 
   enablePluginsDebug() {
+    this.assertRendered();
     this.pym.sendMessage('enablePluginsDebug');
   }
 
   disablePluginsDebug() {
+    this.assertRendered();
     this.pym.sendMessage('disablePluginsDebug');
   }
 
   login(token) {
+    this.assertRendered();
     this.pym.sendMessage('login', token);
   }
 
   logout() {
+    this.assertRendered();
     this.pym.sendMessage('logout');
   }
 
   remove() {
+    this.assertRendered();
     // Remove the event listeners.
     document.removeEventListener('click', this.handleClick.bind(this));
     this.emitter.removeAllListeners();
@@ -191,6 +239,7 @@ export default class Stream {
   }
 
   handleClick() {
+    this.assertRendered();
     this.pym.sendMessage('click');
   }
 }
