e134b4de04c99a1b25f3e6b5ad1e8e55aa4d83d7
Wyatt Johnson <wyattjoh@gmail.com>
2018-12-12 21:49:51 +0000

    [next] Flag Improvements (#2114)
    * fix: applied fix for reaction mutation
    
    * fix: removed comment length flag
    
    * feat: added additionalDetails field to flags
    
    * feat: added other flag type

---
+++ src/core/server/graph/tenant/mutators/util.ts
@@ -0,0 +1,14 @@
+/**
+ * validateMaximumLength will limit the value of an optional string to the
+ * specified amount, otherwise will throw an error.
+ *
+ * @param maxLength maximum length to limit a value to
+ * @param value the value that should be limited
+ */
+export function validateMaximumLength(maxLength: number, value?: string) {
+  if (value && value.length > maxLength) {
+    throw new Error(`Exceeded maximum length of ${maxLength} characters`);
+  }
+
+  return value;
+}
