bc965091e460e4cfb5301d7201a330e9b301ddaa
Chi Vinh Le <vinh@wikiwi.io>
2019-01-04 17:25:44 +0100

    test: add integration test for reaction

---
+++ src/core/client/stream/test/comments/reaction.spec.tsx
@@ -0,0 +1,96 @@
+import sinon from "sinon";
+
+import {
+  createSinonStub,
+  waitForElement,
+  within,
+} from "talk-framework/testHelpers";
+
+import { settings, stories, users } from "../fixtures";
+import create from "./create";
+
+function createTestRenderer() {
+  const resolvers = {
+    Query: {
+      story: createSinonStub(
+        s => s.throws(),
+        s =>
+          s
+            .withArgs(undefined, { id: stories[0].id, url: null })
+            .returns(stories[0])
+      ),
+      me: sinon.stub().returns(users[0]),
+      settings: sinon.stub().returns(settings),
+    },
+    Mutation: {
+      createCommentReaction: createSinonStub(
+        s => s.throws(),
+        s =>
+          s
+            .withArgs(undefined, {
+              input: {
+                commentID: stories[0].comments.edges[0].node.id,
+                commentRevisionID:
+                  stories[0].comments.edges[0].node.revision.id,
+                clientMutationId: "0",
+              },
+            })
+            .returns({
+              comment: {
+                id: stories[0].comments.edges[0].node.id,
+                revision: { id: stories[0].comments.edges[0].node.revision.id },
+                myActionPresence: { reaction: true },
+                actionCounts: { reaction: { total: 1 } },
+              },
+              clientMutationId: "0",
+            })
+      ),
+      removeCommentReaction: createSinonStub(
+        s => s.throws(),
+        s =>
+          s
+            .withArgs(undefined, {
+              input: {
+                commentID: stories[0].comments.edges[0].node.id,
+                clientMutationId: "0",
+              },
+            })
+            .returns({
+              comment: {
+                id: stories[0].comments.edges[0].node.id,
+                revision: { id: stories[0].comments.edges[0].node.revision.id },
+                myActionPresence: { reaction: false },
+                actionCounts: { reaction: { total: 0 } },
+              },
+              clientMutationId: "0",
+            })
+      ),
+    },
+  };
+
+  const { testRenderer } = create({
+    // Set this to true, to see graphql responses.
+    logNetwork: false,
+    resolvers,
+    initLocalState: localRecord => {
+      localRecord.setValue(stories[0].id, "storyID");
+      localRecord.setValue(true, "loggedIn");
+    },
+  });
+  return { testRenderer, resolvers };
+}
+
+it("create and remove reaction", async () => {
+  const commentID = stories[0].comments.edges[0].node.id;
+  const { testRenderer, resolvers } = createTestRenderer();
+  const comment = await waitForElement(() =>
+    within(testRenderer.root).getByTestID(`comment-${commentID}`)
+  );
+  const button = within(comment).getByText("Respect", { selector: "button" });
+  button.props.onClick({});
+  expect(within(button).toJSON()).toMatchSnapshot("Respected");
+  expect(resolvers.Mutation.createCommentReaction.called).toBe(true);
+  button.props.onClick({});
+  expect(within(button).toJSON()).toMatchSnapshot("Unrespected");
+  expect(resolvers.Mutation.removeCommentReaction.called).toBe(true);
+});
