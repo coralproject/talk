159509b20397e3d30496195f42c44ba810c6b341
Wyatt Johnson <wyattjoh@gmail.com>
2019-01-17 11:32:33 -0700

    feat: re-introduce scoped pretty logger

--- src/core/server/logger/streams/index.ts
+++ src/core/server/logger/streams/index.ts
@@ -1,19 +1,24 @@
+import config from "talk-server/config";
+
+import PrettyStream from "@coralproject/bunyan-prettystream";
 import { SecretStream } from "./SecretStream";
 
 export function getStreams() {
-  // TODO: (wyattjoh) re-evaluate a new solution to pretty printing
-  // If we aren't in production mode, use the pretty stream printer.
-  // if (config.get("env") !== "production") {
-  //   const pretty = new PrettyStream();
-
-  //   // Pipe the pretty stream to stdout.
-  //   pretty.pipe(process.stdout);
-
-  //   return [{ type: "raw", stream: pretty }];
-  // }
-
   // Create a new secret stream.
   const secret = new SecretStream();
+
+  // If we aren't in production mode, use the pretty stream printer.
+  if (config.get("env") !== "production") {
+    const pretty = new PrettyStream();
+
+    // Pipe the secret stream to pretty.
+    secret.pipe(pretty);
+
+    // Pipe the pretty stream to stdout.
+    pretty.pipe(process.stdout);
+
+    return [{ type: "raw", stream: pretty }];
+  }
 
   // Pipe the stream to stdout.
   secret.pipe(process.stdout);
