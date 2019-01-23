1dfc4b83c72688226848f94a5b90f8edca0985a5
Mendel Konikov <mkonikov@gmail.com>
2019-01-22 23:11:25 -0500

    Remove unnecessary diffs

--- plugins/talk-plugin-google-auth/server/router.js
+++ plugins/talk-plugin-google-auth/server/router.js
@@ -2,7 +2,7 @@ module.exports = router => {
   const { passport, HandleAuthPopupCallback } = require('services/passport');
 
   /**
-   * Google auth endpoint, this will redirect the user immediately to google
+   * Google auth endpoint, this will redirect the user immediately to Google
    * for authorization.
    */
   router.get(
@@ -16,7 +16,7 @@ module.exports = router => {
 
   /**
    * Google callback endpoint, this will send the user a html page designed to
-   * send back the user credentials upon sucessful login.
+   * send back the user credentials upon successful login.
    */
   router.get('/api/v1/auth/google/callback', (req, res, next) => {
     // Perform the Google login flow and pass the data back through the opener.
