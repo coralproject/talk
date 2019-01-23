d1a22f9dc80df04f2bfb3eca8023e746c78fb8cd
Mendel Konikov <mkonikov@gmail.com>
2019-01-22 22:10:53 -0500

    Remove merge comments

--- plugins/talk-plugin-google-auth/server/router.js
+++ plugins/talk-plugin-google-auth/server/router.js
@@ -2,11 +2,7 @@ module.exports = router => {
   const { passport, HandleAuthPopupCallback } = require('services/passport');
 
   /**
-<<<<<<< HEAD
-   * Google auth endpoint, this will redirect the user immediately to Google
-=======
    * Google auth endpoint, this will redirect the user immediately to google
->>>>>>> da56cd5b18bfb7bb0e9f1ae2dba1f4c729f41ff1
    * for authorization.
    */
   router.get(
@@ -20,11 +16,7 @@ module.exports = router => {
 
   /**
    * Google callback endpoint, this will send the user a html page designed to
-<<<<<<< HEAD
-   * send back the user credentials upon successful login.
-=======
    * send back the user credentials upon sucessful login.
->>>>>>> da56cd5b18bfb7bb0e9f1ae2dba1f4c729f41ff1
    */
   router.get('/api/v1/auth/google/callback', (req, res, next) => {
     // Perform the Google login flow and pass the data back through the opener.
