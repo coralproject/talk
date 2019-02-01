7436ab68a4db70a68530bafeed347cf812f1dc74
Wyatt Johnson <wyattjoh@gmail.com>
2019-02-01 16:08:08 -0700

    fix: patched bug with not found handler

--- src/core/server/app/middleware/notFound.ts
+++ src/core/server/app/middleware/notFound.ts
@@ -3,5 +3,5 @@ import { RequestHandler } from "express";
 import { NotFoundError } from "talk-server/errors";
 
 export const notFoundMiddleware: RequestHandler = (req, res, next) => {
-  next(new NotFoundError(req.method, req.path));
+  next(new NotFoundError(req.method, req.originalUrl));
 };
