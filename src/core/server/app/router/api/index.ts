973b85c7dd932598a9b0a1851cc34ec8a7e77fcd
Wyatt Johnson <wyattjoh@gmail.com>
2019-02-01 16:02:35 -0700

    fix: repaired issue with error management on api

--- src/core/server/app/router/api/index.ts
+++ src/core/server/app/router/api/index.ts
@@ -5,6 +5,7 @@ import { AppOptions } from "talk-server/app";
 import { versionHandler } from "talk-server/app/handlers/api/version";
 import { apiErrorHandler } from "talk-server/app/middleware/error";
 import { errorLogger } from "talk-server/app/middleware/logging";
+import { notFoundMiddleware } from "talk-server/app/middleware/notFound";
 
 import { createManagementRouter } from "./management";
 import { createTenantRouter } from "./tenant";
@@ -27,9 +28,11 @@ export async function createAPIRouter(app: AppOptions, options: RouterOptions) {
   // Configure the management routes.
   router.use("/management", await createManagementRouter(app));
 
+  // Configure the version route.
   router.get("/version", versionHandler);
 
   // General API error handler.
+  router.use(notFoundMiddleware);
   router.use(errorLogger);
   router.use(apiErrorHandler(app.i18n));
 
