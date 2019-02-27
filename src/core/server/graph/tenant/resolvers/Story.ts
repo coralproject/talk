a8234e51c4bd11c04e70ffc5343af08d0a38a7fb
Chi Vinh Le <vinh@wikiwi.io>
2019-02-27 19:11:01 +0100

    fix: isClosed can't be null

--- src/core/server/graph/tenant/resolvers/Story.ts
+++ src/core/server/graph/tenant/resolvers/Story.ts
@@ -9,7 +9,7 @@ export const Story: GQLStoryTypeResolver<story.Story> = {
   comments: (s, input, ctx) => ctx.loaders.Comments.forStory(s.id, input),
   isClosed: (s, input, ctx) => {
     const closedAt = getStoryClosedAt(ctx.tenant, s);
-    return closedAt && new Date() >= closedAt;
+    return !!closedAt && new Date() >= closedAt;
   },
   closedAt: (s, input, ctx) => getStoryClosedAt(ctx.tenant, s),
   commentActionCounts: s => decodeActionCounts(s.commentCounts.action),
