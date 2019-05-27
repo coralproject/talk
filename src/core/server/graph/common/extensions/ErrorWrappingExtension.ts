4a1139b78a7b5d05400789b5b628bc141a567ff6
Wyatt Johnson <wyattjoh@gmail.com>
2019-05-27 15:01:19 -0600

    feat: added development error wrapping

--- src/core/server/graph/common/extensions/ErrorWrappingExtension.ts
+++ src/core/server/graph/common/extensions/ErrorWrappingExtension.ts
@@ -2,23 +2,22 @@ import { GraphQLError } from "graphql";
 import { GraphQLExtension, GraphQLResponse } from "graphql-extensions";
 import { merge } from "lodash";
 
-import { CoralError, InternalError } from "coral-server/errors";
+import {
+  CoralError,
+  InternalDevelopmentError,
+  InternalError,
+} from "coral-server/errors";
 import CommonContext from "coral-server/graph/common/context";
 
 function hoistCoralErrorExtensions(
   ctx: CommonContext,
   err: GraphQLError
 ): void {
-  if (!err.originalError) {
-    // Only errors that have an originalError need to be hoisted.
+  // Grab or wrap the originalError so that it's a CoralError.
+  const originalError = extractOriginalError(err, ctx);
+  if (!originalError) {
     return;
   }
-
-  // Grab or wrap the originalError so that it's a CoralError.
-  const originalError: CoralError =
-    err.originalError instanceof CoralError
-      ? err.originalError
-      : new InternalError(err.originalError, "wrapped internal error");
 
   // Get the translation bundle.
   const bundle = ctx.i18n.getBundle(ctx.lang);
@@ -34,6 +33,35 @@ function hoistCoralErrorExtensions(
   merge(err.extensions, extensions);
 
   return;
+}
+
+/**
+ * extractOriginalError will pull out the original error if available.
+ *
+ * @param err the error to have their original error extracted from.
+ * @param ctx the Context to extract the environment state.
+ */
+function extractOriginalError(
+  err: GraphQLError,
+  ctx: CommonContext
+): CoralError | undefined {
+  if (!err.originalError) {
+    // Only errors that have an originalError need to be hoisted.
+    return;
+  }
+
+  if (err.originalError instanceof CoralError) {
+    return err.originalError;
+  }
+
+  if (ctx.config.get("env") !== "production") {
+    return new InternalDevelopmentError(
+      err.originalError,
+      "wrapped internal development error"
+    );
+  }
+
+  return new InternalError(err.originalError, "wrapped internal error");
 }
 
 /**
