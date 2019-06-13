849bf617a3d5072976d4f27998203fa40a90858d
Chi Vinh Le <vinh@wikiwi.io>
2019-06-13 19:05:34 +0200

    feat: added a dark variant to popover

--- src/core/client/ui/components/Popover/Arrow.tsx
+++ src/core/client/ui/components/Popover/Arrow.tsx
@@ -3,11 +3,20 @@ import React from "react";
 
 import styles from "./Arrow.css";
 
-const Arrow = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->((props, ref) => (
-  <div {...props} className={cn(props.className, styles.root)} ref={ref} />
-));
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  dark?: boolean;
+}
+
+const Arrow = React.forwardRef<HTMLDivElement, Props>(
+  ({ className, dark, ...rest }, ref) => (
+    <div
+      {...rest}
+      className={cn(className, styles.root, {
+        [styles.colorDark]: dark,
+      })}
+      ref={ref}
+    />
+  )
+);
 
 export default Arrow;
