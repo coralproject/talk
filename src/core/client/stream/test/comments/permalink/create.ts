3cd7bf655efeeadab43d80b9bf91b6beb745f0dd
Vinh <vinh@wikiwi.io>
2019-06-14 19:17:30 +0200

    chore: remove obsolete test code (#2363)

--- src/core/client/stream/test/comments/permalink/create.ts
+++ src/core/client/stream/test/comments/permalink/create.ts
@@ -1,26 +1,10 @@
-import { GQLResolver, StoryToCommentsResolver } from "coral-framework/schema";
-import {
-  createQueryResolverOverwrite,
-  CreateTestRendererParams,
-  overwriteQueryResolver,
-} from "coral-framework/testHelpers";
-import { storyWithNoComments } from "coral-stream/test/fixtures";
+import { CreateTestRendererParams } from "coral-framework/testHelpers";
 
 import createTopLevel from "../create";
 
 export default function create(params: CreateTestRendererParams) {
   return createTopLevel({
     ...params,
-    resolvers: overwriteQueryResolver<GQLResolver>(params.resolvers || {}, {
-      story: {
-        featuredComments: createQueryResolverOverwrite<StoryToCommentsResolver>(
-          ({ variables }) => {
-            // TODO: (cvle) Remove this overwrite once we have featured comment counts.
-            return storyWithNoComments.comments;
-          }
-        ),
-      },
-    }),
     initLocalState: (localRecord, source, environment) => {
       if (params.initLocalState) {
         params.initLocalState(localRecord, source, environment);
--- src/core/client/stream/test/comments/stream/create.ts
+++ src/core/client/stream/test/comments/stream/create.ts
@@ -1,26 +1,10 @@
-import { GQLResolver, StoryToCommentsResolver } from "coral-framework/schema";
-import {
-  createQueryResolverOverwrite,
-  CreateTestRendererParams,
-  overwriteQueryResolver,
-} from "coral-framework/testHelpers";
-import { storyWithNoComments } from "coral-stream/test/fixtures";
+import { CreateTestRendererParams } from "coral-framework/testHelpers";
 
 import createTopLevel from "../create";
 
 export default function create(params: CreateTestRendererParams) {
   return createTopLevel({
     ...params,
-    resolvers: overwriteQueryResolver<GQLResolver>(params.resolvers || {}, {
-      story: {
-        featuredComments: createQueryResolverOverwrite<StoryToCommentsResolver>(
-          () => {
-            // TODO: remove this.
-            return storyWithNoComments.comments;
-          }
-        ),
-      },
-    }),
     initLocalState: (localRecord, source, environment) => {
       localRecord.setValue("ALL_COMMENTS", "commentsTab");
       if (params.initLocalState) {
