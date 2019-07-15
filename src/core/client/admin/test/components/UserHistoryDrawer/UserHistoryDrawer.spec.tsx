a5447d92f4774fce19183feeeb7b8aacee0062a7
nick-funk <nick.funk@outlook.com>
2019-07-15 10:09:56 -0600

    Move testRenderer construction outside of act() operations
    CORL-443

--- src/core/client/admin/test/components/UserHistoryDrawer/UserHistoryDrawer.spec.tsx
+++ src/core/client/admin/test/components/UserHistoryDrawer/UserHistoryDrawer.spec.tsx
@@ -53,8 +53,9 @@ async function createTestRenderer(
 it("User drawer is open for user, user name is visible", async () => {
   const story = createStory();
   const user = story.comments.edges[0].node.author!;
+  const { testRenderer } = await createTestRenderer(user);
+
   await act(async () => {
-    const { testRenderer } = await createTestRenderer(user);
     const { getByText } = within(testRenderer.root);
     await waitForElement(() => getByText(user.id, { exact: false }));
   });
@@ -63,8 +64,9 @@ it("User drawer is open for user, user name is visible", async () => {
 it("User drawer is open for user, user name is visible", async () => {
   const story = createStory();
   const user = story.comments.edges[0].node.author!;
+  const { testRenderer } = await createTestRenderer(user);
+
   await act(async () => {
-    const { testRenderer } = await createTestRenderer(user);
     const { getByText } = within(testRenderer.root);
     await waitForElement(() => getByText(user.username!, { exact: false }));
   });
@@ -74,8 +76,9 @@ it("All comments selected, comment is visible in all comments", async () => {
   const story = createStory();
   const user = story.comments.edges[0].node.author!;
   const comment = user.allComments.edges[0].node;
+  const { testRenderer } = await createTestRenderer(user);
+
   await act(async () => {
-    const { testRenderer } = await createTestRenderer(user);
     const { getByText } = within(testRenderer.root);
 
     await waitForElement(() => getByText(comment.body!, { exact: false }));
@@ -86,8 +89,9 @@ it("Select rejected comments, rejected comment is visible.", async () => {
   const story = createStory();
   const user = story.comments.edges[0].node.author!;
   const rejectedComment = user.rejectedComments.edges[0].node;
+  const { testRenderer } = await createTestRenderer(user);
+
   await act(async () => {
-    const { testRenderer } = await createTestRenderer(user);
     const { getByText } = within(testRenderer.root);
     const rejectedTab = await waitForElement(() =>
       getByText("rejected", {
