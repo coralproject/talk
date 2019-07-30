6046f68ce824d7a36efd2317640308b882945d08
Tessa Thornton <tessathornton@gmail.com>
2019-07-30 10:46:49 -0400

    add empty state for comment history

--- src/core/client/stream/tabs/Profile/CommentHistory/CommentHistory.tsx
+++ src/core/client/stream/tabs/Profile/CommentHistory/CommentHistory.tsx
@@ -2,7 +2,15 @@ import { Localized } from "fluent-react/compat";
 import React, { FunctionComponent } from "react";
 
 import { PropTypesOf } from "coral-framework/types";
-import { Button, HorizontalGutter, Typography } from "coral-ui/components";
+import {
+  Button,
+  Flex,
+  HorizontalGutter,
+  Icon,
+  Typography,
+} from "coral-ui/components";
+
+import styles from "./CommentHistory.css";
 
 import HistoryCommentContainer from "./HistoryCommentContainer";
 
@@ -19,9 +27,30 @@ interface CommentHistoryProps {
 const CommentHistory: FunctionComponent<CommentHistoryProps> = props => {
   return (
     <HorizontalGutter size="double" data-testid="profile-commentHistory">
-      <Localized id="profile-historyComment-commentHistory">
-        <Typography variant="heading3">Comment History</Typography>
-      </Localized>
+      {props.comments.length > 0 && (
+        <Localized id="profile-historyComment-commentHistory">
+          <Typography variant="heading3">Comment History</Typography>
+        </Localized>
+      )}
+      {props.comments.length < 1 && (
+        <Flex
+          direction="column"
+          alignItems="center"
+          className={styles.emptyHistory}
+        >
+          <div className={styles.emptyHistoryIcon}>
+            <Icon size="xl">chat_bubble_outline</Icon>
+          </div>
+          <Localized id="Profile-commentHistory-empty">
+            <Typography gutterBottom variant="heading2">
+              You have not written any comments
+            </Typography>
+          </Localized>
+          <Localized id="Profile-commentHistory-empty-subheading">
+            <Typography>A history of your comments will appear here</Typography>
+          </Localized>
+        </Flex>
+      )}
       {props.comments.map(comment => (
         <HistoryCommentContainer
           key={comment.id}
