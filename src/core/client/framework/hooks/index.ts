e242e6f78bf75c574b342d44d5be95e514bf4b88
Chi Vinh Le <vinh@wikiwi.io>
2019-08-06 01:12:31 +0700

    feat: add useResizeObserver hook

--- src/core/client/framework/hooks/index.ts
+++ src/core/client/framework/hooks/index.ts
@@ -3,3 +3,4 @@ export { default as usePrevious } from "./usePrevious";
 export { default as useEffectWhenChanged } from "./useEffectWhenChanged";
 export { default as useUUID } from "./useUUID";
 export { default as useToken } from "./useToken";
+export { default as useResizeObserver } from "./useResizeObserver";
