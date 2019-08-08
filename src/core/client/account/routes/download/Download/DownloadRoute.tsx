02ccacb6ec879ff69ae92d37a0368f59153ddaf2
nick-funk <nick.funk@outlook.com>
2019-08-08 15:02:54 -0600

    Simplify download comment landing page components
    CORL-235

--- src/core/client/account/routes/download/Download/DownloadRoute.tsx
+++ src/core/client/account/routes/download/Download/DownloadRoute.tsx
@@ -1,14 +1,15 @@
 import React, { FunctionComponent } from "react";
 import { Environment } from "relay-runtime";
 
+import Loading from "coral-account/components/Loading";
 import { useToken } from "coral-framework/hooks";
 import { createFetch } from "coral-framework/lib/relay";
 import { withRouteConfig } from "coral-framework/lib/router";
 import { parseHashQuery } from "coral-framework/utils";
+import { HorizontalGutter } from "coral-ui/components";
 
 import DownloadDescription from "./DownloadDescription";
 import DownloadForm from "./DownloadForm";
-import Loading from "./Loading";
 import Sorry from "./Sorry";
 
 const fetcher = createFetch(
@@ -28,14 +29,18 @@ const DownloadRoute: FunctionComponent<Props> = ({ token }) => {
   const [state, error] = useToken(fetcher, token);
 
   if (state === "UNCHECKED") {
-    return <Loading />;
+    return (
+      <HorizontalGutter size="double">
+        <Loading />
+      </HorizontalGutter>
+    );
   }
   if (state !== "VALID" || error) {
     return (
-      <>
+      <HorizontalGutter size="double">
         <DownloadDescription />
         <Sorry />
-      </>
+      </HorizontalGutter>
     );
   }
 
--- src/core/client/account/routes/download/Download/Sorry.tsx
+++ src/core/client/account/routes/download/Download/Sorry.tsx
@@ -1,24 +1,22 @@
 import React, { FunctionComponent } from "react";
 
-import { CallOut, Flex, HorizontalGutter, Icon } from "coral-ui/components";
+import { CallOut, Flex, Icon } from "coral-ui/components";
 import { Localized } from "fluent-react/compat";
 
 import styles from "./Sorry.css";
 
 const Sorry: FunctionComponent = () => {
   return (
-    <HorizontalGutter size="double">
-      <CallOut color="error" fullWidth className={styles.callout}>
-        <Flex>
-          <Icon size="md" className={styles.icon}>
-            error
-          </Icon>
-          <Localized id="download-landingPage-sorry">
-            Your download link is invalid.
-          </Localized>
-        </Flex>
-      </CallOut>
-    </HorizontalGutter>
+    <CallOut color="error" fullWidth className={styles.callout}>
+      <Flex>
+        <Icon size="md" className={styles.icon}>
+          error
+        </Icon>
+        <Localized id="download-landingPage-sorry">
+          Your download link is invalid.
+        </Localized>
+      </Flex>
+    </CallOut>
   );
 };
 
