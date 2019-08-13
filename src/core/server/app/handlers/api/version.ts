a288648dbe90272bf88c8815caa550b4f60edf00
Wyatt Johnson <wyattjoh@gmail.com>
2019-08-13 16:35:09 -0600

    fix: implemented trust configuration

--- src/core/server/app/handlers/api/version.ts
+++ src/core/server/app/handlers/api/version.ts
@@ -2,5 +2,5 @@ import { revision, version } from "coral-common/version";
 import { RequestHandler } from "express";
 
 export const versionHandler: RequestHandler = (req, res, next) => {
-  res.json({ version, revision });
+  res.json({ version, revision, headers: req.headers, ip: req.ip });
 };
