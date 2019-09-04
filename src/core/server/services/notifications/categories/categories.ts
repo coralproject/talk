eccf56b743a5874f9c92b3168819f59573572860
Wyatt Johnson <wyattjoh@gmail.com>
2019-09-04 16:35:36 -0600

    fix: added staff replies back in

--- src/core/server/services/notifications/categories/categories.ts
+++ src/core/server/services/notifications/categories/categories.ts
@@ -1,9 +1,10 @@
 import { NotificationCategory } from "./category";
 import { reply } from "./reply";
+import { staffReply } from "./staffReply";
 
 /**
  * categories stores all the notification categories in a flat list.
  */
-const categories: NotificationCategory[] = [...reply];
+const categories: NotificationCategory[] = [...reply, ...staffReply];
 
 export default categories;
