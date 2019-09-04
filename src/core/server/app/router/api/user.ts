752c058486686b4d3cb701443b04b20d2a89ee49
Wyatt Johnson <wyattjoh@gmail.com>
2019-09-04 15:10:34 -0600

    fix: nick found bug

--- src/core/server/app/router/api/user.ts
+++ src/core/server/app/router/api/user.ts
@@ -1,12 +1,12 @@
 import express from "express";
 
 import { AppOptions } from "coral-server/app";
-import { accountDownloadHandler } from "coral-server/app/handlers";
+import { userDownloadHandler } from "coral-server/app/handlers";
 
 export function createNewUserRouter(app: AppOptions) {
   const router = express.Router();
 
-  router.get("/download", accountDownloadHandler(app));
+  router.get("/download", userDownloadHandler(app));
 
   return router;
 }
