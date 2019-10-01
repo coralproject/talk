97bb6ea25ac4b341270f6ec7b99965a7cbfb053d
Chi Vinh Le <vinh@vinh.tech>
2019-10-02 00:14:29 +0700

    chore: remove unuse file

--- src/core/common/countQuery.ts
+++
@@ -1,9 +0,0 @@
-export interface CountQueryArgs {
-  id?: string;
-  url?: string;
-  notext?: boolean;
-}
-
-export function createCountQueryRef(args: CountQueryArgs) {
-  return btoa(`${JSON.stringify(!!args.notext)};${args.id || args.url}`);
-}
