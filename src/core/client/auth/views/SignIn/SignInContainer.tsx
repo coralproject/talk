4fcf540da1a7e2629eb33434a1536024b281d510
Wyatt Johnson <wyattjoh@gmail.com>
2019-10-21 16:35:44 -0600

    fix: handle redirection loop

--- src/core/client/auth/views/SignIn/SignInContainer.tsx
+++ src/core/client/auth/views/SignIn/SignInContainer.tsx
@@ -51,8 +51,9 @@ const SignInContainer: FunctionComponent<Props> = ({
   // If there's only one enabled auth integration, we should just perform
   // the redirect now.
   if (
-    !auth.integrations.local.enabled ||
-    !auth.integrations.local.targetFilter.stream
+    !local.error &&
+    (!auth.integrations.local.enabled ||
+      !auth.integrations.local.targetFilter.stream)
   ) {
     // Local isn't enabled, so we can look into the rest of the integrations
     // now.
