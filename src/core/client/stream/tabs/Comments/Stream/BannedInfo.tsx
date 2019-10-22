e816f8e019f969c7960c80c153d7601eec35cbd6
Wyatt Johnson <wyattjoh@gmail.com>
2019-10-22 17:33:15 +0000

    fix: adjusted copy, added translation support (#2664)

--- src/core/client/stream/tabs/Comments/Stream/BannedInfo.tsx
+++ src/core/client/stream/tabs/Comments/Stream/BannedInfo.tsx
@@ -1,3 +1,4 @@
+import { Localized } from "fluent-react/compat";
 import React, { FunctionComponent } from "react";
 
 import { CallOut, HorizontalGutter, Typography } from "coral-ui/components";
@@ -6,15 +7,19 @@ const BannedInfo: FunctionComponent = props => {
   return (
     <CallOut fullWidth>
       <HorizontalGutter>
-        <Typography variant="heading3">
-          Your account has been banned from commenting.
-        </Typography>
-        <Typography variant="bodyCopy">
-          Someone with access to your account has violated our community
-          guidelines. As a result, your account has been banned. You will no
-          longer be able to comment, respect or report comments. if you think
-          this has been done in error, please contact our community team.
-        </Typography>
+        <Localized id="comments-bannedInfo-bannedFromCommenting">
+          <Typography variant="heading3">
+            Your account has been banned from commenting.
+          </Typography>
+        </Localized>
+        <Localized id="comments-bannedInfo-violatedCommunityGuidelines">
+          <Typography variant="bodyCopy">
+            Someone with access to your account has violated our community
+            guidelines. As a result, your account has been banned. You will no
+            longer be able to comment, respect or report comments. If you think
+            this has been done in error, please contact our community team.
+          </Typography>
+        </Localized>
       </HorizontalGutter>
     </CallOut>
   );
