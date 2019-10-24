3debcace4659d03f83a3edb7bc7498b539f78781
Nick Funk <nick.funk@outlook.com>
2019-10-24 09:33:14 -0600

    Bring new branding styles to button components (#2667)
    * Create preliminary filled and outline buttons that match branding
    
    * Add adornment button variant to newly branded button
    
    * Add the ghost variant to the newly branded button component
    
    * Add underlined variant to the newly branded button component
    
    * Remove external Icon composes property
    
    * Convert camel-case button variables to kebab-case

---
+++ src/core/client/ui/components/v2/index.ts
@@ -0,0 +1,4 @@
+export { default as BaseButton } from "./BaseButton";
+export { default as Button } from "./Button";
+export { default as ButtonIcon } from "./Button/ButtonIcon";
+export { default as Icon } from "./Icon";
