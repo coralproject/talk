ad54ca52fbe0c54f51504f063289d949fb37f19a
Nick Funk <nick.funk@outlook.com>
2019-10-28 15:08:19 -0600

    [CORL-673] Update the moderate area search menu to use new branding styles (#2672)
    * Create preliminary filled and outline buttons that match branding
    
    * Add adornment button variant to newly branded button
    
    * Add the ghost variant to the newly branded button component
    
    * Add underlined variant to the newly branded button component
    
    * Remove external Icon composes property
    
    * Convert camel-case button variables to kebab-case
    
    * Preliminarily migrate raw components into v2 to prep for branding updates
    
    CORL-673
    
    * Preliminarily update the search results bar to use new branding tokens
    
    CORL-673
    
    * Update styles to new branding on migrated v2 client-ui components
    
    CORL-673
    
    * Update see all and moderate all search option icons
    
    CORL-673
    
    * style config side nav
    
    * general configuration tab branding
    
    * organization tab branding
    
    * moderation configuration branding
    
    * banned/suspect words config branding
    
    * email config branding
    
    * advanced settings branding
    
    * auth config branding
    
    * move helpertext and fieldset components
    
    * add textarea component
    
    * add back gutters on table search
    
    * update snaps
    
    * fix header component to accept labels
    
    * use new spacing form components
    
    * use spacing components for advanced config
    
    * fix tests
    
    * fix line height of radio buttons
    
    * update validation message branding
    
    * update snaps
    
    * fix paths casing
    
    * try lowercase then
    
    * add docs for new form components
    
    * move description component
    
    * update snaps
    
    * move components to /ui/v2
    
    * Set size="md" on the Icon component and not the Localized component
    
    CORL-673

--- src/core/client/admin/routes/Moderate/ModerateSearchBar/SeeAllOption.tsx
+++ src/core/client/admin/routes/Moderate/ModerateSearchBar/SeeAllOption.tsx
@@ -2,7 +2,7 @@ import cn from "classnames";
 import { Localized } from "fluent-react/compat";
 import React, { FunctionComponent, HTMLAttributes } from "react";
 
-import { Icon } from "coral-ui/components";
+import { Icon } from "coral-ui/components/v2";
 
 import styles from "./SeeAllOption.css";
 
@@ -30,7 +30,9 @@ const SeeAllOption: FunctionComponent<Props> = ({
         <Localized id="moderate-searchBar-seeAllResults">
           <span>See all results</span>
         </Localized>
-        <Icon className={styles.icon}>arrow_forward</Icon>
+        <Icon className={styles.icon} size="md">
+          keyboard_arrow_right
+        </Icon>
       </a>
     </li>
   );
--- src/core/client/ui/components/v2/index.ts
+++ src/core/client/ui/components/v2/index.ts
@@ -2,12 +2,22 @@ export { default as BaseButton } from "./BaseButton";
 export { default as Button } from "./Button";
 export { default as ButtonIcon } from "./Button/ButtonIcon";
 export { default as Icon } from "./Icon";
+export { default as AriaInfo } from "./AriaInfo";
+export { default as Backdrop } from "./Backdrop";
+export { default as Box } from "./Box";
+export { default as Flex } from "./Flex";
+export { default as Popover } from "./Popover";
+export {
+  SubBar,
+  Navigation as SubBarNavigation,
+  NavigationItem as SubBarNavigationItem,
+} from "./SubBar";
+export { default as Spinner } from "./Spinner";
 export { default as Counter } from "./Counter";
 export { Marker, Count as MarkerCount } from "./Marker";
 export { Tab, TabBar, TabContent, TabPane } from "./Tabs";
 export { SelectField, Option, OptGroup } from "./SelectField";
 export { default as Timestamp } from "./Timestamp";
-export { default as Popover } from "./Popover";
 export { default as FormField } from "./FormField";
 export { default as FormFieldHeader } from "./FormFieldHeader";
 export { default as TextFieldAdornment } from "./TextFieldAdornment";
