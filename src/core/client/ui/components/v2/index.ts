e777236c7967f68c57c9eeaed3ef11bcd1b989cc
tessalt <tessathornton@gmail.com>
2019-10-30 15:41:47 -0400

    tag component

--- src/core/client/ui/components/v2/index.ts
+++ src/core/client/ui/components/v2/index.ts
@@ -7,6 +7,7 @@ export { default as Backdrop } from "./Backdrop";
 export { default as Box } from "./Box";
 export { default as Flex } from "./Flex";
 export { default as Popover } from "./Popover";
+export { default as Tag } from "./Tag";
 export {
   SubBar,
   Navigation as SubBarNavigation,
