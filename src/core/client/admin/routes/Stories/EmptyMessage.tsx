50bbf903cfe126cde0fb8942958574c70f4b9acd
tessalt <tessathornton@gmail.com>
2019-11-01 10:16:57 -0400

    clean up stories table

--- src/core/client/admin/routes/Stories/EmptyMessage.tsx
+++ src/core/client/admin/routes/Stories/EmptyMessage.tsx
@@ -1,15 +1,11 @@
 import { Localized } from "fluent-react/compat";
 import React, { FunctionComponent } from "react";
 
-import { Typography } from "coral-ui/components";
-
 import styles from "./EmptyMessage.css";
 
 const EmptyMessage: FunctionComponent = props => (
   <Localized id="stories-emptyMessage">
-    <Typography className={styles.root} variant="bodyCopyBold" align="center">
-      There are currently no published stories.
-    </Typography>
+    <div className={styles.root}>There are currently no published stories.</div>
   </Localized>
 );
 
