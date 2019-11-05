b94984c2fd0f814e32f7152340692b652fe6242b
tessalt <tessathornton@gmail.com>
2019-11-05 09:21:52 -0500

    sort exports

--- src/core/client/ui/components/v2/index.ts
+++ src/core/client/ui/components/v2/index.ts
@@ -1,50 +1,4 @@
-export { default as BaseButton } from "./BaseButton";
-export { default as Button } from "./Button";
-export { default as ButtonIcon } from "./Button/ButtonIcon";
-export { default as Icon } from "./Icon";
 export { default as AriaInfo } from "./AriaInfo";
-export { default as Backdrop } from "./Backdrop";
-export { default as Box } from "./Box";
-export { default as Flex } from "./Flex";
-export { default as Popover } from "./Popover";
-export { default as Tag } from "./Tag";
-export {
-  SubBar,
-  Navigation as SubBarNavigation,
-  NavigationItem as SubBarNavigationItem,
-} from "./SubBar";
-export { default as Spinner } from "./Spinner";
-export { default as Counter } from "./Counter";
-export { Marker, Count as MarkerCount } from "./Marker";
-export { Tab, TabBar, TabContent, TabPane } from "./Tabs";
-export { SelectField, Option, OptGroup } from "./SelectField";
-export { default as Timestamp } from "./Timestamp";
-export { default as FormField } from "./FormField";
-export { default as FormFieldHeader } from "./FormFieldHeader";
-export { default as TextFieldAdornment } from "./TextFieldAdornment";
-export { default as RadioButton } from "./RadioButton";
-export { default as Card } from "./Card";
-export { default as HelperText } from "./HelperText";
-export { default as CheckBox } from "./CheckBox";
-export { default as Tooltip, TooltipButton } from "./Tooltip";
-export { default as CallOut } from "./CallOut";
-export { default as Divider } from "./Divider";
-export { default as FieldSet } from "./FieldSet";
-export { default as Textarea } from "./Textarea";
-export { default as FormFieldDescription } from "./FormFieldDescription";
-export { default as PasswordField } from "./PasswordField";
-export { default as Message, MessageIcon } from "./Message";
-export {
-  default as ValidationMessage,
-  ValidationMessageProps,
-} from "./ValidationMessage";
-export { default as DurationField, DURATION_UNIT } from "./DurationField";
-export {
-  Dropdown,
-  Button as DropdownButton,
-  Divider as DropdownDivider,
-} from "./Dropdown";
-export { default as TextLink } from "./TextLink";
 export {
   AppBar,
   Begin as AppBarBegin,
@@ -53,6 +7,52 @@ export {
   Navigation as AppBarNavigation,
   NavigationItem as AppBarNavigationItem,
 } from "./AppBar";
-export { Table, TableHead, TableBody, TableRow, TableCell } from "./Table";
-export { default as TextField } from "./TextField";
+export { default as Backdrop } from "./Backdrop";
+export { default as BaseButton } from "./BaseButton";
+export { default as Box } from "./Box";
+export { default as Button } from "./Button";
+export { default as ButtonIcon } from "./Button/ButtonIcon";
+export { default as CallOut } from "./CallOut";
+export { default as Card } from "./Card";
+export { default as CheckBox } from "./CheckBox";
+export { default as Counter } from "./Counter";
+export { default as Divider } from "./Divider";
+export {
+  Dropdown,
+  Button as DropdownButton,
+  Divider as DropdownDivider,
+} from "./Dropdown";
+export { default as DurationField, DURATION_UNIT } from "./DurationField";
+export { default as FieldSet } from "./FieldSet";
+export { default as Flex } from "./Flex";
+export { default as FormField } from "./FormField";
+export { default as FormFieldDescription } from "./FormFieldDescription";
+export { default as FormFieldHeader } from "./FormFieldHeader";
+export { default as HelperText } from "./HelperText";
+export { default as Icon } from "./Icon";
 export { default as Label } from "./Label";
+export { Marker, Count as MarkerCount } from "./Marker";
+export { default as Message, MessageIcon } from "./Message";
+export { default as PasswordField } from "./PasswordField";
+export { default as Popover } from "./Popover";
+export { default as RadioButton } from "./RadioButton";
+export { SelectField, Option, OptGroup } from "./SelectField";
+export { default as Spinner } from "./Spinner";
+export {
+  SubBar,
+  Navigation as SubBarNavigation,
+  NavigationItem as SubBarNavigationItem,
+} from "./SubBar";
+export { Table, TableHead, TableBody, TableRow, TableCell } from "./Table";
+export { Tab, TabBar, TabContent, TabPane } from "./Tabs";
+export { default as Tag } from "./Tag";
+export { default as Textarea } from "./Textarea";
+export { default as TextField } from "./TextField";
+export { default as TextFieldAdornment } from "./TextFieldAdornment";
+export { default as TextLink } from "./TextLink";
+export { default as Timestamp } from "./Timestamp";
+export { default as Tooltip, TooltipButton } from "./Tooltip";
+export {
+  default as ValidationMessage,
+  ValidationMessageProps,
+} from "./ValidationMessage";
