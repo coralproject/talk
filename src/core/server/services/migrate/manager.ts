10719bd874037b75e8b1963c9ce259a2e43c4a40
Wyatt Johnson <wyattjoh@gmail.com>
2019-11-21 17:24:09 +0000

    [CORL-793] Scheduled Indexes (#2723)
    * fix: modified digesting to use index
    
    * fix: added index for deletion
    
    * review: cleaning up
    
    * review: fixed log copy

--- src/core/server/services/migrate/manager.ts
+++ src/core/server/services/migrate/manager.ts
@@ -70,6 +70,15 @@ export default class Manager {
       const id = parseInt(matches[1], 10);
       const name = matches[2];
 
+      // Skip this migration if it was disabled.
+      if (m.default.disabled) {
+        logger.warn(
+          { migrationID: id, migrationName: name },
+          "skipping disabled migration"
+        );
+        continue;
+      }
+
       // Create the migration instance.
       const migration = new m.default({ id, name, i18n });
 
