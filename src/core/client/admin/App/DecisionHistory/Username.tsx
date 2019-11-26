ed5b33f8dc95861e59d43ef4c426d6f64e950634
tessalt <tessathornton@gmail.com>
2019-11-26 10:08:21 -0500

    Fix username props interface

--- src/core/client/admin/App/DecisionHistory/Username.tsx
+++ src/core/client/admin/App/DecisionHistory/Username.tsx
@@ -2,7 +2,11 @@ import React, { FunctionComponent } from "react";
 
 import styles from "./Username.css";
 
-const Username: FunctionComponent<{ username: string }> = ({ username }) => (
+interface Props {
+  username: string;
+}
+
+const Username: FunctionComponent<Props> = ({ username }) => (
   <strong className={styles.root}>{username}</strong>
 );
 
