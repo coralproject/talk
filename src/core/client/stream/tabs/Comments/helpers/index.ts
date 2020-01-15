5ddf82797b4412d573bd2a6c4224d96d7ecd442b
Chi Vinh Le <vinh@vinh.tech>
2020-01-15 16:36:59 +0100

    feat: normalize rte html

--- src/core/client/stream/tabs/Comments/helpers/index.ts
+++ src/core/client/stream/tabs/Comments/helpers/index.ts
@@ -2,7 +2,7 @@ export { default as getHTMLCharacterLength } from "./getHTMLCharacterLength";
 export {
   default as getCommentBodyValidators,
 } from "./getCommentBodyValidators";
-export { default as cleanupRTEEmptyHTML } from "./cleanupRTEEmptyHTML";
+export { default as normalizeRTEHTML } from "./normalizeRTEHTML";
 export {
   default as shouldTriggerSettingsRefresh,
 } from "./shouldTriggerSettingsRefresh";
