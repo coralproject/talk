1b66e941cc2b85f8fb50c06a6ef8400db199b5b9
Chi Vinh Le <vinh@vinh.tech>
2020-03-05 16:21:45 +0100

    chore: upgrade autoprefixer

--- src/core/build/postcss.config.js
+++ src/core/build/postcss.config.js
@@ -63,12 +63,6 @@ module.exports = {
     postcssFlexbugsFixes,
     // Vendor prefixing.
     autoprefixer({
-      browsers: [
-        ">1%",
-        "last 4 versions",
-        "Firefox ESR",
-        "not ie < 9", // React doesn't support IE8 anyway
-      ],
       flexbox: "no-2009",
     }),
   ],
