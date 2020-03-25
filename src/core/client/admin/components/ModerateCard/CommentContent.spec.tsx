2be1520b81aff10135d60c5723e666b6bf9996e6
hector.melo <hector.melo@corp.globo.com>
2020-03-25 13:13:07 -0300

    Add test for PT-BR regexp in CommentContent - including accented words in comments

--- src/core/client/admin/components/ModerateCard/CommentContent.spec.tsx
+++ src/core/client/admin/components/ModerateCard/CommentContent.spec.tsx
@@ -38,3 +38,37 @@ it("renders empty words correctly", () => {
   renderer.render(<CommentContent {...props} />);
   expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
+
+it("renders correctly in Brazilian Portuguese: it doesn't treat accented characters as word separators", () => {
+  const props: PropTypesOf<typeof CommentContent> = {
+    phrases: {
+      locale: "pt-BR",
+      wordList: {
+        suspect: ["anual"],
+        banned: ["bi", "ESTRANHO"],
+      },
+    },
+    className: "custom",
+    children: "O biólogo analisou o caso.bi Mas não foi ao evento biánual",
+  };
+  const renderer = createRenderer();
+  renderer.render(<CommentContent {...props} />);
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
+
+it("renders empty words correctly in Brazilian Portuguese", () => {
+  const props: PropTypesOf<typeof CommentContent> = {
+    phrases: {
+      locale: "pt-BR",
+      wordList: {
+        suspect: [],
+        banned: [],
+      },
+    },
+    className: "custom",
+    children: "Olá <b>Bob</b>, você é um cara mau",
+  };
+  const renderer = createRenderer();
+  renderer.render(<CommentContent {...props} />);
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
