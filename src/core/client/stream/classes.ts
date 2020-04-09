e1170176953fb2bf2c2717606fef94ede2ac5343
Chi Vinh Le <vinh@vinh.tech>
2020-04-09 17:32:42 +0200

    feat: add coral-indent class

--- src/core/client/stream/classes.ts
+++ src/core/client/stream/classes.ts
@@ -392,13 +392,13 @@ const CLASSES = {
      * indentation classes for the different levels.
      */
     indent: [
-      "coral coral-indent-0",
-      "coral coral-indent-1",
-      "coral coral-indent-2",
-      "coral coral-indent-3",
-      "coral coral-indent-4",
-      "coral coral-indent-5",
-      "coral coral-indent-6",
+      "coral coral-indent coral-indent-0",
+      "coral coral-indent coral-indent-1",
+      "coral coral-indent coral-indent-2",
+      "coral coral-indent coral-indent-3",
+      "coral coral-indent coral-indent-4",
+      "coral coral-indent coral-indent-5",
+      "coral coral-indent coral-indent-6",
     ],
   },
 
