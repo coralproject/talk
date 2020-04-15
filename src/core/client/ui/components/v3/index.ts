b0f3047c2b4469d3f4c6a00b5d4d9eed43ca5c6d
nick-funk <nick.funk@outlook.com>
2020-04-15 08:58:30 -0600

    Create tombstone component
    Use it for ignored and deleted account
    tombstones instead of the old callout.
    
    CORL-969

--- src/core/client/ui/components/v3/index.ts
+++ src/core/client/ui/components/v3/index.ts
@@ -1,3 +1,4 @@
 export { default as CallOut } from "./CallOut";
 export { default as Button } from "./Button";
 export { default as ValidationMessage } from "./ValidationMessage";
+export { default as Tombstone } from "./Tombstone";
