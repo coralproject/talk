c922d6c846d31012719a28e3d34996953d3ccb5c
nick-funk <nick.funk@outlook.com>
2020-04-15 15:47:17 -0600

    Update banned callout to new branding
    CORL-969

--- src/core/client/stream/tabs/Comments/Stream/BannedInfo.tsx
+++ src/core/client/stream/tabs/Comments/Stream/BannedInfo.tsx
@@ -1,26 +1,35 @@
 import { Localized } from "@fluent/react/compat";
 import React, { FunctionComponent } from "react";
 
-import { CallOut, HorizontalGutter, Typography } from "coral-ui/components";
+import { Icon } from "coral-ui/components/v2";
+import { CallOut } from "coral-ui/components/v3";
+
+import styles from "./BannedInfo.css";
 
 const BannedInfo: FunctionComponent = (props) => {
   return (
-    <CallOut fullWidth>
-      <HorizontalGutter>
+    <CallOut
+      color="negative"
+      icon={
+        <Icon size="sm" className={styles.icon}>
+          block
+        </Icon>
+      }
+      iconColor="none"
+      title={
         <Localized id="comments-bannedInfo-bannedFromCommenting">
-          <Typography variant="heading3">
-            Your account has been banned from commenting.
-          </Typography>
+          Your account has been banned from commenting.
         </Localized>
-        <Localized id="comments-bannedInfo-violatedCommunityGuidelines">
-          <Typography variant="bodyCopy">
-            Someone with access to your account has violated our community
-            guidelines. As a result, your account has been banned. You will no
-            longer be able to comment, respect or report comments. If you think
-            this has been done in error, please contact our community team.
-          </Typography>
-        </Localized>
-      </HorizontalGutter>
+      }
+    >
+      <Localized id="comments-bannedInfo-violatedCommunityGuidelines">
+        <div>
+          Someone with access to your account has violated our community
+          guidelines. As a result, your account has been banned. You will no
+          longer be able to comment, respect or report comments. If you think
+          this has been done in error, please contact our community team.
+        </div>
+      </Localized>
     </CallOut>
   );
 };
