6923a72ec5b1cb3f521a5dc1e2833b717675f73d
Wyatt Johnson <me@wyattjoh.ca>
2020-04-15 11:10:31 -0600

    fix: linting

--- src/core/client/admin/routes/Configure/sections/ModerationPhases/ModerationPhasesLayout.tsx
+++ src/core/client/admin/routes/Configure/sections/ModerationPhases/ModerationPhasesLayout.tsx
@@ -11,7 +11,7 @@ interface Props {
   children: React.ReactElement;
 }
 
-const ModerationPhasesLayout: FunctionComponent<Props> = props => {
+const ModerationPhasesLayout: FunctionComponent<Props> = (props) => {
   return (
     <MainLayout>
       <Layout>
--- src/core/client/admin/routes/Configure/sections/ModerationPhases/index.ts
+++ src/core/client/admin/routes/Configure/sections/ModerationPhases/index.ts
@@ -2,9 +2,5 @@ export {
   default,
   default as ModerationPhasesConfigRoute,
 } from "./ModerationPhasesConfigRoute";
-export {
-  default as AddExternalModerationPhaseRoute,
-} from "./AddExternalModerationPhase";
-export {
-  default as ConfigureExternalModerationPhaseRoute,
-} from "./ConfigureExternalModerationPhase";
+export { default as AddExternalModerationPhaseRoute } from "./AddExternalModerationPhase";
+export { default as ConfigureExternalModerationPhaseRoute } from "./ConfigureExternalModerationPhase";
