a9b2f2f2e33f192f4cb7112dd3fbe56588efb10f
tessalt <tessathornton@gmail.com>
2020-05-12 10:17:58 -0400

    Revert "add site selector to nav"
    This reverts commit 1c2b1dee34fb2742b04932079fd45f7f3de98418.

--- src/core/client/admin/App/Navigation/Navigation.tsx
+++ src/core/client/admin/App/Navigation/Navigation.tsx
@@ -1,9 +1,8 @@
 import { Localized } from "@fluent/react/compat";
-import React, { FunctionComponent, useCallback, useState } from "react";
+import React, { FunctionComponent } from "react";
 
-import { AppBarNavigation, Button } from "coral-ui/components/v2";
+import { AppBarNavigation } from "coral-ui/components/v2";
 
-import DashboardLinkQuery from "./DashboardLinkQuery";
 import NavigationLink from "./NavigationLink";
 
 interface Props {
@@ -11,37 +10,28 @@ interface Props {
   showDashboard: boolean;
 }
 
-const Navigation: FunctionComponent<Props> = (props) => {
-  const [showSiteSelect, setShowSiteSelect] = useState<boolean>(false);
-  const toggleSiteSelector = useCallback(() => {
-    setShowSiteSelect(!showSiteSelect);
-  }, [showSiteSelect]);
-  return (
-    <AppBarNavigation>
-      <Localized id="navigation-moderate">
-        <NavigationLink to="/admin/moderate">Moderate</NavigationLink>
+const Navigation: FunctionComponent<Props> = (props) => (
+  <AppBarNavigation>
+    <Localized id="navigation-moderate">
+      <NavigationLink to="/admin/moderate">Moderate</NavigationLink>
+    </Localized>
+    <Localized id="navigation-stories">
+      <NavigationLink to="/admin/stories">Stories</NavigationLink>
+    </Localized>
+    <Localized id="navigation-community">
+      <NavigationLink to="/admin/community">Community</NavigationLink>
+    </Localized>
+    {props.showConfigure && (
+      <Localized id="navigation-configure">
+        <NavigationLink to="/admin/configure">Configure</NavigationLink>
       </Localized>
-      <Localized id="navigation-stories">
-        <NavigationLink to="/admin/stories">Stories</NavigationLink>
+    )}
+    {props.showDashboard && (
+      <Localized id="navigation-dashboard">
+        <NavigationLink to="/admin/dashboard">Dashboard</NavigationLink>
       </Localized>
-      <Localized id="navigation-community">
-        <NavigationLink to="/admin/community">Community</NavigationLink>
-      </Localized>
-      {props.showConfigure && (
-        <Localized id="navigation-configure">
-          <NavigationLink to="/admin/configure">Configure</NavigationLink>
-        </Localized>
-      )}
-      {props.showDashboard && (
-        <>
-          <Localized id="navigation-dashboard">
-            <Button onClick={toggleSiteSelector}>Dashboard</Button>
-          </Localized>
-          {showSiteSelect && <DashboardLinkQuery />}
-        </>
-      )}
-    </AppBarNavigation>
-  );
-};
+    )}
+  </AppBarNavigation>
+);
 
 export default Navigation;
