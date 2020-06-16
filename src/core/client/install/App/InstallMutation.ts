a9d3b8db51d16dcdb31617c211d94aaa0a462916
nick-funk <nick.funk@outlook.com>
2020-06-16 14:34:11 -0600

    Clear session token on tenant install
    CORL-1122

--- src/core/client/install/App/InstallMutation.ts
+++ src/core/client/install/App/InstallMutation.ts
@@ -2,6 +2,7 @@ import { Environment } from "relay-runtime";
 
 import { CoralContext } from "coral-framework/lib/bootstrap";
 import { createMutationContainer } from "coral-framework/lib/relay";
+import { SetAccessTokenMutation } from "coral-framework/mutations";
 import { install, InstallInput } from "coral-framework/rest";
 
 export type InstallMutation = (input: InstallInput) => Promise<void>;
@@ -9,9 +10,10 @@ export type InstallMutation = (input: InstallInput) => Promise<void>;
 export async function commit(
   environment: Environment,
   input: InstallInput,
-  { rest }: CoralContext
+  ctx: CoralContext
 ) {
-  await install(rest, input);
+  await install(ctx.rest, input);
+  await SetAccessTokenMutation.commit(environment, { accessToken: "" }, ctx);
 }
 
 export const withInstallMutation = createMutationContainer("install", commit);
