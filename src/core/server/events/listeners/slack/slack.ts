7f98f139901d6eeb9caf3159ed0dc5230fb13e93
Nick Funk <nick.funk@outlook.com>
2020-07-21 12:01:38 -0600

    Send staff comments to Slack with all comments enabled (#3035)
    When we set up a Slack channel, you can select to have
    "All comments" sent to the slack channel. If we do this
    we want staff comments to come through as well.
    
    This wasn't happening before because there is a `staffCreated`
    flag instead of `created` when a staff member comments.
    
    So we change the trigger return type to an array, and when
    a staff comment appears, return `staffCreated` as well as
    `created` so the all filter lets it through.
    
    I also changed `getBlocks(...)` to `getContent(...)` as it
    now returns a JSON object with both a `blocks` array and a
    `text` string field. This fixes the problem where Slack would
    notifications would say "this content cannot be displayed".
    Now it will say the text provided.
    
    CORL-1172

--- src/core/server/events/listeners/slack/slack.ts
+++ src/core/server/events/listeners/slack/slack.ts
@@ -34,9 +34,9 @@ export class SlackCoralEventListener
    * postMessage will prepare and send the incoming Slack webhook.
    *
    * @param hookURL url to the Slack webhook that we should send the message to
-   * @param blocks the blocks for the message
+   * @param content the content for the message
    */
-  private async postMessage(hookURL: string, blocks: any[]) {
+  private async postMessage(hookURL: string, content: any) {
     // Send the post to the Slack URL. We don't wrap this in a try/catch because
     // it's handled in the calling function.
     const res = await this.fetch(hookURL, {
@@ -44,9 +44,7 @@ export class SlackCoralEventListener
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        blocks,
-      }),
+      body: JSON.stringify(content),
     });
 
     // Check that the request was completed successfully.
@@ -131,7 +129,7 @@ export class SlackCoralEventListener
       if (publishEvent.shouldPublishToChannel(channel)) {
         try {
           // Post the message to slack.
-          await this.postMessage(channel.hookURL, publishEvent.getBlocks(ctx));
+          await this.postMessage(channel.hookURL, publishEvent.getContent(ctx));
         } catch (err) {
           logger.error(
             { err, tenantID, payload, channel },
