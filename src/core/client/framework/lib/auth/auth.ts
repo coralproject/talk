ae2cd698935b64191a65c407aadfa24520c91385
Wyatt Johnson <wyattjoh@gmail.com>
2020-08-05 14:14:12 -0600

    fix: ensure access tokens from sso are not persisted

--- src/core/client/framework/lib/auth/auth.ts
+++ src/core/client/framework/lib/auth/auth.ts
@@ -19,7 +19,7 @@ export interface AuthState {
 
 export type AccessTokenProvider = () => string | undefined;
 
-function parseAccessToken(accessToken: string) {
+export function parseAccessToken(accessToken: string) {
   // Try to parse the access token claims.
   const claims = parseAccessTokenClaims(accessToken);
   if (!claims) {
@@ -59,6 +59,13 @@ export function retrieveAccessToken() {
 }
 
 export function storeAccessToken(accessToken: string) {
+  // Parse the access token that's trying to be stored now. If it can't be
+  // parsed, it can't be stored.
+  const parsed = parseAccessToken(accessToken);
+  if (!parsed) {
+    return;
+  }
+
   try {
     // Update the access token in storage.
     localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
@@ -69,7 +76,7 @@ export function storeAccessToken(accessToken: string) {
   }
 
   // Return the parsed access token.
-  return parseAccessToken(accessToken);
+  return parsed;
 }
 
 export function deleteAccessToken() {
