ce53134736874bb9c76931af69607b45922b80a0
Nick Funk <nick.funk@outlook.com>
2020-08-05 09:34:42 -0600

    [CORL-794] Create a common date formatter (#3073)
    * Create a common date formatter
    
    Updates all uses of Intl.DateTimeFormat(...)
    with re-usable date time formatter functions.
    
    The formatter functions have default formatting
    options, but these can be overridden.
    
    CORL-794
    
    * Include options in useMemo
    
    Co-authored-by: Wyatt Johnson <wyattjoh@gmail.com>
    
    * Make date formatting accept single locale string
    
    Makes formatDate and createDateFormatter accept
    string | string[] | undefined for the locales
    argument.
    
    CORL-794
    
    Co-authored-by: Wyatt Johnson <wyattjoh@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- src/core/client/framework/hooks/useDateTimeFormat.ts
+++ src/core/client/framework/hooks/useDateTimeFormat.ts
@@ -1,10 +1,11 @@
 import { useMemo } from "react";
 
+import { createDateFormatter } from "coral-common/date";
 import { useCoralContext } from "coral-framework/lib/bootstrap";
 
 export default function useDateTimeFormat(options: Intl.DateTimeFormatOptions) {
   const { locales } = useCoralContext();
-  return useMemo(() => new Intl.DateTimeFormat(locales, options), [
+  return useMemo(() => createDateFormatter(locales, options), [
     locales,
     options,
   ]);
