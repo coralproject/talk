24e9b164c2e791d67499d6335bd79337c3ab34be
Nick Funk <nick.funk@outlook.com>
2020-08-05 08:57:53 -0600

    Include options in useMemo
    Co-authored-by: Wyatt Johnson <wyattjoh@gmail.com>

--- src/core/client/ui/hooks/useDateTimeFormat.ts
+++ src/core/client/ui/hooks/useDateTimeFormat.ts
@@ -5,5 +5,5 @@ import { useUIContext } from "coral-ui/components/v2";
 
 export default function useDateTimeFormat(options: Intl.DateTimeFormatOptions) {
   const { locales } = useUIContext();
-  return useMemo(() => createDateFormatter(locales, options), [locales]);
+  return useMemo(() => createDateFormatter(locales, options), [locales, options]);
 }
