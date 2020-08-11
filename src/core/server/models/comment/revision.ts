d67c3d29070699b3b89513cc6857257f41228f0a
Nick Funk <nick.funk@outlook.com>
2020-08-11 16:05:50 -0600

    Mark and allow word list timed out comments (#3097)
    Will mark the comment's metdata as a word list
    time out if it happens and also tag them with
    suspect or banned word.
    
    Then we show "possible suspect/banned word"
    markers on the moderate card so moderators can
    manually review them.
    
    CORL-1264
    
    Co-authored-by: Wyatt Johnson <wyattjoh@gmail.com>

--- src/core/server/models/comment/revision.ts
+++ src/core/server/models/comment/revision.ts
@@ -29,6 +29,18 @@ export interface RevisionMetadata {
   };
 
   /**
+   * wordList metadata stores extra status details about what occurred during
+   * word list analysis of this comment revision.
+   */
+  wordList?: {
+    /**
+     * timedOut is whether the wordlist analysis timed out when this revision
+     * of the comment was sent through the moderation phases.
+     */
+    timedOut?: boolean;
+  };
+
+  /**
    * nudge when true indicates that the comment was written on the first try
    * without a warning.
    */
