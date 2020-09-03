bdf811c520f6f10c9f89a4319cb4148ffea5a879
tessalt <tessathornton@gmail.com>
2020-09-03 09:03:01 -0400

    validate max length of bio

--- src/core/client/stream/tabs/Profile/Preferences/BioContainer.tsx
+++ src/core/client/stream/tabs/Profile/Preferences/BioContainer.tsx
@@ -1,13 +1,17 @@
 import { Localized } from "@fluent/react/compat";
+import { FORM_ERROR } from "final-form";
 import React, { FunctionComponent, useCallback } from "react";
 import { Field, Form } from "react-final-form";
 import { graphql } from "react-relay";
 
+import { MAX_BIO_LENGTH } from "coral-common/constants";
 import { MarkdownEditor } from "coral-framework/components/loadables";
+import { InvalidRequestError } from "coral-framework/lib/errors";
 import { parseEmptyAsNull } from "coral-framework/lib/form";
 import { useMutation, withFragmentContainer } from "coral-framework/lib/relay";
+import { validateMaxLength } from "coral-framework/lib/validation";
 import { Flex, HorizontalGutter } from "coral-ui/components/v2";
-import { Button } from "coral-ui/components/v3";
+import { Button, CallOut, ValidationMessage } from "coral-ui/components/v3";
 
 import { BioContainer_settings } from "coral-stream/__generated__/BioContainer_settings.graphql";
 import { BioContainer_viewer } from "coral-stream/__generated__/BioContainer_viewer.graphql";
@@ -27,7 +31,14 @@ const BioContainer: FunctionComponent<Props> = ({ viewer, settings }) => {
   const onSubmit = useCallback(
     (formData: any) => {
       const { bio } = formData;
-      void updateBio({ bio });
+      try {
+        void updateBio({ bio });
+      } catch (err) {
+        if (err instanceof InvalidRequestError) {
+          return err.invalidArgs;
+        }
+        return { [FORM_ERROR]: err.message };
+      }
     },
     [updateBio]
   );
@@ -36,18 +47,47 @@ const BioContainer: FunctionComponent<Props> = ({ viewer, settings }) => {
   }
   return (
     <Form onSubmit={onSubmit} initialValues={{ bio: viewer.bio }}>
-      {({ handleSubmit, submitting, pristine, invalid }) => (
+      {({
+        handleSubmit,
+        submitting,
+        pristine,
+        invalid,
+        submitError,
+        error,
+      }) => (
         <form autoComplete="off" onSubmit={handleSubmit} id="bio-form">
           <HorizontalGutter>
-            <Field name="bio" parse={parseEmptyAsNull}>
+            <Field
+              name="bio"
+              parse={parseEmptyAsNull}
+              validate={validateMaxLength(MAX_BIO_LENGTH)}
+            >
               {({ input, meta }) => (
-                <MarkdownEditor
-                  name={input.name}
-                  onChange={input.onChange}
-                  value={input.value}
-                />
+                <>
+                  <MarkdownEditor
+                    name={input.name}
+                    onChange={input.onChange}
+                    value={input.value}
+                  />
+                  <div>
+                    {meta.error && (
+                      <ValidationMessage
+                        meta={meta}
+                        justifyContent="flex-end"
+                      />
+                    )}
+                    <Localized
+                      id="profile-bio-maxCharacters"
+                      $maxCharacters={100}
+                    >
+                      <div>Max. 100 Characters</div>
+                    </Localized>
+                  </div>
+                </>
               )}
             </Field>
+
+            {submitError && <CallOut color="error">{submitError}</CallOut>}
             <Flex itemGutter>
               <Localized id="profile-bio-remove">
                 <Button
@@ -60,10 +100,7 @@ const BioContainer: FunctionComponent<Props> = ({ viewer, settings }) => {
                 </Button>
               </Localized>
               <Localized id="profile-bio-update">
-                <Button
-                  type="submit"
-                  disabled={submitting || invalid || pristine}
-                >
+                <Button type="submit" disabled={submitting || pristine}>
                   Update
                 </Button>
               </Localized>
