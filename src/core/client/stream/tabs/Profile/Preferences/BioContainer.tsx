6f06a41321a2d7eddb52e57fc78531dd89b17247
tessalt <tessathornton@gmail.com>
2020-09-04 08:36:34 -0400

    style bio form

--- src/core/client/stream/tabs/Profile/Preferences/BioContainer.tsx
+++ src/core/client/stream/tabs/Profile/Preferences/BioContainer.tsx
@@ -10,13 +10,16 @@ import { InvalidRequestError } from "coral-framework/lib/errors";
 import { parseEmptyAsNull } from "coral-framework/lib/form";
 import { useMutation, withFragmentContainer } from "coral-framework/lib/relay";
 import { validateMaxLength } from "coral-framework/lib/validation";
-import { Flex, HorizontalGutter } from "coral-ui/components/v2";
+import { Flex, HorizontalGutter, HorizontalRule } from "coral-ui/components/v2";
 import { Button, CallOut, ValidationMessage } from "coral-ui/components/v3";
 
 import { BioContainer_settings } from "coral-stream/__generated__/BioContainer_settings.graphql";
 import { BioContainer_viewer } from "coral-stream/__generated__/BioContainer_viewer.graphql";
 
+import RemainingCharactersContainer from "../../Comments/RemainingCharacters";
 import UpdateBioMutation from "./UpdateBioMutation";
+
+import styles from "./BioContainer.css";
 
 interface Props {
   viewer: BioContainer_viewer;
@@ -29,10 +32,11 @@ const BioContainer: FunctionComponent<Props> = ({ viewer, settings }) => {
     void updateBio({ bio: null });
   }, [updateBio]);
   const onSubmit = useCallback(
-    (formData: any) => {
+    async (formData: any) => {
       const { bio } = formData;
       try {
-        void updateBio({ bio });
+        await updateBio({ bio });
+        return;
       } catch (err) {
         if (err instanceof InvalidRequestError) {
           return err.invalidArgs;
@@ -46,69 +50,87 @@ const BioContainer: FunctionComponent<Props> = ({ viewer, settings }) => {
     return null;
   }
   return (
-    <Form onSubmit={onSubmit} initialValues={{ bio: viewer.bio }}>
-      {({
-        handleSubmit,
-        submitting,
-        pristine,
-        invalid,
-        submitError,
-        error,
-      }) => (
-        <form autoComplete="off" onSubmit={handleSubmit} id="bio-form">
-          <HorizontalGutter>
-            <Field
-              name="bio"
-              parse={parseEmptyAsNull}
-              validate={validateMaxLength(MAX_BIO_LENGTH)}
-            >
-              {({ input, meta }) => (
-                <>
-                  <MarkdownEditor
-                    name={input.name}
-                    onChange={input.onChange}
-                    value={input.value}
-                  />
-                  <div>
-                    {meta.error && (
-                      <ValidationMessage
-                        meta={meta}
-                        justifyContent="flex-end"
+    <HorizontalGutter>
+      <HorizontalGutter spacing={1}>
+        <Localized id="profile-bio-title">
+          <h2 className={styles.title}>Bio</h2>
+        </Localized>
+        <Localized id="profile-bio-description">
+          <p className={styles.description}>
+            Write a bio to display publicly on your commenting profile. Must be
+            less than 100 characters.
+          </p>
+        </Localized>
+      </HorizontalGutter>
+      <Form onSubmit={onSubmit} initialValues={{ bio: viewer.bio }}>
+        {({
+          handleSubmit,
+          submitting,
+          pristine,
+          invalid,
+          submitError,
+          error,
+        }) => (
+          <form autoComplete="off" onSubmit={handleSubmit} id="bio-form">
+            <HorizontalGutter>
+              <Field
+                name="bio"
+                parse={parseEmptyAsNull}
+                validate={validateMaxLength(MAX_BIO_LENGTH)}
+              >
+                {({ input, meta }) => (
+                  <>
+                    <MarkdownEditor
+                      name={input.name}
+                      onChange={input.onChange}
+                      value={input.value}
+                    />
+                    <div>
+                      {meta.error && (
+                        <ValidationMessage
+                          meta={meta}
+                          justifyContent="flex-end"
+                        />
+                      )}
+                      <RemainingCharactersContainer
+                        max={MAX_BIO_LENGTH}
+                        value={input.value}
                       />
-                    )}
-                    <Localized
-                      id="profile-bio-maxCharacters"
-                      $maxCharacters={100}
-                    >
-                      <div>Max. 100 Characters</div>
-                    </Localized>
-                  </div>
-                </>
-              )}
-            </Field>
+                    </div>
+                  </>
+                )}
+              </Field>
 
-            {submitError && <CallOut color="error">{submitError}</CallOut>}
-            <Flex itemGutter>
-              <Localized id="profile-bio-remove">
-                <Button
-                  color="secondary"
-                  type="button"
-                  onClick={onRemoveBio}
-                  disabled={submitting || !viewer.bio}
-                >
-                  Remove
-                </Button>
-              </Localized>
-              <Localized id="profile-bio-update">
-                <Button type="submit" disabled={submitting || pristine}>
-                  Update
-                </Button>
-              </Localized>
-            </Flex>
-          </HorizontalGutter>
-        </form>
-      )}
-    </Form>
+              {submitError && <CallOut color="error">{submitError}</CallOut>}
+              <Flex itemGutter className={styles.buttons}>
+                <Localized id="profile-bio-remove">
+                  <Button
+                    upperCase
+                    color="error"
+                    variant="outlined"
+                    type="button"
+                    onClick={onRemoveBio}
+                    disabled={submitting || !viewer.bio}
+                  >
+                    Remove
+                  </Button>
+                </Localized>
+                <Localized id="profile-bio-update">
+                  <Button
+                    upperCase
+                    type="submit"
+                    disabled={submitting || invalid || pristine}
+                  >
+                    Update
+                  </Button>
+                </Localized>
+              </Flex>
+            </HorizontalGutter>
+          </form>
+        )}
+      </Form>
+      <HorizontalRule />
+    </HorizontalGutter>
   );
 };
 
