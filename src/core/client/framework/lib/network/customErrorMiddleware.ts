b0475c05e11f520d3a08a2eebf0cfe88ad487e1e
Chi Vinh Le <vinh@vinh.tech>
2020-09-10 18:48:31 +0200

    chore: better network errors

--- src/core/client/framework/lib/network/customErrorMiddleware.ts
+++ src/core/client/framework/lib/network/customErrorMiddleware.ts
@@ -1,17 +1,35 @@
-import { Middleware } from "react-relay-network-modern/es";
+import { Middleware, RRNLRequestError } from "react-relay-network-modern/es";
 
+import { NetworkOfflineError, RelayNetworkRequestError } from "../errors";
 import extractGraphQLError from "./extractGraphQLError";
 
+function isRRNLRequestError(error: Error): error is RRNLRequestError {
+  return error.name === "RRNLRequestError";
+}
+
 const customErrorMiddleware: Middleware = (next) => async (req) => {
-  const res = await next(req);
-  if (res.errors) {
-    // Extract custom error.
-    const error = extractGraphQLError(res.errors);
-    if (error) {
-      throw error;
+  try {
+    const res = await next(req);
+    if (res.errors) {
+      // Extract custom error.
+      const error = extractGraphQLError(res.errors);
+      if (error) {
+        throw error;
+      }
     }
+    return res;
+  } catch (error) {
+    if (
+      navigator.onLine === false ||
+      (error.name === "TypeError" && error.message === "Failed to fetch")
+    ) {
+      throw new NetworkOfflineError(error);
+    }
+    if (isRRNLRequestError(error)) {
+      throw new RelayNetworkRequestError(error);
+    }
+    throw error;
   }
-  return res;
 };
 
 export default customErrorMiddleware;
