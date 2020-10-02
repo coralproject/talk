d714ce90d46c9629a3e41e87d60c209836dd7c8a
nick-funk <nick.funk@outlook.com>
2020-10-02 10:53:46 -0600

    Complete the existing site moderator role management
    - Site moderators can promote new site moderators
    - Org mods can promote/demote commenters, staff, site mods
    - Admins can do anything they want
    
    CORL-1364

--- src/core/client/admin/components/UserRole/UserRoleChange.tsx
+++ src/core/client/admin/components/UserRole/UserRoleChange.tsx
@@ -2,7 +2,7 @@ import { Localized } from "@fluent/react/compat";
 import React, { FunctionComponent, useCallback, useMemo } from "react";
 
 import { useToggleState } from "coral-framework/hooks";
-import { GQLUSER_ROLE, GQLUSER_ROLE_RL } from "coral-framework/schema";
+import { GQLUSER_ROLE } from "coral-framework/schema";
 import {
   Button,
   ButtonIcon,
@@ -22,24 +22,82 @@ import styles from "./UserRoleChange.css";
 
 interface Props {
   username: string | null;
-  onChangeRole: (role: GQLUSER_ROLE_RL) => Promise<void>;
-  onChangeModerationScopes: (siteIDs: string[]) => Promise<void>;
-  role: GQLUSER_ROLE_RL;
+  onChange: (role: GQLUSER_ROLE, siteIDs?: string[]) => Promise<void>;
+  onPromote: () => Promise<void>;
+  role: GQLUSER_ROLE;
   scoped?: boolean;
   moderationScopes: UserRoleChangeContainer_user["moderationScopes"];
   moderationScopesEnabled?: boolean;
   query: PropTypesOf<typeof SiteModeratorModal>["query"];
-  viewerRole: GQLUSER_ROLE_RL;
+  viewerRole: GQLUSER_ROLE;
   viewerScoped: boolean;
   viewerSites?: string[] | null;
+}
+
+export interface RoleDescription {
+  role: GQLUSER_ROLE;
+  scoped: boolean | undefined;
+}
+
+export function canChangeRole(
+  viewerRole: RoleDescription,
+  existingRole: RoleDescription,
+  newRole: RoleDescription
+) {
+  // admin's can do whatever they want
+  if (viewerRole.role === GQLUSER_ROLE.ADMIN) {
+    return true;
+  }
+
+  // staff and commenters can't do anything
+  if ([GQLUSER_ROLE.STAFF, GQLUSER_ROLE.COMMENTER].includes(viewerRole.role)) {
+    return false;
+  }
+
+  // organization moderators
+  if (viewerRole.role === GQLUSER_ROLE.MODERATOR && !viewerRole.scoped) {
+    // can only update commenters, staff, and site mods
+    if (
+      existingRole.role === GQLUSER_ROLE.ADMIN ||
+      (existingRole.role === GQLUSER_ROLE.MODERATOR && !existingRole.scoped)
+    ) {
+      return false;
+    }
+
+    // cannot upgrade to admin
+    if (newRole.role === GQLUSER_ROLE.ADMIN) {
+      return false;
+    }
+
+    return true;
+  }
+
+  // site moderator
+  if (viewerRole.role === GQLUSER_ROLE.MODERATOR && viewerRole.scoped) {
+    // can only update staff and commenters
+    if (
+      ![GQLUSER_ROLE.STAFF, GQLUSER_ROLE.COMMENTER].includes(existingRole.role)
+    ) {
+      return false;
+    }
+
+    // can only update if the new role is to a site mod
+    if (newRole.role !== GQLUSER_ROLE.MODERATOR || !newRole.scoped) {
+      return false;
+    }
+
+    return true;
+  }
+
+  return false;
 }
 
 const UserRoleChange: FunctionComponent<Props> = ({
   username,
   role,
   scoped,
-  onChangeRole,
-  onChangeModerationScopes,
+  onChange,
+  onPromote,
   moderationScopes,
   moderationScopesEnabled = false,
   query,
@@ -50,31 +108,94 @@ const UserRoleChange: FunctionComponent<Props> = ({
   // Setup state and callbacks for the popover.
   const [
     isPopoverVisible,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setPopoverVisibility,
     togglePopoverVisibility,
   ] = useToggleState();
+
+  const canChangeCommenter = useMemo(
+    () =>
+      canChangeRole(
+        { role: viewerRole, scoped: viewerScoped },
+        { role, scoped },
+        {
+          role: GQLUSER_ROLE.COMMENTER,
+          scoped: false,
+        }
+      ),
+    [role, scoped, viewerRole, viewerScoped]
+  );
+  const canChangeStaff = useMemo(
+    () =>
+      canChangeRole(
+        { role: viewerRole, scoped: viewerScoped },
+        { role, scoped },
+        {
+          role: GQLUSER_ROLE.STAFF,
+          scoped: false,
+        }
+      ),
+    [role, scoped, viewerRole, viewerScoped]
+  );
+  const canChangeSiteMod = useMemo(
+    () =>
+      moderationScopesEnabled &&
+      canChangeRole(
+        { role: viewerRole, scoped: viewerScoped },
+        { role, scoped },
+        {
+          role: GQLUSER_ROLE.MODERATOR,
+          scoped: true,
+        }
+      ),
+    [moderationScopesEnabled, role, scoped, viewerRole, viewerScoped]
+  );
+  const canChangeOrgMod = useMemo(
+    () =>
+      canChangeRole(
+        { role: viewerRole, scoped: viewerScoped },
+        { role, scoped },
+        {
+          role: GQLUSER_ROLE.MODERATOR,
+          scoped: false,
+        }
+      ),
+    [role, scoped, viewerRole, viewerScoped]
+  );
+  const canChangeAdmin = useMemo(
+    () =>
+      canChangeRole(
+        { role: viewerRole, scoped: viewerScoped },
+        { role, scoped },
+        {
+          role: GQLUSER_ROLE.ADMIN,
+          scoped: false,
+        }
+      ),
+    [role, scoped, viewerRole, viewerScoped]
+  );
+  const shouldPromoteSiteMod = useMemo(
+    () => viewerRole === GQLUSER_ROLE.MODERATOR && viewerScoped,
+    [viewerRole, viewerScoped]
+  );
 
   /**
    * handleChangeRole combines the change role function with the change
    * moderation scopes.
    */
   const handleChangeRole = useCallback(
-    async (r: GQLUSER_ROLE_RL, siteIDs: string[] = []) => {
-      await onChangeRole(r);
-
+    async (r: GQLUSER_ROLE, siteIDs: string[] = []) => {
       if (moderationScopesEnabled) {
-        await onChangeModerationScopes(siteIDs);
+        await onChange(r, siteIDs);
+      } else {
+        await onChange(r);
       }
     },
-    [onChangeRole, onChangeModerationScopes, moderationScopesEnabled]
+    [onChange, moderationScopesEnabled]
   );
-  const onClick = useCallback(
-    (r: GQLUSER_ROLE_RL, siteIDs: string[] = []) => async () => {
-      await handleChangeRole(r, siteIDs);
-      togglePopoverVisibility();
-    },
-    [handleChangeRole, togglePopoverVisibility]
-  );
+  const handlePromote = useCallback(async () => {
+    await onPromote();
+  }, [onPromote]);
 
   // Setup state and callbacks for the site moderator modal.
   const [
@@ -98,19 +219,55 @@ const UserRoleChange: FunctionComponent<Props> = ({
     [moderationScopes]
   );
 
-  function canChangeRole(targetRole: GQLUSER_ROLE_RL, unscoped = false) {
-    if (viewerRole === GQLUSER_ROLE.ADMIN) {
-      return true;
+  const MOD_ROLE_NAME = useMemo(() => GQLUSER_ROLE.MODERATOR, []);
+  const onClick = useCallback(
+    (r: GQLUSER_ROLE, siteIDs: string[]) => async () => {
+      // if we're a site mod and we're upgrading someone to site mod
+      // use the simple promotion flow
+      if (
+        r === MOD_ROLE_NAME &&
+        siteIDs &&
+        siteIDs.length > 0 &&
+        viewerRole === MOD_ROLE_NAME &&
+        viewerScoped
+      ) {
+        await handlePromote();
+      } else {
+        await handleChangeRole(r, siteIDs ? siteIDs : []);
+      }
+
+      togglePopoverVisibility();
+    },
+    [
+      MOD_ROLE_NAME,
+      handleChangeRole,
+      handlePromote,
+      togglePopoverVisibility,
+      viewerRole,
+      viewerScoped,
+    ]
+  );
+
+  const onClickChangeSiteMod = useCallback(async () => {
+    // Site mod promote flow
+    if (canChangeSiteMod && shouldPromoteSiteMod) {
+      const sites = viewerSites ? viewerSites : [];
+      await onClick(viewerRole, sites)();
     }
-    if (
-      role !== GQLUSER_ROLE.ADMIN &&
-      targetRole === GQLUSER_ROLE.MODERATOR &&
-      (!unscoped || !viewerScoped)
-    ) {
-      return true;
+    // Other mod/admin site mod selection flow
+    else {
+      setModalVisibility(true);
+      setPopoverVisibility(false);
     }
-    return false;
-  }
+  }, [
+    canChangeSiteMod,
+    onClick,
+    setModalVisibility,
+    setPopoverVisibility,
+    shouldPromoteSiteMod,
+    viewerRole,
+    viewerSites,
+  ]);
 
   return (
     <>
@@ -135,36 +292,32 @@ const UserRoleChange: FunctionComponent<Props> = ({
           body={
             <ClickOutside onClickOutside={togglePopoverVisibility}>
               <Dropdown>
-                {canChangeRole(GQLUSER_ROLE.COMMENTER) && (
+                {canChangeCommenter && (
                   <UserRoleChangeButton
                     active={role === GQLUSER_ROLE.COMMENTER}
                     role={GQLUSER_ROLE.COMMENTER}
                     moderationScopesEnabled={moderationScopesEnabled}
-                    onClick={onClick(GQLUSER_ROLE.COMMENTER)}
+                    onClick={onClick(GQLUSER_ROLE.COMMENTER, [])}
                   />
                 )}
-                {canChangeRole(GQLUSER_ROLE.STAFF) && (
+                {canChangeStaff && (
                   <UserRoleChangeButton
                     active={role === GQLUSER_ROLE.STAFF}
                     role={GQLUSER_ROLE.STAFF}
                     moderationScopesEnabled={moderationScopesEnabled}
-                    onClick={onClick(GQLUSER_ROLE.STAFF)}
+                    onClick={onClick(GQLUSER_ROLE.STAFF, [])}
                   />
                 )}
-                {moderationScopesEnabled &&
-                  canChangeRole(GQLUSER_ROLE.MODERATOR) && (
-                    <UserRoleChangeButton
-                      active={scoped && role === GQLUSER_ROLE.MODERATOR}
-                      role={GQLUSER_ROLE.MODERATOR}
-                      scoped
-                      moderationScopesEnabled
-                      onClick={() => {
-                        setModalVisibility(true);
-                        setPopoverVisibility(false);
-                      }}
-                    />
-                  )}
-                {canChangeRole(GQLUSER_ROLE.MODERATOR, true) && (
+                {canChangeSiteMod && (
+                  <UserRoleChangeButton
+                    active={scoped && role === GQLUSER_ROLE.MODERATOR}
+                    role={GQLUSER_ROLE.MODERATOR}
+                    scoped
+                    moderationScopesEnabled
+                    onClick={onClickChangeSiteMod}
+                  />
+                )}
+                {canChangeOrgMod && (
                   <UserRoleChangeButton
                     active={
                       (!moderationScopesEnabled ||
@@ -173,15 +326,15 @@ const UserRoleChange: FunctionComponent<Props> = ({
                     }
                     role={GQLUSER_ROLE.MODERATOR}
                     moderationScopesEnabled={moderationScopesEnabled}
-                    onClick={onClick(GQLUSER_ROLE.MODERATOR)}
+                    onClick={onClick(GQLUSER_ROLE.MODERATOR, [])}
                   />
                 )}
-                {canChangeRole(GQLUSER_ROLE.ADMIN) && (
+                {canChangeAdmin && (
                   <UserRoleChangeButton
                     active={role === GQLUSER_ROLE.ADMIN}
                     role={GQLUSER_ROLE.ADMIN}
                     moderationScopesEnabled={moderationScopesEnabled}
-                    onClick={onClick(GQLUSER_ROLE.ADMIN)}
+                    onClick={onClick(GQLUSER_ROLE.ADMIN, [])}
                   />
                 )}
               </Dropdown>
