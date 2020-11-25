bd016a88189a0c853ff124c4130bad68aa646324
Chi Vinh Le <vinh@vinh.tech>
2020-11-25 17:01:01 +0100

    fix: restore CommentContainer test

--- src/core/client/stream/tabs/Comments/Comment/CommentContainer.spec.tsx
+++ src/core/client/stream/tabs/Comments/Comment/CommentContainer.spec.tsx
@@ -1,169 +1,175 @@
-it("ignored", () => {
-  expect(true);
+import { EventEmitter2 } from "eventemitter2";
+import { noop } from "lodash";
+import React from "react";
+import { createRenderer } from "react-test-renderer/shallow";
+
+import { pureMerge } from "coral-common/utils";
+import { removeFragmentRefs } from "coral-framework/testHelpers";
+import { DeepPartial, PropTypesOf } from "coral-framework/types";
+
+import { CommentContainer } from "./CommentContainer";
+
+// Context is not possible in shallow renderings.
+// In the future we would avoid doing shallow rendering tests and
+// go straight for integration tests.
+jest.mock("coral-framework/lib/relay/useLocal", () => (fragment: any) => [
+  {
+    featureFlags: [],
+  },
+  () => {},
+]);
+
+// Remove relay refs so we can stub the props.
+const CommentContainerN = removeFragmentRefs(CommentContainer);
+
+type Props = PropTypesOf<typeof CommentContainerN>;
+
+function createDefaultProps(add: DeepPartial<Props> = {}): Props {
+  return pureMerge(
+    {
+      eventEmitter: new EventEmitter2(),
+      viewer: null,
+      story: {
+        url: "http://localhost/story",
+        isClosed: false,
+        canModerate: false,
+        settings: {
+          mode: "COMMENTS",
+        },
+      },
+      comment: {
+        id: "comment-id",
+        status: "NONE",
+        author: {
+          id: "author-id",
+          username: "Marvin",
+          badges: [],
+          avatar: null,
+        },
+        actionCounts: {
+          reaction: {
+            total: 0,
+          },
+        },
+        parent: null,
+        body: "Woof",
+        createdAt: "1995-12-17T03:24:00.000Z",
+        editing: {
+          edited: false,
+          editableUntil: "1995-12-17T03:24:30.000Z",
+        },
+        pending: false,
+        tags: [],
+        lastViewerAction: null,
+        deleted: false,
+        viewerActionPresence: {
+          dontAgree: false,
+          flag: false,
+        },
+      },
+      settings: {
+        disableCommenting: {
+          enabled: false,
+        },
+        featureFlags: [],
+      },
+      indentLevel: 1,
+      showAuthPopup: noop as any,
+      setCommentID: noop as any,
+      localReply: false,
+      disableReplies: false,
+      onRemoveAnswered: undefined,
+      collapsed: undefined,
+      toggleCollapsed: noop as any,
+    },
+    add
+  );
+}
+
+it("renders username and body", () => {
+  const props = createDefaultProps();
+  const renderer = createRenderer();
+  renderer.render(<CommentContainerN {...props} />);
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
 
-// import { EventEmitter2 } from "eventemitter2";
-// import { noop } from "lodash";
-// import React from "react";
-// import { createRenderer } from "react-test-renderer/shallow";
+it("renders body only", () => {
+  const props = createDefaultProps({
+    comment: {
+      author: {
+        username: null,
+      },
+    },
+  });
+  const renderer = createRenderer();
+  renderer.render(<CommentContainerN {...props} />);
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
 
-// import { pureMerge } from "coral-common/utils";
-// import { removeFragmentRefs } from "coral-framework/testHelpers";
-// import { DeepPartial, PropTypesOf } from "coral-framework/types";
+it("hide reply button", () => {
+  const props = createDefaultProps({
+    disableReplies: true,
+  });
+  const renderer = createRenderer();
+  renderer.render(<CommentContainerN {...props} />);
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
 
-// import { CommentContainer } from "./CommentContainer";
+it("shows conversation link", () => {
+  const props = createDefaultProps({
+    showConversationLink: true,
+  });
+  const renderer = createRenderer();
+  renderer.render(<CommentContainerN {...props} />);
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
 
-// // Remove relay refs so we can stub the props.
-// const CommentContainerN = removeFragmentRefs(CommentContainer);
+it("renders in reply to", () => {
+  const props = createDefaultProps({
+    comment: {
+      parent: {
+        author: {
+          username: "ParentAuthor",
+        },
+      },
+    },
+  });
+  const renderer = createRenderer();
+  renderer.render(<CommentContainerN {...props} />);
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
 
-// type Props = PropTypesOf<typeof CommentContainerN>;
+it("renders disabled reply when story is closed", () => {
+  const props = createDefaultProps({
+    story: {
+      isClosed: true,
+    },
+  });
+  const renderer = createRenderer();
+  renderer.render(<CommentContainerN {...props} />);
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
 
-// function createDefaultProps(add: DeepPartial<Props> = {}): Props {
-//   return pureMerge(
-//     {
-//       eventEmitter: new EventEmitter2(),
-//       viewer: null,
-//       story: {
-//         url: "http://localhost/story",
-//         isClosed: false,
-//         canModerate: false,
-//         settings: {
-//           mode: "COMMENTS",
-//         },
-//       },
-//       comment: {
-//         id: "comment-id",
-//         status: "NONE",
-//         author: {
-//           id: "author-id",
-//           username: "Marvin",
-//           badges: [],
-//           avatar: null,
-//         },
-//         actionCounts: {
-//           reaction: {
-//             total: 0,
-//           },
-//         },
-//         parent: null,
-//         body: "Woof",
-//         createdAt: "1995-12-17T03:24:00.000Z",
-//         editing: {
-//           edited: false,
-//           editableUntil: "1995-12-17T03:24:30.000Z",
-//         },
-//         pending: false,
-//         tags: [],
-//         lastViewerAction: null,
-//         deleted: false,
-//         viewerActionPresence: {
-//           dontAgree: false,
-//           flag: false,
-//         },
-//       },
-//       settings: {
-//         disableCommenting: {
-//           enabled: false,
-//         },
-//         featureFlags: [],
-//       },
-//       indentLevel: 1,
-//       showAuthPopup: noop as any,
-//       setCommentID: noop as any,
-//       localReply: false,
-//       disableReplies: false,
-//       onRemoveAnswered: undefined,
-//       collapsed: undefined,
-//       toggleCollapsed: noop as any,
-//     },
-//     add
-//   );
-// }
+it("renders disabled reply when commenting has been disabled", () => {
+  const props = createDefaultProps({
+    settings: {
+      disableCommenting: {
+        enabled: true,
+      },
+    },
+  });
+  const renderer = createRenderer();
+  renderer.render(<CommentContainerN {...props} />);
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
 
-// it("renders username and body", () => {
-//   const props = createDefaultProps();
-//   const renderer = createRenderer();
-//   renderer.render(<CommentContainerN {...props} />);
-//   expect(renderer.getRenderOutput()).toMatchSnapshot();
-// });
-
-// it("renders body only", () => {
-//   const props = createDefaultProps({
-//     comment: {
-//       author: {
-//         username: null,
-//       },
-//     },
-//   });
-//   const renderer = createRenderer();
-//   renderer.render(<CommentContainerN {...props} />);
-//   expect(renderer.getRenderOutput()).toMatchSnapshot();
-// });
-
-// it("hide reply button", () => {
-//   const props = createDefaultProps({
-//     disableReplies: true,
-//   });
-//   const renderer = createRenderer();
-//   renderer.render(<CommentContainerN {...props} />);
-//   expect(renderer.getRenderOutput()).toMatchSnapshot();
-// });
-
-// it("shows conversation link", () => {
-//   const props = createDefaultProps({
-//     showConversationLink: true,
-//   });
-//   const renderer = createRenderer();
-//   renderer.render(<CommentContainerN {...props} />);
-//   expect(renderer.getRenderOutput()).toMatchSnapshot();
-// });
-
-// it("renders in reply to", () => {
-//   const props = createDefaultProps({
-//     comment: {
-//       parent: {
-//         author: {
-//           username: "ParentAuthor",
-//         },
-//       },
-//     },
-//   });
-//   const renderer = createRenderer();
-//   renderer.render(<CommentContainerN {...props} />);
-//   expect(renderer.getRenderOutput()).toMatchSnapshot();
-// });
-
-// it("renders disabled reply when story is closed", () => {
-//   const props = createDefaultProps({
-//     story: {
-//       isClosed: true,
-//     },
-//   });
-//   const renderer = createRenderer();
-//   renderer.render(<CommentContainerN {...props} />);
-//   expect(renderer.getRenderOutput()).toMatchSnapshot();
-// });
-
-// it("renders disabled reply when commenting has been disabled", () => {
-//   const props = createDefaultProps({
-//     settings: {
-//       disableCommenting: {
-//         enabled: true,
-//       },
-//     },
-//   });
-//   const renderer = createRenderer();
-//   renderer.render(<CommentContainerN {...props} />);
-//   expect(renderer.getRenderOutput()).toMatchSnapshot();
-// });
-
-// it("renders with tombstone when comment has been deleted", () => {
-//   const props = createDefaultProps({
-//     comment: {
-//       deleted: true,
-//     },
-//   });
-//   const renderer = createRenderer();
-//   renderer.render(<CommentContainerN {...props} />);
-//   expect(renderer.getRenderOutput()).toMatchSnapshot();
-// });
+it("renders with tombstone when comment has been deleted", () => {
+  const props = createDefaultProps({
+    comment: {
+      deleted: true,
+    },
+  });
+  const renderer = createRenderer();
+  renderer.render(<CommentContainerN {...props} />);
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
