8ad3d4da44b8380fcac03303e90e3d2775365aa2
Vinh <vinh@vinh.tech>
2020-11-27 18:36:42 +0100

    [CORL-1532] GraphQL VS Code Support (#3308)
    * feat: integrate graphql.vscode-graphql extension
    
    * fix: rename server to tenant
    
    * fix: upgrade eveything to graphql-config 3.x
    
    * fix: workaround
    
    * fix: use documents instead of 'include'

--- src/core/common/graphql/loadSchema.ts
+++ src/core/common/graphql/loadSchema.ts
@@ -1,17 +1,16 @@
 import { addResolversToSchema } from "@graphql-tools/schema";
 import { IResolvers, IResolverValidationOptions } from "@graphql-tools/utils";
-import { getGraphQLProjectConfig } from "graphql-config";
+import { loadConfigSync } from "graphql-config";
 
 export default function loadSchema(
   projectName: string,
   resolvers: IResolvers,
   options?: IResolverValidationOptions
 ) {
-  // Load the configuration from the provided `.graphqlconfig` file.
-  const config = getGraphQLProjectConfig(__dirname, projectName);
-
-  // Get the GraphQLSchema from the configuration.
-  const schema = config.getSchema();
+  // Load the configuration from the provided `graphql-config` configuration file.
+  const config = loadConfigSync({});
+  const project = config.getProject(projectName);
+  const schema = project.getSchemaSync();
 
   // Attach the resolvers to the schema.
   return addResolversToSchema(schema, resolvers, options);
