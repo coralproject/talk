dc53ea878fa3c6309a66b1c8a9de40340526d9e1
tessalt <tessathornton@gmail.com>
2020-11-27 08:06:12 -0500

    use store-and-network fetch policy

--- src/core/client/admin/routes/Moderate/Queue/ModerationQueueQuery.tsx
+++ src/core/client/admin/routes/Moderate/Queue/ModerationQueueQuery.tsx
@@ -24,6 +24,7 @@ const ModerationQueueQuery: FunctionComponent<Props> = () => {
         }
       `}
       variables={{}}
+      fetchPolicy="store-and-network"
       render={({ props }) => {
         if (!props) {
           return null;
