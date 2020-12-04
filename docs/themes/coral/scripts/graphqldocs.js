f2f8a931e3dd4cb8b11c7b7b1778de1181a5cb66
tessalt <tessathornton@gmail.com>
2020-12-04 14:43:26 -0500

    add graphql docs for v5

--- docs/themes/coral/scripts/graphqldocs.js
+++ docs/themes/coral/scripts/graphqldocs.js
@@ -9,8 +9,26 @@ const docsScript = fs.readFileSync(
 );
 
 hexo.extend.tag.register('graphqldocs', args => {
-  const filename = path.resolve(hexo.source_dir, args[0]);
-  const introspectionQuery = fs.readFileSync(filename, { encoding: 'utf8' });
+  const schema = args[0];
+  let fetcher = "";
+
+  if (schema.match(/^http[s]?/)) {
+    fetcher = `
+      function fetcher() {
+        return fetch("${schema}").then((result) => result.json()).then((json) => {return {data: json}});
+      }
+    `
+  } else {
+    const filename = path.resolve(hexo.source_dir, schema);
+    const introspectionQuery = fs.readFileSync(filename, { encoding: 'utf8' });
+    fetcher = `
+      function fetcher() {
+        return new Promise(function(resolve) {
+            resolve({"data": ${introspectionQuery}});
+        });
+      }
+    `
+  }
 
   return stripIndent`
     <div id="graphql-docs"></div>
@@ -18,12 +36,8 @@ hexo.extend.tag.register('graphqldocs', args => {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.0.2/react-dom.min.js" integrity="sha256-sqgMIZkGTh7B/tF2nSyXc+tGBYCsfWiTl2II167jrOQ=" crossorigin="anonymous"></script>
     <script>${docsScript}</script>
     <script>
-        function fetcher() {
-            return new Promise(function(resolve) {
-                resolve({"data": ${introspectionQuery}});
-            });
-        }
         const rootElem = document.getElementById('graphql-docs');
+        ${fetcher}
         ReactDOM.render(
             React.createElement(
                 GraphQLDocs.GraphQLDocs,
