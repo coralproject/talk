21e342f60637241196a180fda0d79cb579f28e39
Chi Vinh Le <vinh@vinh.tech>
2020-12-10 16:23:28 +0100

    test: add tests for flatten reply

--- src/core/client/stream/test/comments/stream/showConversation.spec.tsx
+++ src/core/client/stream/test/comments/stream/showConversation.spec.tsx
@@ -1,9 +1,11 @@
-import { ReactTestRenderer } from "react-test-renderer";
 import sinon from "sinon";
 
+import { pureMerge } from "coral-common/utils";
+import { GQLResolver } from "coral-framework/schema";
 import {
   act,
-  createSinonStub,
+  createResolversStub,
+  CreateTestRendererParams,
   waitForElement,
   within,
 } from "coral-framework/testHelpers";
@@ -11,59 +13,81 @@ import {
 import { comments, settings, storyWithDeepestReplies } from "../../fixtures";
 import create from "./create";
 
-let testRenderer: ReactTestRenderer;
-beforeEach(() => {
-  const resolvers = {
-    Query: {
-      story: createSinonStub(
-        (s) => s.throws(),
-        (s) => s.returns(storyWithDeepestReplies)
-      ),
-      stream: createSinonStub(
-        (s) => s.throws(),
-        (s) => s.returns(storyWithDeepestReplies)
-      ),
-      comment: createSinonStub(
-        (s) => s.throws(),
-        (s) =>
-          s
-            .withArgs(undefined, { id: "comment-with-deepest-replies-3" })
-            .returns({
+const createTestRenderer = async (
+  params: CreateTestRendererParams<GQLResolver> = {}
+) => {
+  const { testRenderer, context } = create({
+    ...params,
+    resolvers: pureMerge(
+      createResolversStub<GQLResolver>({
+        Query: {
+          story: () => storyWithDeepestReplies,
+          stream: () => storyWithDeepestReplies,
+          comment: ({ variables }) => {
+            expectAndFail(variables.id).toBe("comment-with-deepest-replies-3");
+            return {
               ...comments[0],
               id: "comment-with-deepest-replies-3",
-            })
-      ),
-      settings: sinon.stub().returns(settings),
-    },
-  };
-
-  ({ testRenderer } = create({
-    // Set this to true, to see graphql responses.
-    logNetwork: false,
-    resolvers,
-    initLocalState: (localRecord) => {
+            };
+          },
+          settings: () => settings,
+        },
+      }),
+      params.resolvers
+    ),
+    initLocalState: (localRecord, source, environment) => {
       localRecord.setValue(storyWithDeepestReplies.id, "storyID");
+      if (params.initLocalState) {
+        params.initLocalState(localRecord, source, environment);
+      }
     },
-  }));
-});
+  });
+
+  const streamLog = await act(
+    async () =>
+      await waitForElement(() =>
+        within(testRenderer.root).getByTestID("comments-allComments-log")
+      )
+  );
+
+  return {
+    testRenderer,
+    context,
+    streamLog,
+  };
+};
 
 it("renders deepest comment with link", async () => {
-  const streamLog = await waitForElement(() =>
-    within(testRenderer.root).getByTestID("comments-allComments-log")
-  );
+  const { streamLog } = await createTestRenderer();
   const deepestReply = within(streamLog).getByTestID(
     "comment-comment-with-deepest-replies-3"
   );
+  within(streamLog).getByText("Read More of this Conversation", {
+    exact: false,
+  });
   expect(within(deepestReply).toJSON()).toMatchSnapshot();
+});
+
+describe("flatten replies", () => {
+  it("doesn't renders deepest comment with link", async () => {
+    const { streamLog } = await createTestRenderer({
+      initLocalState: (local) => {
+        local.setValue(true, "flattenReplies");
+      },
+    });
+    expect(() =>
+      within(streamLog).getByText("Read More of this Conversation", {
+        exact: false,
+      })
+    ).toThrow();
+  });
 });
 
 it("shows conversation", async () => {
   const mockEvent = {
     preventDefault: sinon.mock().once(),
   };
-  const streamLog = await waitForElement(() =>
-    within(testRenderer.root).getByTestID("comments-allComments-log")
-  );
+  const { streamLog, testRenderer } = await createTestRenderer();
   await act(async () => {
     within(streamLog)
       .getByText("Read More of this Conversation", { exact: false })
