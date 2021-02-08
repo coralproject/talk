5d304fcc751b2328ba3a3fd79876dad74ca1fd1b
nick-funk <nick.funk@outlook.com>
2021-02-08 09:36:33 -0700

    Auto focus email field in sign in and sign up modal's

--- src/core/client/auth/components/EmailField.tsx
+++ src/core/client/auth/components/EmailField.tsx
@@ -1,5 +1,5 @@
 import { Localized } from "@fluent/react/compat";
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect } from "react";
 import { Field } from "react-final-form";
 
 import { streamColorFromMeta } from "coral-framework/lib/form";
@@ -13,33 +13,49 @@ import { ValidationMessage } from "coral-ui/components/v3";
 
 interface Props {
   disabled: boolean;
+  focus?: boolean;
 }
 
-const EmailField: FunctionComponent<Props> = (props) => (
-  <Field name="email" validate={composeValidators(required, validateEmail)}>
-    {({ input, meta }) => (
-      <FormField>
-        <Localized id="general-emailAddressLabel">
-          <InputLabel htmlFor={input.name}>Email Address</InputLabel>
-        </Localized>
-        <Localized
-          id="general-emailAddressTextField"
-          attrs={{ placeholder: true }}
-        >
-          <TextField
-            {...input}
-            id={input.name}
-            placeholder="Email Address"
-            type="email"
-            color={streamColorFromMeta(meta)}
-            disabled={props.disabled}
-            fullWidth
-          />
-        </Localized>
-        <ValidationMessage meta={meta} />
-      </FormField>
-    )}
-  </Field>
-);
+const EmailField: FunctionComponent<Props> = (props) => {
+  let inputRef: HTMLInputElement | null = null;
+  useEffect(() => {
+    if (props.focus) {
+      // eslint-disable-next-line no-unused-expressions
+      inputRef?.focus();
+    }
+  }, [inputRef, props.focus]);
+
+  return (
+    <Field name="email" validate={composeValidators(required, validateEmail)}>
+      {({ input, meta }) => (
+        <FormField>
+          <Localized id="general-emailAddressLabel">
+            <InputLabel htmlFor={input.name}>Email Address</InputLabel>
+          </Localized>
+          <Localized
+            id="general-emailAddressTextField"
+            attrs={{ placeholder: true }}
+          >
+            <TextField
+              {...input}
+              id={input.name}
+              placeholder="Email Address"
+              type="email"
+              color={streamColorFromMeta(meta)}
+              disabled={props.disabled}
+              fullWidth
+              ref={(field) => {
+                if (props.focus) {
+                  inputRef = field;
+                }
+              }}
+            />
+          </Localized>
+          <ValidationMessage meta={meta} />
+        </FormField>
+      )}
+    </Field>
+  );
+};
 
 export default EmailField;
