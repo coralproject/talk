b3c9069cfc91d1ae346bfa68182bf700a4eaa150
Wyatt Johnson <accounts+github@wyattjoh.ca>
2021-02-22 16:02:32 -0700

    fix: css + template tweaks

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,44 +1,48 @@
+import Link from "next/link";
+import PageHeader from "../components/PageHeader";
+
 import DocumentationLayout from "../layouts/DocumentationLayout";
 
 export default function Index() {
   // FIXME: implement
   return (
     <DocumentationLayout title="Coral Documentation" currentPagePath="/">
-      <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
-        <h1 className="text-2xl font-bold my-2">Getting started with Coral</h1>
-        <p>
-          Online comments are broken. Our open-source commenting platform,
-          Coral, reimagines moderation, comment display, and conversation. Use
-          Coral to add smarter, safer discussions to your site without giving
-          away your data.
-        </p>
-        <ul className="list-disc text-blue-400 pl-4 my-2">
-          <li>
-            <a
-              className="text-blue-500 font-md font-bold"
-              href="/docs/installation"
-            >
+      <PageHeader title="Getting started with Coral" />
+      <p>
+        Online comments are broken. Our open-source commenting platform, Coral,
+        reimagines moderation, comment display, and conversation. Use Coral to
+        add smarter, safer discussions to your site without giving away your
+        data.
+      </p>
+      <ul className="list-disc pl-4 my-2">
+        <li>
+          <Link href="/installation">
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a className="underline text-coral hover:text-coral-dark">
               Install Coral on your server
             </a>
-          </li>
-          <li>
-            <a
-              className="text-blue-500 font-md font-bold"
-              href="/docs/development"
-            >
+          </Link>
+        </li>
+        <li>
+          <Link href="/development">
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a className="underline text-coral hover:text-coral-dark">
               Run Coral locally
             </a>
-          </li>
-          <li>
+          </Link>
+        </li>
+        <li>
+          <Link href="https://coralproject.net/pricing/">
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
             <a
-              className="text-blue-500 font-md font-bold"
-              href="https://coralproject.net/pricing/"
+              className="underline text-coral hover:text-coral-dark"
+              target="_blank"
             >
               Contact us about hosting and support
             </a>
-          </li>
-        </ul>
-      </div>
+          </Link>
+        </li>
+      </ul>
     </DocumentationLayout>
   );
 }
