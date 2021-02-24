f8b8ffdab5dca76458488c4e6f661e42ecc87b2c
Chi Vinh Le <vinh@vinh.tech>
2021-02-24 20:29:25 +0100

    fix: polyfill object.assign for newer version of eventemitter2

--- src/core/client/embed/index.ts
+++ src/core/client/embed/index.ts
@@ -1,8 +1,1 @@
-import polyfillNodeListForEach from "coral-framework/helpers/polyfillNodeListForEach";
-
-// Include minimal polyfills for the intersection observer.
-import "intersection-observer";
-
-polyfillNodeListForEach();
-
 export * from "./Coral";
