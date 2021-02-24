1e55a037f103feeab476ec9d940d1c439505784b
nick-funk <nick.funk@outlook.com>
2021-02-24 15:42:19 -0700

    Preliminarily hook up comments to conversation view

--- src/core/client/stream/tabs/Live/LiveCommentReply/LiveCommentRepliesQuery.tsx
+++ src/core/client/stream/tabs/Live/LiveCommentReply/LiveCommentRepliesQuery.tsx
@@ -1,11 +1,38 @@
 import React, { FunctionComponent } from "react";
+import { graphql } from "react-relay";
+
+import { QueryRenderer } from "coral-framework/lib/relay";
+
+// import { LiveCommentRepliesQuery } from "coral-stream/__generated__/LiveCommentRepliesQuery.graphql";
+
+import LiveCommentRepliesContainer from "./LiveCommentRepliesContainer";
 
 interface Props {
   commentID: string;
 }
 
 const LiveCommentRepliesQuery: FunctionComponent<Props> = ({ commentID }) => {
-  return <div>replies for {commentID}</div>;
+  return (
+    <QueryRenderer<any>
+      query={graphql`
+        query LiveCommentRepliesQuery($commentID: ID!) {
+          comment(id: $commentID) {
+            ...LiveCommentRepliesContainer_comment
+          }
+        }
+      `}
+      variables={{
+        commentID,
+      }}
+      render={(data) => {
+        if (!data || !data.props || !data.props.comment) {
+          return null;
+        }
+
+        return <LiveCommentRepliesContainer comment={data.props.comment} />;
+      }}
+    />
+  );
 };
 
 export default LiveCommentRepliesQuery;
