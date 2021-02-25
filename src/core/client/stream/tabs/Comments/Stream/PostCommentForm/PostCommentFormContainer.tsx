59bde1d8b9f16428e49d2bf9bf50d9afde020ded
Chi Vinh Le <vinh@vinh.tech>
2021-02-25 17:41:48 +0100

    fix: keep session storage during login

--- src/core/client/stream/tabs/Comments/Stream/PostCommentForm/PostCommentFormContainer.tsx
+++ src/core/client/stream/tabs/Comments/Stream/PostCommentForm/PostCommentFormContainer.tsx
@@ -10,7 +10,7 @@ import React, {
 import { graphql } from "react-relay";
 
 import { ERROR_CODES } from "coral-common/errors";
-import { usePersistedLocalState } from "coral-framework/hooks";
+import { usePersistedSessionState } from "coral-framework/hooks";
 import {
   InvalidRequestError,
   ModerationNudgeError,
@@ -92,10 +92,10 @@ export const PostCommentFormContainer: FunctionComponent<Props> = ({
   const [nudge, setNudge] = useState(true);
   const [submitStatus, setSubmitStatus] = useState<SubmitStatus | null>(null);
 
-  const [draft = "", setDraft, initialDraft] = usePersistedLocalState<string>(
+  const [draft = "", setDraft, initialDraft] = usePersistedSessionState<string>(
     "PostCommentFormContainer:draft"
   );
-  const [toggle, setToggle] = usePersistedLocalState<Toggle>(
+  const [toggle, setToggle] = usePersistedSessionState<Toggle>(
     "PostCommentFormContainer:toggle"
   );
 
