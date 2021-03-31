e6673e03afa02732b4b49ae8dd2177304fdc3bfe
nick-funk <nick.funk@outlook.com>
2021-03-31 11:48:27 -0600

    Add resolver for CommentEditedPayload

---
+++ src/core/server/graph/resolvers/CommentEditedPayload.ts
@@ -0,0 +1,9 @@
+import { GQLCommentEditedPayloadTypeResolver } from "coral-server/graph/schema/__generated__/types";
+
+import { maybeLoadOnlyID } from "./Comment";
+import { CommentEditedInput } from "./Subscription/commentEdited";
+
+export const CommentEditedPayload: GQLCommentEditedPayloadTypeResolver<CommentEditedInput> = {
+  comment: ({ commentID }, args, ctx, info) =>
+    maybeLoadOnlyID(ctx, info, commentID),
+};
