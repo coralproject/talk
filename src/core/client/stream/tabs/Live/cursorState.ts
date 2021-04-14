33540871aee51a61289d1c0523c028c5456630f9
nick-funk <nick.funk@outlook.com>
2021-04-14 14:55:04 -0600

    Store cursor state in relay
    - currentCursor depicts the current "NEW" dividing cursor
    we see in our live chat until we reload or jump around the
    stream
    - latestCursor stores the latest cursor we have actively
    read to in the stream. This allows us to always access both
    the current new marker we're showing as well as how far the
    viewer has traversed in the stream past that point.
    
    The latestCursor is persisted to both the local relay but
    also the localStorage, so when the stream is reloaded, that
    will become the new currentCursor.
    
    Similarly, when the user jumps around in the stream via a
    refresh or jump to live, the currentCursor is set along with
    the pagination state set in the LiveTabQuery.

--- src/core/client/stream/tabs/Live/cursorState.ts
+++ src/core/client/stream/tabs/Live/cursorState.ts
@@ -1,4 +1,40 @@
+import { DeepPartial } from "coral-common/types";
+import { PromisifiedStorage } from "coral-framework/lib/storage";
+import { RecordProxy } from "relay-runtime";
+
+type AdvancedUpdater = (record: RecordProxy) => void;
+type LocalUpdater = DeepPartial<any> | AdvancedUpdater;
+
 export default interface CursorState {
   cursor: string;
   createdAt: string;
 }
+
+export const persistCursorToLocalStorage = async (
+  localStorage: PromisifiedStorage,
+  storyID: string | null,
+  storyURL: string | null,
+  state: CursorState
+) => {
+  // Set the constant updating cursor
+  const key = `liveCursor:${storyID}:${storyURL}`;
+  await localStorage.setItem(key, JSON.stringify(state));
+};
+
+export const persistLatestCursorToRelay = async (
+  setLocal: LocalUpdater,
+  state: CursorState
+) => {
+  setLocal({ liveChat: { latestCursor: state.cursor } });
+};
+
+export const persistCursor = async (
+  localStorage: PromisifiedStorage,
+  setLocal: LocalUpdater,
+  storyID: string | null,
+  storyURL: string | null,
+  state: CursorState
+) => {
+  await persistCursorToLocalStorage(localStorage, storyID, storyURL, state);
+  await persistLatestCursorToRelay(setLocal, state);
+};
