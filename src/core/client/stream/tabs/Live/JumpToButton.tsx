40713e4fd37db653c4e946538411f4966dd01038
nick-funk <nick.funk@outlook.com>
2021-04-14 09:09:11 -0600

    Refactor live chat jump buttons into re-usable component

--- src/core/client/stream/tabs/Live/JumpToButton.tsx
+++ src/core/client/stream/tabs/Live/JumpToButton.tsx
@@ -1,6 +1,7 @@
+import cn from "classnames";
 import React, { FunctionComponent } from "react";
 
-import { Flex } from "coral-ui/components/v2";
+import { Flex, Icon } from "coral-ui/components/v2";
 import { Button } from "coral-ui/components/v3";
 
 import styles from "./JumpToButton.css";
@@ -8,9 +9,15 @@ import styles from "./JumpToButton.css";
 interface Props {
   onClick: () => void;
   children: string | React.ReactNode;
+
+  onCancel?: () => void;
 }
 
-const JumpToButton: FunctionComponent<Props> = ({ onClick, children }) => {
+const JumpToButton: FunctionComponent<Props> = ({
+  onClick,
+  children,
+  onCancel,
+}) => {
   return (
     <div className={styles.jumpToContainer}>
       <Flex justifyContent="center" alignItems="center">
@@ -18,10 +25,23 @@ const JumpToButton: FunctionComponent<Props> = ({ onClick, children }) => {
           <Button
             onClick={onClick}
             color="primary"
-            className={styles.jumpButton}
+            className={cn({
+              [styles.jumpButton]: !onCancel,
+              [styles.jumpToWithCancel]: onCancel,
+            })}
           >
             {children}
           </Button>
+          {onCancel && (
+            <Button
+              onClick={onCancel}
+              color="primary"
+              aria-valuetext="close"
+              className={styles.jumpToCancelButton}
+            >
+              <Icon>close</Icon>
+            </Button>
+          )}
         </Flex>
       </Flex>
     </div>
