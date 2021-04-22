4f178689b410a5400b25207d399fdf02bca80c4d
parth364 <82989426+parth364@users.noreply.github.com>
2021-04-21 22:44:26 -0500

    When a canonical link attribute is not included, ensure that inferred story_url is encoded and capitalized properly

--- src/core/client/framework/helpers/resolveStoryURL.ts
+++ src/core/client/framework/helpers/resolveStoryURL.ts
@@ -13,7 +13,18 @@ function resolveStoryURL() {
     "This page does not include a canonical link tag. Coral has inferred this story_url from the window object. Query params have been stripped, which may cause a single thread to be present across multiple pages."
   );
 
-  return getLocationOrigin() + window.location.pathname;
+  // ensure that urls are encoded properly and lowercase
+  let pathname = window.location.pathname
+  // if the pathname is already encoded, decode it
+  // this keeps percent escapes uppercase
+  if(pathname !== decodeURIComponent(pathname || '')){
+    pathname = decodeURI(pathname)
+  }
+  // first make the path lowercase and then encode it again
+  pathname = pathname.toLowerCase()
+  pathname = encodeURI(pathname)
+
+  return getLocationOrigin() + pathname;
 }
 
 export default resolveStoryURL;
