8c962f34419b9f2231b743e81067050b491eb76e
Nick Funk <nick.funk@outlook.com>
2021-05-05 15:23:19 -0600

    [CORL-1755] Live Chat: "Go to start" scroll fix (#3536)
    * Add options for scrolling to footer when setting cursor
    
    This allows us to conditionally scroll the chat footer into
    view depending on where we're jumping in the stream.
    
    Prior to the "go to start" functionality, always scrolling the
    footer into view after setting the cursor was correct, but
    when jumping to the start of the stream, we don't want to do this
    and doing so can cause use to scroll away from the iframe if
    the scroll bubbles up into the main page.
    
    This allows us to do both, by default, setting a cursor will make
    sure the footer is visible, but for other stream operations we
    can conditionally tell it not to do so.
    
    * fix: remove scrollIntoView when setting cursor
    
    * fix: min-height 650px and cleanups
    
    Co-authored-by: Chi Vinh Le <vinh@vinh.tech>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- src/core/client/stream/tabs/Live/LiveTabQuery.tsx
+++ src/core/client/stream/tabs/Live/LiveTabQuery.tsx
@@ -183,26 +183,16 @@ const LiveTabQuery: FunctionComponent = () => {
             inclusiveAfter: true,
             inclusiveBefore: false,
           });
-
-          window.requestAnimationFrame(() => {
-            const el = document.getElementById("live-chat-footer");
-            if (el) {
-              el.scrollIntoView();
-            }
-          });
         };
 
         return (
-          <>
-            <LiveStreamContainer
-              story={data.props.story}
-              viewer={data.props.viewer}
-              settings={data.props.settings}
-              cursor={paginationState.cursor}
-              setCursor={deleteConnectionsAndSetCursor}
-            />
-            <div id="live-chat-footer"></div>
-          </>
+          <LiveStreamContainer
+            story={data.props.story}
+            viewer={data.props.viewer}
+            settings={data.props.settings}
+            cursor={paginationState.cursor}
+            setCursor={deleteConnectionsAndSetCursor}
+          />
         );
       }}
     />
