d7c38a426317bb6d4663b1f2c66584f524cb17a7
Vinh <vinh@vinh.tech>
2021-05-20 13:20:50 +0200

    [CORL-1671, CORL-1686] Comments tab is now called reviews in RR mode (#3563)
    * feat: comments tab is now called reviews in rr mode
    
    * fix: snapshots
    
    * fix: add back aria-label
    
    * fix: bring back aria-label part 2

--- src/core/client/framework/lib/i18n/index.ts
+++ src/core/client/framework/lib/i18n/index.ts
@@ -2,3 +2,4 @@ export { default as generateBundles } from "./generateBundles";
 export { BundledLocales, LoadableLocales, LocalesData } from "./locales";
 export { default as getMessage } from "./getMessage";
 export { default as withGetMessage, GetMessage } from "./withGetMessage";
+export { default as useGetMessage } from "./useGetMessage";
