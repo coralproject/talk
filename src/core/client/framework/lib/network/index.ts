cf373ff0fe8ef27dc57908098cc60e406b9935a6
Vinh <vinh@vinh.tech>
2021-05-27 15:55:15 +0200

    [CORL-1722] implement useOnlineStatus and useSubscriptionConnectionStatus (#3568)
    * feat: implement useOnlineStatus and useSubscriptionConnectionStatus
    
    * fix: linting
    
    * chore: add some comments
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- src/core/client/framework/lib/network/index.ts
+++ src/core/client/framework/lib/network/index.ts
@@ -4,5 +4,9 @@ export { default as extractError } from "./extractError";
 export {
   default as createManagedSubscriptionClient,
   ManagedSubscriptionClient,
+  CONNECTION_STATUS,
+  ConnectionStatusListenerCallback,
 } from "./createManagedSubscriptionClient";
 export { default as assertOnline } from "./assertOnline";
+export { default as useSubscriptionConnectionStatus } from "./useSubscriptionConnectionStatus";
+export { default as useOnlineStatus } from "./useOnlineStatus";
