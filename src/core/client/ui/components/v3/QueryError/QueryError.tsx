98ee2578e53746bb8fd9f421f405972c86af66ff
nick-funk <nick.funk@outlook.com>
2021-07-08 09:56:39 -0600

    Simplify QueryError and always return nice CallOut

--- src/core/client/ui/components/v3/QueryError/QueryError.tsx
+++ src/core/client/ui/components/v3/QueryError/QueryError.tsx
@@ -11,7 +11,12 @@ interface Props {
   error: Error | null;
 }
 
-const render = (traceID: string, message: string) => {
+const QueryError: FunctionComponent<Props> = ({ error }) => {
+  if (!error) {
+    return null;
+  }
+
+  const traceID = error instanceof TraceableError ? error.traceID : "";
   return (
     <CallOut
       color="error"
@@ -21,36 +26,20 @@ const render = (traceID: string, message: string) => {
         </Localized>
       }
     >
-      <>
-        <Localized id="common-error-traceID">
-          <div className={styles.heading}>Trace ID</div>
-        </Localized>
-        <div className={styles.section}>{traceID}</div>
-        <Localized id="common-error-message">
-          <div className={styles.heading}>Message</div>
-        </Localized>
-        <div className={styles.section}>{message}</div>
-      </>
+      {traceID && (
+        <>
+          <Localized id="common-error-traceID">
+            <div className={styles.heading}>Trace ID</div>
+          </Localized>
+          <div className={styles.section}>{traceID}</div>
+        </>
+      )}
+      <Localized id="common-error-message">
+        <div className={styles.heading}>Message</div>
+      </Localized>
+      <div className={styles.section}>{error.message}</div>
     </CallOut>
   );
-};
-
-const QueryError: FunctionComponent<Props> = ({ error }) => {
-  if (!error) {
-    return null;
-  }
-
-  try {
-    const traceID = error instanceof TraceableError ? error.traceID : "";
-
-    if (traceID) {
-      return render(traceID, error.message);
-    } else {
-      return <div>{error.message}</div>;
-    }
-  } catch {
-    return <div>{error.message}</div>;
-  }
 };
 
 export default QueryError;
