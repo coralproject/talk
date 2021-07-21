86af4a8f893ec82e091865c1536eb8cd00aa2f86
Chi Vinh Le <vinh@vinh.tech>
2021-07-21 18:27:11 +0200

    fix: remaining merge conflicts

--- src/core/client/framework/lib/browserInfo.ts
+++ src/core/client/framework/lib/browserInfo.ts
@@ -26,34 +26,32 @@ export function supportsCSSVars(window: Window): boolean {
  * feature support results.
  */
 export function getBrowserInfo(window: Window): BrowserInfo {
-  if (!browserInfo) {
-    const browser = Bowser.getParser(window.navigator.userAgent);
-    const ios = browser.is(Bowser.OS_MAP.iOS);
-    const macos = browser.is(Bowser.OS_MAP.MacOS);
-    const msie = browser.is(Bowser.BROWSER_MAP.ie);
-    const mobile = browser.is(Bowser.PLATFORMS_MAP.mobile);
-    const version = Number.parseFloat(browser.getBrowserVersion());
-    browserInfo = {
-      version,
-      supports: {
-        intl: typeof Intl !== "undefined",
-        intlPluralRules:
-          typeof Intl !== "undefined" && Boolean(Intl.PluralRules),
-        proxyObject: Boolean(window.Proxy),
-        cssVariables: window.CSS && CSS.supports("color", "var(--fake-var)"),
-        fetch: Boolean(window.fetch),
-        intersectionObserver:
-          "IntersectionObserver" in window &&
-          "IntersectionObserverEntry" in window &&
-          "intersectionRatio" in
-            (window as any).IntersectionObserverEntry.prototype,
-      },
-      ios,
-      msie,
-      mobile,
-      macos,
-    };
-  }
+  const browser = Bowser.getParser(window.navigator.userAgent);
+  const ios = browser.is(Bowser.OS_MAP.iOS);
+  const macos = browser.is(Bowser.OS_MAP.MacOS);
+  const msie = browser.is(Bowser.BROWSER_MAP.ie);
+  const mobile = browser.is(Bowser.PLATFORMS_MAP.mobile);
+  const version = Number.parseFloat(browser.getBrowserVersion());
+  const browserInfo = {
+    version,
+    supports: {
+      intl: typeof Intl !== "undefined",
+      intlPluralRules: typeof Intl !== "undefined" && Boolean(Intl.PluralRules),
+      proxyObject: Boolean((window as any).Proxy),
+      cssVariables:
+        (window as any).CSS && CSS.supports("color", "var(--fake-var)"),
+      fetch: Boolean(window.fetch),
+      intersectionObserver:
+        "IntersectionObserver" in window &&
+        "IntersectionObserverEntry" in window &&
+        "intersectionRatio" in
+          (window as any).IntersectionObserverEntry.prototype,
+    },
+    ios,
+    msie,
+    mobile,
+    macos,
+  };
 
   return browserInfo;
 }
