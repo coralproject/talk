9d3a24d862f6e7cd65dd2f7cc2fa7a523f4ec062
Marcus Haddon <marcus.haddon@voxmedia.com>
2021-09-14 14:27:41 -0700

    ignore dirty when displaying form field errors

--- src/core/client/framework/lib/form/helpers.tsx
+++ src/core/client/framework/lib/form/helpers.tsx
@@ -97,15 +97,11 @@ export const formatStringList = (v: string[] | null) => {
 
 export type FieldMeta = Pick<
   FieldRenderProps<any, HTMLElement>["meta"],
-  "error" | "submitError" | "dirty" | "submitFailed"
+  "error" | "submitError" | "submitFailed"
 >;
 
-export const hasError = ({
-  error,
-  submitError,
-  dirty,
-  submitFailed,
-}: FieldMeta) => (dirty || submitFailed) && (error || submitError);
+export const hasError = ({ error, submitError, submitFailed }: FieldMeta) =>
+  submitFailed && (error || submitError);
 
 export const colorFromMeta = (meta: FieldMeta) =>
   hasError(meta) ? "error" : "regular";
