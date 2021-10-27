724b955798318eb5495243ec4868463be39b0bf5
Marcus Haddon <haddon.marcus@gmail.com>
2021-10-27 11:48:23 -0700

    corl-2216-dont-submit-invalid-external-image (#3669)
    * process onWillPaste even if external media set
    
    * Revert "process onWillPaste even if external media set"
    
    This reverts commit 7e4e3a111a0dbb2d7b5004c5be9b1b6b820b3b1b.
    
    * dont submit external img url unless valid
    
    * remove comment
    dont call setState while rendering externalImgInput
    
    * reduce nesting, remove unused import
    
    * remove unused import, fix indentation
    
    * use FinalForm api
    fix linting
    
    * dont create unmanaged input
    
    * use ValidationMessage from framework
    
    * fix: small improvements
    
    * feat: enter submits the form
    
    * chore: sort imports
    
    Co-authored-by: Marcus Haddon <marcus.haddon@voxmedia.com>
    Co-authored-by: Chi Vinh Le <vinh@vinh.tech>

--- src/core/client/stream/tabs/Comments/helpers/getMediaValidators.ts
+++ src/core/client/stream/tabs/Comments/helpers/getMediaValidators.ts
@@ -10,6 +10,15 @@ const hasExternalMediaAttached: Condition = (value, values) => {
   return !!values.media && values.media.type === "external";
 };
 
+// TODO: this is more a const variable.
+export function getImageValidators() {
+  return composeValidators(
+    (v, values) => validateURL(v, values),
+    (v, values) => validateImageURL(v, values)
+  );
+}
+
+// TODO: this is more a const variable.
 export default function getMediaFieldValidators() {
   return validateWhen(
     hasExternalMediaAttached,
