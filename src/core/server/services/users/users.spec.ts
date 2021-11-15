337f05201cda2a9aebc612edf42f72cbea0d0cfe
Marcus Haddon <haddon.marcus@gmail.com>
2021-11-15 10:44:49 -0800

    WIP fix mocks

--- src/core/server/services/users/users.spec.ts
+++ src/core/server/services/users/users.spec.ts
@@ -1,21 +1,19 @@
-/* eslint-disable */
-import { updateUserBan } from "./users";
+jest.mock("../../models/user");
 
-// TODO: move these to fixtures dir
 import {
+  createMockMailer,
+  createMockMongoContex,
+  createMockRejector,
   createSiteFixture,
   createTenantFixture,
   createUserFixture,
-  createMockMongoContex,
-  createMockMailer,
-  createMockRejector,
 } from "coral-server/test/fixtures";
+import { updateUserBan } from "./users";
+
 import { GQLUSER_ROLE } from "coral-server/graph/schema/__generated__/types";
 
 describe("updateUserBan", () => {
-  afterEach(jest.clearAllMocks);
-  // MARCUS: does this need to be in a beforeEach?
-  jest.mock("coral-server/models/user");
+  // afterEach(jest.clearAllMocks);
 
   const mailer = createMockMailer();
   const rejector = createMockRejector();
@@ -28,77 +26,89 @@ describe("updateUserBan", () => {
   it("rejects updates by non mod users", async () => {
     const commentor = createUserFixture({
       tenantID: tenant.id,
-      role: GQLUSER_ROLE.COMMENTER
+      role: GQLUSER_ROLE.COMMENTER,
     });
-    await expect(async () => await updateUserBan(
-      mongo,
-      mailer,
-      rejector,
-      tenant,
-      commentor,
-      badUser.id,
-      "Test message",
-      false,
-      [siteA.id],
-    )).rejects.toThrow(); // TODO: assert correct error
+    await expect(
+      async () =>
+        await updateUserBan(
+          mongo,
+          mailer,
+          rejector,
+          tenant,
+          commentor,
+          badUser.id,
+          "Test message",
+          false,
+          [siteA.id]
+        )
+    ).rejects.toThrow(); // TODO: assert correct error
 
     const staff = createUserFixture({
       tenantID: tenant.id,
-      role: GQLUSER_ROLE.STAFF
+      role: GQLUSER_ROLE.STAFF,
     });
 
-    await expect(async () => await updateUserBan(
-      mongo,
-      mailer,
-      rejector,
-      tenant,
-      staff,
-      badUser.id,
-      "Test message",
-      false,
-      [siteA.id],
-    )).rejects.toThrow(); // TODO: assert correct error
+    await expect(
+      async () =>
+        await updateUserBan(
+          mongo,
+          mailer,
+          rejector,
+          tenant,
+          staff,
+          badUser.id,
+          "Test message",
+          false,
+          [siteA.id]
+        )
+    ).rejects.toThrow(); // TODO: assert correct error
   });
 
   it("rejects updates out of mods scope", async () => {
     const siteAMod = createUserFixture({
       tenantID: tenant.id,
       moderationScopes: {
-        siteIDs: [siteA.id]
-      }
+        siteIDs: [siteA.id],
+      },
     });
 
-    await expect(async () => await updateUserBan(
-      mongo,
-      mailer,
-      rejector,
-      tenant,
-      siteAMod,
-      badUser.id,
-      "Test message",
-      false,
-      [siteB.id],
-    )).rejects.toThrow(); // TODO: assert correct error
+    await expect(
+      async () =>
+        await updateUserBan(
+          mongo,
+          mailer,
+          rejector,
+          tenant,
+          siteAMod,
+          badUser.id,
+          "Test message",
+          false,
+          [siteB.id]
+        )
+    ).rejects.toThrow(); // TODO: assert correct error
   });
 
   it("rejects if banIds and unbanIds are overlapping", async () => {
     const orgMod = createUserFixture({
       tenantID: tenant.id,
-      role: GQLUSER_ROLE.MODERATOR
+      role: GQLUSER_ROLE.MODERATOR,
     });
 
-    await expect(async () => await updateUserBan(
-      mongo,
-      mailer,
-      rejector,
-      tenant,
-      orgMod,
-      badUser.id,
-      "Test message",
-      false,
-      [siteA.id, siteB.id],
-      [siteA.id]
-    )).rejects.toThrow(); // TODO: assert correct error
+    await expect(
+      async () =>
+        await updateUserBan(
+          mongo,
+          mailer,
+          rejector,
+          tenant,
+          orgMod,
+          badUser.id,
+          "Test message",
+          false,
+          [siteA.id, siteB.id],
+          [siteA.id]
+        )
+    ).rejects.toThrow(); // TODO: assert correct error
   });
 
   it("skips already banned sites", async () => {
@@ -106,11 +116,12 @@ describe("updateUserBan", () => {
       tenantID: tenant.id,
       status: {
         ban: {
-          siteIDs: [siteA.id]
-        }
-      }
+          siteIDs: [siteA.id],
+        },
+      },
     });
 
+    /* eslint-disable-next-line */
     require("coral-server/models/user").retrieveUser.mockResolvedValue(bannedOnSiteA);
 
     const admin = createUserFixture({
@@ -127,11 +138,14 @@ describe("updateUserBan", () => {
       bannedOnSiteA.id,
       "Test message",
       false,
-      [siteA.id],
+      [siteA.id]
     );
 
     expect(res.id).toEqual(bannedOnSiteA.id);
-    expect(require("coral-server/models/user").siteBanUser).toHaveBeenCalledTimes(0);
+    expect(
+      /* eslint-disable-next-line */
+      require("coral-server/models/user").siteBanUser
+    ).toHaveBeenCalledTimes(0);
     expect(mailer.add).toHaveBeenCalledTimes(0); // MARCUS: not getting the mocks, hmm
-  })
+  });
 });
