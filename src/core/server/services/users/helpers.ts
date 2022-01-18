0caa77b8ab814a94eb4c29bd632d4f0c1ba46267
Kathryn Beaty <kathryn.beaty@voxmedia.com>
2022-01-18 10:48:33 -0500

    update naming

--- src/core/server/services/users/helpers.ts
+++ src/core/server/services/users/helpers.ts
@@ -94,10 +94,13 @@ export function checkForNewUserEmailDomainModeration(
   if (userEmail && emailDomainModeration) {
     const userEmailDomain = userEmail.substring(userEmail.indexOf("@") + 1);
     const matchingEmailDomain = emailDomainModeration.find((d) => {
-      const domainIndex = userEmailDomain.indexOf(d.domain);
-      if (domainIndex !== -1) {
+      const domainMatchIndex = userEmailDomain.indexOf(d.domain);
+      if (domainMatchIndex !== -1) {
         // either matches the userEmailDomain or is a subdomain of it
-        return domainIndex === 0 || userEmailDomain[domainIndex - 1] === ".";
+        return (
+          domainMatchIndex === 0 ||
+          userEmailDomain[domainMatchIndex - 1] === "."
+        );
       }
       return false;
     });
