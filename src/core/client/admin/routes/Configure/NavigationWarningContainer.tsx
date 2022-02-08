8347db590a614757ae461bf26b60522d82cd7e3e
Kathryn Beaty <kathryn.beaty@voxmedia.com>
2022-02-08 09:25:11 -0500

    refactor to useGetMessage

--- src/core/client/admin/routes/Configure/NavigationWarningContainer.tsx
+++ src/core/client/admin/routes/Configure/NavigationWarningContainer.tsx
@@ -1,8 +1,7 @@
 import { useRouter } from "found";
 import { FunctionComponent, useEffect } from "react";
 
-import { useCoralContext } from "coral-framework/lib/bootstrap";
-import { getMessage } from "coral-framework/lib/i18n";
+import { useGetMessage } from "coral-framework/lib/i18n";
 
 interface Props {
   active: boolean;
@@ -10,9 +9,8 @@ interface Props {
 
 const NavigationWarningContainer: FunctionComponent<Props> = ({ active }) => {
   const { router } = useRouter();
-  const { localeBundles } = useCoralContext();
+  const getMessage = useGetMessage();
   const warningMessage = getMessage(
-    localeBundles,
     "configure-unsavedInputWarning",
     "You have unsaved changes. Are you sure you want to continue?"
   );
