445513d2bd771139e72953ad9bb1ba5103d9bd02
Kathryn Beaty <kathryn.beaty@voxmedia.com>
2022-02-08 15:16:41 -0500

    refactor AuthCheckRoute

--- src/core/client/admin/routes/AuthCheck/AuthCheckRoute.tsx
+++ src/core/client/admin/routes/AuthCheck/AuthCheckRoute.tsx
@@ -1,11 +1,11 @@
-import { Match, Router, withRouter } from "found";
-import React from "react";
+import { useRouter } from "found";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import { graphql } from "react-relay";
 
 import { SetRedirectPathMutation } from "coral-admin/mutations";
 import { AbilityType, can } from "coral-admin/permissions";
 import { roleIsAtLeast } from "coral-framework/helpers";
-import { MutationProp, withMutation } from "coral-framework/lib/relay";
+import { useMutation } from "coral-framework/lib/relay";
 import { withRouteConfig } from "coral-framework/lib/router";
 import { GQLUSER_ROLE } from "coral-framework/schema";
 
@@ -15,9 +15,6 @@ import NetworkError from "./NetworkError";
 import RestrictedContainer from "./RestrictedContainer";
 
 interface Props {
-  match: Match;
-  router: Router;
-  setRedirectPath: MutationProp<typeof SetRedirectPathMutation>;
   data: AuthCheckRouteQueryResponse;
   error: Error | null;
 }
@@ -33,32 +30,38 @@ type CheckParams =
     };
 
 function createAuthCheckRoute(check: CheckParams) {
-  class AuthCheckRoute extends React.Component<Props> {
-    private wasLoggedIn = false;
-    constructor(props: Props) {
-      super(props);
-      void this.redirectIfNotLoggedIn();
-    }
+  const AuthCheckRoute: FunctionComponent<Props> = ({
+    data,
+    error,
+    children,
+  }) => {
+    const { router, match } = useRouter();
+    const [wasLoggedIn, setWasLoggedIn] = useState(false);
+    const setRedirectPathMutation = useMutation(SetRedirectPathMutation);
 
-    public UNSAFE_componentWillReceiveProps(nextProps: Props) {
-      if (nextProps.data && nextProps.data.viewer) {
-        this.wasLoggedIn = true;
-      }
-      void this.redirectIfNotLoggedIn(nextProps, this.props);
-      if (nextProps.data && !nextProps.data.viewer) {
-        this.wasLoggedIn = false;
-      }
-    }
+    useEffect(() => {
+      void redirectIfNotLoggedIn();
+    }, []);
 
-    private shouldRedirectTo(props: Props = this.props) {
-      if (!props.data || (props.data.viewer && props.data.viewer.email)) {
+    useEffect(() => {
+      if (data && data.viewer) {
+        setWasLoggedIn(true);
+      }
+      void redirectIfNotLoggedIn();
+      if (data && !data.viewer) {
+        setWasLoggedIn(false);
+      }
+    }, [data]);
+
+    function shouldRedirectTo() {
+      if (!data || (data.viewer && data.viewer.email)) {
         return false;
       }
       return true;
     }
 
-    private hasAccess(props: Props = this.props) {
-      const { viewer } = props.data;
+    function hasAccess() {
+      const { viewer } = data;
       if (viewer) {
         if (
           (check.role && !roleIsAtLeast(viewer.role, check.role)) ||
@@ -71,39 +74,34 @@ function createAuthCheckRoute(check: CheckParams) {
       return false;
     }
 
-    private async redirectIfNotLoggedIn(
-      props: Props = this.props,
-      prevProps: Props | null = null
-    ) {
-      if (!this.shouldRedirectTo(props)) {
+    async function redirectIfNotLoggedIn() {
+      if (!shouldRedirectTo()) {
         return;
       }
       // If I was previously logged in then logged out, we don't need to set the redirect path.
-      if (!this.wasLoggedIn) {
-        const location = props.match.location;
-        await props.setRedirectPath({
+      if (!wasLoggedIn) {
+        const location = match.location;
+        await setRedirectPathMutation({
           path: location.pathname + location.search + location.hash,
         });
       }
-      props.router.replace("/admin/login");
+      router.replace("/admin/login");
     }
 
-    public render() {
-      if (this.props.error) {
-        return <NetworkError />;
-      }
-
-      if (!this.props.data || this.shouldRedirectTo()) {
-        return null;
-      }
-
-      if (this.hasAccess()) {
-        return this.props.children;
-      }
-
-      return <RestrictedContainer viewer={this.props.data.viewer} />;
+    if (error) {
+      return <NetworkError />;
     }
-  }
+
+    if (!data || shouldRedirectTo()) {
+      return null;
+    }
+
+    if (hasAccess()) {
+      return <>{children}</>;
+    }
+
+    return <RestrictedContainer viewer={data.viewer} />;
+  };
 
   const enhanced = withRouteConfig<Props>({
     query: graphql`
@@ -133,7 +131,7 @@ function createAuthCheckRoute(check: CheckParams) {
         }
       }
     `,
-  })(withRouter(withMutation(SetRedirectPathMutation)(AuthCheckRoute)));
+  })(AuthCheckRoute);
 
   return enhanced;
 }
