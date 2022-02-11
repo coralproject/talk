2f194dce122956dd3f85de4296a347b51c549556
Kathryn Beaty <kathryn.beaty@voxmedia.com>
2022-02-11 09:47:20 -0500

    refactor LoginRoute

--- src/core/client/admin/routes/Login/LoginRoute.tsx
+++ src/core/client/admin/routes/Login/LoginRoute.tsx
@@ -1,44 +1,35 @@
-import { RouteProps } from "found";
-import React, { Component } from "react";
+import React, { FunctionComponent } from "react";
 import { graphql } from "react-relay";
 
-import { withLocalStateContainer } from "coral-framework/lib/relay";
+import { useLocal } from "coral-framework/lib/relay";
 import { withRouteConfig } from "coral-framework/lib/router";
 
-import { LoginRouteLocal as LocalData } from "coral-admin/__generated__/LoginRouteLocal.graphql";
+import { LoginRouteLocal } from "coral-admin/__generated__/LoginRouteLocal.graphql";
 import { LoginRouteQueryResponse } from "coral-admin/__generated__/LoginRouteQuery.graphql";
 
 import AccountCompletionContainer from "./AccountCompletionContainer";
 import Login from "./Login";
 
 interface Props {
-  local: LocalData;
   data: LoginRouteQueryResponse;
-  error?: Error | null;
 }
 
-class LoginRoute extends Component<Props> {
-  public static routeConfig: RouteProps;
-
-  public render() {
-    if (!this.props.data) {
-      return null;
+const LoginRoute: FunctionComponent<Props> = ({ data }) => {
+  const [{ authView }] = useLocal<LoginRouteLocal>(graphql`
+    fragment LoginRouteLocal on Local {
+      authView
     }
-
-    return (
-      <AccountCompletionContainer
-        auth={this.props.data.settings.auth}
-        viewer={this.props.data.viewer}
-      >
-        <Login
-          auth={this.props.data.settings.auth}
-          view={this.props.local.authView!}
-          viewer={this.props.data.viewer}
-        />
-      </AccountCompletionContainer>
-    );
+  `);
+  if (!data) {
+    return null;
   }
-}
+
+  return (
+    <AccountCompletionContainer auth={data.settings.auth} viewer={data.viewer}>
+      <Login auth={data.settings.auth} view={authView!} viewer={data.viewer} />
+    </AccountCompletionContainer>
+  );
+};
 
 const enhanced = withRouteConfig<LoginRouteQueryResponse>({
   query: graphql`
@@ -55,14 +46,6 @@ const enhanced = withRouteConfig<LoginRouteQueryResponse>({
       }
     }
   `,
-})(
-  withLocalStateContainer(
-    graphql`
-      fragment LoginRouteLocal on Local {
-        authView
-      }
-    `
-  )(LoginRoute)
-);
+})(LoginRoute);
 
 export default enhanced;
