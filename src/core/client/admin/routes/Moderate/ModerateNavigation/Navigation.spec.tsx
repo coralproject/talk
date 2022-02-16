06f5fea662b26ed26123db860a3cd03ac337e78e
Kathryn Beaty <kathryn.beaty@voxmedia.com>
2022-02-16 15:02:15 -0500

    add createMockRouter to testHelpers

--- src/core/client/admin/routes/Moderate/ModerateNavigation/Navigation.spec.tsx
+++ src/core/client/admin/routes/Moderate/ModerateNavigation/Navigation.spec.tsx
@@ -1,52 +1,15 @@
 import React from "react";
 import { createRenderer } from "react-test-renderer/shallow";
 
+import { createMockRouter } from "coral-framework/testHelpers";
 import { PropTypesOf } from "coral-framework/types";
 
 import Navigation from "./Navigation";
 
+const mockRouter = createMockRouter();
 jest.mock("found/useRouter", () => {
   return function useRouter() {
-    return {
-      router: {
-        addNavigationListener: jest.fn(),
-        createHref: jest.fn(),
-        createLocation: jest.fn(),
-        go: jest.fn(),
-        isActive: jest.fn(),
-        matcher: {
-          match: jest.fn(),
-          getRoutes: jest.fn(),
-          isActive: jest.fn(),
-          format: jest.fn(),
-        },
-        push: jest.fn(),
-        replace: jest.fn(),
-        replaceRouterConfig: jest.fn(),
-      },
-      match: {
-        context: undefined,
-        location: {
-          action: "",
-          delta: 0,
-          hash: "",
-          index: 0,
-          key: "",
-          pathname: "",
-          query: {},
-          search: "",
-          state: undefined,
-        },
-        params: {
-          org: "",
-          view: "",
-        },
-        route: {},
-        routeIndices: [],
-        routeParams: null,
-        routes: [],
-      },
-    };
+    return mockRouter;
   };
 });
 
