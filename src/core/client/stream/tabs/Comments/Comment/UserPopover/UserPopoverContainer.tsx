fe6c453d515436706d68bfb2c05e9b4f48aa6848
nick-funk <nick.funk@outlook.com>
2022-02-25 10:30:07 -0700

    Refactor UserPopover to use useFragment

--- src/core/client/stream/tabs/Comments/Comment/UserPopover/UserPopoverContainer.tsx
+++ src/core/client/stream/tabs/Comments/Comment/UserPopover/UserPopoverContainer.tsx
@@ -4,15 +4,14 @@ import React, {
   useEffect,
   useState,
 } from "react";
-import { graphql } from "react-relay";
+import { graphql, useFragment } from "react-relay";
 
 import { useViewerEvent } from "coral-framework/lib/events";
-import { withFragmentContainer } from "coral-framework/lib/relay";
 import { ShowUserPopoverEvent } from "coral-stream/events";
 
-import { UserPopoverContainer_settings$data as UserPopoverContainer_settings } from "coral-stream/__generated__/UserPopoverContainer_settings.graphql";
-import { UserPopoverContainer_user$data as UserData } from "coral-stream/__generated__/UserPopoverContainer_user.graphql";
-import { UserPopoverContainer_viewer$data as ViewerData } from "coral-stream/__generated__/UserPopoverContainer_viewer.graphql";
+import { UserPopoverContainer_settings$key } from "coral-stream/__generated__/UserPopoverContainer_settings.graphql";
+import { UserPopoverContainer_user$key } from "coral-stream/__generated__/UserPopoverContainer_user.graphql";
+import { UserPopoverContainer_viewer$key } from "coral-stream/__generated__/UserPopoverContainer_viewer.graphql";
 
 import UserIgnorePopoverContainer from "../UserIgnorePopover/UserIgnorePopoverContainer";
 import UserPopoverOverviewContainer from "./UserPopoverOverviewContainer";
@@ -21,9 +20,9 @@ type View = "OVERVIEW" | "IGNORE";
 
 interface Props {
   onDismiss: () => void;
-  user: UserData;
-  viewer: ViewerData | null;
-  settings: UserPopoverContainer_settings;
+  user: UserPopoverContainer_user$key;
+  viewer: UserPopoverContainer_viewer$key | null;
+  settings: UserPopoverContainer_settings$key;
 }
 
 export const UserPopoverContainer: FunctionComponent<Props> = ({
@@ -32,46 +31,53 @@ export const UserPopoverContainer: FunctionComponent<Props> = ({
   onDismiss,
   settings,
 }) => {
+  const userData = useFragment(
+    graphql`
+      fragment UserPopoverContainer_user on User {
+        id
+        ...UserPopoverOverviewContainer_user
+        ...UserIgnorePopoverContainer_user
+      }
+    `,
+    user
+  );
+  const viewerData = useFragment(
+    graphql`
+      fragment UserPopoverContainer_viewer on User {
+        ...UserPopoverOverviewContainer_viewer
+      }
+    `,
+    viewer
+  );
+  const settingsData = useFragment(
+    graphql`
+      fragment UserPopoverContainer_settings on Settings {
+        ...UserPopoverOverviewContainer_settings
+      }
+    `,
+    settings
+  );
+
   const emitShowUserPopover = useViewerEvent(ShowUserPopoverEvent);
   useEffect(() => {
-    emitShowUserPopover({ userID: user.id });
-  }, []);
+    emitShowUserPopover({ userID: userData.id });
+  }, [emitShowUserPopover, userData.id]);
   const [view, setView] = useState<View>("OVERVIEW");
   const onIgnore = useCallback(() => setView("IGNORE"), [setView]);
   return (
     <div>
       {view === "OVERVIEW" ? (
         <UserPopoverOverviewContainer
-          user={user}
-          viewer={viewer}
-          settings={settings}
+          user={userData}
+          viewer={viewerData}
+          settings={settingsData}
           onIgnore={onIgnore}
         />
       ) : (
-        <UserIgnorePopoverContainer user={user} onDismiss={onDismiss} />
+        <UserIgnorePopoverContainer user={userData} onDismiss={onDismiss} />
       )}
     </div>
   );
 };
 
-const enhanced = withFragmentContainer<Props>({
-  viewer: graphql`
-    fragment UserPopoverContainer_viewer on User {
-      ...UserPopoverOverviewContainer_viewer
-    }
-  `,
-  user: graphql`
-    fragment UserPopoverContainer_user on User {
-      id
-      ...UserPopoverOverviewContainer_user
-      ...UserIgnorePopoverContainer_user
-    }
-  `,
-  settings: graphql`
-    fragment UserPopoverContainer_settings on Settings {
-      ...UserPopoverOverviewContainer_settings
-    }
-  `,
-})(UserPopoverContainer);
-
-export default enhanced;
+export default UserPopoverContainer;
