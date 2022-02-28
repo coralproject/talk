5c8969fd3eea2ad433d1035a09421098957ff8cc
nick-funk <nick.funk@outlook.com>
2022-02-28 15:10:50 -0700

    Update AppContainer/UserBoxContainer to useFragment

--- src/core/client/auth/App/AppContainer.tsx
+++ src/core/client/auth/App/AppContainer.tsx
@@ -1,10 +1,8 @@
 import React, { FunctionComponent } from "react";
-import { graphql } from "react-relay";
+import { graphql, useFragment } from "react-relay";
 
-import { withFragmentContainer } from "coral-framework/lib/relay";
-
-import { AppContainer_auth as AuthData } from "coral-auth/__generated__/AppContainer_auth.graphql";
-import { AppContainer_viewer as UserData } from "coral-auth/__generated__/AppContainer_viewer.graphql";
+import { AppContainer_auth$key as AuthData } from "coral-auth/__generated__/AppContainer_auth.graphql";
+import { AppContainer_viewer$key as UserData } from "coral-auth/__generated__/AppContainer_viewer.graphql";
 
 import AccountCompletionContainer from "./AccountCompletion";
 import App, { View } from "./App";
@@ -16,35 +14,40 @@ interface Props {
 }
 
 const AppContainer: FunctionComponent<Props> = ({ auth, viewer, view }) => {
+  const authData = useFragment(
+    graphql`
+      fragment AppContainer_auth on Auth {
+        ...SignInContainer_auth
+        ...SignUpContainer_auth
+        ...AccountCompletionContainer_auth
+      }
+    `,
+    auth
+  );
+
+  const viewerData = useFragment(
+    graphql`
+      fragment AppContainer_viewer on User {
+        ...AccountCompletionContainer_viewer
+        ...ForgotPasswordContainer_viewer
+        ...LinkAccountContainer_viewer
+      }
+    `,
+    viewer
+  );
+
   // If we're dealing with a password reset, we can't possibly worry about
   // account completion (because they are not logged in, or have already
   // completed their account), so disregard here, and just return the App.
   if (view === "FORGOT_PASSWORD") {
-    return <App view={view} auth={auth} viewer={viewer} />;
+    return <App view={view} auth={authData} viewer={viewerData} />;
   }
 
   return (
-    <AccountCompletionContainer auth={auth} viewer={viewer}>
-      <App view={view} auth={auth} viewer={viewer} />
+    <AccountCompletionContainer auth={authData} viewer={viewerData}>
+      <App view={view} auth={authData} viewer={viewerData} />
     </AccountCompletionContainer>
   );
 };
 
-const enhanced = withFragmentContainer<Props>({
-  auth: graphql`
-    fragment AppContainer_auth on Auth {
-      ...SignInContainer_auth
-      ...SignUpContainer_auth
-      ...AccountCompletionContainer_auth
-    }
-  `,
-  viewer: graphql`
-    fragment AppContainer_viewer on User {
-      ...AccountCompletionContainer_viewer
-      ...ForgotPasswordContainer_viewer
-      ...LinkAccountContainer_viewer
-    }
-  `,
-})(AppContainer);
-
-export default enhanced;
+export default AppContainer;
--- src/core/client/stream/common/UserBox/UserBoxContainer.tsx
+++ src/core/client/stream/common/UserBox/UserBoxContainer.tsx
@@ -1,16 +1,11 @@
 import React, { FunctionComponent } from "react";
-import { graphql } from "react-relay";
+import { graphql, useFragment } from "react-relay";
 
-import {
-  MutationProp,
-  useLocal,
-  useMutation,
-  withFragmentContainer,
-} from "coral-framework/lib/relay";
+import { MutationProp, useLocal, useMutation } from "coral-framework/lib/relay";
 import { SignOutMutation } from "coral-stream/mutations";
 
-import { UserBoxContainer_settings as SettingsData } from "coral-stream/__generated__/UserBoxContainer_settings.graphql";
-import { UserBoxContainer_viewer as ViewerData } from "coral-stream/__generated__/UserBoxContainer_viewer.graphql";
+import { UserBoxContainer_settings$key as SettingsData } from "coral-stream/__generated__/UserBoxContainer_settings.graphql";
+import { UserBoxContainer_viewer$key as ViewerData } from "coral-stream/__generated__/UserBoxContainer_viewer.graphql";
 import { UserBoxContainerLocal } from "coral-stream/__generated__/UserBoxContainerLocal.graphql";
 
 import { supportsRegister, weControlAuth } from "../authControl";
@@ -41,6 +36,22 @@ export const UserBoxContainer: FunctionComponent<Props> = ({
       accessTokenExp
     }
   `);
+  const viewerData = useFragment(
+    graphql`
+      fragment UserBoxContainer_viewer on User {
+        username
+      }
+    `,
+    viewer
+  );
+  const settingsData = useFragment(
+    graphql`
+      fragment UserBoxContainer_settings on Settings {
+        ...authControl_settings @relay(mask: false)
+      }
+    `,
+    settings
+  );
   const signOut = useMutation(SignOutMutation);
   const handleSignIn = () => showAuthPopup({ view: "SIGN_IN" });
   const handleRegister = () => showAuthPopup({ view: "SIGN_UP" });
@@ -49,17 +60,17 @@ export const UserBoxContainer: FunctionComponent<Props> = ({
     !accessToken || (accessTokenJTI !== null && accessTokenExp !== null)
   );
 
-  if (viewer) {
+  if (viewerData) {
     return (
       <UserBoxAuthenticated
         onSignOut={handleSignOut}
-        username={viewer.username!}
+        username={viewerData.username ?? ""}
         showLogoutButton={supportsLogout}
       />
     );
   }
 
-  if (!weControlAuth(settings)) {
+  if (!weControlAuth(settingsData)) {
     return null;
   }
 
@@ -68,26 +79,15 @@ export const UserBoxContainer: FunctionComponent<Props> = ({
       <AuthPopup />
       <UserBoxUnauthenticated
         onSignIn={handleSignIn}
-        onRegister={(supportsRegister(settings) && handleRegister) || undefined}
-        showRegisterButton={supportsRegister(settings)}
+        onRegister={
+          (supportsRegister(settingsData) && handleRegister) || undefined
+        }
+        showRegisterButton={supportsRegister(settingsData)}
       />
     </>
   );
 };
 
-const enhanced = withShowAuthPopupMutation(
-  withFragmentContainer<Props>({
-    viewer: graphql`
-      fragment UserBoxContainer_viewer on User {
-        username
-      }
-    `,
-    settings: graphql`
-      fragment UserBoxContainer_settings on Settings {
-        ...authControl_settings @relay(mask: false)
-      }
-    `,
-  })(UserBoxContainer)
-);
+const enhanced = withShowAuthPopupMutation(UserBoxContainer);
 
 export default enhanced;
