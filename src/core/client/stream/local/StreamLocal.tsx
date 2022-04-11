2874f81e2ba3b2290c52535bb5c5664fba39cea8
nick-funk <nick.funk@outlook.com>
2022-04-11 15:44:25 -0600

    Add featureFlags and flattenReplies to StreamLocal

--- src/core/client/stream/local/StreamLocal.tsx
+++ src/core/client/stream/local/StreamLocal.tsx
@@ -1,11 +1,51 @@
 import React, { FunctionComponent, useState } from "react";
+import { Environment, fetchQuery, graphql } from "relay-runtime";
 
+import { StaticConfig } from "coral-common/config";
 import { parseQuery } from "coral-common/utils";
 import { CoralContext } from "coral-framework/lib/bootstrap";
+import getStaticConfig from "coral-framework/lib/getStaticConfig";
 import { TabValue } from "coral-stream/App/App";
 import { COMMENTS_ORDER_BY } from "coral-stream/constants";
 
 import { GQLCOMMENT_SORT } from "coral-framework/schema/__generated__/types";
+import { StreamLocalQuery } from "coral-stream/__generated__/StreamLocalQuery.graphql";
+
+interface ResolvedConfig {
+  readonly featureFlags: string[];
+  readonly flattenReplies?: boolean | null;
+}
+
+async function resolveConfig(
+  environment: Environment,
+  staticConfig?: StaticConfig | null
+): Promise<ResolvedConfig> {
+  if (staticConfig) {
+    return staticConfig as ResolvedConfig;
+  }
+  if (process.env.NODE_ENV === "development") {
+    // Send a graphql query to server during development to get the needed settings.
+    // The reason is that we don't have static config during development.
+    const data = await fetchQuery<StreamLocalQuery>(
+      environment,
+      graphql`
+        query StreamLocalQuery {
+          settings {
+            flattenReplies
+            featureFlags
+          }
+        }
+      `,
+      {}
+    );
+
+    return data.settings as ResolvedConfig;
+  }
+  return {
+    featureFlags: [],
+    flattenReplies: false,
+  };
+}
 
 interface StreamLocalValue {
   storyID: string | undefined;
@@ -28,17 +68,28 @@ interface StreamLocalValue {
 
   profileTab: string;
   setProfileTab: React.Dispatch<React.SetStateAction<string>>;
+
+  flattenReplies: boolean;
+  setFlattenReplies: React.Dispatch<React.SetStateAction<boolean>>;
+
+  featureFlags: string[];
+  setFeatureFlags: React.Dispatch<React.SetStateAction<string[]>>;
 }
 
 const StreamLocalContext = React.createContext<StreamLocalValue>({} as any);
 
-export const useStreamLocal = () => React.useContext(StreamLocalContext);
-
 const createContext = async (context: CoralContext) => {
-  let commentsOrder: GQLCOMMENT_SORT = GQLCOMMENT_SORT.CREATED_AT_DESC;
+  const staticConfig = getStaticConfig(context.window);
+
+  const { featureFlags, flattenReplies } = await resolveConfig(
+    context.relayEnvironment,
+    staticConfig
+  );
+
+  let commentsOrderBy: GQLCOMMENT_SORT = GQLCOMMENT_SORT.CREATED_AT_DESC;
   const stringOrderBy = await context.localStorage.getItem(COMMENTS_ORDER_BY);
   if (stringOrderBy) {
-    commentsOrder = (typeof GQLCOMMENT_SORT as any)[stringOrderBy];
+    commentsOrderBy = (typeof GQLCOMMENT_SORT as any)[stringOrderBy];
   }
 
   const StreamLocal: FunctionComponent = ({ children }) => {
@@ -55,12 +106,20 @@ const createContext = async (context: CoralContext) => {
       query.commentID
     );
 
-    const [commentsOrderBy, setCommentsOrderBy] = useState<GQLCOMMENT_SORT>(
-      commentsOrder
-    );
+    const [commentsOrderByVal, setCommentsOrderByVal] = useState<
+      GQLCOMMENT_SORT
+    >(commentsOrderBy);
 
     const [activeTab, setActiveTab] = useState<TabValue>("COMMENTS");
     const [profileTab, setProfileTab] = useState<string>("MY_COMMENTS");
+
+    const [flattenRepliesVal, setFlattenRepliesVal] = useState<boolean>(
+      !!flattenReplies
+    );
+
+    const [featureFlagsVal, setFeatureFlagsVal] = useState<string[]>(
+      featureFlags ?? []
+    );
 
     const value: StreamLocalValue = {
       storyID,
@@ -71,12 +130,16 @@ const createContext = async (context: CoralContext) => {
       setStoryMode,
       commentID,
       setCommentID,
-      commentsOrderBy,
-      setCommentsOrderBy,
+      commentsOrderBy: commentsOrderByVal,
+      setCommentsOrderBy: setCommentsOrderByVal,
       activeTab,
       setActiveTab,
       profileTab,
       setProfileTab,
+      flattenReplies: flattenRepliesVal,
+      setFlattenReplies: setFlattenRepliesVal,
+      featureFlags: featureFlagsVal,
+      setFeatureFlags: setFeatureFlagsVal,
     };
 
     return (
@@ -88,5 +151,7 @@ const createContext = async (context: CoralContext) => {
 
   return StreamLocal;
 };
+
+export const useStreamLocal = () => React.useContext(StreamLocalContext);
 
 export default createContext;
