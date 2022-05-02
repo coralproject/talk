449f4d573284e7f72d99601a13fca9c213362008
nick-funk <nick.funk@outlook.com>
2022-05-02 16:25:25 -0600

    Show expand button on Tweets in moderation cards

--- src/core/client/admin/components/MediaContainer/TwitterMedia.tsx
+++ src/core/client/admin/components/MediaContainer/TwitterMedia.tsx
@@ -8,15 +8,23 @@ interface Props {
   id: string;
   url: string;
   siteID: string;
+  wasToggled?: boolean;
 }
 
-const TwitterMedia: FunctionComponent<Props> = ({ id, url, siteID }) => {
+const TwitterMedia: FunctionComponent<Props> = ({
+  id,
+  url,
+  siteID,
+  wasToggled,
+}) => {
   const component = encodeURIComponent(url);
   return (
     <div className={styles.embed}>
       <Frame
         id={id}
         src={`/api/oembed?type=twitter&url=${component}&siteID=${siteID}`}
+        wasToggled={wasToggled}
+        type="twitter"
       />
     </div>
   );
