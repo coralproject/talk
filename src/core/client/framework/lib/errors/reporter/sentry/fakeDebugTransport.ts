0f831a57221f2a76d67778bde02e462773e37341
nick-funk <nick.funk@outlook.com>
2022-05-05 10:44:19 -0600

    Use new sentry event status type

--- src/core/client/framework/lib/errors/reporter/sentry/fakeDebugTransport.ts
+++ src/core/client/framework/lib/errors/reporter/sentry/fakeDebugTransport.ts
@@ -1,7 +1,7 @@
 import {
   Event as SentryEvent,
+  EventStatus,
   Response as SentryResponse,
-  Status,
   Transport,
 } from "@sentry/types";
 
@@ -13,7 +13,14 @@ export class FakeDebugTransport implements Transport {
     // eslint-disable-next-line no-console
     console.debug("sentry event", event);
     return Promise.resolve({
-      status: Status.Success,
+      // This type keeps changing with each new version of Sentry,
+      // so I'm casting this to ensure we're checking this status
+      // value is correct. Previously, it was a `Status.Success` strongly
+      // typed Enum, now it's just a generic TS `type`, so we have
+      // to cast the string to get any real type information. This
+      // will ensure this throws an error if it changes and we can check the
+      // type value when that happens to make sure it's accurate.
+      status: "success" as EventStatus,
     });
   }
   public close() {
