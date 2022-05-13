32ede159c402d6d8ba66c9ebb241b54a7a6deb48
nick-funk <nick.funk@outlook.com>
2022-05-13 10:17:20 -0600

    Remove marking seen on pagination/loading replies
    We will only mark seen using the `markCommentsAsSeen` mutation

--- src/core/server/graph/loaders/Comments.ts
+++ src/core/server/graph/loaders/Comments.ts
@@ -323,13 +323,6 @@ export default (ctx: GraphContext) => ({
       story.isArchived
     ).then(primeCommentsFromConnection(ctx));
 
-    if (ctx.user) {
-      // Append comments to seenComments update list.
-      // These will be set after the GraphQL request has completed.
-      const commentIDs = connection.nodes.map((n) => n.id);
-      ctx.seenComments.insertMany(ctx.user.id, storyID, commentIDs);
-    }
-
     return connection;
   },
   forParent: async (
@@ -357,13 +350,6 @@ export default (ctx: GraphContext) => ({
       },
       story.isArchived
     ).then(primeCommentsFromConnection(ctx));
-
-    if (ctx.user) {
-      // Append comments to seenComments update list.
-      // These will be set after the GraphQL request has completed.
-      const commentIDs = connection.nodes.map((n) => n.id);
-      ctx.seenComments.insertMany(ctx.user.id, storyID, commentIDs);
-    }
 
     return connection;
   },
