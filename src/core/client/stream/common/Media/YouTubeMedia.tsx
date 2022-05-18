74430cb5cbee7a9f08b735c844ad77918de63107
nick-funk <nick.funk@outlook.com>
2022-05-18 13:15:05 -0600

    Make youtube embeds full width

--- src/core/client/stream/common/Media/YouTubeMedia.tsx
+++ src/core/client/stream/common/Media/YouTubeMedia.tsx
@@ -2,6 +2,8 @@ import React, { FunctionComponent } from "react";
 
 import Frame from "coral-framework/components/Frame";
 
+import styles from "./YouTubeMedia.css";
+
 interface Props {
   id?: string;
   url: string;
@@ -11,10 +13,13 @@ interface Props {
 const YouTubeMedia: FunctionComponent<Props> = ({ id, url, siteID }) => {
   const component = encodeURIComponent(url);
   return (
-    <Frame
-      id={id}
-      src={`/api/oembed?type=youtube&url=${component}&siteID=${siteID}`}
-    />
+    <div className={styles.container}>
+      <Frame
+        id={id}
+        width="100%"
+        src={`/api/oembed?type=youtube&url=${component}&siteID=${siteID}`}
+      />
+    </div>
   );
 };
 
