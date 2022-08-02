fea3e2d607b894a6d57e79ebf955ed7f1e9ad5ce
Marcus Haddon <haddon.marcus@gmail.com>
2022-08-02 16:42:37 -0700

    WIP: refactoring permissions

--- src/core/server/services/users/auth/validateScopeChange.ts
+++ src/core/server/services/users/auth/validateScopeChange.ts
@@ -1,4 +1,6 @@
+import { GQLUSER_ROLE } from "coral-server/graph/schema/__generated__/types";
 import { User } from "coral-server/models/user";
+import { isSiteModerationScoped } from "coral-server/models/user/helpers";
 
 export interface ScopeChangeValidationParams {
   viewer: Readonly<User>;
@@ -13,5 +15,31 @@ export const validateScopeChange = ({
   additions,
   deletions,
 }: ScopeChangeValidationParams): boolean => {
-  return false;
+  if (viewer.tenantID !== user.tenantID) {
+    return false;
+  }
+  // if viewer is admin, yes
+  if (viewer.role === GQLUSER_ROLE.ADMIN) {
+    return true;
+  }
+
+  // if viewer is moderator, sub function
+  if (viewer.role !== GQLUSER_ROLE.MODERATOR) {
+    return false;
+  }
+
+  // if viewer moderator is scoped, false
+  if (isSiteModerationScoped(viewer.moderationScopes)) {
+    return false;
+  }
+
+  // if user is unscoped, false
+  if (
+    user.role === GQLUSER_ROLE.MODERATOR &&
+    !isSiteModerationScoped(user.moderationScopes)
+  ) {
+    return false;
+  }
+
+  return true;
 };
