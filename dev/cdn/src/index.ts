6bc61a18695c6229c293c5e9d87a64b08911069b
nick-funk <code@nickfunk.com>
2022-08-08 13:58:41 -0600

    Add cdn and host dev tools to the Coral repo

---
+++ dev/cdn/src/index.ts
@@ -0,0 +1,33 @@
+import Logger from "bunyan";
+import cors from "cors";
+import express from "express";
+
+const PORT = 3001;
+
+const createLogger = () => {
+  return Logger.createLogger({
+    name: "coral-dev-host",
+    src: true,
+    level: "debug",
+    color: true,
+  });
+};
+
+const run = () => {
+  const logger = createLogger();
+  const app = express();
+
+  app.use(cors());
+
+  app.use("/", express.static("dist/static"));
+
+  app.get("/up", (req, res) => {
+    res.send("coral:fakeCDN");
+  });
+
+  app.listen(PORT, () => {
+    logger.info({ port: PORT }, "server is listening");
+  });
+};
+
+run();
