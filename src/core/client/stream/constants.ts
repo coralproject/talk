73b360c9e0555c8d2970428d0e107ba2afcd2b1d
Marcus Haddon <haddon.marcus@gmail.com>
2022-09-22 14:26:01 -0700

    fix off by one error in MAX_REPLY_INDENT_DEPTH

--- src/core/client/stream/constants.ts
+++ src/core/client/stream/constants.ts
@@ -4,7 +4,7 @@ export const VIEWER_STATUS_CONTAINER_ID = "viewer-status";
 
 export const POST_COMMENT_FORM_ID = "post-comment-form";
 
-export const MAX_REPLY_INDENT_DEPTH = 7;
+export const MAX_REPLY_INDENT_DEPTH = 8;
 
 export const NUM_INITIAL_COMMENTS = 20;
 
