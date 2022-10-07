e953016c3a0f08b28e75019caf0ddb24f5294657
Marcus Haddon <haddon.marcus@gmail.com>
2022-10-07 12:33:17 -0700

    handle non scoped users in predicate

--- src/core/common/permissions/predicates.ts
+++ src/core/common/permissions/predicates.ts
@@ -1,4 +1,8 @@
-import { PermissionsActionPredicate } from "./types";
+import {
+  isSiteMember,
+  isSiteModerator,
+  PermissionsActionPredicate,
+} from "./types";
 
 const usersCantUpdateSelves: PermissionsActionPredicate = ({
   viewer,
@@ -25,8 +29,43 @@ const onlyAdminsCanDemoteStaffDirectly: PermissionsActionPredicate = ({
   reason: "Only admins may change staffs roles",
 });
 
+/**
+ * This (terribly named) predicate ensures that a scoped viewer cannot
+ * change a scoped user's role in a way that inadvertantly
+ * remove scopes the viewer does not posses.
+ */
+const scopedUsersCantIndirectlyRemoveScopesTheyDontPosses: PermissionsActionPredicate =
+  ({ viewer, user, newUserRole }) => {
+    const reason =
+      "This role change would remove scopes outside of the viewers authorization";
+
+    const changingAwayFromCurrentScopedRole =
+      (isSiteModerator(user) && !!newUserRole && newUserRole !== "MODERATOR") ||
+      (isSiteMember(user) && !!newUserRole && newUserRole !== "MEMBER");
+
+    if (!isSiteModerator(viewer) || !changingAwayFromCurrentScopedRole) {
+      return {
+        pass: true,
+        reason,
+      };
+    }
+
+    const relevantScopes =
+      user.role === "MODERATOR" ? user.moderationScopes : user.membershipScopes;
+
+    const userHasScopesViewerDoesnt = !!relevantScopes?.siteIDs?.find(
+      (userSiteID) => !viewer.moderationScopes?.siteIDs?.includes(userSiteID)
+    );
+
+    return {
+      pass: !userHasScopesViewerDoesnt,
+      reason,
+    };
+  };
+
 export const predicates: PermissionsActionPredicate[] = [
   usersCantUpdateSelves,
   onlyAdminsCanAllocateStaff,
   onlyAdminsCanDemoteStaffDirectly,
+  scopedUsersCantIndirectlyRemoveScopesTheyDontPosses,
 ];
