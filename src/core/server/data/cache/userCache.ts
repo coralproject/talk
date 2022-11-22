8d18209bda960bf2c4d6ea6a5ab82b1c38ceaff2
nick-funk <code@nickfunk.com>
2022-11-22 15:49:56 -0700

    batch user retrieval to improve load speeds

--- src/core/server/data/cache/userCache.ts
+++ src/core/server/data/cache/userCache.ts
@@ -2,6 +2,8 @@ import { UserNotFoundError } from "coral-server/errors";
 import { User } from "coral-server/models/user";
 import { MongoContext } from "../context";
 
+const USER_BATCH_SIZE = 50;
+
 export class UserCache {
   private mongo: MongoContext;
 
@@ -15,13 +17,33 @@ export class UserCache {
 
   public async loadUsersForIDs(tenantID: string, userIDs: string[]) {
     const uniqueUserIDs = Array.from(new Set(userIDs));
-    const users = await this.mongo
-      .users()
-      .find({ tenantID, id: { $in: uniqueUserIDs } })
-      .toArray();
 
-    for (const user of users) {
-      this.userLookup.set(user.id, user);
+    const batches: string[][] = [];
+    batches.push([]);
+
+    let currentBatch = 0;
+    for (const userID of uniqueUserIDs) {
+      batches[currentBatch].push(userID);
+
+      if (batches[currentBatch].length >= USER_BATCH_SIZE) {
+        batches.push([]);
+        currentBatch++;
+      }
+    }
+
+    const results = await Promise.all(
+      batches.map((batch) => {
+        return this.mongo
+          .users()
+          .find({ tenantID, id: { $in: batch } })
+          .toArray();
+      })
+    );
+
+    for (const batch of results) {
+      for (const user of batch) {
+        this.userLookup.set(user.id, user);
+      }
     }
   }
 
