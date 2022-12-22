caa8ced04fb8e610ca7b7d7bb99a5dc2ec82e4f5
Marcus Haddon <haddon.marcus@gmail.com>
2022-12-21 16:02:07 -0800

    update createEmailDomain mutation do some boilerplate  for testing tenant service

--- src/core/server/test/mocks.ts
+++ src/core/server/test/mocks.ts
@@ -1,6 +1,9 @@
 import { MongoContext } from "coral-server/data/context";
+// import { Tenant } from "coral-server/models/tenant";
 import { MailerQueue } from "coral-server/queue/tasks/mailer";
 import { RejectorQueue } from "coral-server/queue/tasks/rejector";
+import { TenantCache } from "coral-server/services/tenant/cache";
+import { Redis } from "ioredis";
 
 const createMockCollection = () => ({
   findOneAndUpdate: jest.fn(),
@@ -17,6 +20,14 @@ export const createMockMongoContex = () => {
     users,
   };
 };
+
+export const createMockRedis = () => ({} as Redis);
+
+// TODO: this can be moved to tenant service dir
+export const createMockTenantCache = (): TenantCache =>
+  ({
+    // ADD METHODS
+  } as unknown as TenantCache);
 
 export const createMockMailer = () =>
   ({
