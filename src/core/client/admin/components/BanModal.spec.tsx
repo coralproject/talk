a0ada787734ffc8c17f193c3d0685350aeb5650a
Marcus Haddon <haddon.marcus@gmail.com>
2023-01-11 10:31:01 -0800

    test for already moderated domains

--- src/core/client/admin/components/BanModal.spec.tsx
+++ src/core/client/admin/components/BanModal.spec.tsx
@@ -9,7 +9,7 @@ import {
 } from "coral-framework/testHelpers";
 
 import { pureMerge } from "coral-common/utils";
-import { GQLResolver, GQLUSER_STATUS } from "coral-framework/schema";
+import { GQLNEW_USER_MODERATION, GQLResolver, GQLSettings, GQLUSER_STATUS } from "coral-framework/schema";
 
 import { createContext } from "../test/create";
 import customRenderAppWithContext from "../test/customRenderAppWithContext";
@@ -29,37 +29,45 @@ const commenter = pureMerge(
   users.commenters[0]
 );
 
+const resolvers = createResolversStub<GQLResolver>({
+  Query: {
+    users: () => communityUsers,
+    settings: () => settings,
+    site: () => site,
+    viewer: () => users.admins[0],
+    sites: () => siteConnection,
+  },
+  Mutation: {
+    createEmailDomain: ({ variables }) => {
+      expectAndFail(variables).toMatchObject({
+        domain: "test.com",
+        newUserModeration: GQLNEW_USER_MODERATION.BAN,
+      })
+      return {};
+    },
+    updateUserBan: ({ variables }) => {
+      return {};
+    },
+    banUser: ({ variables }) => {
+      expectAndFail(variables).toMatchObject({
+        userID: commenter.id,
+      });
+      const userRecord = pureMerge<typeof commenter>(commenter, {
+        status: {
+          current: commenter.status.current.concat(GQLUSER_STATUS.BANNED),
+          ban: { active: true },
+        },
+      });
+      return {
+        user: userRecord,
+      };
+    },
+  },
+})
+
 async function createTestRenderer(
   params: CreateTestRendererParams<GQLResolver> = {}
 ) {
-  const resolvers = createResolversStub<GQLResolver>({
-    Query: {
-      users: () => communityUsers,
-      settings: () => settings,
-      site: () => site,
-      viewer: () => users.admins[0],
-      sites: () => siteConnection,
-    },
-    Mutation: {
-      updateUserBan: ({ variables }) => {
-        return {};
-      },
-      banUser: ({ variables }) => {
-        expectAndFail(variables).toMatchObject({
-          userID: commenter.id,
-        });
-        const userRecord = pureMerge<typeof commenter>(commenter, {
-          status: {
-            current: commenter.status.current.concat(GQLUSER_STATUS.BANNED),
-            ban: { active: true },
-          },
-        });
-        return {
-          user: userRecord,
-        };
-      },
-    },
-  });
   const { context } = createContext({
     ...params,
     resolvers: pureMerge(resolvers, params.resolvers),
@@ -77,20 +85,7 @@ beforeEach(async () => {
 afterEach(jest.clearAllMocks);
 
 it("creates domain ban for unmoderated domain while updating user ban status", async () => {
-  const emailResolver = createResolversStub<GQLResolver>({
-    Mutation: {
-      createEmailDomain: ({ variables }) => {
-        expectAndFail(variables).toMatchObject({
-          domain: "test.com",
-          newUserModeration: "BAN"
-        })
-        return {};
-      }
-    },
-  });
-  const{ container, resolvers } = await createTestRenderer({
-    resolvers: emailResolver
-  });
+  const{ container, resolvers } = await createTestRenderer();
 
   const userRow = within(container).getByRole("row", {
     name: "Isabelle isabelle@test.com 07/06/18, 06:24 PM Commenter Active",
@@ -113,9 +108,49 @@ it("creates domain ban for unmoderated domain while updating user ban status", a
   screen.debug(banDomainButton);
   userEvent.click(within(modal).getByRole("button", { name: "Ban" }));
 
-  await waitFor(() => expect(emailResolver.Mutation!.createEmailDomain!.called).toBeTruthy());
+  await waitFor(() => expect(resolvers.Mutation!.createEmailDomain!.called).toBeTruthy());
 
   expect(within(userRow).getByText("Banned")).toBeVisible();
   expect(resolvers.Mutation!.banUser!.called).toBe(true);
-  expect(emailResolver.Mutation!.createEmailDomain!.called).toBeTruthy(); // do we need to create it in this test for some reason?
+  expect(resolvers.Mutation!.createEmailDomain!.called).toBeTruthy();
+});
+
+it("does not display ban domain option for moderated domain", async () => {
+  const moderatedSettings: GQLSettings = {
+    ...settings,
+    emailDomainModeration: [
+      {
+        id: "test-id",
+        domain: "test.com",
+        newUserModeration: GQLNEW_USER_MODERATION.BAN,
+      }
+    ]
+  };
+
+  const moderatedResolvers = createResolversStub<GQLResolver>({
+    Query: {
+      settings: () => moderatedSettings
+    }
+  });
+
+  const { container } = await createTestRenderer({ resolvers: moderatedResolvers });
+
+  const userRow = within(container).getByRole("row", {
+    name: "Isabelle isabelle@test.com 07/06/18, 06:24 PM Commenter Active",
+  });
+  userEvent.click(
+    within(userRow).getByRole("button", { name: "Change user status" })
+  );
+
+  const dropdown = within(userRow).getByLabelText(
+    "A dropdown to change the user status"
+  );
+  fireEvent.click(within(dropdown).getByRole("button", { name: "Manage Ban" }));
+  const modal = screen.getByLabelText("Are you sure you want to ban Isabelle?");
+
+  const banDomainButton = within(modal).queryByText(
+    `Ban all new accounts on test.com`
+  );
+
+  expect(banDomainButton).not.toBeInTheDocument();
 });
