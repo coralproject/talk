ca423059ac39c1ac0f1c6b5984874156543dacf5
nick-funk <code@nickfunk.com>
2023-05-01 09:00:52 -0600

    simplify word list service to only use one worker

--- src/core/server/services/comments/pipeline/phases/wordList/service.ts
+++ src/core/server/services/comments/pipeline/phases/wordList/service.ts
@@ -25,8 +25,7 @@ const WORKER_SCRIPT =
   "./dist/core/server/services/comments/pipeline/phases/wordList/worker.js";
 
 export class WordListService {
-  private pool: Worker[];
-  private selectedWorkerIndex: number;
+  private worker: Worker;
 
   private onMessageDelegate: (event: MessageEvent) => void;
   private results: Map<string, WordListWorkerResult>;
@@ -39,15 +38,8 @@ export class WordListService {
     this.results = new Map<string, WordListWorkerResult>();
     this.onMessageDelegate = this.onMessage.bind(this);
 
-    this.pool = [];
-    for (let i = 0; i < numWorkers; i++) {
-      const worker = new Worker(WORKER_SCRIPT);
-      worker.on("message", this.onMessageDelegate);
-
-      this.pool.push(worker);
-    }
-
-    this.selectedWorkerIndex = 0;
+    this.worker = new Worker(WORKER_SCRIPT);
+    this.worker.on("message", this.onMessageDelegate);
 
     this.sanitizer = createSanitize(new JSDOM("", {}).window as any, {
       // We need normalized text nodes to mark nodes for suspect/banned words.
@@ -82,63 +74,54 @@ export class WordListService {
     category: WordListCategory,
     phrases: string[]
   ) {
-    const operations: Promise<WordListWorkerResult>[] = [];
-    for (const worker of this.pool) {
-      const data: InitializationPayload = {
-        tenantID,
-        locale,
-        category,
-        phrases,
-      };
+    const data: InitializationPayload = {
+      tenantID,
+      locale,
+      category,
+      phrases,
+    };
 
-      const message: WordListWorkerMessage = {
-        id: uuid(),
-        type: MessageType.Initialize,
-        data,
-      };
+    const message: WordListWorkerMessage = {
+      id: uuid(),
+      type: MessageType.Initialize,
+      data,
+    };
 
-      worker.postMessage(message);
-
-      const builder = async () => {
-        let hasResult = this.results.has(message.id);
-        while (!hasResult) {
-          await this.sleep(1);
-          hasResult = this.results.has(message.id);
-        }
-
-        const result = this.results.get(message.id);
-        if (!result) {
-          this.results.delete(message.id);
-          return {
-            id: message.id,
-            tenantID,
-            ok: false,
-            err: new Error("result was undefined"),
-          };
-        }
-
-        this.results.delete(message.id);
-        return result;
-      };
-
-      operations.push(builder());
-    }
-
-    let errors = 0;
-
-    const results = await Promise.all(operations);
-    for (const result of results) {
-      if (!result.ok || result.err) {
-        this.logger.error(
-          { tenantID: result.tenantID, id: result.id },
-          "unable to initialize"
-        );
-
-        errors++;
+    const builder = async () => {
+      let hasResult = this.results.has(message.id);
+      while (!hasResult) {
+        await this.sleep(1);
+        hasResult = this.results.has(message.id);
       }
+
+      const result = this.results.get(message.id);
+      if (!result) {
+        this.results.delete(message.id);
+        return {
+          id: message.id,
+          tenantID,
+          ok: false,
+          err: new Error("result was undefined"),
+        };
+      }
+
+      this.results.delete(message.id);
+      return result;
+    };
+
+    this.worker.postMessage(message);
+    const result = await builder();
+
+    if (!result.ok || result.err) {
+      this.logger.error(
+        { tenantID: result.tenantID, id: result.id },
+        "unable to initialize"
+      );
+
+      return false;
     }
 
-    return errors === 0;
+    return true;
   }
 
   public async process(
@@ -146,13 +129,6 @@ export class WordListService {
     category: WordListCategory,
     testString: string
   ): Promise<WordListMatchResult> {
-    const worker = this.pool[this.selectedWorkerIndex];
-
-    this.selectedWorkerIndex++;
-    if (this.selectedWorkerIndex >= this.pool.length) {
-      this.selectedWorkerIndex = 0;
-    }
-
     const sanitizedTestString = this.sanitizer(testString).innerHTML;
 
     const data: ProcessPayload = {
@@ -167,7 +143,7 @@ export class WordListService {
       data,
     };
 
-    worker.postMessage(message);
+    this.worker.postMessage(message);
 
     const builder = async () => {
       let hasResult = this.results.has(message.id);
