4e9d414057cbfdf620565e940bfe5075b72a192c
nick-funk <code@nickfunk.com>
2023-06-13 10:48:07 -0600

    force relay to always regenerate persisted queries

--- scripts/compileRelay.ts
+++ scripts/compileRelay.ts
@@ -1,10 +1,17 @@
 #!/usr/bin/env ts-node
+/* eslint-disable no-console */
 
+import { exec, ExecException } from "child_process";
 import program from "commander";
 import fs from "fs-extra";
-import { ExecException, exec } from "child_process";
 
 const CLIENT_ROOT = "./src/core/client";
+
+const sleep = (ms: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
 
 program
   .version("0.1.0")
@@ -24,19 +31,6 @@ if (!program.bundle) {
 const persist = program.persist
   ? `${CLIENT_ROOT}/${program.bundle}/persisted-queries.json`
   : null;
-
-if (persist && fs.existsSync(persist)) {
-  // Create the new filename.
-  const generated = "./src/core/server/graph/persisted/__generated__";
-
-  // Create the generated directory if it doesn't exist.
-  fs.ensureDirSync(generated);
-
-  // Copy the file over to the destination directory.
-  fs.copySync(persist, `${generated}/${program.bundle}.json`, {
-    overwrite: true,
-  });
-}
 
 // create artifact directory if it does not exist
 const artifactDir = `./src/core/client/${program.bundle}/__generated__`;
@@ -59,16 +53,45 @@ const logExec = (
   }
 };
 
-// combine graphql files together for bundle
-exec(
-  `cat ./src/core/server/graph/schema/schema.graphql \
+// ensure a persisted-queries.json exists so that relay
+// doesn't panic when we tell it to `repersist` to output
+// our persisted-queries.json since the way it knows to
+// make a new file is a diffing algorithm against a previous
+// file.
+if (persist) {
+  fs.writeFileSync(persist, "{}", "utf-8");
+}
+
+const run = async () => {
+  // combine graphql files together for bundle
+  exec(
+    `cat ./src/core/server/graph/schema/schema.graphql \
     ./src/core/client/framework/lib/relay/local.graphql \
     ./src/core/client/${program.bundle}/local/local.graphql > \
     ./src/core/client/${program.bundle}/local/combined.graphql`,
-  logExec
-);
+    logExec
+  );
 
-exec(
-  `npm run relay -- src/core/client/${program.bundle}/relay.config.json`,
-  logExec
-);
+  exec(
+    `npm run relay -- --repersist src/core/client/${program.bundle}/relay.config.json`,
+    logExec
+  );
+
+  // it takes a moment to dump out the persisted queries.
+  await sleep(1000);
+
+  if (persist && fs.existsSync(persist)) {
+    // Create the new folder.
+    const generated = "./src/core/server/graph/persisted/__generated__";
+
+    // Create the generated directory if it doesn't exist.
+    fs.ensureDirSync(generated);
+
+    // Copy the file over to the destination directory.
+    fs.copySync(persist, `${generated}/${program.bundle}.json`, {
+      overwrite: true,
+    });
+  }
+};
+
+void run();
