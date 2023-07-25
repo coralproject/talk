af6d1356b064abc0415158534ce3d435411fe670
nick-funk <code@nickfunk.com>
2023-07-25 14:44:54 -0600

    remove duplicate client build in build script

--- build.sh
+++ build.sh
@@ -14,11 +14,6 @@ cd client
 npm run build
 cd ..
 
-echo "running \`npm run build\` for \`client\`"
-cd client
-npm run build
-cd ..
-
 echo "running \`npm run build\` for \`server\`"
 cd server
 npm run build
