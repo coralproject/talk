a14561edc1d9cf393f1cc0570f40760a34c89378
nick-funk <code@nickfunk.com>
2023-08-16 13:28:15 -0600

    add root level clean.sh and clear-node_modules.sh

---
+++ clean.sh
@@ -0,0 +1,14 @@
+echo "clearing \`client/dist/\`..."
+rm -r client/dist
+
+echo "clearing \`server/dist/\`..."
+rm -r server/dist
+
+echo "clearing \`common/dist/\`..."
+rm -r common/dist
+
+echo "clearing \`config/dist/\`..."
+rm -r config/dist
+
+echo "clearing \`docs/.docusaurus/\`..."
+rm -r docs/.docusaurus
