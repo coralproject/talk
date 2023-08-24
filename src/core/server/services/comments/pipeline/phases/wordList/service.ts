0bf9bf25649aa61a233cc36b2f10e1afa329d0cc
nick-funk <code@nickfunk.com>
2023-08-24 09:38:14 -0600

    Revert "Merge pull request #4312 from coralproject/wyattjoh/promise-worker-threads"
    This reverts commit 9e31852cadf5292d0841fc95b61b5dc79c81d00f, reversing
    changes made to b177389e275dbf59295dd24643f13ab040a04f23.

--- src/core/server/services/comments/pipeline/phases/wordList/service.ts
+++ src/core/server/services/comments/pipeline/phases/wordList/service.ts
@@ -24,25 +24,18 @@ import {
 const WORKER_SCRIPT =
   "./dist/core/server/services/comments/pipeline/phases/wordList/worker.js";
 
-interface PromiseCallbacks<R> {
-  resolve: (result: R) => void;
-  reject: (err: Error) => void;
-}
-
 export class WordListService {
   private worker: Worker;
 
   private onMessageDelegate: (event: MessageEvent) => void;
-  private readonly callbacks: Map<
-    string,
-    PromiseCallbacks<WordListWorkerResult>
-  > = new Map();
+  private results: Map<string, WordListWorkerResult>;
   private logger: Logger;
   private sanitizer: Sanitize;
 
-  constructor(logger: Logger) {
+  constructor(logger: Logger, numWorkers = 3) {
     this.logger = logger;
 
+    this.results = new Map<string, WordListWorkerResult>();
     this.onMessageDelegate = this.onMessage.bind(this);
 
     this.worker = new Worker(WORKER_SCRIPT);
@@ -59,37 +52,20 @@ export class WordListService {
     });
   }
 
+  private sleep(ms: number) {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, ms);
+    });
+  }
+
   private onMessage(result: WordListWorkerResult) {
     if (!result) {
       return;
     }
 
-    // Get the callbacks for this result.
-    const callbacks = this.callbacks.get(result.id);
-    if (!callbacks) {
-      throw new Error(`Invalid result id: ${result.id}`);
-    }
-
-    // Delete the callbacks for this result.
-    this.callbacks.delete(result.id);
-
-    // Resolve the promise.
-    if (result.ok) {
-      callbacks.resolve(result);
-    } else {
-      callbacks.reject(result.err!);
-    }
-  }
-
-  private send(message: WordListWorkerMessage) {
-    // Create a new promise to wait for the worker to finish.
-    const promise = new Promise<WordListWorkerResult>((resolve, reject) => {
-      this.callbacks.set(message.id, { resolve, reject });
-    });
-
-    this.worker.postMessage(message);
-
-    return promise;
+    this.results.set(result.id, result);
   }
 
   public async initialize(
@@ -111,7 +87,31 @@ export class WordListService {
       data,
     };
 
-    const result = await this.send(message);
+    const builder = async () => {
+      let hasResult = this.results.has(message.id);
+      while (!hasResult) {
+        await this.sleep(1);
+        hasResult = this.results.has(message.id);
+      }
+
+      const result = this.results.get(message.id);
+      if (!result) {
+        this.results.delete(message.id);
+        return {
+          id: message.id,
+          tenantID,
+          ok: false,
+          err: new Error("result was undefined"),
+        };
+      }
+
+      this.results.delete(message.id);
+      return result;
+    };
+
+    this.worker.postMessage(message);
+    const result = await builder();
+
     if (!result.ok || result.err) {
       this.logger.error(
         { tenantID: result.tenantID, id: result.id },
@@ -143,8 +143,32 @@ export class WordListService {
       data,
     };
 
-    // Send the message to the worker.
-    const result = await this.send(message);
+    this.worker.postMessage(message);
+
+    const builder = async () => {
+      let hasResult = this.results.has(message.id);
+      while (!hasResult) {
+        await this.sleep(1);
+        hasResult = this.results.has(message.id);
+      }
+
+      const result = this.results.get(message.id);
+      if (!result) {
+        this.results.delete(message.id);
+        return {
+          id: message.id,
+          tenantID,
+          ok: false,
+          err: new Error("result was undefined"),
+        };
+      }
+
+      this.results.delete(message.id);
+      return result;
+    };
+
+    const result = await builder();
+
     if (!result.ok || result.err) {
       return {
         isMatched: false,
