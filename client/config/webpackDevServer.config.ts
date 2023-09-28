e07ee7a13ee816fa305faddbc4082e301f374f1e
nick-funk <code@nickfunk.com>
2023-09-28 09:57:25 -0600

    set quiet back to true now that we're done debugging webpack

--- client/config/webpackDevServer.config.ts
+++ client/config/webpackDevServer.config.ts
@@ -69,7 +69,7 @@ export default function ({
     publicPath,
     // WebpackDevServer is noisy by default so we emit custom message instead
     // by listening to the compiler events with `compiler.plugin` calls above.
-    quiet: false,
+    quiet: true,
     // Reportedly, this avoids CPU overload on some systems.
     // https://github.com/facebookincubator/create-react-app/issues/293
     // src/node_modules is not ignored to support absolute imports
