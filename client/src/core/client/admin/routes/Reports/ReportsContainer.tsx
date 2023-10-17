cf3a518b11d53799b5d22ce753fe088f69db6133
Kathryn Beaty <kathryn.beaty@voxmedia.com>
2023-10-17 10:20:29 -0400

    add initial reports table plus main styles

--- client/src/core/client/admin/routes/Reports/ReportsContainer.tsx
+++ client/src/core/client/admin/routes/Reports/ReportsContainer.tsx
@@ -1,11 +1,20 @@
 import React, { useMemo } from "react";
 import { graphql, RelayPaginationProp } from "react-relay";
 
+import MainLayout from "coral-admin/components/MainLayout";
 import { withPaginationContainer } from "coral-framework/lib/relay";
-import { HorizontalGutter } from "coral-ui/components/v2";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "coral-ui/components/v2";
 
 import { ReportsContainer_query as QueryData } from "coral-admin/__generated__/ReportsContainer_query.graphql";
 import { ReportsContainerPaginationQueryVariables } from "coral-admin/__generated__/ReportsContainerPaginationQuery.graphql";
+
+import styles from "./ReportsContainer.css";
 
 interface Props {
   query: QueryData | null;
@@ -19,19 +28,38 @@ const ReportsContainer: React.FunctionComponent<Props> = (props) => {
   }, [props.query]);
 
   return (
-    <>
-      {dsaReports.map((dsaReport) => {
-        return (
-          <HorizontalGutter key={dsaReport.publicID} spacing={2} marginTop={3}>
-            <div>Report</div>
-            <div>{dsaReport.publicID}</div>
-            <div>{dsaReport.createdAt}</div>
-            <div>{dsaReport.status}</div>
-            <div>{dsaReport.lawBrokenDescription}</div>
-          </HorizontalGutter>
-        );
-      })}
-    </>
+    <MainLayout className={styles.root}>
+      {/* TODO: Add sort by */}
+      <Table fullWidth>
+        <TableHead>
+          {/* TODO: Need to localize all of these tableheads */}
+          <TableRow>
+            <TableCell>Report date</TableCell>
+            <TableCell>Report age</TableCell>
+            <TableCell>Reporter</TableCell>
+            <TableCell>Report number</TableCell>
+            <TableCell>Law broken</TableCell>
+            <TableCell>Comment author</TableCell>
+            <TableCell>Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {dsaReports.map((report) => {
+            return (
+              <TableRow key={report.publicID}>
+                <TableCell>{report.createdAt}</TableCell>
+                <TableCell>Age TODO</TableCell>
+                <TableCell>{report.reporter?.username}</TableCell>
+                <TableCell>{report.publicID}</TableCell>
+                <TableCell>{report.lawBrokenDescription}</TableCell>
+                <TableCell>Comment author TODO</TableCell>
+                <TableCell>{report.status}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </MainLayout>
   );
 };
 
