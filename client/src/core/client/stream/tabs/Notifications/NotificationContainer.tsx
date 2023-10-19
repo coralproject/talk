6c018c7f04897801e94a5ba32eb8b55aad31072b
nick-funk <code@nickfunk.com>
2023-10-19 14:42:55 -0600

    preliminarily style notifications

--- client/src/core/client/stream/tabs/Notifications/NotificationContainer.tsx
+++ client/src/core/client/stream/tabs/Notifications/NotificationContainer.tsx
@@ -1,3 +1,4 @@
+import cn from "classnames";
 import React, { FunctionComponent } from "react";
 import { graphql } from "react-relay";
 
@@ -5,15 +6,19 @@ import { withFragmentContainer } from "coral-framework/lib/relay";
 
 import { NotificationContainer_notification } from "coral-stream/__generated__/NotificationContainer_notification.graphql";
 
+import styles from "./NotificationContainer.css";
+
 interface Props {
   notification: NotificationContainer_notification;
 }
 
-const NotificationContainer: FunctionComponent<Props> = ({ notification }) => {
+const NotificationContainer: FunctionComponent<Props> = ({
+  notification: { title, body },
+}) => {
   return (
-    <div>
-      {notification.body} -{" "}
-      {notification.comment ? notification.comment.body : ""}
+    <div className={cn(styles.root)}>
+      {title && <div className={cn(styles.title)}>{title}</div>}
+      {body && <div className={cn(styles.body)}>{body}</div>}
     </div>
   );
 };
@@ -22,8 +27,10 @@ const enhanced = withFragmentContainer<Props>({
   notification: graphql`
     fragment NotificationContainer_notification on Notification {
       id
+      title
       body
       comment {
+        id
         body
       }
     }
