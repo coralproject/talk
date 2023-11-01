1ed656c970d2bb483d37acaa9fdaa4838ec04e0b
Marcus Haddon <haddon.marcus@gmail.com>
2023-11-01 15:07:29 -0700

    remove eslint disable

--- client/src/core/client/admin/components/ModerationReason/ModerationReason.tsx
+++ client/src/core/client/admin/components/ModerationReason/ModerationReason.tsx
@@ -1,17 +1,16 @@
-/* eslint-disable */
 import React, { FunctionComponent, useCallback, useState } from "react";
 
-import { Button, Card, Flex, RadioButton } from "coral-ui/components/v2";
+import { Button, Card, Flex } from "coral-ui/components/v2";
 
 import { RejectCommentReasonInput } from "coral-stream/__generated__/RejectCommentMutation.graphql";
 
 import { Localized } from "@fluent/react/compat";
 import { GQLREJECTION_REASON_CODE } from "coral-framework/schema";
 
+import DetailedExplantion from "./DetailedExplanation";
 import Reasons from "./Reasons";
 
 import styles from "./ModerationReason.css";
-import DetailedExplantion from "./DetailedExplanation";
 
 type Reason = RejectCommentReasonInput;
 type ReasonCode = GQLREJECTION_REASON_CODE;
@@ -24,10 +23,11 @@ export interface Props {
 const ModerationReason: FunctionComponent<Props> = ({ onCancel, onReason }) => {
   const [view, setView] = useState<"REASON" | "EXPLANATION">("REASON");
   const [reasonCode, setReasonCode] = useState<ReasonCode | null>(null);
-  const [legalGrounds, setLegalGrounds] = useState<string | null>(null);
-  const [detailedExplanation, setDetailedExplanation] = useState<
-    string | null
-  >(null);
+  // TODO (marcushaddon): will we ever submit legal grounds via this component?
+  const [legalGrounds] = useState<string | null>(null);
+  const [detailedExplanation, setDetailedExplanation] = useState<string | null>(
+    null
+  );
 
   const submitReason = useCallback(() => {
     onReason({
@@ -42,34 +42,44 @@ const ModerationReason: FunctionComponent<Props> = ({ onCancel, onReason }) => {
 
   return (
     <Card className={styles.root} data-testid="moderation-reason-modal">
-
       {view === "REASON" ? (
         <Reasons
           selected={reasonCode}
           onCode={setReasonCode}
           onAddExplanation={() => setView("EXPLANATION")}
         />
-      ) : <DetailedExplantion onBack={() => setView("REASON")} code={reasonCode!} value={detailedExplanation} onChange={setDetailedExplanation} />}
+      ) : (
+        <DetailedExplantion
+          onBack={() => setView("REASON")}
+          code={reasonCode!}
+          value={detailedExplanation}
+          onChange={setDetailedExplanation}
+        />
+      )}
 
       <Flex className={styles.buttons}>
-      <Localized id="moderationReason-cancel">
-        <Button className={styles.cancelButton} variant="outlined" color="mono" onClick={onCancel}>
-          Cancel
-        </Button>
-      </Localized>
+        <Localized id="moderationReason-cancel">
+          <Button
+            className={styles.cancelButton}
+            variant="outlined"
+            color="mono"
+            onClick={onCancel}
+          >
+            Cancel
+          </Button>
+        </Localized>
 
-      <Localized id="moderationReason-reject">
-        <Button
-          className={styles.rejectButton}
-          disabled={reasonCode === null}
-          onClick={submitReason}
-          color="alert"
-        >
-          Reject
-        </Button>
-      </Localized>
-    </Flex>
-
+        <Localized id="moderationReason-reject">
+          <Button
+            className={styles.rejectButton}
+            disabled={reasonCode === null}
+            onClick={submitReason}
+            color="alert"
+          >
+            Reject
+          </Button>
+        </Localized>
+      </Flex>
     </Card>
   );
 };
