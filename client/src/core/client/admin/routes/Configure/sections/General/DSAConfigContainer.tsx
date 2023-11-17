8a102760249b31fbc6f312f8b9337e68c246fcb8
nick-funk <code@nickfunk.com>
2023-11-17 10:44:47 -0700

    update DSA config with new description copy

--- client/src/core/client/admin/routes/Configure/sections/General/DSAConfigContainer.tsx
+++ client/src/core/client/admin/routes/Configure/sections/General/DSAConfigContainer.tsx
@@ -2,8 +2,17 @@ import { Localized } from "@fluent/react/compat";
 import React, { FunctionComponent } from "react";
 import { graphql } from "react-relay";
 
+import { useCoralContext } from "coral-framework/lib/bootstrap";
+import { getMessage } from "coral-framework/lib/i18n";
 import { withFragmentContainer } from "coral-framework/lib/relay";
-import { FieldSet, FormField, HelperText, Label } from "coral-ui/components/v2";
+import HTMLContent from "coral-stream/common/HTMLContent";
+import {
+  FieldSet,
+  FormField,
+  FormFieldDescription,
+  HelperText,
+  Label,
+} from "coral-ui/components/v2";
 
 import { DSAConfigContainer_settings } from "coral-admin/__generated__/DSAConfigContainer_settings.graphql";
 
@@ -35,15 +44,36 @@ export const DSAConfigContainer: FunctionComponent<Props> = ({
   disabled,
   settings,
 }) => {
+  const { localeBundles } = useCoralContext();
+
+  const descriptionText = getMessage(
+    localeBundles,
+    "configure-general-dsaConfig-description",
+    `The EU Digital Services Act (DSA) requires that publishers in or targeting EU citizens provide certain features to their commenters and moderators.
+    <br/>
+    <br/>
+    Coral's DSA toolset includes:
+    <br/>
+    <ul style="padding-inline-start: var(--spacing-5);">
+      <li>A dedicated flow for comments reported as illegal</li>
+      <li>Compulsory moderation reasons for every rejected comment</li>
+      <li>Commenter notifications for illegal comment reporting and rejected comments</li>
+      <li>Compulsory text explaining methods of redress/appeal, if any</li>
+    </ul>`
+  );
+
   return (
     <ConfigBox
       data-testid="configure-general-dsaConfig"
       title={
         <Localized id="configure-general-dsaConfig-title">
-          <Header container="h2">Digital services act</Header>
+          <Header container="h2">Digital Services Act feature set</Header>
         </Localized>
       }
     >
+      <FormFieldDescription>
+        <HTMLContent>{descriptionText}</HTMLContent>
+      </FormFieldDescription>
       <FormField container={<FieldSet />}>
         <Localized id="configure-general-dsaConfig-reportingAndModerationExperience">
           <Label component="legend">
--- locales/en-US/admin.ftl
+++ locales/en-US/admin.ftl
@@ -1612,7 +1612,19 @@ configure-general-rte-spoilerDesc =
   Words and phrases formatted as Spoiler are hidden behind a
   dark background until the reader chooses to reveal the text.
 
-configure-general-dsaConfig-title = Digital services act
+configure-general-dsaConfig-title = Digital Services Act feature set
+configure-general-dsaConfig-description =
+  The EU Digital Services Act (DSA) requires that publishers in or targeting EU citizens provide certain features to their commenters and moderators.
+  <br/>
+  <br/>
+  Coral's DSA toolset includes:
+  <br/>
+  <ul style="padding-inline-start: var(--spacing-5);">
+    <li>A dedicated flow for comments reported as illegal</li>
+    <li>Compulsory moderation reasons for every rejected comment</li>
+    <li>Commenter notifications for illegal comment reporting and rejected comments</li>
+    <li>Compulsory text explaining methods of redress/appeal, if any</li>
+  </ul>
 configure-general-dsaConfig-reportingAndModerationExperience =
   DSA reporting and moderation experience
 configure-general-dsaConfig-methodOfRedress =
