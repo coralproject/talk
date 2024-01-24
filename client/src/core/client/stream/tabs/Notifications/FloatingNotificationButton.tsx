d5da5c6b5e08c6c5b99826f026dad3d3a787d579
nick-funk <code@nickfunk.com>
2024-01-24 15:05:42 -0700

    constrain the floating notification bell to the stream area

--- client/src/core/client/stream/tabs/Notifications/FloatingNotificationButton.tsx
+++ client/src/core/client/stream/tabs/Notifications/FloatingNotificationButton.tsx
@@ -7,8 +7,9 @@ import React, {
 
 import { useCoralContext } from "coral-framework/lib/bootstrap";
 
-import styles from "./FloatingNotificationButton.css";
 import { LiveBellIcon } from "./LiveBellIcon";
+
+import styles from "./FloatingNotificationButton.css";
 
 interface Props {
   viewerID?: string;
@@ -17,6 +18,8 @@ interface Props {
 const FloatingNotificationButton: FunctionComponent<Props> = ({ viewerID }) => {
   const { window } = useCoralContext();
   const [leftPos, setLeftPos] = useState<number>(0);
+  const [topPos, setTopPos] = useState<number>(0);
+
   const onWindowResize = useCallback(() => {
     const element = window.document.getElementById("coral-shadow-container");
     const rect = element?.getBoundingClientRect();
@@ -27,21 +30,43 @@ const FloatingNotificationButton: FunctionComponent<Props> = ({ viewerID }) => {
     setLeftPos(rect.left + rect.width - 65);
   }, [window.document]);
 
+  const onWindowScroll = useCallback(() => {
+    const element = window.document.getElementById("coral-shadow-container");
+    const rect = element?.getBoundingClientRect();
+    if (!rect) {
+      return;
+    }
+
+    if (rect.bottom < 0) {
+      setTopPos(rect.bottom);
+    } else if (rect.top < 0) {
+      setTopPos(0);
+    } else {
+      setTopPos(rect.top);
+    }
+  }, [window.document]);
+
   useEffect(() => {
     window.addEventListener("resize", onWindowResize);
     onWindowResize();
 
+    window.addEventListener("scroll", onWindowScroll);
+
     return () => {
       window.removeEventListener("resize", onWindowResize);
+      window.removeEventListener("scroll", onWindowScroll);
     };
-  }, [onWindowResize, window]);
+  }, [onWindowResize, onWindowScroll, window]);
 
   if (!viewerID) {
     return null;
   }
 
   return (
-    <div style={{ left: `${leftPos}px` }} className={styles.root}>
+    <div
+      style={{ left: `${leftPos}px`, top: `${topPos}px` }}
+      className={styles.root}
+    >
       <LiveBellIcon userID={viewerID} size="lg" />
     </div>
   );
