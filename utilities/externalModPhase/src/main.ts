4c44d1547d32d75c53d1eac73e374d415cf4346e
nick-funk <code@nickfunk.com>
2024-01-31 15:51:01 -0700

    add external mod phases test utility

---
+++ utilities/externalModPhase/src/main.ts
@@ -0,0 +1,39 @@
+import { config } from "dotenv";
+import express, { json } from "express";
+
+const run = async () => {
+  config();
+
+  const PORT = process.env.PORT ? parseInt(process.env.PORT) : 7000;
+  const HOST = process.env.HOST ? process.env.HOST : "localhost";
+
+  const app = express();
+  app.use(json());
+
+  app.get("/", (req, res) => {
+    res.status(200).send("external mod phase tester");
+  });
+
+  app.post("/api/rejectWithReason", (req, res) => {
+    const result = {
+      moderationAction: {
+        status: "REJECTED",
+        rejectionReason: {
+          code: "ABUSIVE",
+          legalGrounds: "some legal grounds",
+          detailedExplanation: "a detailed explanation",
+          customReason: "some custom reason",
+        },
+      },
+      status: "REJECTED",
+    }
+
+    res.send(result);
+  });
+
+  app.listen(PORT, HOST, () => {
+    console.log(`external mod phase tester is listening on "${HOST}:${PORT}"...`);
+  });
+};
+
+void run();
