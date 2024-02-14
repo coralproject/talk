5099aed67370b21c86c3d3be43f315020d44f127
Kathryn Beaty <kathryn.beaty@voxmedia.com>
2024-02-14 09:36:46 -0500

    Revert "Merge pull request #4512 from coralproject/chore/CORL-3020-pnpm"
    This reverts commit 2a91122c94adddb621e6521c0fd890f4d5bf0551, reversing
    changes made to 5053586bb4df2f036836c52aab7ec910c84f9a4d.

---
+++ scripts/npm-ci.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$0")"
+cd ..
+
+echo "installing git hooks"
+
+cd scripts
+sh install-git-hooks.sh
+cd ..
+
+echo "running \`npm ci\` for \`config\`"
+cd config
+npm ci
+cd ..
+
+echo "running \`npm ci\` for \`common\`"
+cd common
+npm ci
+cd ..
+
+echo "running \`npm ci\` for \`client\`"
+cd client
+npm ci
+cd ..
+
+echo "running \`npm ci\` for \`server\`"
+cd server
+npm ci
+cd ..
+
+echo "running \`npm ci\` for \`docs\`"
+cd docs
+npm ci
