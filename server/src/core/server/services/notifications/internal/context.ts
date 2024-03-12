b3144f27d77f2be5441981a2dede97d821ef8b60
nick-funk <code@nickfunk.com>
2024-03-12 12:56:38 -0600

    don't increment notification counts for replies still in premod

--- server/src/core/server/services/notifications/internal/context.ts
+++ server/src/core/server/services/notifications/internal/context.ts
@@ -259,6 +259,14 @@ export class InternalNotificationContext {
         shouldIncrementCount = false;
       }
 
+      // Don't increment the count if the reply is still in pre-mod
+      if (
+        type === GQLNOTIFICATION_TYPE.REPLY &&
+        reply?.status === GQLCOMMENT_STATUS.PREMOD
+      ) {
+        shouldIncrementCount = false;
+      }
+
       if (shouldIncrementCount) {
         await this.incrementCountForUser(tenantID, targetUserID);
       }
