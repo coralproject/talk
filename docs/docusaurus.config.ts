1fd004846a7dc64b5e37ce1ed3406210f4d39775
Kathryn Beaty <kathryn.beaty@voxmedia.com>
2024-10-01 15:08:38 -0400

    fix linting setup

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,6 +1,6 @@
-import { themes as prismThemes } from "prism-react-renderer";
-import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+import { themes as prismThemes } from "prism-react-renderer";
 
 const config: Config = {
   title: "Coral",
