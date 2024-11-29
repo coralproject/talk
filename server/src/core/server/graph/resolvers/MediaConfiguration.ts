5710379da3dc2283c5fc7a1a28595f0f14a0bf89
nick-funk <code@nickfunk.com>
2024-11-29 10:23:01 -0700

    a bit more plumbing bluesky through types and resolvers
    it's a bit worrying just how many places you need to update to
    add one media type :sweat_smile:

--- server/src/core/server/graph/resolvers/MediaConfiguration.ts
+++ server/src/core/server/graph/resolvers/MediaConfiguration.ts
@@ -7,6 +7,7 @@ export const MediaConfiguration: GQLMediaConfigurationTypeResolver<
   Partial<GQLMediaConfiguration>
 > = {
   twitter: ({ twitter = {} }) => twitter,
+  bluesky: ({ bluesky = {} }) => bluesky,
   youtube: ({ youtube = {} }) => youtube,
   gifs: ({ gifs = {} }) => gifs,
   external: () => ({}),
