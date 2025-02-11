f3cf5ac13e7d94fdf36ae60c8fe020c9a391b277
Kathryn Beaty <kathryn.beaty@voxmedia.com>
2025-02-11 09:41:40 -0500

    remove experimentalwebhookscallout

--- client/src/core/client/admin/routes/Configure/sections/WebhookEndpoints/ConfigureWebhookEndpoint/ConfigureWebhookEndpointContainer.tsx
+++ client/src/core/client/admin/routes/Configure/sections/WebhookEndpoints/ConfigureWebhookEndpoint/ConfigureWebhookEndpointContainer.tsx
@@ -10,7 +10,6 @@ import { HorizontalGutter } from "coral-ui/components/v2";
 import { ConfigureWebhookEndpointContainer_settings } from "coral-admin/__generated__/ConfigureWebhookEndpointContainer_settings.graphql";
 import { ConfigureWebhookEndpointContainer_webhookEndpoint } from "coral-admin/__generated__/ConfigureWebhookEndpointContainer_webhookEndpoint.graphql";
 
-import ExperimentalWebhooksCallOut from "../ExperimentalWebhooksCallOut";
 import EndpointDangerZone from "./EndpointDangerZone";
 import EndpointDetails from "./EndpointDetails";
 import EndpointStatus from "./EndpointStatus";
@@ -26,7 +25,6 @@ const ConfigureWebhookEndpointContainer: FunctionComponent<Props> = ({
 }) => {
   return (
     <HorizontalGutter size="double" data-testid="webhook-endpoint-container">
-      <ExperimentalWebhooksCallOut />
       <ConfigBox
         title={
           <Localized id="configure-webhooks-configureWebhookEndpoint">
