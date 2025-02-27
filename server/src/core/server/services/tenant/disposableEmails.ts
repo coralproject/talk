554a1efbd3659e0eab78a850c2e582bdd29555b8
Kathryn Beaty <kathryn.beaty@voxmedia.com>
2025-02-27 11:45:39 -0500

    update how fetching domains and adding to redis

--- server/src/core/server/services/tenant/disposableEmails.ts
+++ server/src/core/server/services/tenant/disposableEmails.ts
@@ -1,12 +1,10 @@
-import axios from "axios";
 import Logger from "bunyan";
-import { Parser } from "stream-json";
-import { streamArray } from "stream-json/streamers/StreamArray";
 
 import {
   DISPOSABLE_EMAIL_DOMAINS_LIST_URL,
   DISPOSABLE_EMAIL_DOMAINS_REDIS_KEY,
 } from "coral-common/common/lib/constants";
+import { createFetch } from "coral-server/services/fetch";
 
 import { AugmentedRedis } from "../redis";
 
@@ -16,31 +14,39 @@ export async function readDisposableEmailDomainsAndAddToRedis(
 ) {
   const now = new Date().toISOString();
 
-  // clear previous disposable domains before loading in new
-  await redis.del(DISPOSABLE_EMAIL_DOMAINS_REDIS_KEY);
+  const fetch = createFetch({ name: "disposableDomains" });
 
   try {
-    const stream = await axios.get(`${DISPOSABLE_EMAIL_DOMAINS_LIST_URL}`, {
-      responseType: "stream",
-    });
+    // the list is 242 KB, even if it were 1000 times larger,
+    // that's still less than 250 MB, well within a pods capability
+    // so we are okay loading with fetch
+    const response = await fetch(DISPOSABLE_EMAIL_DOMAINS_LIST_URL);
+    if (!response) {
+      logger.error("Error retrieving disposable emails from request");
+      return;
+    }
 
-    const pipeline = stream.data.pipe(new Parser()).pipe(streamArray());
-
-    pipeline.on("data", async (data: { value: string }) => {
-      const domain = data.value;
-      await redis.hset(DISPOSABLE_EMAIL_DOMAINS_REDIS_KEY, domain, now);
-    });
-
-    pipeline.on("end", () => {
-      logger.info("Finished streaming disposable emails list");
-    });
-
-    pipeline.on("error", (error: any) => {
+    const json = (await response.json()) as string[];
+    if (!json) {
       logger.error(
-        "Error reading and setting disposable emails in Redis:",
-        error
+        "Error reading disposable emails from request to json array"
       );
-    });
+      return;
+    }
+
+    // need to use a transaction to make the delete followed by
+    // populating the hash set an atomic operation so that
+    // pods don't race condition delete the set out from under
+    // the writes
+    const transaction = redis.multi();
+
+    transaction.del(DISPOSABLE_EMAIL_DOMAINS_REDIS_KEY);
+
+    for (const value of json) {
+      transaction.hset(DISPOSABLE_EMAIL_DOMAINS_REDIS_KEY, value, now);
+    }
+
+    await transaction.exec();
   } catch (error) {
     logger.error(
       "Error streaming and setting disposable emails in Redis:",
