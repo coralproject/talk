64a2690e483a512430fe87d1dde7cbb76d20395d
Kathryn Beaty <kathryn.beaty@voxmedia.com>
2025-02-28 11:38:14 -0500

    add tests

--- server/src/core/server/services/users/user.emailPremodFilter.spec.ts
+++ server/src/core/server/services/users/user.emailPremodFilter.spec.ts
@@ -12,6 +12,9 @@ const tooManyPeriodsEmail = "this.has.too.many.periods@test.com";
 const justEnoughPeriodsEmail = "just.enough.periods@test.com";
 const noPeriodsEmail = "noperiodshere@test.com";
 
+const isSubdomainEmail = "test@thisisa.subdomain.com";
+const isNotSubdomainEmail = "test@regulardomain.com";
+
 it("does not premod filter emails when feature is disabled", () => {
   const tenant = createTenantFixture({
     premoderateEmailAddress: {
@@ -74,6 +77,57 @@ it(`does premod filter emails when feature is enabled and has too many (${EMAIL_
 
   const user = createUserFixture({
     email: tooManyPeriodsEmail,
+  });
+
+  const result = shouldPremodDueToLikelySpamEmail(undefined, tenant, user);
+  expect(result);
+});
+
+it("does not premod filter emails when domain alias feature is disabled", () => {
+  const tenant = createTenantFixture({
+    premoderateEmailAddress: {
+      domainAliases: {
+        enabled: false,
+      },
+    },
+  });
+
+  const user = createUserFixture({
+    email: isSubdomainEmail,
+  });
+
+  const result = shouldPremodDueToLikelySpamEmail(undefined, tenant, user);
+  expect(!result);
+});
+
+it("does premod filter emails when domain alias feature is enabled and email is subdomain", () => {
+  const tenant = createTenantFixture({
+    premoderateEmailAddress: {
+      domainAliases: {
+        enabled: true,
+      },
+    },
+  });
+
+  const user = createUserFixture({
+    email: isSubdomainEmail,
+  });
+
+  const result = shouldPremodDueToLikelySpamEmail(undefined, tenant, user);
+  expect(result);
+});
+
+it("does not premod filter emails when domain alias feature is enabled and email is not subdomain", () => {
+  const tenant = createTenantFixture({
+    premoderateEmailAddress: {
+      domainAliases: {
+        enabled: true,
+      },
+    },
+  });
+
+  const user = createUserFixture({
+    email: isNotSubdomainEmail,
   });
 
   const result = shouldPremodDueToLikelySpamEmail(undefined, tenant, user);
