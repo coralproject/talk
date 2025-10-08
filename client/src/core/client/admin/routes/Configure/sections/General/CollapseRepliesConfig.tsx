e528dc33e2a546fd3448ca28e70e21030c7a5f86
Tessa Thornton <tessathornton@gmail.com>
2025-10-08 10:30:19 -0400

    add admin ui

---
+++ client/src/core/client/admin/routes/Configure/sections/General/CollapseRepliesConfig.tsx
@@ -0,0 +1,48 @@
+import { Localized } from "@fluent/react/compat";
+import {
+  FieldSet,
+  FormField,
+  FormFieldDescription,
+  Label,
+} from "coral-ui/components/v2";
+import React, { FunctionComponent } from "react";
+import { graphql } from "react-relay";
+import ConfigBox from "../../ConfigBox";
+import Header from "../../Header";
+import OnOffField from "../../OnOffField";
+
+// eslint-disable-next-line no-unused-expressions
+graphql`
+  fragment CollapseRepliesConfig_formValues on Settings {
+    collapseReplies
+  }
+`;
+
+interface Props {
+  disabled: boolean;
+}
+
+const CollapseRepliesConfig: FunctionComponent<Props> = ({ disabled }) => (
+  <ConfigBox
+    title={
+      <Localized id="configure-general-collapseReplies-title">
+        <Header container="h2">Collapse replies</Header>
+      </Localized>
+    }
+  >
+    <Localized id="configure-general-collapseReplies-explanation">
+      <FormFieldDescription>
+        When enabled, top-level replies to comments will be collapsed by
+        default.
+      </FormFieldDescription>
+    </Localized>
+    <FormField container={<FieldSet />}>
+      <Localized id="configure-general-collapseReplies-enabled">
+        <Label component="legend">Collapse replies enabled</Label>
+      </Localized>
+      <OnOffField name="collapseReplies" disabled={disabled} />
+    </FormField>
+  </ConfigBox>
+);
+
+export default CollapseRepliesConfig;
